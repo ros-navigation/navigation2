8acca22d82a823066bd2a98c383c55c849e1eccf
Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>
2018-08-16 16:19:18 -0700

    Added dummy BT mission planner

---
+++ src/MissionPlanner_BT/src/CancelCondition.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "CancelCondition.h"
+
+CancelCondition::CancelCondition(std::string name) : BT::ConditionNode::ConditionNode(name)
+{
+    type_ = BT::CONDITION_NODE;
+    boolean_value_ = true;
+}
+
+CancelCondition::~CancelCondition() {}
+
+BT::ReturnStatus CancelCondition::Tick()
+{
+    if (boolean_value_)
+    {
+        set_status(BT::SUCCESS);
+        return BT::SUCCESS;
+    }
+    else
+    {
+        set_status(BT::FAILURE);
+        return BT::FAILURE;
+    }
+}
+
+void CancelCondition::set_boolean_value(bool boolean_value)
+{
+    boolean_value_ = boolean_value;
+}
