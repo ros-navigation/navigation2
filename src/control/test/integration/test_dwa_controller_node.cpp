fae84e59cecd76223aa8cb1026b4ef0414cf0689
Carl Delsey <carl.r.delsey@intel.com>
2018-08-28 14:59:34 -0700

    Fixing a bug in fixture
    rclcpp::init can only be called once per process, even if shutdown
    is called in between.

--- src/control/test/integration/test_dwa_controller_node.cpp
+++ src/control/test/integration/test_dwa_controller_node.cpp
@@ -17,24 +17,26 @@
 #include <control/FollowPathTaskClient.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <memory>
-#include <iostream>
+
+// rclcpp::init can only be called once per process, so this needs to be a global variable
+class RclCppFixture
+{
+public:
+  RclCppFixture() {rclcpp::init(0, nullptr);}
+  ~RclCppFixture() {rclcpp::shutdown();}
+};
+RclCppFixture g_rclcppfixture;
 
 class TestNode : public ::testing::Test
 {
 public:
   TestNode()
   {
-    std::cout << "Starting" << std::endl;
-    rclcpp::init(0, nullptr);
     node = rclcpp::Node::make_shared("dwa_controller_test");
     client = std::make_unique<FollowPathTaskClient>("DwaController", node.get());
     while (node->count_subscribers("/DwaController_command") < 1) {
       rclcpp::spin_some(node);
     }
-  }
-  ~TestNode()
-  {
-    rclcpp::shutdown();
   }
 
 protected:
@@ -48,8 +50,7 @@ TEST_F(TestNode, ResultReturned)
   client->executeAsync(std::make_shared<FollowPathCommand>(c));
   FollowPathResult r;
   auto r_ptr = std::make_shared<FollowPathResult>(r);
-  while (client->waitForResult(r_ptr, 10) == TaskStatus::RUNNING) {
-    std::cout << "Waiting" << std::endl;
+  while (client->waitForResult(r_ptr, 1000) == TaskStatus::RUNNING) {
     rclcpp::spin_some(node);
   }
   SUCCEED();
