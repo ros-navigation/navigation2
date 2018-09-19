651416728a5876c5b611485eac54d38844a6ae61
Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>
2018-09-19 10:40:53 -0700

    Addressing issue #80

--- src/libs/nav2_util/src/map/map_draw.c
+++ src/libs/nav2_util/src/map/map_draw.c
@@ -25,6 +25,7 @@
  * CVS: $Id: map_draw.c 7057 2008-10-02 00:44:06Z gbiggs $
 **************************************************************************/
 
+#pragma GCC diagnostic ignored "-Wpedantic"
 #ifdef INCLUDE_RTKGUI
 
 #include <errno.h>
