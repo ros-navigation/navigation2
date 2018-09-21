b2596b13d11d8386ef81ac605423b966ce2d701f
Michael Jeronimo <michael.jeronimo@intel.com>
2018-09-21 10:59:24 -0700

    Integrate Carlos' nplanning test code

---
+++ src/planning/nav2_planning_tests/planner_tester.cpp
@@ -0,0 +1,552 @@
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. Reserved.
+
+#include <string>
+#include <random>
+#include <tuple>
+#include <utility>
+#include <memory>
+#include <chrono>
+#include "planner_tester.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav2_util/map_loader/map_loader.hpp"
+#include "nav2_libs_msgs/msg/costmap_meta_data.hpp"
+
+using namespace std::chrono_literals;
+using nav2_tasks::TaskStatus;
+
+namespace nav2_util
+{
+
+PlannerTester::PlannerTester()
+: Node("PlannerTester"), map_publish_rate_(100s), map_set_(false), costmap_set_(false),
+  using_fake_costmap_(true), costmap_server_running_(false), trinary_costmap_(true),
+  track_unknown_space_(false), lethal_threshold_(100), unknown_cost_value_(-1),
+  testCostmapType_(TestCostmap::open_space), spin_thread_(nullptr), spinning_ok_(false)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::PlannerTester");
+
+  // We start with a 10x10 grid with no obstacles
+  loadSimpleCostmap(TestCostmap::open_space);
+
+  // TODO(orduno): get service name from param server
+  startCostmapServer("CostmapService");
+
+  // TODO(orduno): get planner name from param server
+  createPlannerClient("DijkstraPlanner");
+
+  map_publisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("map");
+
+  endpoints_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("endpoints", 1);
+
+  spinning_ok_ = true;
+  spin_thread_ = new std::thread(&PlannerTester::spinThread, this);
+}
+
+PlannerTester::~PlannerTester()
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::~PlannerTester");
+
+  spinning_ok_ = false;
+  spin_thread_->join();
+  delete spin_thread_;
+  spin_thread_ = nullptr;
+}
+
+void PlannerTester::spinThread()
+{
+  while (spinning_ok_) {
+    rclcpp::spin_some(this->get_node_base_interface());
+  }
+  // rclcpp::spin(this->get_node_base_interface());
+}
+
+void PlannerTester::loadMap(const std::string image_file_path, const std::string yaml_file_name)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::loadMap");
+
+  // Specs for the default map
+  // double resolution = 0.05;
+  double resolution = 1.0;
+  bool negate = false;
+  double occupancy_threshold = 0.65;
+  double free_threshold = 0.196;
+
+  // Define origin offset
+  geometry_msgs::msg::Twist origin;
+  origin.linear.x = 0.0;
+  origin.linear.y = 0.0;
+  origin.linear.z = 0.0;
+  origin.angular.x = 0.0;
+  origin.angular.y = 0.0;
+  origin.angular.z = 0.0;
+
+  MapMode mode = TRINARY;
+
+  std::string file_path = "";
+
+  if (!image_file_path.empty()) {
+    file_path = image_file_path;
+  } else {
+    // User can set an environment variable to the location of the test src
+    char const * path = getenv("TEST_MAP");
+    if (path == NULL) {
+      file_path = "../../../src/navigation2/src/planning/test/maps/map.pgm";
+    } else {
+      file_path = std::string(path);
+    }
+  }
+
+  if (!yaml_file_name.empty()) {
+    // TODO(orduno): parse yaml file
+    RCLCPP_WARN(this->get_logger(), "PlannerTester::loadMap: yaml file parser not implemented"
+      " yet, will use default values instead");
+  }
+
+  try {
+    map_ =
+      std::make_shared<nav_msgs::msg::OccupancyGrid>(
+      map_loader::loadMapFromFile(
+        file_path, resolution, negate,
+        occupancy_threshold, free_threshold, origin, mode));
+  } catch (...) {
+    RCLCPP_ERROR(this->get_logger(),
+      "PlannerTester::loadDefaultMap: failed to load image from file");
+    throw;
+  }
+
+  map_->header.stamp = this->now();
+  map_->header.frame_id = "map";
+  map_->info.map_load_time = this->now();
+
+  // TODO(orduno): replace with a latched topic
+  map_timer_ = create_wall_timer(1s, std::bind(&PlannerTester::mapCallback, this));
+
+  map_set_ = true;
+  costmap_set_ = false;
+  using_fake_costmap_ = false;
+
+  setCostmap();
+}
+
+void PlannerTester::mapCallback()
+{
+  map_publisher_->publish(map_);
+}
+
+void PlannerTester::setCostmap()
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::setCostmap");
+
+  if (!map_set_) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "PlannerTester::setCostmap: map has not been provided");
+
+    // TODO(orduno) clarify how to handle errors here
+    return;
+  }
+
+  // TODO(orduno): clarify if we should be passing the node as shared or raw pointer
+  costmap_ = std::make_unique<Costmap>(
+    this, trinary_costmap_, track_unknown_space_, lethal_threshold_, unknown_cost_value_);
+
+  costmap_->setStaticMap(*map_);
+
+  costmap_set_ = true;
+  using_fake_costmap_ = false;
+}
+
+void PlannerTester::loadSimpleCostmap(const TestCostmap & testCostmapType)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::loadSimpleCostmap");
+
+  if (costmap_set_) {
+    RCLCPP_WARN(
+      this->get_logger(),
+      "PlannerTester::loadSimpleCostmap: setting a new costmap with fake values");
+  }
+
+  costmap_ = std::make_unique<Costmap>(this);
+
+  costmap_->setTestCostmap(testCostmapType);
+
+  costmap_set_ = true;
+  using_fake_costmap_ = true;
+}
+
+void PlannerTester::startCostmapServer(std::string serviceName)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::startCostmapServer");
+
+  if (!costmap_set_) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "PlannerTester::startCostmapServer:: you need to set a costmap before starting the service");
+    return;
+  }
+
+  auto costmap_service_callback = [this](
+    const std::shared_ptr<rmw_request_id_t>/*request_header*/,
+    const std::shared_ptr<nav2_world_model_msgs::srv::GetCostmap::Request> request,
+    std::shared_ptr<nav2_world_model_msgs::srv::GetCostmap::Response> response) -> void
+    {
+      RCLCPP_INFO(this->get_logger(), "PlannerTester: Incoming costmap request");
+
+      response->map = costmap_->getCostmap(request->specs);
+    };
+
+  // TODO(orduno): Enable parameter server and get costmap service name from there
+
+  // Create a service that will use the callback function to handle requests.
+  costmap_server_ = create_service<nav2_world_model_msgs::srv::GetCostmap>(
+    serviceName, costmap_service_callback);
+
+  costmap_server_running_ = true;
+}
+
+void PlannerTester::createPlannerClient(std::string plannerName)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::createPlannerClient");
+
+  planner_client_ = std::make_unique<nav2_tasks::ComputePathToPoseTaskClient>(/*plannerName,*/ this);
+}
+
+bool PlannerTester::defaultPlannerTest(
+  nav2_tasks::ComputePathToPoseResult::SharedPtr & path,
+  const double deviation_tolerance)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerTest");
+
+  if (!costmap_set_) {
+    RCLCPP_ERROR(this->get_logger(), "PlannerTester::defaultPlannerTest:"
+      " you need to set the costmap before requesting a plan");
+    return false;
+  }
+
+  auto endpoints = std::make_shared<nav2_tasks::ComputePathToPoseCommand>();
+  auto costmap_properties = costmap_->getProperties();
+
+  // Compose the PathEndPoints message
+  if (using_fake_costmap_) {
+    RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerTest:"
+      " planning using a fake costmap");
+
+    endpoints->start.position.x = 1.0;
+    endpoints->start.position.y = 1.0;
+    endpoints->goal.position.x = 9.0;
+    endpoints->goal.position.y = 9.0;
+    endpoints->tolerance = 2.0;
+
+  } else {
+    RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerTest:"
+      " planning using the provided map");
+
+    // Defined with respect to world coordinate system
+    //  Planner will do coordinate transformation to map internally
+    endpoints->start.position.x = 390.0;
+    endpoints->start.position.y = 10.0;
+    endpoints->goal.position.x = 10.0;
+    endpoints->goal.position.y = 390.0;
+    endpoints->tolerance = 2.0;
+  }
+
+  bool pathIsCollisionFree = plannerTest(endpoints, path);
+
+  // TODO(orduno): On a default test, provide the 'right answer' to compare with the planner result
+  //               given that we know the start, end and costmap is either preloaded or coming from
+  //               the provided map
+
+  bool pathIsWithinTolerance = isWithinTolerance(*path, deviation_tolerance);
+
+  return pathIsCollisionFree && pathIsWithinTolerance;
+}
+
+bool PlannerTester::defaultPlannerRandomTests(const unsigned int number_tests)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerRandomTests");
+
+  if (!costmap_set_) {
+    RCLCPP_ERROR(this->get_logger(), "PlannerTester::defaultPlannerRandomTests:"
+      " you need to set the costmap before requesting a plan");
+    return false;
+  }
+
+  // TODO(orduno): enable for costmaps
+  if (using_fake_costmap_) {
+    RCLCPP_ERROR(this->get_logger(), "PlannerTester::defaultPlannerRandomTests:"
+      " randomized testing with hardcoded costmaps not implemented yet");
+    return false;
+  }
+
+  // Initialize random number generator
+  std::random_device random_device;
+  std::mt19937 generator(random_device());
+  std::uniform_int_distribution<> distribution_x(0, costmap_->getProperties().size_x);
+  std::uniform_int_distribution<> distribution_y(0, costmap_->getProperties().size_y);
+
+  auto generate_random = [&]() mutable -> std::pair<int, int>{
+      bool point_is_free = false;
+      int x, y;
+      while (!point_is_free) {
+        x = distribution_x(generator);
+        y = distribution_y(generator);
+        // publishPoint(x, y, 0);
+        point_is_free = costmap_->isFree(x, y);
+      }
+      return std::make_pair(x, y);
+    };
+
+  auto endpoints = std::make_shared<nav2_tasks::ComputePathToPoseCommand>();
+  auto path = std::make_shared<nav2_tasks::ComputePathToPoseResult>();
+
+  bool all_tests_OK = true;
+  unsigned int num_fail = 0;
+  for (unsigned int test_num = 0; test_num < number_tests; ++test_num) {
+    RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerRandomTests:"
+      " running test #%u", test_num + 1);
+
+    // Compose the path endpoints using random numbers
+    // Defined with respect to world coordinate system
+    // Planner will do coordinate transformation to map internally
+    auto start = generate_random();
+    auto goal = generate_random();
+    endpoints->start.position.x = start.first;
+    endpoints->start.position.y = start.second;
+    endpoints->goal.position.x = goal.first;
+    endpoints->goal.position.y = goal.second;
+
+    endpoints->tolerance = 2.0;
+
+    // TODO(orduno): Tweak criteria for defining if a path goes into obstacles.
+    //               Current Dijkstra planner will sometimes produce paths that cut corners
+    //               i.e. some points are around the corner are actually inside the obstacle
+    bool pathIsCollisionFree = plannerTest(endpoints, path);
+
+    publishEndpoints(*endpoints, test_num);
+
+    if (!pathIsCollisionFree) {
+      RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerRandomTests:"
+        " failed or found a collision with start at %0.2f, %0.2f and end at %0.2f, %0.2f",
+        endpoints->start.position.x, endpoints->start.position.y,
+        endpoints->goal.position.x, endpoints->goal.position.y);
+      all_tests_OK = false;
+      ++num_fail;
+    }
+  }
+
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::defaultPlannerRandomTests:"
+    " tested with %u endpoints. Planner failed on %u", number_tests, num_fail);
+
+  return all_tests_OK;
+}
+
+bool PlannerTester::plannerTest(
+  const nav2_tasks::ComputePathToPoseCommand::SharedPtr & endpoints,
+  nav2_tasks::ComputePathToPoseResult::SharedPtr & path)
+{
+  RCLCPP_INFO(this->get_logger(), "PlannerTester::plannerTest:"
+    " getting the path from the planner");
+
+  TaskStatus status = sendRequest(endpoints, path);
+
+  if (status == TaskStatus::FAILED) {
+    return false;
+  } else if (status == TaskStatus::SUCCEEDED) {
+    // TODO(orduno): check why task may report success while planner fails in some cases
+    //               in those cases, planner returns a path of 0 points
+    RCLCPP_INFO(this->get_logger(), "PlannerTester::plannerTest:"
+      " got path, checking for collisions");
+
+    return isCollisionFree(*path);
+  }
+
+  return false;
+}
+
+TaskStatus PlannerTester::sendRequest(
+  const nav2_tasks::ComputePathToPoseCommand::SharedPtr & endpoints,
+  nav2_tasks::ComputePathToPoseResult::SharedPtr & path)
+{
+  while (this->count_subscribers("/DijkstraPlanner_command") < 1) {
+  }
+
+  planner_client_->sendCommand(endpoints);
+
+  // Loop until the subtask is completed
+  while (true) {
+    TaskStatus status = planner_client_->waitForResult(path, std::chrono::milliseconds(100));
+
+    if (status != TaskStatus::RUNNING) {
+      return status;
+    }
+  }
+}
+
+bool PlannerTester::isCollisionFree(const nav2_tasks::ComputePathToPoseResult & path)
+{
+  // At each point of the path, check if the corresponding cell is free
+  // TODO(orduno): for now we are assuming the robot is the size of a single cell
+  //               costmap/world_model has consider the robot footprint
+
+  bool collisionFree = true;
+
+  for (auto pose : path.poses) {
+    collisionFree = costmap_->isFree(
+      static_cast<unsigned int>(pose.position.x), static_cast<unsigned int>(pose.position.y));
+
+    if (!collisionFree) {
+      RCLCPP_INFO(this->get_logger(), "PathTester::isCollisionFree: path has collisions :(");
+      return false;
+    }
+  }
+
+  RCLCPP_INFO(this->get_logger(), "PathTester::isCollisionFree: path no has collisions :)");
+  return true;
+}
+
+bool PlannerTester::isWithinTolerance(
+  const nav2_tasks::ComputePathToPoseResult & /*path*/,
+  const double /*deviationTolerance*/)
+{
+  // TODO(orduno)
+  return true;
+}
+
+bool PlannerTester::sendCancel()
+{
+  RCLCPP_ERROR(this->get_logger(), "PlannerTester::sendCancel:"
+    " function not implemented yet");
+
+  // TODO(orduno)
+  return false;
+}
+
+void PlannerTester::publishEndpoints(
+  const nav2_tasks::ComputePathToPoseCommand & endpoints, const int index)
+{
+  visualization_msgs::msg::Marker marker;
+
+  builtin_interfaces::msg::Time time;
+  time.sec = 0;
+  time.nanosec = 0;
+  marker.header.stamp = time;
+  marker.header.frame_id = "map";
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "endpoints";
+  marker.id = index;
+
+  marker.type = visualization_msgs::msg::Marker::SPHERE_LIST;
+
+  // Set the marker action.
+  marker.action = visualization_msgs::msg::Marker::ADD;
+
+  // Set the pose of the marker.
+  // This is a full 6DOF pose relative to the frame/time specified in the header
+  geometry_msgs::msg::Pose pose;
+  pose.orientation.w = 1.0;
+
+  marker.pose.orientation = pose.orientation;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 3.0;
+  marker.scale.y = 3.0;
+  marker.scale.z = 3.0;
+
+  builtin_interfaces::msg::Duration duration;
+  duration.sec = 0;
+  duration.nanosec = 0;
+
+  // 0 indicates the object should last forever
+  marker.lifetime = duration;
+
+  marker.frame_locked = false;
+
+  marker.points.resize(2);
+  marker.points[0] = endpoints.start.position;
+  marker.points[1] = endpoints.goal.position;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  std_msgs::msg::ColorRGBA start_color;
+  start_color.r = 0.0;
+  start_color.g = 0.0;
+  start_color.b = 1.0;
+  start_color.a = 1.0;
+  std_msgs::msg::ColorRGBA goal_color;
+  goal_color.r = 0.0;
+  goal_color.g = 1.0;
+  goal_color.b = 0.0;
+  goal_color.a = 1.0;
+
+  marker.colors.resize(2);
+  marker.colors[0] = start_color;
+  marker.colors[1] = goal_color;
+
+  endpoints_publisher_->publish(marker);
+}
+
+void PlannerTester::publishPoint(const int x, const int y, const int index)
+{
+  visualization_msgs::msg::Marker marker;
+
+  builtin_interfaces::msg::Time time;
+  time.sec = 0;
+  time.nanosec = 0;
+  marker.header.stamp = time;
+  marker.header.frame_id = "map";
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "endpoints";
+  marker.id = index;
+
+  marker.type = visualization_msgs::msg::Marker::SPHERE;
+
+  // Set the marker action.
+  marker.action = visualization_msgs::msg::Marker::ADD;
+
+  // Set the pose of the marker.
+  // This is a full 6DOF pose relative to the frame/time specified in the header
+  geometry_msgs::msg::Pose pose;
+  pose.position.x = x;
+  pose.position.y = y;
+  pose.orientation.w = 1.0;
+
+  marker.pose = pose;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 3.0;
+  marker.scale.y = 3.0;
+  marker.scale.z = 3.0;
+
+  marker.color.r = 0.0;
+  marker.color.g = 0.0;
+  marker.color.b = 1.0;
+  marker.color.a = 1.0;
+
+  builtin_interfaces::msg::Duration duration;
+  duration.sec = 0;
+  duration.nanosec = 0;
+
+  // 0 indicates the object should last forever
+  marker.lifetime = duration;
+
+  marker.frame_locked = false;
+
+  endpoints_publisher_->publish(marker);
+}
+
+}  // namespace nav2_util
