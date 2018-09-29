c714cf04ade0719a4e27f091490faa994e72b97e
Brian <bpwilcox@eng.ucsd.edu>
2018-09-29 02:32:08 -0700

    bug fixes for PR feedback

--- src/libs/costmap_2d/src/costmap_2d_node.cpp
+++ src/libs/costmap_2d/src/costmap_2d_node.cpp
@@ -43,8 +43,8 @@ int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("costmap_2d");
-  std::string name = (argc == 1) ? "costmap" : std::string(argv[1]);
+  auto node = rclcpp::Node::make_shared("costmap_node");
+  std::string name = "costmap";
   tf2_ros::Buffer buffer(tf2::durationFromSec(10));
   tf2_ros::TransformListener tf(buffer);
   costmap_2d::Costmap2DROS lcr(name, buffer);
