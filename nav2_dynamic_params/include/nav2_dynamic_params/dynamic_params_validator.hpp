f9143687b9caf105c9960429596e9797008fa96b
Brian <bpwilcox@eng.ucsd.edu>
2018-10-17 18:14:02 -0700

    add parameter client to DynamicParamClient, added cache map of parameters, passing all linters & tests
    spacing/tab changes

--- nav2_dynamic_params/include/nav2_dynamic_params/dynamic_params_validator.hpp
+++ nav2_dynamic_params/include/nav2_dynamic_params/dynamic_params_validator.hpp
@@ -12,10 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_DYNAMIC_PARAMS_DYNAMIC_PARAMS_VALIDATOR_H_
-#define NAV2_DYNAMIC_PARAMS_DYNAMIC_PARAMS_VALIDATOR_H_
+#ifndef NAV2_DYNAMIC_PARAMS__DYNAMIC_PARAMS_VALIDATOR_HPP_
+#define NAV2_DYNAMIC_PARAMS__DYNAMIC_PARAMS_VALIDATOR_HPP_
 
 #include <map>
+#include <string>
+#include <utility>
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
 
 namespace nav2_dynamic_params
@@ -24,44 +27,45 @@ namespace nav2_dynamic_params
 class DynamicParamsValidator
 {
 public:
+  explicit DynamicParamsValidator(rclcpp::Node::SharedPtr node);
 
-  DynamicParamsValidator(rclcpp::Node::SharedPtr node);
-
-  ~DynamicParamsValidator(){}
+  ~DynamicParamsValidator() {}
 
   void add_param(std::string param_name, rclcpp::ParameterType type);
 
-  void add_param(std::string param_name, rclcpp::ParameterType type, std::pair<double,double> bounds);
+  void add_param(
+    std::string param_name, rclcpp::ParameterType type,
+    std::pair<double, double> bounds);
 
-  void add_param(std::string param_name, rclcpp::ParameterType type, std::pair<double,double> bounds, int ignore_bound);
+  void add_param(
+    std::string param_name, rclcpp::ParameterType type,
+    std::pair<double, double> bounds, int ignore_bound);
 
   void add_param(std::map<std::string, rclcpp::ParameterType> map);
 
-  void set_validation_callback(std::function<rcl_interfaces::msg::SetParametersResult(
-    const std::vector<rclcpp::Parameter> &)> callback);
- 
-private:
+  void set_validation_callback(
+    std::function<rcl_interfaces::msg::SetParametersResult(
+      const std::vector<rclcpp::Parameter> &)> callback);
 
+private:
   rcl_interfaces::msg::SetParametersResult param_validation_callback(
     std::vector<rclcpp::Parameter> parameters);
 
   bool validate_param(std::string param_name, rclcpp::Parameter param, rclcpp::ParameterType type);
 
-  bool validate_param_bounds(std::string param_name, rclcpp::Parameter param, std::pair<double,double> bound);
+  bool validate_param_bounds(
+    std::string param_name, rclcpp::Parameter param,
+    std::pair<double, double> bound);
 
   rclcpp::Node::SharedPtr node_;
 
   std::map<std::string, rclcpp::ParameterType> param_map_;
-  std::map<std::string, std::pair<double,double>> param_bound_map_;
+  std::map<std::string, std::pair<double, double>> param_bound_map_;
 
   std::function<rcl_interfaces::msg::SetParametersResult(
-    const std::vector<rclcpp::Parameter> &)> validation_callback_;
-
-  rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr parameter_sub_;
-
+      const std::vector<rclcpp::Parameter> &)> validation_callback_;
 };
 
 }  // namespace nav2_dynamic_params
 
-#endif  // NAV2_DYNAMIC_PARAMS_DYNAMIC_PARAMS_VALIDATOR_H_
-
+#endif  // NAV2_DYNAMIC_PARAMS__DYNAMIC_PARAMS_VALIDATOR_HPP_
