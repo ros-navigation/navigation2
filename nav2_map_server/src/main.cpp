2f09930dbe5dc94624359bf900c052a0309e8d90
Michael Jeronimo <michael.jeronimo@intel.com>
2018-10-23 14:17:20 -0700

    Use a YAML file to configure the map server
    This allows the map server to be used as a standalone node or composed
    with other nodes into a single executable.
    
    Also, update the costmap_world_model to make sure its got the latest map
    from the map_server.

--- nav2_map_server/src/main.cpp
+++ nav2_map_server/src/main.cpp
@@ -12,56 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "rcutils/cmdline_parser.h"
 #include "nav2_map_server/map_factory.hpp"
-
-void print_usage()
-{
-  printf("Usage: map_server [options]\n");
-  printf("Options:\n");
-  printf("  -h                Display usage information\n");
-  printf("  -f                YAML file for map to load\n");
-  printf("  -t map_type       Type of the map server to run\n");
-}
 
 int main(int argc, char ** argv)
 {
-  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
-    print_usage();
-    return 0;
-  }
-
-  // Get the YAML filename (required)
-  char * option = rcutils_cli_get_option(argv, argv + argc, "-f");
-
-  if (option == nullptr) {
-    print_usage();
-    return -1;
-  }
-
-  std::string file_name = std::string(option);
-
-  // Get the map type (optional, defaults to occupancy grid)
-  std::string map_type("occupancy");
-  option = rcutils_cli_get_option(argv, argv + argc, "-t");
-
-  if (option != nullptr) {
-    map_type = std::string(option);
-  }
-
-  // Now that we have the parameters, get started
   rclcpp::init(argc, argv);
 
   // Create the node that will be used by the map server. Creating the node first
-  // allows any sub-objects, such as the map_server to use the Node::SharedPtr in their
-  // constructors, which wouldn't be the case if the MapServer itself was directly a Node
-  rclcpp::Node::SharedPtr map_server_node = std::make_shared<rclcpp::Node>("map_server_node");
+  // allows the map_server to use the Node::SharedPtr in its constructor, which
+  // wouldn't be possible if the MapServer was directly a Node
+  rclcpp::Node::SharedPtr map_server_node = std::make_shared<rclcpp::Node>("map_server");
 
   // Create the map server that uses this node
-  auto map_server = nav2_map_server::MapFactory::createMap(map_server_node, map_type, file_name);
+  auto map_server = nav2_map_server::MapFactory::createMap(map_server_node);
 
   rclcpp::spin(map_server_node);
   rclcpp::shutdown();
