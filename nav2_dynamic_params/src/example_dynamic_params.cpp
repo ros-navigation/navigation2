a4e6f16732087e98792fdbba2f80970d561768ed
Brian <bpwilcox@eng.ucsd.edu>
2018-10-29 13:33:43 -0700

    added forced callback, custom validation callback example, and passed linters (except xmllint) & uncrustify

--- nav2_dynamic_params/src/example_dynamic_params.cpp
+++ nav2_dynamic_params/src/example_dynamic_params.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <memory>
+#include <vector>
 #include "nav2_dynamic_params/dynamic_params_client.hpp"
 #include "nav2_dynamic_params/dynamic_params_validator.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -64,21 +65,52 @@ int main(int argc, char ** argv)
   param_validator->add_param("foo", rclcpp::ParameterType::PARAMETER_DOUBLE);
   param_validator->add_param("bar", rclcpp::ParameterType::PARAMETER_INTEGER, {0, 10});
 
+  // Create a custom validation callback
+  std::function<rcl_interfaces::msg::SetParametersResult(
+      const std::vector<rclcpp::Parameter> &)> custom_validation_callback = [node](
+    const std::vector<rclcpp::Parameter> & parameters)
+    -> rcl_interfaces::msg::SetParametersResult
+    {
+      auto result = rcl_interfaces::msg::SetParametersResult();
+      result.successful = true;
+
+      for (const auto & parameter : parameters) {
+        // Filter for parameter "foo"
+        if (parameter.get_name() == "foo") {
+          auto value = parameter.get_value<double>();
+          // Reject any set requests between 10 & 20
+          if (value > 10.0 && value < 20.0) {
+            RCLCPP_INFO(node->get_logger(),
+              "Parameter Change Denied::Failing Custom Validation: %s",
+              parameter.get_name().c_str());
+            result.successful = false;
+            return result;
+          }
+        }
+      }
+      return result;
+    };
+
+  param_validator->set_validation_callback(custom_validation_callback);
+
   // Change Parameters
-  RCLCPP_INFO(node->get_logger(), "First Service Request:");
+  RCLCPP_INFO(node->get_logger(), "1st Service Request:");
   node->set_parameters({rclcpp::Parameter("foo", 2.0), rclcpp::Parameter("bar", 3)});
 
   // Try to set illegal values
-  RCLCPP_INFO(node->get_logger(), "Second Service Request:");
+  RCLCPP_INFO(node->get_logger(), "2nd Service Request:");
   node->set_parameters({rclcpp::Parameter("foo", 1), rclcpp::Parameter("bar", 11)});
 
   // Add New Parameter
-  RCLCPP_INFO(node->get_logger(), "Third Service Request:");
+  RCLCPP_INFO(node->get_logger(), "3rd Service Request:");
   node->set_parameters({rclcpp::Parameter("foobar", 28.0)});
   param_validator->add_static_params({"foobar"});
 
-  RCLCPP_INFO(node->get_logger(), "Fourth Service Request:");
+  RCLCPP_INFO(node->get_logger(), "4th Service Request:");
   node->set_parameters({rclcpp::Parameter("foobar", 5.0)});
+
+  RCLCPP_INFO(node->get_logger(), "5th Service Request:");
+  node->set_parameters({rclcpp::Parameter("foo", 15.0)});
 
   rclcpp::spin(node);
   rclcpp::shutdown();
