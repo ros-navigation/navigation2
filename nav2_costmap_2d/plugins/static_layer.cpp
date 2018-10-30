5d6532cf5cbd6a76fd2bead308a6bf6020a17d00
Brian <bpwilcox@eng.ucsd.edu>
2018-10-29 17:17:42 -0700

    modified costmap layers and costmap2DROS to use redesigned interface
    remove extra test node

--- nav2_costmap_2d/plugins/static_layer.cpp
+++ nav2_costmap_2d/plugins/static_layer.cpp
@@ -121,9 +121,9 @@ void StaticLayer::onInitialize()
   }
 
   dynamic_param_client_ = new nav2_dynamic_params::DynamicParamsClient(node_);
+  dynamic_param_client_->add_parameters({"enabled_static_layer"});
   dynamic_param_client_->set_callback(std::bind(&StaticLayer::reconfigureCB, this, std::placeholders::_1));
   // TODO(bpwilcox): Add new parameters to parameter validation class from plugins
-  // TODO(bpwilcox): Initialize callback for dynamic parameters
 }
 
 void StaticLayer::reconfigureCB(const rcl_interfaces::msg::ParameterEvent::SharedPtr event)
