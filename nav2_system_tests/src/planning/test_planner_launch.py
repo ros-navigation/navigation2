c86812f6cc0f57b60a176345a050def6c8135b70
stevemacenski <stevenmacenski@gmail.com>
2018-11-05 14:24:55 -0800

    update tests in system tests to use the smart planner

--- nav2_system_tests/src/planning/test_planner_launch.py
+++ nav2_system_tests/src/planning/test_planner_launch.py
@@ -30,8 +30,8 @@ def main(argv=sys.argv[1:]):
     testExecutable = os.getenv('TEST_EXECUTABLE')
 
     ld = LaunchDescription([launch_ros.actions.Node(
-        package='nav2_dijkstra_planner',
-        node_executable='dijkstra_planner',
+        package='nav2_smart_planner',
+        node_executable='smart_planner',
         output='screen'),
     ])
 
