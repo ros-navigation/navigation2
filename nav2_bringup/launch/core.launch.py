f79c6ab4e903fd081ba354143e3e117f79dd9c6d
stevemacenski <stevenmacenski@gmail.com>
2018-11-07 17:57:36 -0800

    changing bringup to use node nominclature

--- nav2_bringup/launch/core.launch.py
+++ nav2_bringup/launch/core.launch.py
@@ -9,30 +9,30 @@ def generate_launch_description():
     return LaunchDescription([
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_map_server', node_executable='map_server', output='screen', arguments=[[mapFile], 'occupancy'])
+                launch_ros.actions.Node( package='nav2_map_server', node_executable='map_server_node', output='screen', arguments=[[mapFile], 'occupancy'])
                 ], period = 1.0),
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_controller_dwb', node_executable='nav2_controller_dwb', output='screen')
+                launch_ros.actions.Node( package='nav2_controller_dwb', node_executable='nav2_controller_dwb_node', output='screen')
                 ], period = 5.0),
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_smart_planner', node_executable='smart_planner', output='screen')
+                launch_ros.actions.Node( package='nav2_smart_planner', node_executable='smart_planner_node', output='screen')
                 ], period = 10.0),
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_simple_navigator', node_executable='simple_navigator', output='screen')
+                launch_ros.actions.Node( package='nav2_simple_navigator', node_executable='simple_navigator_node', output='screen')
                 ], period = 15.0),
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_mission_executor', node_executable='mission_executor', output='screen')
+                launch_ros.actions.Node( package='nav2_mission_executor', node_executable='mission_executor_node', output='screen')
                 ], period = 20.0),
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_costmap_world_model', node_executable='costmap_world_model', output='screen')
+                launch_ros.actions.Node( package='nav2_costmap_world_model', node_executable='costmap_world_model_node', output='screen')
                 ], period = 25.0),
         launch.actions.TimerAction(
             actions = [
-                launch_ros.actions.Node( package='nav2_amcl', node_executable='amcl', output='screen')
+                launch_ros.actions.Node( package='nav2_amcl', node_executable='amcl_node', output='screen')
                 ], period = 30.0),
     ])
