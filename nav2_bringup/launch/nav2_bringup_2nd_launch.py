4716dbe3c165a65cb8d5e7e49ca395ab81d97eaa
Carl Delsey <carl.r.delsey@intel.com>
2018-12-05 15:44:36 -0800

    Add dwb param yaml file to 2-part launch

--- nav2_bringup/launch/nav2_bringup_2nd_launch.py
+++ nav2_bringup/launch/nav2_bringup_2nd_launch.py
@@ -5,6 +5,8 @@ import launch_ros.actions
 
 def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
+    params_file = launch.substitutions.LaunchConfiguration('params', default=
+        [launch.substitutions.ThisLaunchFileDir(), '/nav2_params.yaml'])
 
     return LaunchDescription([
         launch.actions.DeclareLaunchArgument(
@@ -22,7 +24,7 @@ def generate_launch_description():
             node_executable='dwb_controller',
             node_name='FollowPathNode',
             output='screen',
-            parameters=[{ 'prune_plan': False }, {'debug_trajectory_details': True }, { 'use_sim_time': use_sim_time }]),
+            parameters=[params_file]),
 
         launch_ros.actions.Node(
             package='nav2_navfn_planner',
