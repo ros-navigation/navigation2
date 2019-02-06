3fb9759d6ccc90b819f65b297af2c245f2f614db
Orduno, Carlos A <carlos.a.orduno@intel.com>
2019-02-06 13:58:45 -0800

    Adding costmap cleaner

---
+++ nav2_costmap_2d/include/nav2_costmap_2d/costmap_cleaner.hpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_COSTMAP_2D__COSTMAP_CLEANER_HPP
+#define NAV2_COSTMAP_2D__COSTMAP_CLEANER_HPP
+
+#include <vector>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_msgs/srv/clean_costmap.hpp"
+#include "costmap_2d/costmap_layer.hpp"
+
+namespace nav2_costmap_2d
+{
+
+// Clears the region outside of a user-specified area
+// Currently reverting to the static map
+class CostmapCleaner
+{
+public:
+  CostmapCleaner(rclcpp::Node::SharedPtr & node, Costmap2DROS * costmap);
+
+  CostmapCleaner() = delete;
+
+  void clean();
+
+private:
+  // The ROS node to use for getting parameters, creating the service and logging
+  rclcpp::Node::SharedPtr node_;
+
+  // The costmap to clear
+  Costmap2DROS * costmap_;
+
+  // Cleaning parameters
+  double reset_distance_;
+  std::vector<std::string> cleanable_layers_;
+
+  // Server for cleaning the costmap
+  rclcpp::Service<nav2_msgs::srv::CleanCostmap>::SharedPtr server_;
+  void cleanCallback(
+    const std::shared_ptr<rmw_request_id_t> request_header,
+    const std::shared_ptr<nav2_msgs::srv::CleanCostmap::Request> request,
+    const std::shared_ptr<nav2_msgs::srv::CleanCostmap::Response> response);
+
+  void cleanLayer(std::shared_ptr<CostmapLayer> & costmap, double x, double y);
+
+  bool getPose(double & x, double & y) const;
+
+  std::string getLayerName(const Layer & layer) const
+};
+
+}  // namespace nav2_costmap_2d
+
+#endif  // NAV2_COSTMAP_2D__COSTMAP_CLEANER_HPP
---
+++ nav2_costmap_2d/src/costmap_cleaner.cpp
@@ -0,0 +1,144 @@
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nav2_costmap_2d/costmap_cleaner.hpp"
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+namespace nav2_costmap_2d
+{
+
+using std::vector;
+using std::string;
+using std::shared_ptr;
+using std::any_of;
+
+CostmapCleaner::CostmapCleaner(rclcpp::Node::SharedPtr & node, Costmap2DROS * costmap)
+: node_(node), costmap_(costmap)
+{
+  node_->get_parameter_or<double>("reset_distance", reset_distance_, 3.0);
+
+  node_->get_parameter_or_set("cleanable_layers", cleanable_layers_, {"obstacle_layer"});
+
+  server_ = node_->create_service<nav2_msgs::srv::CleanCostmap>("Clean" + costmap->getName(),
+    std::bind(&CostmapCleaner::clean_callback, this,
+    std::placeholders::_1, std::placeholders::_2, std::placeholder::_3));
+}
+
+void CostmapCleaner::cleanCallback(
+  const shared_ptr<rmw_request_id_t>/*request_header*/,
+  const shared_ptr<nav2_msgs::srv::CleanCostmap::Request> /*request*/,
+  const shared_ptr<nav2_msgs::srv::CleanCostmap::Response> /*response*/)
+)
+{
+  RCLCPP_INFO(node_->get_logger(), "Received request to clean " + costmap->getName().c_str());
+
+  if (costmap_ == nullptr) {
+    RCLCPP_ERROR("Costmap is undefined. Doing nothing.");
+    return;
+  }
+
+  RCLCPP_INFO(node_->get_logger(), "Will proceed with cleaning the costmap");
+
+  clean();
+}
+
+void CostmapCleaner::clean()
+{
+  double x, y;
+
+  if (!getPose(x,y)) {
+    RCLCPP_ERROR("Cannot clean map because robot pose cannot be retrieved.");
+    return;
+  }
+
+  auto layers = costmap_->getLayeredCostmap()->getPlugins();
+
+  for (auto & layer : layers) {
+    auto name = getLayerName(*layer);
+
+    if (any_of(begin(cleanable_layers_), end(cleanable_layers_), [](auto l){return l == name; })) {
+      auto costmap_layer = std::static_pointer_cast<costmap_2d::CostmapLayer>(layer);
+      cleanLayer(costmap_layer, x, y);
+    }
+  }
+}
+
+void CostmapCleaner::cleanLayer(shared_ptr<CostmapLayer> & costmap, double x, double y)
+{
+  std::unique_lock<Costmap2D::mutex_t> lock(*(costmap.getMutex()));
+
+  double start_point_x = pose_x - reset_distance_ / 2;
+  double start_point_y = pose_y - reset_distance_ / 2;
+  double end_point_x = start_point_x + reset_distance_;
+  double end_point_y = start_point_y + reset_distance_;
+
+  int start_x, start_y, end_x, end_y;
+  costmap->worldToMapNoBounds(start_point_x, start_point_y, start_x, start_y);
+  costmap->worldToMapNoBounds(end_point_x, end_point_y, end_x, end_y);
+
+  unsigned char * grid = costmap->getCharMap();
+
+  for (int x = 0; x < static_cast<int>(costmap->getSizeInCellsX(); x++)) {
+    bool isOutXrange = x < start_x && x > end_x;
+
+    for (int y = 0; y < static_cast<int>(costmap->getSizeInCellsY(); y++)) {
+      bool isOutYrange = y < start_y && y > end_y;
+
+      if (isOutXrange && isOutYrange) {
+        int index = costmap_layer->getIndex(x, y);
+
+        if (grid[index] != NO_INFORMATION) {
+          grid[index] = NO_INFORMATION;
+        }
+      }
+    }
+  }
+
+  double ox = costmap->getOriginX(), oy = costmap->getOriginY();
+  double width = costmap->getSizeInMetersX(), height = costmap->getSizeInMetersY();
+  costmap->addExtraBounds(ox, oy, ox + width, oy + height);
+
+  return;
+}
+
+bool CostmapCleaner::getPose(double & x, double & y) const
+{
+  geometry_msgs::msg::PoseStamped pose;
+  if (!costmap_->getRobotPose(pose)) {
+    return false;
+  }
+
+  x = pose.pose.position.x;
+  y = pose.pose.position.y;
+
+  return true;
+}
+
+string CostmapCleaner::getLayerName(const Layer & layer) const
+{
+  string name = layer.getName();
+
+  int slash = name.rfind('/');
+
+  if (slash != std::string::npos) {
+    name = name.substr(slash + 1);
+  }
+
+  return name;
+}
+
+}  // namespace nav2_costmap_2d
