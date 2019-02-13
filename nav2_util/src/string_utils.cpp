e3ace7e270bfa648bd72a97ea94923995e84055d
Carl Delsey <carl.r.delsey@intel.com>
2019-02-13 13:16:35 -0800

    Clean up and rename existing string utils module
    I need to add another function to it, and it could stand some
    restructuring before hand.

---
+++ nav2_util/src/string_utils.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nav2_util/string_utils.hpp"
+#include <string>
+
+namespace nav2_util
+{
+
+std::string stripLeadingSlash(const std::string & in)
+{
+  std::string out = in;
+
+  if ((!in.empty()) && (in[0] == '/')) {
+    out.erase(0, 1);
+  }
+
+  return out;
+}
+
+}  // namespace nav2_util
