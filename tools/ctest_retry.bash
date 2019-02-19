1c1f4cd71ee55c13ac2864d4a6437bc4f85d87ed
Carl Delsey <carl.r.delsey@intel.com>
2019-02-19 13:47:17 -0800

    Add BT Navigator System Test (#557)
    * Clean up ctest retry script to allow running in place.
    
    * Use the new ctest_retry script and retry each test individually.
    
    * Remove colcon version of test retry since we can't get debug output.
    
    * Enable bt navigator system test.
    
    * Add comments to address review issues.

--- tools/ctest_retry.bash
+++ tools/ctest_retry.bash
@@ -1,14 +1,54 @@
 #!/bin/bash
 
-fail=0
-total=$1
-# Change the file path manually to where you want to log fails
+usage() {
+  echo "ctest_retry.bash [options]"
+  echo "Reruns ctest until the test passes or it has run 3 times (by default)."
+  echo "  -r <number> Retry the test up to <number> times [Default: 3]"
+  echo "  -d <path> Execute ctest from the given path [Default: Current working directory]"
+  echo "  -t <testname> Execute only <testname> [Default: execute all tests available in <path> ]"
+  echo "  -h displays this usage summary"
+  exit 0
+}
+
+RETRIES=3
+TESTDIR=.
+SPECIFIC_TEST=""
+
+# Check Options
+while getopts ":hr:d:t:" opt; do
+  case "$opt" in
+    h)
+      usage
+      ;;
+    r)
+      RETRIES=$OPTARG
+      ;;
+    d)
+      TESTDIR=$OPTARG
+      ;;
+    t)
+      SPECIFIC_TEST="-R $OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+total=$RETRIES
+cd $TESTDIR
 echo "Retrying Ctest up to " $total " times."
 for ((i=1;i<=total;i++))
   do
-    ctest
+    ctest -V $SPECIFIC_TEST
     result=$?
-    if [ "$result" == "0" ]
+    if [ "$result" == "0" ]  # if ctest succeeded, then exit the retry loop
     then
       echo "Test succeeded on try " $i
       exit 0
--- tools/run_test_suite.bash
+++ tools/run_test_suite.bash
@@ -2,8 +2,11 @@
 
 set -ex
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # gets the directory of this script
+
 colcon test --packages-skip nav2_system_tests
+colcon test --packages-select nav2_system_tests --ctest-args --exclude-regex "test_.*"  # run the linters
 colcon test-result --verbose
-cp src/navigation2/tools/ctest_retry.bash build/nav2_system_tests
-cd build/nav2_system_tests
-./ctest_retry.bash 3
+$SCRIPT_DIR/ctest_retry.bash -r 3 -d build/nav2_system_tests -t test_localization
+$SCRIPT_DIR/ctest_retry.bash -r 3 -d build/nav2_system_tests -t test_simple_navigator
+$SCRIPT_DIR/ctest_retry.bash -r 3 -d build/nav2_system_tests -t test_bt_navigator
