2de0b47433060f2286bb30af4bbc489681f4121a
Michael Jeronimo <michael.jeronimo@intel.com>
2019-04-30 10:10:21 -0700

    Port the world model lifecycle to lifecycle nodes (#654)
    * Port the world model to lifecycle nodes
    * Remove a couple stray printfs
    * Address review feedback

--- nav2_world_model/src/main.cpp
+++ nav2_world_model/src/main.cpp
@@ -10,19 +10,18 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License. Reserved.
+// limitations under the License.
 
 #include <memory>
-#include "rclcpp/rclcpp.hpp"
+
 #include "nav2_world_model/world_model.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::executors::SingleThreadedExecutor exec;
-  auto world_model_node = std::make_shared<nav2_world_model::WorldModel>(exec);
-  exec.add_node(world_model_node);
-  exec.spin();
+  auto node = std::make_shared<nav2_world_model::WorldModel>();
+  rclcpp::spin(node->get_node_base_interface());
   rclcpp::shutdown();
 
   return 0;
