7da91657d5ac05cd8b8260c6c610cdb163d0503a
Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>
2019-05-02 11:45:59 -0700

    factored out dqn class and parameters

--- nav2_experimental_rl/random_crawl_train.py
+++ nav2_experimental_rl/random_crawl_train.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from turtlebot3_env import TurtlebotEnv
+from dqn import DQN
 
 import random
 import numpy as np
@@ -21,93 +22,15 @@ import tensorflow as tf
 import rclpy
 from rclpy.node import Node
 from time import sleep
+import parameters
 
-from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras.models import load_model
-
-import matplotlib.pyplot as plt
-
-GAMMA = 0.90
-LEARNING_RATE = 0.005
-MEMORY_SIZE = 1000000
-BATCH_SIZE = 64
-EXPLORATION_MIN = 0.25
-EXPLORATION_MAX = 1.0
-EXPLORATION_TARGET_STEP = 1000
-TARGET_MODEL_UPDATE_STEP = 100
-EPISODES = 10000
-LOOP_RATE = 0.2 #Seconds
-
-class DQN:
-    def __init__(self, observation_space, action_size):
-        self.action_size = action_size
-        self.memory = deque(maxlen=MEMORY_SIZE)
-        self.build_model(observation_space, False)
-        self.target_model = self.model
-        self.step = 0
-
-    def build_model(self, observation_space, load):
-        if load:
-         self.model = load_model('random_crawl_model.h5')
-        else:
-         self.model = Sequential()
-         self.model.add(Dense(8, input_shape=(observation_space,), activation="relu"))
-         self.model.add(Dense(8, activation="relu"))
-         self.model.add(Dense(8, activation="relu"))
-         self.model.add(Dense(self.action_size, activation="linear"))
-         self.model.compile(loss="mse", optimizer=Adam(lr=LEARNING_RATE))
-
-    def save_to_memory(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-   
-    def get_linear_decay_epsilon(self, step):
-        if step <= EXPLORATION_TARGET_STEP:
-           epsilon = EXPLORATION_MAX - step *\
-                ((EXPLORATION_MAX - EXPLORATION_MIN)/EXPLORATION_TARGET_STEP)
-        else:
-           epsilon = EXPLORATION_MIN
-        return epsilon
-
-    def get_action(self, state):
-        exploration_rate = self.get_linear_decay_epsilon(self.step)
-        # Explore
-        if np.random.rand() <= exploration_rate:
-            action = random.randrange(self.action_size)
-            return action
-        # Exploit
-        q_values = self.model.predict(state,batch_size=1)
-        action = np.argmax(q_values)
-        return action
-
-    # Train with previous experiences
-    def experience_replay(self):
-        if len(self.memory) < BATCH_SIZE:
-            return
-        mini_batch = random.sample(self.memory, BATCH_SIZE)
-        for state, action, reward, next_state, done in mini_batch:
-            q_target = reward if done else reward +\
-                 GAMMA * np.amax(self.target_model.predict(next_state,batch_size=1))
-            
-            q_values = self.model.predict(state,batch_size=1)
-            q_values[0][action] = q_target
-            
-            self.model.fit(state, q_values, verbose=0)
-
-    # update target model every TARGET_MODEL_UPDATE_STEP steps
-    def save_load_model_weights(self):
-        self.model.save_weights('target_model.h5')
-        self.target_model.load_weights('target_model.h5', by_name=False)
- 
 def trainModel(env, action_size):
     state = env.reset()
     observation_space = len(state)
     agent = DQN(observation_space, action_size)
     target_model_update_counter = 0
     agent.step = 0
-    for _ in range(EPISODES):
+    for _ in range(parameters.EPISODES):
         print("Episode number: " + str(_))
         state = env.reset()
         observation_size = len(state)
@@ -116,7 +39,7 @@ def trainModel(env, action_size):
         while not done:
             agent.step += 1
             target_model_update_counter += 1
-            if target_model_update_counter%TARGET_MODEL_UPDATE_STEP == 0:
+            if target_model_update_counter%parameters.TARGET_MODEL_UPDATE_STEP == 0:
              agent.save_load_model_weights()
              target_model_update_counter = 0
             action = agent.get_action(state)
@@ -126,7 +49,7 @@ def trainModel(env, action_size):
             state = next_state
             if not done: 
              agent.experience_replay()
-            sleep(LOOP_RATE)
+            sleep(parameters.LOOP_RATE)
         agent.model.save('random_crawl_model.h5')
 
 def main(args=None):
