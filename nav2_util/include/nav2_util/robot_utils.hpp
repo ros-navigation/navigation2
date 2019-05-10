8a85061068aebd19aea6741aac68c4e51444e0b0
Steve <stevenmacenski@gmail.com>
2019-05-10 13:25:44 -0700

    robot_util classes for publishing and states

---
+++ nav2_util/include/nav2_util/robot_utils.hpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_UTIL__ROBOT_UTILS_HPP_
+#define NAV2_UTIL__ROBOT_UTILS_HPP_
+
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+namespace nav2_util
+{
+
+class VelocityPublisher
+{
+public:
+  VelocityPublisher(rclcpp::Node::SharedPtr & node,
+    const std::string topic = std::string("/cmd_vel"));
+
+  void publishCommand(const geometry_msgs::msg::Twist& cmd_vel);
+
+private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
+};
+
+class RobotStateHelper
+{
+public:
+  RobotStateHelper(rclcpp::Node::SharedPtr & node,
+    const std::string odom_topic = std::string("/odom"));
+
+  bool getOdometry();
+
+  bool getCurrentPose(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose,
+    const bool use_topic = false);
+
+private:
+
+  void onPoseReceived(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose);
+  void onOdomReceived();
+
+  bool getGlobalLocalizerPose(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose);
+  bool getTfPose(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose);
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  std::shared_mutex state_mutex_;
+  bool initial_pose_received_, initial_odom_received_;
+  geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr current_pose_;
+  nav_msgs::msg::Odometry::SharedPtr current_odom_;
+}
+
+} // end namespace nav2_util
---
+++ nav2_util/src/robot_utils.cpp
@@ -0,0 +1,108 @@
+// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2019 Steven Macenski
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_UTIL__ROBOT_UTILS_HPP_
+#define NAV2_UTIL__ROBOT_UTILS_HPP_
+
+#include <nav2_util/robot_utils.hpp>
+
+namespace nav2_util
+{
+
+VelocityPublisher::VelocityPublisher(rclcpp::Node::SharedPtr & node,
+    const std::string topic = std::string("/cmd_vel")) {
+    vel_pub_ = node->create_publisher<geometry_msgs::msg::Twist>(topic, 1);
+  }
+
+void 
+VelocityPublisher::publishCommand(const geometry_msgs::msg::Twist& cmd_vel) {
+    vel_pub_->publish(cmd_vel);
+}
+
+RobotStateHelper::RobotStateHelper(rclcpp::Node::SharedPtr & node,
+  const std::string odom_topic = std::string("/odom")) {
+  node_ = node;
+  pose_sub_ = node_->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+    "amcl_pose", std::bind(&Robot::onPoseReceived, this, std::placeholders::_1));
+
+  odom_sub_ = node_->create_subscription<nav_msgs::msg::Odometry>(
+    odom_topic, std::bind(&Robot::onOdomReceived, this, 
+    std::placeholders::_1));
+
+  initial_odom_received_ = false;
+  initial_pose_received_ = false;
+}
+
+bool
+RobotStateHelper::getOdometry() {
+  std::shared_lock lock(state_mutex_);
+  if (!initial_odom_received_) {
+    RCLCPP_DEBUG(node_->get_logger(),
+      "getOdometry: Can't return current velocity: Initial odometry not yet received.");
+    return false;
+  }
+
+  robot_odom = current_odom_;
+  return true;
+}
+
+bool
+RobotStateHelper::getCurrentPose(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose,
+  const bool use_topic = false) {
+  if (use_topic) {
+    return getGlobalLocalizerPose(robot_pose);
+  }
+
+  return getTfPose(robot_pose);
+}
+
+void
+RobotStateHelper::onPoseReceived(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose) {
+  std::scoped_lock lock(state_mutex_);
+  current_pose_ = robot_pose;
+  if (!initial_pose_received_) {
+    initial_pose_received_ = true;
+  }
+}
+
+void
+RobotStateHelper::onOdomReceived() {
+  std::scoped_lock lock(state_mutex_);
+  current_odom_ = msg;
+  if (!initial_odom_received_) {
+    initial_odom_received_ = true;
+  }
+}
+
+bool
+RobotStateHelper::getGlobalLocalizerPose(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose) {
+  std::shared_lock lock(state_mutex_);
+  if (!initial_pose_received_) {
+    RCLCPP_DEBUG(node_->get_logger(),
+      "Robot: Can't return current pose: Initial pose not yet received.");
+    return false;
+  }
+
+  robot_pose = current_pose_;
+  return true;
+}
+
+bool
+RobotStateHelper::getTfPose(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr & robot_pose) {
+  RCLCPP_DEBUG(node_->get_logger(), "getTfPose is not yet implemented.");
+  return false;
+}
+
+} // end namespace nav2_util
