0be555b059005a0c50c2c06745095f5e92a66fcf
Michael Jeronimo <michael.jeronimo@intel.com>
2019-05-14 09:13:21 -0700

    Update the launch scripts for lifecycle nodes (#667)
    * Update the launch scripts for lifecycle nodes
    
    * Use the turtlebot3 map by default

--- nav2_bringup/launch/nav2_bringup_launch.py
+++ nav2_bringup/launch/nav2_bringup_launch.py
@@ -1,70 +1,160 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-from launch import LaunchDescription
-import launch.actions
-import launch_ros.actions
+
 from ament_index_python.packages import get_package_prefix
+from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition
+
+import launch.actions
+
 
 def generate_launch_description():
-    map_yaml_file = launch.substitutions.LaunchConfiguration('map')
-    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
-    params_file = launch.substitutions.LaunchConfiguration('params', default=
-        [launch.substitutions.ThisLaunchFileDir(), '/nav2_params.yaml'])
+    use_simulation = launch.substitutions.LaunchConfiguration('use_simulation')
+    simulator = launch.substitutions.LaunchConfiguration('simulator')
+    world = launch.substitutions.LaunchConfiguration('world')
+    params_file = launch.substitutions.LaunchConfiguration(
+        'params', default=[launch.substitutions.ThisLaunchFileDir(), '/nav2_params.yaml'])
 
-    bt_navigator_install_path = get_package_prefix('nav2_bt_navigator')
-    bt_navigator_xml = os.path.join(bt_navigator_install_path,
-                                        'behavior_trees',
-                                        'navigate_w_recovery_retry.xml') # TODO(mkhansen): change to an input parameter
+    declare_use_simulation_cmd = launch.actions.DeclareLaunchArgument(
+        'use_simulation', condition=IfCondition('True'),
+        default_value='True', description='Whether to run in simulation')
 
-    return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'map', description='Full path to map file to load'),
-        launch.actions.DeclareLaunchArgument(
-            'use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
+    declare_simulator_cmd = launch.actions.DeclareLaunchArgument(
+        'simulator',
+        default_value='gzserver', description='The simulator to use (gazebo or gzserver)')
 
-        launch_ros.actions.Node(
-            package='nav2_map_server',
-            node_executable='map_server',
-            node_name='map_server',
-            output='screen',
-            parameters=[{ 'use_sim_time': use_sim_time}, { 'yaml_filename': map_yaml_file }]),
+    declare_world_cmd = launch.actions.DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(
+            get_package_share_directory('turtlebot3_gazebo'),
+            'worlds/turtlebot3_worlds/burger.model'),
+        description='Full path to world file to load')
 
-        launch_ros.actions.Node(
-            package='nav2_world_model',
-            node_executable='world_model',
-            output='screen',
-            parameters=[params_file]),
+    declare_params_file_cmd = launch.actions.DeclareLaunchArgument(
+        'params_file',
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
 
-        launch_ros.actions.Node(
-            package='nav2_amcl',
-            node_executable='amcl',
-            node_name='amcl',
-            output='screen',
-            parameters=[{ 'use_sim_time': use_sim_time}]),
+    launch_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    gz = launch.substitutions.LaunchConfiguration('gz', default=['gzserver'])
 
-        launch_ros.actions.Node(
-            package='dwb_controller',
-            node_executable='dwb_controller',
-            output='screen',
-            parameters=[params_file]),
+    # Specify the actions
 
-        launch_ros.actions.Node(
-            package='nav2_navfn_planner',
-            node_executable='navfn_planner',
-            node_name='navfn_planner',
-            output='screen',
-            parameters=[{ 'use_sim_time': use_sim_time}]),
+    start_gazebo_cmd = launch.actions.ExecuteProcess(
+        condition=IfCondition(use_simulation),
+        cmd=[simulator, '-s', 'libgazebo_ros_init.so', world, ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
 
-        launch_ros.actions.Node(
-            package='nav2_motion_primitives',
-            node_executable='motion_primitives_node',
-            node_name='motion_primitives',
-            output='screen',
-            parameters=[{ 'use_sim_time': use_sim_time}]),
-        
-        launch_ros.actions.Node(
-            package='nav2_bt_navigator',
-            node_executable='bt_navigator',
-            node_name='bt_navigator',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}, {'bt_xml_filename': bt_navigator_xml}])
-    ])
+    start_robot_state_publisher_cmd = launch.actions.ExecuteProcess(
+        condition=IfCondition(use_simulation),
+        cmd=[
+            os.path.join(
+                get_package_prefix('robot_state_publisher'),
+                'lib/robot_state_publisher/robot_state_publisher'),
+            os.path.join(
+                get_package_share_directory('turtlebot3_description'),
+                'urdf', 'turtlebot3_burger.urdf'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_rviz_cmd = launch.actions.ExecuteProcess(
+        cmd=[os.path.join(get_package_prefix('rviz2'), 'lib/rviz2/rviz2'), ],
+        cwd=[launch_dir], output='screen')
+
+    exit_event_handler = launch.actions.RegisterEventHandler(
+        event_handler=launch.event_handlers.OnProcessExit(
+            target_action=start_rviz_cmd,
+            on_exit=launch.actions.EmitEvent(event=launch.events.Shutdown(reason='rviz exited'))))
+
+    start_map_server_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('nav2_map_server'),
+                'lib/nav2_map_server/map_server'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_localizer_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('nav2_amcl'),
+                'lib/nav2_amcl/amcl'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_world_model_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('nav2_world_model'),
+                'lib/nav2_world_model/world_model'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_dwb_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('dwb_controller'),
+                'lib/dwb_controller/dwb_controller'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_planner_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('nav2_navfn_planner'),
+                'lib/nav2_navfn_planner/navfn_planner'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_navigator_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('nav2_bt_navigator'),
+                'lib/nav2_bt_navigator/bt_navigator'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    start_controller_cmd = launch.actions.ExecuteProcess(
+        cmd=[
+            os.path.join(
+                get_package_prefix('nav2_controller'),
+                'lib/nav2_controller/nav2_controller'),
+            ['__params:=', params_file]],
+        cwd=[launch_dir], output='screen')
+
+    # Create the launch description and populate
+    ld = launch.LaunchDescription()
+
+    # First, declare the launch options
+    ld.add_action(declare_use_simulation_cmd)
+    ld.add_action(declare_simulator_cmd)
+    ld.add_action(declare_world_cmd)
+
+    # Then, add the actions to launch the simulator-related nodes (conditioned on 'use_simulation')
+    ld.add_action(start_gazebo_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_rviz_cmd)
+    ld.add_action(exit_event_handler)
+
+    # Next, add the actions to launch all of the navigation nodes
+    ld.add_action(start_controller_cmd)
+    ld.add_action(start_map_server_cmd)
+    ld.add_action(start_localizer_cmd)
+    ld.add_action(start_world_model_cmd)
+    ld.add_action(start_dwb_cmd)
+    ld.add_action(start_planner_cmd)
+    ld.add_action(start_navigator_cmd)
+
+    return ld
