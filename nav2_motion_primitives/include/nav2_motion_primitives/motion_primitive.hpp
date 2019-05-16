379bdfd8abc5c2b69c6653fe291e856e317ee2f2
Carlos A. Orduno <39749557+orduno@users.noreply.github.com>
2019-05-16 15:16:17 -0700

    Fixed bug (#733)

--- nav2_motion_primitives/include/nav2_motion_primitives/motion_primitive.hpp
+++ nav2_motion_primitives/include/nav2_motion_primitives/motion_primitive.hpp
@@ -144,8 +144,6 @@ protected:
           RCLCPP_WARN(node_->get_logger(), "%s failed", primitive_name_.c_str());
           goal_handle->abort(result);
           return;
-
-        default:
       }
 
       loop_rate.sleep();
