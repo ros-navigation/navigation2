3d123cbedf137fe0dbccd2ac64d9d9e768563f8a
Michael Jeronimo <michael.jeronimo@intel.com>
2019-05-30 13:09:06 -0700

    Per review feedback, use RCLCPP_FATAL upon a lifecycle node entering an error state. (#748)
    In the future, there can be some error recovery here.

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -144,7 +144,7 @@ BtNavigator::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 nav2_lifecycle::CallbackReturn
 BtNavigator::on_error(const rclcpp_lifecycle::State & /*state*/)
 {
-  RCLCPP_ERROR(get_logger(), "Handling error state");
+  RCLCPP_FATAL(get_logger(), "Lifecycle node entered error state");
   return nav2_lifecycle::CallbackReturn::SUCCESS;
 }
 
