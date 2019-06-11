4ccbdf75ee6c3f1a984f6e56e106ae842a635c86
Michael Jeronimo <michael.jeronimo@intel.com>
2019-06-11 09:50:20 -0700

    Run lint tools on lifecycle_manager and nav2_tasks
    Also checked bt_navigator and nav2_robot, which are clean.

--- nav2_tasks/include/nav2_tasks/bt_action_node.hpp
+++ nav2_tasks/include/nav2_tasks/bt_action_node.hpp
@@ -174,7 +174,8 @@ new_goal_received:
       {
         std::lock_guard<std::mutex> guard(spin_mutex_);
         if (rclcpp::spin_until_future_complete(node_, future_cancel) !=
-          rclcpp::executor::FutureReturnCode::SUCCESS) {
+          rclcpp::executor::FutureReturnCode::SUCCESS)
+        {
           RCLCPP_ERROR(node_->get_logger(),
             "Failed to cancel action server for %s", action_name_.c_str());
         }
