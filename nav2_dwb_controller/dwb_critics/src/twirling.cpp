d719479d994a04150eb4c3568ee7d6f2fbb8c737
Scott K Logan <logans@cottsay.net>
2019-07-02 14:09:30 -0700

    Fixes for parameter declarations (#879)
    Without these changes, I've been seeing lots of 'foo has already been
    declared' errors.

--- nav2_dwb_controller/dwb_critics/src/twirling.cpp
+++ nav2_dwb_controller/dwb_critics/src/twirling.cpp
@@ -39,7 +39,9 @@ namespace dwb_critics
 {
 void TwirlingCritic::onInit()
 {
-  nh_->declare_parameter(name_ + ".scale", rclcpp::ParameterValue(0.0));
+  if (!nh_->has_parameter(name_ + ".scale")) {
+    nh_->declare_parameter(name_ + ".scale", rclcpp::ParameterValue(0.0));
+  }
 
   // Scale is set to 0 by default, so if it was not set otherwise, set to 0
   nh_->get_parameter(name_ + ".scale", scale_);
