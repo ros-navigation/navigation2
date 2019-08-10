1c6e4a85d1743c7551304c2367b0e7992e97c300
ruffsl <roxfoxpox@gmail.com>
2019-08-10 16:18:30 -0700

    Simplify code coverage script to prevent untented shell glob expansion

--- tools/code_coverage_report.bash
+++ tools/code_coverage_report.bash
@@ -15,6 +15,8 @@ set -e
 LCOVDIR=lcov
 PWD=`pwd`
 
+COVERAGE_REPORT_VIEW="none"
+
 for opt in "$@" ; do
   case "$opt" in
     clean)
@@ -22,60 +24,58 @@ for opt in "$@" ; do
       exit 0
       ;;
     codecovio)
-      COVERAGE_REPORT=codecovio
+      COVERAGE_REPORT_VIEW=codecovio
       ;;
     genhtml)
-      COVERAGE_REPORT=genhtml
+      COVERAGE_REPORT_VIEW=genhtml
       ;;
   esac
 done
 
+set -o xtrace
 mkdir -p $LCOVDIR
-
-# Re-enable branch coverage.
-# This is disabled by default in the latest version of LCOV
-lcov () {
-  set -o xtrace
-  command lcov $@ --rc lcov_branch_coverage=1
-  set +o xtrace
-}
 
 # Generate initial zero-coverage data.
 # This adds files that were otherwise not run to the report
 lcov --capture --initial \
   --directory build \
-  --output-file ${LCOVDIR}/initial_coverage.info
+  --output-file ${LCOVDIR}/initial_coverage.info \
+  --rc lcov_branch_coverage=1
 
 # Capture executed code data.
 lcov --capture \
   --directory build \
-  --output-file ${LCOVDIR}/test_coverage.info
+  --output-file ${LCOVDIR}/test_coverage.info \
+  --rc lcov_branch_coverage=1
 
 # Combine the initial zero-coverage report with the executed lines report.
 lcov \
   --add-tracefile ${LCOVDIR}/initial_coverage.info \
   --add-tracefile ${LCOVDIR}/test_coverage.info \
-  --output-file ${LCOVDIR}/full_coverage.info
+  --output-file ${LCOVDIR}/full_coverage.info \
+  --rc lcov_branch_coverage=1
 
 # Only include files that are within this workspace.
 # (eg filter out stdio.h etc)
 lcov \
   --extract ${LCOVDIR}/full_coverage.info \
     "${PWD}/*" \
-  --output-file ${LCOVDIR}/workspace_coverage.info
+  --output-file ${LCOVDIR}/workspace_coverage.info \
+  --rc lcov_branch_coverage=1
 
 # Remove files in the build subdirectory.
 # Those are generated files (like messages, services, etc)
 lcov \
   --remove ${LCOVDIR}/workspace_coverage.info \
     "${PWD}/build/*" \
-  --output-file ${LCOVDIR}/project_coverage.info
+  --output-file ${LCOVDIR}/project_coverage.info \
+  --rc lcov_branch_coverage=1
 
-if [ $COVERAGE_REPORT = codecovio ]; then
+if [ $COVERAGE_REPORT_VIEW = codecovio ]; then
   bash <(curl -s https://codecov.io/bash) \
     -f ${LCOVDIR}/project_coverage.info \
     -R src/navigation2
-elif [ $COVERAGE_REPORT = genhtml ]; then
+elif [ $COVERAGE_REPORT_VIEW = genhtml ]; then
   genhtml ${LCOVDIR}/project_coverage.info \
     --output-directory ${LCOVDIR}/html \
     --branch-coverage -p ${PWD}
