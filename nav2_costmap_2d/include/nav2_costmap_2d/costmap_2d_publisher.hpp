d7f440db8bbf2c233b7d3c732c6002119ff8a281
stevemacenski <stevenmacenski@gmail.com>
2019-08-28 15:10:57 -0700

    updating copyright headers

--- nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_publisher.hpp
+++ nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_publisher.hpp
@@ -3,6 +3,7 @@
  * Software License Agreement (BSD License)
  *
  *  Copyright (c) 2008, 2013, Willow Garage, Inc.
+ *  Copyright (c) 2008, 2019, Samsung Research America, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
--- nav2_navfn_planner/include/nav2_navfn_planner/navfn_planner.hpp
+++ nav2_navfn_planner/include/nav2_navfn_planner/navfn_planner.hpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2018 Intel Corporation
 // Copyright (c) 2018 Simbe Robotics
+// Copyright (c) 2019 Samsung Research America
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
