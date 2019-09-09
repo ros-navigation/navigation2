a64870012a34eaeecd0b08db2fd9f5042acd3bd9
Carlos Orduno <carlos.a.orduno@intel.com>
2019-09-09 11:41:49 -0700

    Fixed build bugs

--- nav2_bringup/spawn_turtlebot3/setup.py
+++ nav2_bringup/spawn_turtlebot3/setup.py
@@ -7,6 +7,11 @@ setup(
     version='1.0.0',
     package_dir={'': 'src'},
     packages=[PACKAGE_NAME],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + PACKAGE_NAME]),
+        ('share/' + PACKAGE_NAME, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     tests_require=['pytest'],
