e49ab57307f56e441b13b872976982631fce6a1e
bpwilcox <bpwilcox@eng.ucsd.edu>
2019-09-10 20:58:02 -0700

    add pause, resume, and repeat button transitions

--- nav2_rviz_plugins/include/nav2_rviz_plugins/nav2_panel.hpp
+++ nav2_rviz_plugins/include/nav2_rviz_plugins/nav2_panel.hpp
@@ -83,7 +83,7 @@ private:
   // The client used to control the nav2 stack
   nav2_lifecycle_manager::LifecycleManagerClient client_;
 
-  QPushButton * start_stop_button_{nullptr};
+  QPushButton * start_reset_button_{nullptr};
   QPushButton * pause_resume_button_{nullptr};
 
   QStateMachine state_machine_;
@@ -91,17 +91,15 @@ private:
   QState * pre_initial_{nullptr};
   QState * initial_{nullptr};
   QState * idle_{nullptr};
-  QState * stopping_{nullptr};
-  // The following states are added to allow for the state of the button to only expose shutdown
+  QState * reset_{nullptr};
+  QState * paused_{nullptr};
+  QState * resumed_{nullptr};
+  // The following states are added to allow for the state of the button to only expose reset
   // while the NavigateToPose action is not active. While running, the user will be allowed to
   // cancel the action. The ROSActionTransition allows for the state of the action to be detected
-  // and the button state to change automatically. The additional states canceled_ and
-  // completed_ support the transitions from running into states that can transition to shutdown
-  // but do nothing upon entrance.
+  // and the button state to change automatically.
   QState * running_{nullptr};
   QState * canceled_{nullptr};
-  QState * pausing_{nullptr};
-  QState * resuming_{nullptr};
 };
 
 class InitialThread : public QThread
