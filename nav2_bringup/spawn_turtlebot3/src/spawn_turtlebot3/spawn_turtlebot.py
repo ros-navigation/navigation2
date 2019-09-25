9cb8638cc8dc9df398aa827249d3cd129a1cecd3
Carlos Orduno <carlos.a.orduno@intel.com>
2019-09-25 14:37:00 -0700

    Adding argument parsing

--- nav2_bringup/spawn_turtlebot3/src/spawn_turtlebot3/spawn_turtlebot.py
+++ nav2_bringup/spawn_turtlebot3/src/spawn_turtlebot3/spawn_turtlebot.py
@@ -6,15 +6,29 @@ Script used to spawn a turtlebot in a generic position
 import os
 import sys
 import rclpy
+import argparse
 from ament_index_python.packages import get_package_share_directory
 from gazebo_msgs.srv import SpawnEntity
 import xml.etree.ElementTree as ET
 
 
 def main():
-    """ Main for spwaning turtlebot node """
+    """ Main for spawning turtlebot node """
     # Get input arguments from user
-    argv = sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Spawn Turtlebot3 into Gazebo')
+    parser.add_argument('-n', '--robot_name', type=str, default='robot',
+                        help='Name of the robot to spawn')
+    parser.add_argument('-ns', '--robot_namespace', type=str, default='robot',
+                        help='ROS namespace to apply to the tf and plugins')
+    parser.add_argument('-t', '--turtlebot_type', type=str, default='waffle',
+                        choices=['waffle', 'burger'])
+    parser.add_argument('-x', type=float, default=0,
+                        help='the x component of the initial position [meters]')
+    parser.add_argument('-y', type=float, default=0,
+                        help='the y component of the initial position [meters]')
+    parser.add_argument('-z', type=float, default=0,
+                        help='the z component of the initial position [meters]')
+    args = parser.parse_args()
 
     # Start node
     rclpy.init()
@@ -29,10 +43,13 @@ def main():
         client.wait_for_service()
         node.get_logger().info("...connected!")
 
+    node.get_logger().info('spawning a `{}` with name `{}` on namespace `{}` at {}, {}, {}'.format(
+        args.turtlebot_type, args.robot_name, args.robot_namespace, args.x, args.y, args.z))
+
     # Get path to the turtlebot3 burgerbot
     sdf_file_path = os.path.join(
         get_package_share_directory("turtlebot3_gazebo"), "models",
-        "turtlebot3_burger", "model.sdf")
+        "turtlebot3_{}".format(args.turtlebot_type), "model.sdf")
 
     # We need to remap the transform (/tf) topic so each robot has its own.
     # We do this by adding `ROS argument entries` to the sdf file for
@@ -48,16 +65,16 @@ def main():
 
     ros_params = plugin.find('ros')
     ros_tf_remap = ET.SubElement(ros_params, 'argument')
-    ros_tf_remap.text = '/tf:=/' + argv[1] + '/tf'
+    ros_tf_remap.text = '/tf:=/' + args.robot_namespace + '/tf'
 
     # Set data for request
     request = SpawnEntity.Request()
-    request.name = argv[0]
+    request.name = args.robot_name
     request.xml = ET.tostring(root, encoding="unicode")
-    request.robot_namespace = argv[1]
-    request.initial_pose.position.x = float(argv[2])
-    request.initial_pose.position.y = float(argv[3])
-    request.initial_pose.position.z = float(argv[4])
+    request.robot_namespace = args.robot_namespace
+    request.initial_pose.position.x = float(args.x)
+    request.initial_pose.position.y = float(args.y)
+    request.initial_pose.position.z = float(args.z)
 
     node.get_logger().info("Sending service request to `/spawn_entity`")
     future = client.call_async(request)
