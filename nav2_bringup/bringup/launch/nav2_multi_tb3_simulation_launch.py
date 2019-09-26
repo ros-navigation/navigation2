80f4b75224e6ee013eea3992686b5734b411e543
Carlos Orduno <carlos.a.orduno@intel.com>
2019-09-26 10:39:13 -0700

    Extending `RewrittenYaml` to apply a root-level namespace

--- nav2_bringup/bringup/launch/nav2_multi_tb3_simulation_launch.py
+++ nav2_bringup/bringup/launch/nav2_multi_tb3_simulation_launch.py
@@ -67,7 +67,7 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(bringup_dir, 'params', 'nav2_params_namespaced.yaml'),
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_bt_xml_cmd = DeclareLaunchArgument(
