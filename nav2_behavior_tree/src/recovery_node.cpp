7544f1ad3528d51b1edc63bebed8da5980d8cf16
Mohammad Haghighipanah <39755151+mhpanah@users.noreply.github.com>
2019-10-03 11:27:50 -0700

    Halt method in the recovery node is not being called after successfull navigation (#1189)
    * fixed issue 1179

--- nav2_behavior_tree/src/recovery_node.cpp
+++ nav2_behavior_tree/src/recovery_node.cpp
@@ -49,6 +49,7 @@ BT::NodeStatus RecoveryNode::tick()
         case BT::NodeStatus::SUCCESS:
           {
             retry_count_ = 0;
+            halt();
             return BT::NodeStatus::SUCCESS;
           }
           break;
@@ -91,6 +92,7 @@ BT::NodeStatus RecoveryNode::tick()
           {
             current_child_idx_--;
             retry_count_ = 0;
+            halt();
             return BT::NodeStatus::FAILURE;
           }
           break;
@@ -108,6 +110,7 @@ BT::NodeStatus RecoveryNode::tick()
     }
   }  // end while loop
   retry_count_ = 0;
+  halt();
   return BT::NodeStatus::FAILURE;
 }
 
