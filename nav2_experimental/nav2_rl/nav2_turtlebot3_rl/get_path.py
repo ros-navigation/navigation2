d891b619a2693fdb6c03fc004f8368852097d81b
Yathartha Tuladhar <yathartha3@gmail.com>
2019-10-03 18:32:15 -0700

    Use ComputePathToPose in get_path.py

--- nav2_experimental/nav2_rl/nav2_turtlebot3_rl/get_path.py
+++ nav2_experimental/nav2_rl/nav2_turtlebot3_rl/get_path.py
@@ -17,12 +17,13 @@ import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 from nav2_msgs.action import NavigateToPose
-from nav2_msgs.action import NavigateToPose
+from nav2_msgs.action import ComputePathToPose
 import nav2_msgs
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.executors import MultiThreadedExecutor
+from action_msgs.msg import GoalStatus
 
 
 class RLPathClient(Node):
@@ -38,27 +39,48 @@ class RLPathClient(Node):
     '''
     def __init__(self):
         super().__init__('rl_path_client')
-        self.action_client_ = ActionClient(self, NavigateToPose, 'NavigateToPose')
-        self.sub_path = self.create_subscription(Path, 'plan', self.path_callback,
-                                                       qos_profile_sensor_data)
-        self.path = None
-
-    def path_callback(self, PathMsg):
-        self.path = PathMsg
-        self.path_updated_time = self._clock.now()
-        print("Path length = "+str(len(PathMsg.poses)) + "\n")
-        
-        for n in range(len(PathMsg.poses)):
-            print("x, y = " + str(PathMsg.poses[n].pose.position.x) + str(PathMsg.poses[n].pose.position.y))
-
-        # print(PathMsg.poses)
+        #self.action_client_ = ActionClient(self, NavigateToPose, 'NavigateToPose')
+        self.action_client_ = ActionClient(self, ComputePathToPose, 'ComputePathToPose')
+        self.result_path = None
         
     def send_goal(self, goal_pose):
-        goal_msg = nav2_msgs.action.NavigateToPose.Goal()
+        goal_msg = nav2_msgs.action.ComputePathToPose.Goal()
         goal_msg.pose = goal_pose
 
         self.action_client_.wait_for_server()
-        self.action_client_.send_goal_async(goal_msg)
+        print("Server ready : "+ str(self.action_client_.server_is_ready()) + str('. Goal Sent!'))
+
+        self._send_goal_future = self.action_client_.send_goal_async(
+            goal_msg,
+            feedback_callback=self.feedback_callback)
+        
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            print('Goal rejected :(')
+            return
+
+        print('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        status = future.result().status
+
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            print('Goal succeeded! Received path of length ' + str(len(result.path.poses)) + str('.'))
+        else:
+            print("Failed to get path :S")
+
+        self.result_path = result.path.poses
+
+    def feedback_callback(self, feedback_msg):
+        print("Feedback not implemented?")
+
 
 def test(args=None):
     rclpy.init(args=args)
@@ -68,10 +90,14 @@ def test(args=None):
     test_goal.pose.position.y = 2.0
     test_goal.pose.position.z = 0.0
     
-    action_client = RLPathClient()
-    action_client.send_goal(test_goal)
+    RLaction_client = RLPathClient()
+    RLaction_client.send_goal(test_goal)
+    
+    if RLaction_client.result_path!=None:
+        print(RLaction_client.result_path)
+
     executor = MultiThreadedExecutor()
-    rclpy.spin(action_client, executor=executor)
+    rclpy.spin(RLaction_client, executor=executor)
     #rclpy.shutdown()
 
 
