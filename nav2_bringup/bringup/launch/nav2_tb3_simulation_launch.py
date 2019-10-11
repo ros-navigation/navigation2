e09cabde2bbf1a8d3a87352e11a201700ebba089
Carl Delsey <carl.r.delsey@intel.com>
2019-10-11 16:47:30 -0700

    Install Behavior Trees in correct location (#1231)
    * Install behavior trees in correct location
    
    * Fix up bt paths in launch files.

--- nav2_bringup/bringup/launch/nav2_tb3_simulation_launch.py
+++ nav2_bringup/bringup/launch/nav2_tb3_simulation_launch.py
@@ -84,7 +84,7 @@ def generate_launch_description():
     declare_bt_xml_cmd = DeclareLaunchArgument(
         'bt_xml_file',
         default_value=os.path.join(
-            get_package_prefix('nav2_bt_navigator'),
+            get_package_share_directory('nav2_bt_navigator'),
             'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
         description='Full path to the behavior tree xml file to use')
 
