83155d083175feae244c33a85502b609c0352f15
stevemacenski <stevenmacenski@gmail.com>
2019-10-25 10:55:38 -0700

    noqa on lin

--- nav2_system_tests/src/system/test_multi_robot_launch.py
+++ nav2_system_tests/src/system/test_multi_robot_launch.py
@@ -40,10 +40,10 @@ def generate_launch_description():
                                os.getenv('BT_NAVIGATOR_XML'))
 
     bringup_dir = get_package_share_directory('nav2_bringup')
-    robot1_params_file = os.path.join(bringup_dir,
-        'params/nav2_multirobot_params_1.yaml')  # noqa: F841
-    robot2_params_file = os.path.join(bringup_dir,
-        'params/nav2_multirobot_params_2.yaml')  # noqa: F841
+    robot1_params_file = os.path.join(bringup_dir,  # noqa: F841
+        'params/nav2_multirobot_params_1.yaml')
+    robot2_params_file = os.path.join(bringup_dir,  # noqa: F841
+        'params/nav2_multirobot_params_2.yaml')
 
     # Names and poses of the robots
     robots = [
