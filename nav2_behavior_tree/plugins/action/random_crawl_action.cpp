6ad0115f7b369e4b48aecac709ece4c16bf7e71f
stevemacenski <stevenmacenski@gmail.com>
2019-11-14 14:16:40 -0800

    port plugin actions

---
+++ nav2_behavior_tree/plugins/action/random_crawl_action.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_BEHAVIOR_TREE__RANDOM_CRAWL_ACTION_HPP_
+#define NAV2_BEHAVIOR_TREE__RANDOM_CRAWL_ACTION_HPP_
+
+#include <string>
+
+#include "nav2_behavior_tree/bt_action_node.hpp"
+#include "nav2_msgs/action/random_crawl.hpp"
+
+namespace nav2_behavior_tree
+{
+
+class RandomCrawlAction : public BtActionNode<nav2_msgs::action::RandomCrawl>
+{
+public:
+  explicit RandomCrawlAction(const std::string & action_name)
+  : BtActionNode<nav2_msgs::action::RandomCrawl>(action_name)
+  {
+  }
+};
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<nav2_behavior_tree::RandomCrawlAction>("RandomCrawl");
+}
+
+#endif  // NAV2_BEHAVIOR_TREE__RANDOM_CRAWL_ACTION_HPP_
