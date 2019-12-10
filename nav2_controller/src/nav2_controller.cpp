ros-navigation/navigation2

commit 25981de1b5ce259c6497a5d983aa41b3a639f461

Improvements to SimpleActionServer (#1356)
* Handle action server deactivation while the action is still running.
* Handle race condition between completing an action and receiving a preemption.
* Terminate all goals if cancel was requested on any goal handle.
* Terminate only the current goal if the module fails to execute the action.

nav2_controller/src/nav2_controller.cpp
@@ -222,7 +222,7 @@ void ControllerServer::computeControl()
 
       if (action_server_->is_cancel_requested()) {
         RCLCPP_INFO(get_logger(), "Goal was canceled. Stopping the robot.");
-        action_server_->terminate_goals();
+        action_server_->terminate_all();
         publishZeroVelocity();
         return;
       }
@@ -244,7 +244,7 @@ void ControllerServer::computeControl()
   } catch (nav2_core::PlannerException & e) {
     RCLCPP_ERROR(this->get_logger(), e.what());
     publishZeroVelocity();
-    action_server_->terminate_goals();
+    action_server_->terminate_current();
     return;
   }
 

nav2_bt_navigator/src/bt_navigator.cpp
@@ -226,12 +226,12 @@ BtNavigator::navigateToPose()
 
     case nav2_behavior_tree::BtStatus::FAILED:
       RCLCPP_ERROR(get_logger(), "Navigation failed");
-      action_server_->terminate_goals();
+      action_server_->terminate_current();
       break;
 
     case nav2_behavior_tree::BtStatus::CANCELED:
       RCLCPP_INFO(get_logger(), "Navigation canceled");
-      action_server_->terminate_goals();
+      action_server_->terminate_all();
       // Reset the BT so that it can be run again in the future
       bt_->resetTree(tree_->root_node);
       break;

nav2_planner/src/planner_server.cpp
@@ -207,7 +207,7 @@ PlannerServer::computePlan()
 
     if (action_server_->is_cancel_requested()) {
       RCLCPP_INFO(get_logger(), "Goal was canceled. Canceling planning action.");
-      action_server_->terminate_goals();
+      action_server_->terminate_all();
       return;
     }
 
@@ -247,8 +247,7 @@ PlannerServer::computePlan()
       RCLCPP_WARN(get_logger(), "Planning algorithm %s failed to generate a valid"
         " path to (%.2f, %.2f)", goal->planner_id.c_str(),
         goal->pose.pose.position.x, goal->pose.pose.position.y);
-      // TODO(orduno): define behavior if a preemption is available
-      action_server_->terminate_goals();
+      action_server_->terminate_current();
       return;
     }
 
@@ -267,19 +266,17 @@ PlannerServer::computePlan()
     RCLCPP_WARN(get_logger(), "%s plugin failed to plan calculation to (%.2f, %.2f): \"%s\"",
       goal->planner_id.c_str(), goal->pose.pose.position.x,
       goal->pose.pose.position.y, ex.what());
-
     // TODO(orduno): provide information about fail error to parent task,
     //               for example: couldn't get costmap update
-    action_server_->terminate_goals();
+    action_server_->terminate_current();
     return;
   } catch (...) {
     RCLCPP_WARN(get_logger(), "Plan calculation failed, "
       "An unexpected error has occurred. The planner server"
       " may not be able to continue operating correctly.");
-
-    // TODO(orduno): provide information about the failure to the parent task,
+    // TODO(orduno): provide information about fail error to parent task,
     //               for example: couldn't get costmap update
-    action_server_->terminate_goals();
+    action_server_->terminate_current();
     return;
   }
 }

nav2_recoveries/include/nav2_recoveries/recovery.hpp
@@ -148,7 +148,7 @@ protected:
 
     if (onRun(action_server_->get_current_goal()) != Status::SUCCEEDED) {
       RCLCPP_INFO(node_->get_logger(), "Initial checks failed for %s", recovery_name_.c_str());
-      action_server_->terminate_goals();
+      action_server_->terminate_current();
       return;
     }
 
@@ -162,7 +162,7 @@ protected:
       if (action_server_->is_cancel_requested()) {
         RCLCPP_INFO(node_->get_logger(), "Canceling %s", recovery_name_.c_str());
         stopRobot();
-        action_server_->terminate_goals();
+        action_server_->terminate_all();
         return;
       }
 
@@ -172,7 +172,7 @@ protected:
           " however feature is currently not implemented. Aborting and stopping.",
           recovery_name_.c_str());
         stopRobot();
-        action_server_->terminate_goals();
+        action_server_->terminate_current();
         return;
       }
 
@@ -184,7 +184,7 @@ protected:
 
         case Status::FAILED:
           RCLCPP_WARN(node_->get_logger(), "%s failed", recovery_name_.c_str());
-          action_server_->terminate_goals();
+          action_server_->terminate_current();
           return;
 
         case Status::RUNNING:

nav2_util/include/nav2_util/simple_action_server.hpp
@@ -18,6 +18,9 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <thread>
+#include <future>
+#include <chrono>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
@@ -35,13 +38,14 @@ public:
     typename nodeT::SharedPtr node,
     const std::string & action_name,
     ExecuteCallback execute_callback,
-    bool autostart = true)
+    bool autostart = true,
+    std::chrono::milliseconds server_timeout = std::chrono::milliseconds(500))
   : SimpleActionServer(
       node->get_node_base_interface(),
       node->get_node_clock_interface(),
       node->get_node_logging_interface(),
       node->get_node_waitables_interface(),
-      action_name, execute_callback, autostart)
+      action_name, execute_callback, autostart, server_timeout)
   {}
 
   explicit SimpleActionServer(
@@ -51,76 +55,21 @@ public:
     rclcpp::node_interfaces::NodeWaitablesInterface::SharedPtr node_waitables_interface,
     const std::string & action_name,
     ExecuteCallback execute_callback,
-    bool autostart = true)
+    bool autostart = true,
+    std::chrono::milliseconds server_timeout = std::chrono::milliseconds(500))
   : node_base_interface_(node_base_interface),
     node_clock_interface_(node_clock_interface),
     node_logging_interface_(node_logging_interface),
     node_waitables_interface_(node_waitables_interface),
-    action_name_(action_name), execute_callback_(execute_callback)
+    action_name_(action_name),
+    execute_callback_(execute_callback),
+    server_timeout_(server_timeout)
   {
     if (autostart) {
       server_active_ = true;
     }
 
-    auto handle_goal =
-      [this](const rclcpp_action::GoalUUID &, std::shared_ptr<const typename ActionT::Goal>)
-      {
-        std::lock_guard<std::recursive_mutex> lock(update_mutex_);
-
-        if (!server_active_) {
-          return rclcpp_action::GoalResponse::REJECT;
-        }
-
-        debug_msg("Received request for goal acceptance");
-        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
-      };
-
-    auto handle_cancel =
-      [this](std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>>)
-      {
-        std::lock_guard<std::recursive_mutex> lock(update_mutex_);
-        // TODO(orduno) could goal handle be aborted (and on a terminal state) before reaching here?
-        debug_msg("Received request for goal cancellation");
-        return rclcpp_action::CancelResponse::ACCEPT;
-      };
-
-    auto handle_accepted =
-      [this](std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>> handle)
-      {
-        std::lock_guard<std::recursive_mutex> lock(update_mutex_);
-        debug_msg("Receiving a new goal");
-
-        if (is_active(current_handle_)) {
-          debug_msg("An older goal is active, moving the new goal to a pending slot.");
-
-          if (is_active(pending_handle_)) {
-            debug_msg("The pending slot is occupied."
-              " The previous pending goal will be aborted and replaced.");
-
-            pending_handle_->abort(empty_result());
-            pending_handle_.reset();
-            preempt_requested_ = false;
-          }
-
-          debug_msg("Setting flag so the action server can grab the preempt request.");
-          preempt_requested_ = true;
-          pending_handle_ = handle;
-        } else {
-          if (is_active(pending_handle_)) {
-            // Shouldn't reach a state with a pending goal but no current one.
-            error_msg("Forgot to handle a preemption. Aborting the pending goal.");
-
-            pending_handle_->abort(empty_result());
-            pending_handle_.reset();
-            preempt_requested_ = false;
-          }
-
-          debug_msg("Starting a thread to process the goals");
-
-          current_handle_ = handle;
-          std::thread{execute_callback_}.detach();
-        }
-      };
+    using namespace std::placeholders;  // NOLINT
 
     action_server_ = rclcpp_action::create_server<ActionT>(
       node_base_interface_,
@@ -128,31 +77,140 @@ public:
       node_logging_interface_,
       node_waitables_interface_,
       action_name_,
-      handle_goal,
-      handle_cancel,
-      handle_accepted);
+      std::bind(&SimpleActionServer::handle_goal, this, _1, _2),
+      std::bind(&SimpleActionServer::handle_cancel, this, _1),
+      std::bind(&SimpleActionServer::handle_accepted, this, _1));
+  }
+
+  rclcpp_action::GoalResponse handle_goal(
+    const rclcpp_action::GoalUUID & /*uuid*/,
+    std::shared_ptr<const typename ActionT::Goal>/*goal*/)
+  {
+    std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+
+    if (!server_active_) {
+      return rclcpp_action::GoalResponse::REJECT;
+    }
+
+    debug_msg("Received request for goal acceptance");
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>>/*handle*/)
+  {
+    std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+    debug_msg("Received request for goal cancellation");
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>> handle)
+  {
+    std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+    debug_msg("Receiving a new goal");
+
+    if (is_active(current_handle_)) {
+      debug_msg("An older goal is active, moving the new goal to a pending slot.");
+
+      if (is_active(pending_handle_)) {
+        debug_msg("The pending slot is occupied."
+          " The previous pending goal will be terminated and replaced.");
+        terminate(pending_handle_);
+      }
+      pending_handle_ = handle;
+      preempt_requested_ = true;
+    } else {
+      if (is_active(pending_handle_)) {
+        // Shouldn't reach a state with a pending goal but no current one.
+        error_msg("Forgot to handle a preemption. Terminating the pending goal.");
+        terminate(pending_handle_);
+        preempt_requested_ = false;
+      }
+
+      current_handle_ = handle;
+
+      // Return quickly to avoid blocking the executor, so spin up a new thread
+      debug_msg("Executing goal asynchronously.");
+      execution_future_ = std::async(std::launch::async, [this]() {work();});
+    }
+  }
+
+  void work()
+  {
+    while (rclcpp::ok() && !stop_execution_ && is_active(current_handle_)) {
+      debug_msg("Executing the goal...");
+      try {
+        execute_callback_();
+      } catch (std::exception & ex) {
+        RCLCPP_ERROR(node_logging_interface_->get_logger(),
+          "Action server failed while executing action callback: \"%s\"", ex.what());
+        terminate_all();
+        return;
+      }
+
+      debug_msg("Blocking processing of new goal handles.");
+      std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+
+      if (stop_execution_) {
+        warn_msg("Stopping the thread per request.");
+        terminate_all();
+        break;
+      }
+
+      if (is_active(current_handle_)) {
+        warn_msg("Current goal was not completed successfully.");
+        terminate(current_handle_);
+      }
+
+      if (is_active(pending_handle_)) {
+        debug_msg("Executing a pending handle on the existing thread.");
+        accept_pending_goal();
+      } else {
+        debug_msg("Done processing available goals.");
+        break;
+      }
+    }
+    debug_msg("Worker thread done.");
   }
 
   void activate()
   {
     std::lock_guard<std::recursive_mutex> lock(update_mutex_);
     server_active_ = true;
+    stop_execution_ = false;
   }
 
   void deactivate()
   {
-    std::lock_guard<std::recursive_mutex> lock(update_mutex_);
-    server_active_ = false;
+    debug_msg("Deactivating...");
 
-    if (is_active(current_handle_)) {
-      warn_msg("Taking action server to deactive state with an active goal.");
+    {
+      std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+      server_active_ = false;
+      stop_execution_ = true;
     }
 
-    if (is_active(pending_handle_)) {
-      warn_msg("Taking action server to deactive state with a pending preemption.");
+    if (is_running()) {
+      warn_msg("Requested to deactivate server but goal is still executing."
+        " Should check if action server is running before deactivating.");
     }
 
-    terminate_goals();
+    using namespace std::chrono;  //NOLINT
+    auto start_time = steady_clock::now();
+    while (execution_future_.wait_for(milliseconds(100)) != std::future_status::ready) {
+      info_msg("Waiting for async process to finish.");
+      if (steady_clock::now() - start_time >= server_timeout_) {
+        terminate_all();
+        throw std::runtime_error("Action callback is still running and missed deadline to stop");
+      }
+    }
+
+    debug_msg("Deactivation completed.");
+  }
+
+  bool is_running()
+  {
+    return execution_future_.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready;
   }
 
   bool is_server_active()
@@ -220,39 +278,22 @@ public:
     return current_handle_->is_canceling();
   }
 
-  void terminate_goals(
+  void terminate_all(
     typename std::shared_ptr<typename ActionT::Result> result =
     std::make_shared<typename ActionT::Result>())
   {
     std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+    terminate(current_handle_, result);
+    terminate(pending_handle_, result);
+    preempt_requested_ = false;
+  }
 
-    if (is_active(current_handle_)) {
-      if (current_handle_->is_canceling()) {
-        debug_msg("Client requested to cancel the current goal. Cancelling.");
-        current_handle_->canceled(result);
-        current_handle_.reset();
-      } else {
-        debug_msg("Aborting the current goal.");
-        current_handle_->abort(result);
-        current_handle_.reset();
-      }
-    }
-
-    if (is_active(pending_handle_)) {
-      if (pending_handle_->is_canceling()) {
-        warn_msg("Client requested to cancel the pending goal."
-          " Cancelling. Should check for pre-empt requests before terminating the goal.");
-        pending_handle_->canceled(result);
-        pending_handle_.reset();
-        preempt_requested_ = false;
-      } else {
-        warn_msg("Aborting a pending goal. "
-          " Should check for pre-empt requests before terminating the goal.");
-        pending_handle_->abort(result);
-        pending_handle_.reset();
-        preempt_requested_ = false;
-      }
-    }
+  void terminate_current(
+    typename std::shared_ptr<typename ActionT::Result> result =
+    std::make_shared<typename ActionT::Result>())
+  {
+    std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+    terminate(current_handle_, result);
   }
 
   void succeeded_current(
@@ -265,13 +306,6 @@ public:
       debug_msg("Setting succeed on current goal.");
       current_handle_->succeed(result);
       current_handle_.reset();
-    }
-
-    if (is_active(pending_handle_)) {
-      warn_msg("A preemption request was available before succeeding on the current goal.");
-      pending_handle_->abort(empty_result());
-      pending_handle_.reset();
-      preempt_requested_ = false;
     }
   }
 
@@ -293,10 +327,14 @@ protected:
   std::string action_name_;
 
   ExecuteCallback execute_callback_;
+  std::future<void> execution_future_;
+  bool stop_execution_;
 
   mutable std::recursive_mutex update_mutex_;
   bool server_active_{false};
   bool preempt_requested_{false};
+  std::chrono::milliseconds server_timeout_;
+
   std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>> current_handle_;
   std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>> pending_handle_;
 
@@ -311,6 +349,31 @@ protected:
     const std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>> handle) const
   {
     return handle != nullptr && handle->is_active();
+  }
+
+  void terminate(
+    std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>> handle,
+    typename std::shared_ptr<typename ActionT::Result> result =
+    std::make_shared<typename ActionT::Result>())
+  {
+    std::lock_guard<std::recursive_mutex> lock(update_mutex_);
+
+    if (is_active(handle)) {
+      if (handle->is_canceling()) {
+        warn_msg("Client requested to cancel the goal. Cancelling.");
+        handle->canceled(result);
+      } else {
+        warn_msg("Aborting handle.");
+        handle->abort(result);
+      }
+      handle.reset();
+    }
+  }
+
+  void info_msg(const std::string & msg) const
+  {
+    RCLCPP_INFO(node_logging_interface_->get_logger(),
+      "[%s] [ActionServer] %s", action_name_.c_str(), msg.c_str());
   }
 
   void debug_msg(const std::string & msg) const

nav2_util/test/test_actions.cpp
@@ -20,6 +20,7 @@
 #include "nav2_util/node_utils.hpp"
 #include "nav2_util/simple_action_server.hpp"
 #include "test_msgs/action/fibonacci.hpp"
+#include "std_msgs/msg/empty.hpp"
 
 using Fibonacci = test_msgs::action::Fibonacci;
 using GoalHandle = rclcpp_action::ServerGoalHandle<Fibonacci>;
@@ -45,6 +46,30 @@ public:
       shared_from_this(),
       "fibonacci",
       std::bind(&FibonacciServerNode::execute, this));
+
+    deactivate_subs_ = create_subscription<std_msgs::msg::Empty>(
+      "deactivate_server",
+      1,
+      [this](std_msgs::msg::Empty::UniquePtr /*msg*/) {
+        RCLCPP_INFO(this->get_logger(), "Deactivating");
+        action_server_->deactivate();
+      });
+
+    activate_subs_ = create_subscription<std_msgs::msg::Empty>(
+      "activate_server",
+      1,
+      [this](std_msgs::msg::Empty::UniquePtr /*msg*/) {
+        RCLCPP_INFO(this->get_logger(), "Activating");
+        action_server_->activate();
+      });
+
+    omit_preempt_subs_ = create_subscription<std_msgs::msg::Empty>(
+      "omit_preemption",
+      1,
+      [this](std_msgs::msg::Empty::UniquePtr /*msg*/) {
+        RCLCPP_INFO(this->get_logger(), "Ignoring preemptions");
+        do_premptions_ = false;
+      });
   }
 
   void on_term()
@@ -68,15 +93,15 @@ preempted:
     sequence.push_back(1);
 
     for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
-      // Check if this action has been canceled
-      if (action_server_->is_cancel_requested()) {
+      // Should be check periodically if this action has been canceled
+      // or if the server has been deactivated.
+      if (action_server_->is_cancel_requested() || !action_server_->is_server_active()) {
         result->sequence = sequence;
-        action_server_->terminate_goals(result);
         return;
       }
 
       // Check if we've gotten an new goal, pre-empting the current one
-      if (action_server_->is_preempt_requested()) {
+      if (do_premptions_ && action_server_->is_preempt_requested()) {
         action_server_->accept_pending_goal();
         goto preempted;
       }
@@ -98,6 +123,11 @@ preempted:
 
 private:
   std::unique_ptr<nav2_util::SimpleActionServer<Fibonacci>> action_server_;
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr deactivate_subs_;
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr activate_subs_;
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr omit_preempt_subs_;
+
+  bool do_premptions_{true};
 };
 
 class RclCppFixture
@@ -142,6 +172,10 @@ public:
   {
     action_client_ = rclcpp_action::create_client<Fibonacci>(shared_from_this(), "fibonacci");
     action_client_->wait_for_action_server();
+
+    deactivate_pub_ = this->create_publisher<std_msgs::msg::Empty>("deactivate_server", 1);
+    activate_pub_ = this->create_publisher<std_msgs::msg::Empty>("activate_server", 1);
+    omit_prempt_pub_ = this->create_publisher<std_msgs::msg::Empty>("omit_preemption", 1);
   }
 
   void on_term()
@@ -149,7 +183,25 @@ public:
     action_client_.reset();
   }
 
+  void deactivate_server()
+  {
+    deactivate_pub_->publish(std_msgs::msg::Empty());
+  }
+
+  void activate_server()
+  {
+    activate_pub_->publish(std_msgs::msg::Empty());
+  }
+
+  void omit_server_preemptions()
+  {
+    omit_prempt_pub_->publish(std_msgs::msg::Empty());
+  }
+
   rclcpp_action::Client<Fibonacci>::SharedPtr action_client_;
+  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr deactivate_pub_;
+  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr activate_pub_;
+  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr omit_prempt_pub_;
 };
 
 class ActionTest : public ::testing::Test
@@ -244,4 +296,156 @@ TEST_F(ActionTest, test_simple_action_with_feedback)
 
   ASSERT_EQ(sum, 143);
   ASSERT_GE(feedback_sum, 0);  // We should have received *some* feedback
+}
+
+TEST_F(ActionTest, test_simple_action_activation_cycling)
+{
+  // The goal for this invocation
+  auto goal = Fibonacci::Goal();
+
+  // Sending a goal that will take a long time to calculate
+  goal.order = 12'000'000;
+
+  // Start by sending goal on an active server
+
+  // Send the goal
+  auto future_goal_handle = node_->action_client_->async_send_goal(goal);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_,
+    future_goal_handle), rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // Deactivate while running
+  node_->deactivate_server();
+
+  auto goal_handle = future_goal_handle.get();
+
+  // Wait for the result
+  auto future_result = node_->action_client_->async_get_result(goal_handle);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_, future_result),
+    rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // The action should be reported as aborted.
+  ASSERT_EQ(future_result.get().code, rclcpp_action::ResultCode::ABORTED);
+
+  // Cycle back to active
+  node_->activate_server();
+
+  goal.order = 12;
+
+  // Send the goal
+  future_goal_handle = node_->action_client_->async_send_goal(goal);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_,
+    future_goal_handle), rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  goal_handle = future_goal_handle.get();
+
+  // Wait for the result
+  future_result = node_->action_client_->async_get_result(goal_handle);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_, future_result),
+    rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // Now the action should have been successfully executed.
+  ASSERT_EQ(future_result.get().code, rclcpp_action::ResultCode::SUCCEEDED);
+}
+
+TEST_F(ActionTest, test_simple_action_preemption)
+{
+  // The goal for this invocation
+  auto goal = Fibonacci::Goal();
+
+  // Sending a goal that will take a long time to calculate
+  goal.order = 12'000'000;
+
+  // Send the goal
+  auto future_goal_handle = node_->action_client_->async_send_goal(goal);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_,
+    future_goal_handle), rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // Preempt the goal
+  auto preemption_goal = Fibonacci::Goal();
+  preemption_goal.order = 1;
+
+  // Send the goal
+  future_goal_handle = node_->action_client_->async_send_goal(preemption_goal);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_,
+    future_goal_handle), rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  auto goal_handle = future_goal_handle.get();
+
+  // Wait for the result
+  auto future_result = node_->action_client_->async_get_result(goal_handle);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_, future_result),
+    rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // The final result
+  rclcpp_action::ClientGoalHandle<Fibonacci>::WrappedResult result = future_result.get();
+  ASSERT_EQ(result.code, rclcpp_action::ResultCode::SUCCEEDED);
+
+  // Sum all of the values in the requested fibonacci series
+  int sum = 0;
+  for (auto number : result.result->sequence) {
+    sum += number;
+  }
+
+  ASSERT_EQ(sum, 1);
+}
+
+TEST_F(ActionTest, test_simple_action_preemption_after_succeeded)
+{
+  // Test race condition between successfully completing an action and receiving a preemption.
+  auto goal = Fibonacci::Goal();
+  goal.order = 20;
+
+  auto preemption = Fibonacci::Goal();
+  preemption.order = 1;
+
+  // Send the goal
+  auto future_goal_handle = node_->action_client_->async_send_goal(goal);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_,
+    future_goal_handle), rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  node_->omit_server_preemptions();
+
+  auto future_preempt_handle = node_->action_client_->async_send_goal(preemption);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_,
+    future_goal_handle), rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // Get the results
+  auto goal_handle = future_goal_handle.get();
+
+  // Wait for the result of initial goal
+  auto future_result = node_->action_client_->async_get_result(goal_handle);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_, future_result),
+    rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // The final result
+  rclcpp_action::ClientGoalHandle<Fibonacci>::WrappedResult result = future_result.get();
+  ASSERT_EQ(result.code, rclcpp_action::ResultCode::SUCCEEDED);
+
+  // Sum all of the values in the requested fibonacci series
+  int sum = 0;
+  for (auto number : result.result->sequence) {
+    sum += number;
+  }
+
+  ASSERT_EQ(sum, 17710);
+
+  // Now get the preemption result
+  goal_handle = future_preempt_handle.get();
+
+  // Wait for the result of initial goal
+  future_result = node_->action_client_->async_get_result(goal_handle);
+  ASSERT_EQ(rclcpp::spin_until_future_complete(node_, future_result),
+    rclcpp::executor::FutureReturnCode::SUCCESS);
+
+  // The final result
+  result = future_result.get();
+  ASSERT_EQ(result.code, rclcpp_action::ResultCode::SUCCEEDED);
+
+  // Sum all of the values in the requested fibonacci series
+  sum = 0;
+  for (auto number : result.result->sequence) {
+    sum += number;
+  }
+
+  ASSERT_EQ(sum, 1);
 }

commit 3c736f87dfcbba029e92f2f40de332073aba742a

Fix build errors (#1427)
Fixing build error introduced by #1356

nav2_controller/src/nav2_controller.cpp
@@ -198,7 +198,7 @@ void ControllerServer::computeControl()
         RCLCPP_ERROR(get_logger(), "FollowPath called with controller name %s, "
           "which does not exist. Available controllers are %s.",
           c_name.c_str(), controller_ids_concat_.c_str());
-        action_server_->terminate_goals();
+        action_server_->terminate_current();
         return;
       }
     } else {

nav2_waypoint_follower/src/waypoint_follower.cpp
@@ -132,7 +132,7 @@ WaypointFollower::followWaypoints()
     // Check if asked to stop processing action
     if (action_server_->is_cancel_requested()) {
       RCLCPP_INFO(get_logger(), "Cancelling action.");
-      action_server_->terminate_goals();
+      action_server_->terminate_all();
       return;
     }
 
@@ -171,7 +171,7 @@ WaypointFollower::followWaypoints()
           "list and stop on failure is enabled."
           " Terminating action.", goal_index);
         result->missed_waypoints = failed_ids_;
-        action_server_->terminate_goals(result);
+        action_server_->terminate_current(result);
         failed_ids_.clear();
         return;
       } else {