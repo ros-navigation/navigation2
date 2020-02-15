efcd0678ba423d7116187cd8fcb166da54d446ad
Steve Macenski <stevenmacenski@gmail.com>
2020-02-14 20:36:55 -0800

    adding full list of default plugins (#1529)

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -32,10 +32,26 @@ BtNavigator::BtNavigator()
 {
   RCLCPP_INFO(get_logger(), "Creating");
 
+  const std::vector<std::string> plugin_libs = {
+    "nav2_compute_path_to_pose_action_bt_node",
+    "nav2_follow_path_action_bt_node",
+    "nav2_back_up_action_bt_node",
+    "nav2_spin_action_bt_node",
+    "nav2_wait_action_bt_node",
+    "nav2_clear_costmap_service_bt_node",
+    "nav2_is_stuck_condition_bt_node",
+    "nav2_goal_reached_condition_bt_node",
+    "nav2_initial_pose_received_condition_bt_node",
+    "nav2_reinitialize_global_localization_service_bt_node",
+    "nav2_rate_controller_bt_node",
+    "nav2_recovery_node_bt_node",
+    "nav2_pipeline_sequence_bt_node",
+    "nav2_round_robin_node_bt_node"
+  };
+
   // Declare this node's parameters
   declare_parameter("bt_xml_filename");
-
-  declare_parameter("plugin_lib_names");
+  declare_parameter("plugin_lib_names", plugin_libs);
 }
 
 BtNavigator::~BtNavigator()
