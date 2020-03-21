0f6353d43966c16c851055718927a0265e82b920
Davide Faconti <davide.faconti@gmail.com>
2020-03-21 17:49:07 +0100

    correctly haltAllActions (related to #1600)

--- nav2_behavior_tree/include/nav2_behavior_tree/behavior_tree_engine.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/behavior_tree_engine.hpp
@@ -44,21 +44,21 @@ public:
     const std::string & xml_string,
     BT::Blackboard::Ptr blackboard);
 
+  // In order to re-run a Behavior Tree, we must be able to reset all nodes to the initial state
   void haltAllActions(BT::TreeNode * root_node)
   {
+    // this halt signal should propagate through the entire tree.
+    root_node->halt();
+    root_node->setStatus(BT::NodeStatus::IDLE);
+
+    // but, just in case...
     auto visitor = [](BT::TreeNode * node) {
         if (auto action = dynamic_cast<BT::CoroActionNode *>(node)) {
-          action->halt();
+          if( action->status()==BT::NodeStatus::RUNNING) {
+            action->halt();
+          }
+          action->setStatus(BT::NodeStatus::IDLE);
         }
-      };
-    BT::applyRecursiveVisitor(root_node, visitor);
-  }
-
-  // In order to re-run a Behavior Tree, we must be able to reset all nodes to the initial state
-  void resetTree(BT::TreeNode * root_node)
-  {
-    auto visitor = [](BT::TreeNode * node) {
-        node->setStatus(BT::NodeStatus::IDLE);
       };
     BT::applyRecursiveVisitor(root_node, visitor);
   }
