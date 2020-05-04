bd4ca7487e1f4cd16e5b014a16af00ba65405368
Shrijit Singh <shrijitsingh99@gmail.com>
2020-05-04 23:43:54 +0530

    Switch from map<double> to vector<> in using precached integer distances
    Credits to original author from https://github.com/ros-planning/navigation/pull/839
    
    Signed-off-by: Shrijit Singh <shrijitsingh99@gmail.com>

--- nav2_costmap_2d/include/nav2_costmap_2d/inflation_layer.hpp
+++ nav2_costmap_2d/include/nav2_costmap_2d/inflation_layer.hpp
@@ -102,10 +102,9 @@ public:
   inline unsigned char computeCost(double distance) const
   {
     unsigned char cost = 0;
-    double euclidean_distance = distance * resolution_;
     if (distance == 0) {
       cost = LETHAL_OBSTACLE;
-    } else if (euclidean_distance <= inscribed_radius_) {
+    } else if (distance * resolution_ <= inscribed_radius_) {
       cost = INSCRIBED_INFLATED_OBSTACLE;
     } else {
       // make sure cost falls off by Euclidean distance
