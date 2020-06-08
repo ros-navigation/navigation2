51879e679b80c2e207b4f8c37fb3bf4464552356
ymd-stella <world.applepie@gmail.com>
2020-06-09 07:04:57 +0900

    Move to navigate_to_pose/
    Signed-off-by: ymd-stella <world.applepie@gmail.com>

--- nav2_bt_navigator/src/navigate_to_pose/bt_navigator.cpp
+++ nav2_bt_navigator/src/navigate_to_pose/bt_navigator.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "nav2_bt_navigator/navigate_to_pose_bt_navigator.hpp"
+#include "nav2_bt_navigator/navigate_to_pose/bt_navigator.hpp"
 
 #include <fstream>
 #include <memory>
