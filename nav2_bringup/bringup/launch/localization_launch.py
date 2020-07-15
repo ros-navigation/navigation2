de5efdd56f7199c3cbedb5b6a47482f1a7ffc377
Michael Equi <32988490+Michael-Equi@users.noreply.github.com>
2020-07-15 14:09:29 -0700

    Changes RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED to RCUTILS_LOGGING_BUFFERED_STREAM (#1868)

--- nav2_bringup/bringup/launch/localization_launch.py
+++ nav2_bringup/bringup/launch/localization_launch.py
@@ -56,7 +56,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         # Set env var to print messages to stdout immediately
-        SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1'),
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
 
         DeclareLaunchArgument(
             'namespace', default_value='',
