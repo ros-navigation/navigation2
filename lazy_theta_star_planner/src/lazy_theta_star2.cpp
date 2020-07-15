99efed3524ecf493c7b58e0e8d416cc5efc3d28d
Anshu-man567 <56119116+Anshu-man567@users.noreply.github.com>
2020-07-15 18:57:05 +0530

    replaced the files
    - refactored code
    - improved reliability
    - have to write a code similar to that in nav2_system_tests, to test it (working on it)

---
+++ lazy_theta_star_planner/src/lazy_theta_star2.cpp
@@ -0,0 +1,364 @@
+//  Copyright 2020 Anshumaan Singh
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http:// www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "lazy_theta_star_planner/lazy_theta_star2.h"
+#include <vector>
+
+namespace lazyThetaStar
+{
+
+LazyThetaStar::LazyThetaStar()
+{
+  sizeX = 0;
+  sizeY = 0;
+}
+
+bool LazyThetaStar::getPath(std::vector<coords<world_pts>> & raw_path)
+{
+  RCLCPP_INFO(node_->get_logger(), "Path Planning Begins....");
+
+  initializeStuff();
+
+  std::cout << src.x << '\t' << src.y << '\t' << dst.x << '\t' << dst.y << '\n';
+
+  data.push_back({src.x, src.y, 0, dist(src.x, src.y, dst.x, dst.y),
+      0, 1, dist(src.x, src.y, dst.x, dst.y)});
+  addIndex(src.x, src.y, id_gen);
+  pushToPq(id_gen);
+  pushToPq(id_gen);
+  // extra one added due to binaryHeapDelMin starting from index 1
+
+  id_gen++;
+  id curr_id = 0;
+//  map_pts cx = src.x, cy = src.y;
+
+  if (!losCheck(src.x, src.y, dst.x, dst.y)) {
+
+    while (pq.size() > 0) {
+
+      map_pts cx = data[curr_id].x;
+      map_pts cy = data[curr_id].y;
+      std::cout << "the current point is : \t\t\t" << cx << '\t' << cy << '\t' << data[curr_id].f <<
+        '\n';
+      id curr_par = data[curr_id].parent_id;
+
+      if (isGoal(cx, cy)) {
+        if (!(losCheck(cx, cy, data[curr_par].x, data[curr_par].y))) {
+          resetParent(cx, cy, curr_id);
+        }
+        break;
+      }
+
+      std::cout << "ITS PARENTS WERE : " << data[curr_par].x << '\t' << data[curr_par].y << '\t' <<
+        curr_par << '\n';
+      if (!(losCheck(cx, cy, data[curr_par].x, data[curr_par].y))) {
+        resetParent(cx, cy, curr_id);
+      }
+
+      setNeighbors(cx, cy, curr_id);
+
+      getNextNode(curr_id);
+    }
+
+    if (pq.size() < 1) {
+      RCLCPP_INFO(node_->get_logger(), "No Path Found !!!!!!!!!!!");
+      raw_path.clear();
+      return false;
+    }
+
+  } else {
+    RCLCPP_INFO(node_->get_logger(), "Straight Line Path Found!");
+    data.push_back({dst.x, dst.y, dist(src.x, src.y, dst.x, dst.y), 0, 0, 1,
+        dist(src.x, src.y, dst.x, dst.y)});
+    curr_id = id_gen;
+  }
+
+  RCLCPP_INFO(node_->get_logger(), "REACHED DEST  %i,   %i", data[curr_id].x, data[curr_id].y);
+
+  backtrace(raw_path, curr_id);
+
+  RCLCPP_INFO(node_->get_logger(), "Out of it with path_length %i", raw_path.size());
+  return true;
+}
+
+void LazyThetaStar::initializePosn()
+{
+  posn.clear();
+  for (int i = 0; i < sizeX * sizeY; i++) {
+    posn.push_back(-1);
+  }
+}
+
+bool LazyThetaStar::losCheck(map_pts & x0, map_pts & y0, map_pts & x1, map_pts & y1) const
+{
+  int dy = y1 - y0, dx = x1 - x0, f = 0;
+  int sx, sy;
+  int cx = x0, cy = y0;
+
+  if (dy < 0) {
+    dy = -dy;
+    sy = -1;
+  } else {
+    sy = 1;
+  }
+
+  if (dx < 0) {
+    dx = -dx;
+    sx = -1;
+  } else {
+    sx = 1;
+  }
+
+  if (dx >= dy) {
+    while (cx != x1) {
+      f += dy;
+      if (f >= dx) {
+        if (!isSafe(cx + (sx - 1) / 2, cy + (sy - 1) / 2)) {
+          return false;
+        }
+        cy += sy;
+        f -= dx;
+      }
+      if (f != 0 && !isSafe(cx + (sx - 1) / 2, cy + (sy - 1) / 2)) {
+        return false;
+      }
+      if (dy == 0 && !isSafe(cx + (sx - 1) / 2, cy) && !isSafe(cx + (sx - 1) / 2, cy - 1)) {
+        return false;
+      }
+      cx += sx;
+    }
+  } else {
+    while (cy != y1) {
+      f = f + dx;
+      if (f >= dy) {
+        if (!isSafe(cx + (sx - 1) / 2, cy + (sy - 1) / 2)) {
+          return false;
+        }
+        cx += sx;
+        f -= dy;
+      }
+      if (f != 0 && !isSafe(cx + (sx - 1) / 2, cy + (sy - 1) / 2)) {
+        return false;
+      }
+      if (dx == 0 && !isSafe(cx, cy + (sy - 1) / 2) && !isSafe(cx - 1, cy + (sy - 1) / 2)) {
+        return false;
+      }
+      cy += sy;
+    }
+  }
+  return true;
+}
+
+void LazyThetaStar::binaryHeapDelMin()
+{
+  pop_heap(pq.begin() + 1, pq.end(), comp);
+  pq.pop_back();
+//  int hole = 1;
+//  int succ = 2, size = pq.size() - 1, sz = size;
+//  // the main bottom up part where it compares and assigns the smallest value to the hole
+//  // remember that for this part the heap starts at index 1
+//
+//  while (succ < sz) {
+//    double k1 = *(pq[succ].f);
+//    double k2 = *(pq[succ + 1].f);
+//    // std::cout<<k1<<'\t'<<k2<<'\n';
+//    if (k1 > k2) {
+//      // std::cout<<"came 1"<<'\n';
+//      succ++;
+//      // data[hole].key = k2;
+//      pq[hole] = pq[succ];
+//    } else {
+//      // std::cout<<"came 2"<<'\n';
+//      //  data[hole].key = k1;
+//      pq[hole] = pq[succ];
+//    }
+//    hole = succ;
+//    succ <<= 1;
+//  }
+//
+//  // this part checks if the value to be used to fill the last row's hole is small or not
+//  // if not small then it slides the small value up till it reaches the apt position
+//  double bubble = *(pq[sz].f);
+//  int pred = hole >> 1;
+//  //  std::cout<<"HERE"<<'\t'<<data[pred].f<<'\t'<<bubble<<'\n';
+//  while (*(pq[pred].f) > bubble) {
+//    //    std::cout<<"HERE"<<'\t'<<hole<<'\n';
+//    pq[hole] = pq[pred];
+//    hole = pred;
+//    pred >>= 1;
+//  }
+//  // std::cout<<"HERE AT:"<<'\t'<<hole<<'\n';
+//
+//  // this part simply assigns the last value in the haep to the hole after checking it
+//  // for maintaining the heap data structure
+//  pq[hole] = pq[sz];
+//  pq.pop_back();
+}
+
+void LazyThetaStar::pushToPq(id id_this)
+{
+  pq.push_back({id_this, &(data[id_this].f)});
+  push_heap(pq.begin() + 1, pq.end(), comp);
+}
+
+void LazyThetaStar::backtrace(std::vector<coords<world_pts>> & raw_points, id & curr_id)
+{
+  std::vector<coords<world_pts>> path_rev;
+  do {
+    coords<world_pts> world;
+    costmap_->mapToWorld(data[curr_id].x, data[curr_id].y, world.x, world.y);
+    path_rev.push_back({world.x, world.y});
+    RCLCPP_INFO(
+      node_->get_logger(), "%i,   %i  ---> %f, %f", data[curr_id].x, data[curr_id].y, world.x,
+      world.y);
+    if (data[curr_id].got_here == 1) {
+      std::cout <<
+        "------------------------------------------------------>  got the same error   <-------------------------------------------------------"
+                <<
+        '\n';
+      break;
+    }
+    data[curr_id].got_here = 1;
+    curr_id = data[curr_id].parent_id;
+  } while (curr_id != 0);
+  coords<world_pts> w;
+  costmap_->mapToWorld(data[curr_id].x, data[curr_id].y, w.x, w.y);
+  path_rev.push_back({w.x, w.y});
+  RCLCPP_INFO(
+    node_->get_logger(), "%i,   %i  ---> %f, %f", data[curr_id].x, data[curr_id].y, w.x, w.y);
+
+  for (int i = path_rev.size() - 1; i >= 0; i--) {
+    raw_points.push_back(path_rev[i]);
+  }
+}
+
+void LazyThetaStar::resetParent(map_pts & cx, map_pts & cy, id & curr_id)
+{
+  map_pts mx, my;
+  id m_id;
+  cost min_dist = INF_COST;
+  id min_dist_id;
+
+  for (int i = 0; i < how_many_corners; i++) {
+    mx = cx + moves[i][0];
+    my = cy + moves[i][1];
+    if (withinLimits(mx, my)) {
+      getIndex(mx, my, m_id);
+      if (m_id != -1) {
+        if (data[m_id].f < min_dist) {
+          min_dist = data[m_id].f;
+          min_dist_id = m_id;
+        }
+      }
+    }
+  }
+  data[curr_id].parent_id = min_dist_id;
+  data[curr_id].g = data[min_dist_id].g + dist(cx, cy, data[min_dist_id].x, data[min_dist_id].y);
+  data[curr_id].f = data[curr_id].g + data[curr_id].h;
+}
+
+bool LazyThetaStar::isGoal(map_pts & cx, map_pts & cy)
+{
+  return cx == dst.x && cy == dst.y;
+}
+
+void LazyThetaStar::setNeighbors(map_pts & cx, map_pts & cy, id & curr_id)
+{
+  id curr_par = data[curr_id].parent_id;
+  std::cout << "ITS PARENTS ARE : " << data[curr_par].x << '\t' << data[curr_par].y << '\t' <<
+    curr_par << '\n';
+  map_pts px = data[curr_par].x, py = data[curr_par].y;
+  cost g_cost_par = data[curr_par].g;
+
+  map_pts mx, my;
+  id m_id;
+  for (int i = 0; i < 8; i++) {
+    mx = cx + moves[i][0];
+    my = cy + moves[i][1];
+    if (mx == px && my == py) {
+      continue;
+    }
+
+    if (withinLimits(mx, my)) {
+      cost g_cost = g_cost_par + dist(mx, my, px, py);
+      cost h_cost, cal_cost;
+      getIndex(mx, my, m_id);
+
+      if (isSafe(mx, my) && m_id == -1) {
+        h_cost = dist(mx, my, dst.x, dst.y);
+        cal_cost = g_cost + h_cost;
+        data.push_back({mx, my, g_cost, h_cost, curr_par, -1, cal_cost});
+        pushToPq(id_gen);
+        std::cout << "ADDED INDEX 1:" << data[id_gen].x << '\t' << data[id_gen].y << '\t' <<
+          data[id_gen].f << '\n';
+        addIndex(mx, my, id_gen);
+        id_gen++;
+        continue;
+      } else if (m_id != -1) {
+        tree_node & curr_node = data[m_id];
+        h_cost = curr_node.h;
+        cal_cost = g_cost + h_cost;
+        if (curr_node.f > cal_cost) {
+          curr_node.g = g_cost;
+          curr_node.f = cal_cost;
+          curr_node.parent_id = curr_par;
+          if (curr_node.closed == 1) {
+            curr_node.closed = -1;
+            pushToPq(m_id);
+            std::cout << "ADDED INDEX  2:" << data[m_id].x << '\t' << data[m_id].y << '\t' <<
+              data[m_id].f << '\n';
+          }
+        }
+        continue;
+      }
+    }
+  }
+}
+
+void LazyThetaStar::initializeStuff()
+{
+  id_gen = 0;
+  sizeX = costmap_->getSizeInCellsX();
+  sizeY = costmap_->getSizeInCellsY();
+  std::cout << sizeX << '\t' << sizeY << '\n';
+  initializePosn();
+  data.reserve(static_cast<int>(sizeX * sizeY * 0.05));
+}
+
+void LazyThetaStar::clearStuff()
+{
+  data.clear();
+  pq.clear();
+  posn.clear();
+}
+
+void LazyThetaStar::getNextNode(id & curr_id)
+{
+  curr_id = pq[1].pos_id;
+  data[curr_id].closed = -1;
+  binaryHeapDelMin();
+}
+
+
+//void LazyThetaStar::tfMapToWorld(
+//  map_pts & mx,
+//  map_pts & my,
+//  world_pts & wx,
+//  world_pts & wy);
+//{
+//wx = costmap_->getOriginX() + (mx) * costmap_->getResolution();
+//wy = costmap_->getOriginY() + (my) * costmap_->getResolution();
+//}
+
+} //  namespace lazyThetaStar
---
+++ lazy_theta_star_planner/src/lazy_theta_star_planner.cpp
@@ -0,0 +1,151 @@
+// Copyright 2020 Anshumaan Singh
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+#include <memory>
+#include <string>
+#include "lazy_theta_star_planner/lazy_theta_star_planner.h"
+#include "lazy_theta_star_planner/lazy_theta_star2.h"
+
+
+namespace lazyThetaStarPlanner
+{
+void LazyThetaStarPlanner::configure(
+  rclcpp_lifecycle::LifecycleNode::SharedPtr parent,
+  std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmapRos)
+{
+  planner_ = std::make_unique<lazyThetaStar::LazyThetaStar>();
+  node_ = parent;
+  name_ = name;
+  planner_->costmap_ = costmapRos->getCostmap();
+  globalFrame_ = costmapRos->getGlobalFrameID();
+
+  nav2_util::declare_parameter_if_not_declared(
+    node_, name_ + ".how_many_corners", rclcpp::ParameterValue(
+      8));
+
+  node_->get_parameter(name_ + ".how_many_corners", how_many_corners_);
+
+  if (how_many_corners_ != 8) {
+    how_many_corners_ = 4;
+  }
+
+  nav2_util::declare_parameter_if_not_declared(
+    node_, name_ + ".interpolation_dist", rclcpp::ParameterValue(
+      0.1));
+
+  node_->get_parameter(name_ + ".interpolation_dist", interpolation_dist_);
+
+}
+
+void LazyThetaStarPlanner::cleanup()
+{
+  RCLCPP_INFO(
+    node_->get_logger(), "CleaningUp plugin %s of type lazyThetaStarPlanner",
+    name_.c_str());
+}
+
+void LazyThetaStarPlanner::activate()
+{
+  RCLCPP_INFO(
+    node_->get_logger(), "Activating plugin %s of type lazyThetaStarPlanner",
+    name_.c_str());
+}
+
+void LazyThetaStarPlanner::deactivate()
+{
+  RCLCPP_INFO(
+    node_->get_logger(), "Deactivating plugin %s of type lazyThetaStarPlanner",
+    name_.c_str());
+}
+
+nav_msgs::msg::Path LazyThetaStarPlanner::createPlan(
+  const geometry_msgs::msg::PoseStamped & start,
+  const geometry_msgs::msg::PoseStamped & goal)
+{
+  nav_msgs::msg::Path global_path;
+  std::vector<coords<world_pts>> path;
+
+  planner_->costmap_->worldToMap(start.pose.position.x, start.pose.position.y, src_[0], src_[1]);
+  planner_->src = {static_cast<int>(src_[0]), static_cast<int>(src_[1])};
+
+  planner_->costmap_->worldToMap(goal.pose.position.x, goal.pose.position.y, dst_[0], dst_[1]);
+  planner_->dst = {static_cast<int>(dst_[0]), static_cast<int>(dst_[1])};
+
+  RCLCPP_INFO(
+    node_->get_logger(), "Got the src and dst... (%i, %i) && (%i, %i)", planner_->src.x, planner_->src.y,
+    planner_->dst.x, planner_->dst.y);
+
+  planner_->node_ = node_;
+  planner_->how_many_corners = how_many_corners_;
+  planner_->lethal_cost = LETHAL_COST;
+
+  planner_->initializeStuff();
+  planner_->getPath(path);
+  planner_->clearStuff();
+
+  RCLCPP_INFO(node_->get_logger(), "RECEIVED THE PATH");
+
+  if (path.size() > 1) {
+
+    for (int i = 0; i < path.size(); i++) {
+      std::cout << path[i].x << '\t' << path[i].y << '\n';
+    }
+
+    global_path = linearInterpolation(path, 0.1);
+  } else {
+    global_path.poses.clear();
+  }
+  path.clear();
+  std::cout << global_path.poses.size() << '\n';
+  global_path.header.stamp = node_->now();
+  global_path.header.frame_id = globalFrame_;
+  RCLCPP_INFO(node_->get_logger(), "SENT OUT THE PATH");
+  return global_path;
+}
+
+nav_msgs::msg::Path LazyThetaStarPlanner::linearInterpolation(
+  std::vector<coords<world_pts>> & raw_path,
+  double dist_bw_points)
+{
+  nav_msgs::msg::Path pa;
+
+  for (int j = 0; j < raw_path.size() - 1; j++) {
+    geometry_msgs::msg::PoseStamped p;
+    coords<world_pts> pt1 = {raw_path[j].x, raw_path[j].y};
+    p.pose.position.x = pt1.x;
+    p.pose.position.y = pt1.y;
+    pa.poses.push_back(p);
+
+    coords<world_pts> pt2 = {raw_path[j + 1].x, raw_path[j + 1].y};
+    geometry_msgs::msg::PoseStamped p1;
+    double distance = std::hypot(pt2.x - pt1.x, pt2.y - pt1.y);
+    int loops = distance / dist_bw_points;
+    double sin_alpha = (pt2.y - pt1.y) / distance;
+    double cos_alpha = (pt2.x - pt1.x) / distance;
+    for (int k = 1; k < loops; k++) {
+      p1.pose.position.x = pt1.x + k * dist_bw_points * cos_alpha;
+      p1.pose.position.y = pt1.y + k * dist_bw_points * sin_alpha;
+      pa.poses.push_back(p1);
+    }
+  }
+  return pa;
+}
+
+}  // namespace lazyThetaStarPlanner
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(lazyThetaStarPlanner::LazyThetaStarPlanner, nav2_core::GlobalPlanner)
