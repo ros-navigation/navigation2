6d4fcf4244bff913461279241f5a92b6e3028206
Jose Miguel TORRES CAMARA <josemigueltorrescamara@gmail.com>
2020-07-16 11:28:47 +0200

    Add PDF with localization design to /doc  (#1391)

--- nav2_localization/include/plugins/matcher_plugins.hpp
+++ nav2_localization/include/plugins/matcher_plugins.hpp
@@ -9,13 +9,13 @@
 namespace nav2_localization_plugins
 {
     // Implements a vanilla matcher for 2D NDT maps (for NDT-MCL localization)
-    class Vanilla2d : public nav2_localization_base::Matcher
+    class VanillaNDT2d : public nav2_localization_base::Matcher
     {
 		private:
             ndt_grid_2d_t map;
 
         public:
-			Vanilla2d(){}
+			VanillaNDT2d(){}
 
             // TODO - How to send position?
             float get_overlap(const nav2_localization::measurement_t& measurement, const vector<float>& pos, const nav2_localization::ndt_grid_2d_t& map); // Computes the overlap between a measurement and what should be measured given the current position and a map
