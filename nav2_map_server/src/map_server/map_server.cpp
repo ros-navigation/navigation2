5008ceb4edf99faf32a01784d113d13b26806e78
Steve Macenski <stevenmacenski@gmail.com>
2020-07-30 16:19:20 -0700

    Bond support + fixing various action server issues (#1894)
    * prototype of lifecycle bond system
    
    * adding more structure to get around weak ptr issue
    
    * working prototype for manager
    
    * adding some ns -> s conversions
    
    * changing to service node
    
    * adding bond connections to all servers
    
    * update logs
    
    * fixing review comments
    
    * fix types
    
    * remove extraneous functions
    
    * make linters happy
    
    * simplifications
    
    * adding spinner to get working but now unstable
    
    * moving bond connections to activate state
    
    * adding defaults
    
    * working complete prototype for review
    
    * update dependencies
    
    * adding connection logging
    
    * remove accidental file
    
    * fix server side timeout for heartbeats
    
    * adding complete unit coverage of bond support
    
    * fixing lifecycle test
    
    * trying to activate since autostart was removed

--- nav2_map_server/src/map_server/map_server.cpp
+++ nav2_map_server/src/map_server/map_server.cpp
@@ -129,6 +129,9 @@ MapServer::on_activate(const rclcpp_lifecycle::State & /*state*/)
   auto occ_grid = std::make_unique<nav_msgs::msg::OccupancyGrid>(msg_);
   occ_pub_->publish(std::move(occ_grid));
 
+  // create bond connection
+  createBond();
+
   return nav2_util::CallbackReturn::SUCCESS;
 }
 
@@ -138,6 +141,9 @@ MapServer::on_deactivate(const rclcpp_lifecycle::State & /*state*/)
   RCLCPP_INFO(get_logger(), "Deactivating");
 
   occ_pub_->on_deactivate();
+
+  // destroy bond connection
+  destroyBond();
 
   return nav2_util::CallbackReturn::SUCCESS;
 }
