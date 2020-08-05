ad2bdca1dd9a843db5fe50c9ba963e7fe0d1a79a
stevemacenski <stevenmacenski@gmail.com>
2020-08-04 17:56:24 -0700

    adding copyrights

--- nav2_system_tests/src/system_failure/tester_node.py
+++ nav2_system_tests/src/system_failure/tester_node.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 # Copyright 2018 Intel Corporation.
+# Copyright 2020 Samsung Research America
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
