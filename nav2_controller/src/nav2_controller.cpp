ros-navigation/navigation2

commit d7494ec8c501522200089c54947232fe86c0ef94

Move dwb goal/progress checker plugins to nav2_controller (#1857)
* Move dwb goal/progress checker plugins to nav2_controller

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Move goal/progress checker plugins to nav2_controller

Address review comments

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Move goal/progress checker plugins to nav2_controller

Use new plugin declaration format and doc update

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Update bringup.yaml for new plugins in nav2_controller

Also remove redundent file from dwb_plugins
Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Fix doc errors and update remaining yaml files

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Remove mention of goal_checker from dwb docs

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Add .plugin params to doc

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Tests for progress_checker plugin

declare .plugin only if not declared before

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

* Tweak plugin names/description in doc

Signed-off-by: Siddarth Gore <siddarth.gore@gmail.com>

nav2_controller/src/nav2_controller.cpp
@@ -22,7 +22,6 @@
 #include "nav_2d_utils/conversions.hpp"
 #include "nav2_util/node_utils.hpp"
 #include "nav2_util/geometry_utils.hpp"
-#include "nav2_controller/progress_checker.hpp"
 #include "nav2_controller/nav2_controller.hpp"
 
 using namespace std::chrono_literals;
@@ -32,6 +31,12 @@ namespace nav2_controller
 
 ControllerServer::ControllerServer()
 : LifecycleNode("controller_server", "", true),
+  progress_checker_loader_("nav2_core", "nav2_core::ProgressChecker"),
+  default_progress_checker_id_{"progress_checker"},
+  default_progress_checker_type_{"nav2_controller::SimpleProgressChecker"},
+  goal_checker_loader_("nav2_core", "nav2_core::GoalChecker"),
+  default_goal_checker_id_{"goal_checker"},
+  default_goal_checker_type_{"nav2_controller::SimpleGoalChecker"},
   lp_loader_("nav2_core", "nav2_core::Controller"),
   default_ids_{"FollowPath"},
   default_types_{"dwb_core::DWBLocalPlanner"}
@@ -40,6 +45,8 @@ ControllerServer::ControllerServer()
 
   declare_parameter("controller_frequency", 20.0);
 
+  declare_parameter("progress_checker_plugin", default_progress_checker_id_);
+  declare_parameter("goal_checker_plugin", default_goal_checker_id_);
   declare_parameter("controller_plugins", default_ids_);
   declare_parameter("min_x_velocity_threshold", rclcpp::ParameterValue(0.0001));
   declare_parameter("min_y_velocity_threshold", rclcpp::ParameterValue(0.0001));
@@ -61,12 +68,29 @@ ControllerServer::~ControllerServer()
 nav2_util::CallbackReturn
 ControllerServer::on_configure(const rclcpp_lifecycle::State & state)
 {
+  auto node = shared_from_this();
+
   RCLCPP_INFO(get_logger(), "Configuring controller interface");
+
+  get_parameter("progress_checker_plugin", progress_checker_id_);
+  if (progress_checker_id_ == default_progress_checker_id_) {
+    nav2_util::declare_parameter_if_not_declared(
+      node, default_progress_checker_id_ + ".plugin",
+      rclcpp::ParameterValue(default_progress_checker_type_));
+  }
+  get_parameter("goal_checker_plugin", goal_checker_id_);
+  if (goal_checker_id_ == default_goal_checker_id_) {
+    nav2_util::declare_parameter_if_not_declared(
+      node, default_goal_checker_id_ + ".plugin",
+      rclcpp::ParameterValue(default_goal_checker_type_));
+  }
 
   get_parameter("controller_plugins", controller_ids_);
   if (controller_ids_ == default_ids_) {
     for (size_t i = 0; i < default_ids_.size(); ++i) {
-      declare_parameter(default_ids_[i] + ".plugin", default_types_[i]);
+      nav2_util::declare_parameter_if_not_declared(
+        node, default_ids_[i] + ".plugin",
+        rclcpp::ParameterValue(default_types_[i]));
     }
   }
   controller_types_.resize(controller_ids_.size());
@@ -79,9 +103,28 @@ ControllerServer::on_configure(const rclcpp_lifecycle::State & state)
 
   costmap_ros_->on_configure(state);
 
-  auto node = shared_from_this();
-
-  progress_checker_ = std::make_unique<ProgressChecker>(node);
+  try {
+    progress_checker_type_ = nav2_util::get_plugin_type_param(node, progress_checker_id_);
+    progress_checker_ = progress_checker_loader_.createUniqueInstance(progress_checker_type_);
+    RCLCPP_INFO(
+      get_logger(), "Created progress_checker : %s of type %s",
+      progress_checker_id_.c_str(), progress_checker_type_.c_str());
+    progress_checker_->initialize(node, progress_checker_id_);
+  } catch (const pluginlib::PluginlibException & ex) {
+    RCLCPP_FATAL(get_logger(), "Failed to create controller. Exception: %s", ex.what());
+    exit(-1);
+  }
+  try {
+    goal_checker_type_ = nav2_util::get_plugin_type_param(node, goal_checker_id_);
+    goal_checker_ = goal_checker_loader_.createUniqueInstance(goal_checker_type_);
+    RCLCPP_INFO(
+      get_logger(), "Created goal_checker : %s of type %s",
+      goal_checker_id_.c_str(), goal_checker_type_.c_str());
+    goal_checker_->initialize(node, goal_checker_id_);
+  } catch (const pluginlib::PluginlibException & ex) {
+    RCLCPP_FATAL(get_logger(), "Failed to create controller. Exception: %s", ex.what());
+    exit(-1);
+  }
 
   for (size_t i = 0; i != controller_ids_.size(); i++) {
     try {
@@ -172,6 +215,7 @@ ControllerServer::on_cleanup(const rclcpp_lifecycle::State & state)
 
   vel_publisher_.reset();
   action_server_.reset();
+  goal_checker_->reset();
 
   return nav2_util::CallbackReturn::SUCCESS;
 }
@@ -295,6 +339,7 @@ void ControllerServer::setPlannerPath(const nav_msgs::msg::Path & path)
   controllers_[current_controller_]->setPlan(path);
 
   auto end_pose = *(path.poses.end() - 1);
+  goal_checker_->reset();
 
   RCLCPP_DEBUG(
     get_logger(), "Path end point is (%.2f, %.2f)",
@@ -310,7 +355,9 @@ void ControllerServer::computeAndPublishVelocity()
     throw nav2_core::PlannerException("Failed to obtain robot pose");
   }
 
-  progress_checker_->check(pose);
+  if (!progress_checker_->check(pose)) {
+    throw nav2_core::PlannerException("Failed to make progress");
+  }
 
   nav_2d_msgs::msg::Twist2D twist = getThresholdedTwist(odom_sub_->getTwist());
 
@@ -375,7 +422,7 @@ bool ControllerServer::isGoalReached()
 
   nav_2d_msgs::msg::Twist2D twist = getThresholdedTwist(odom_sub_->getTwist());
   geometry_msgs::msg::Twist velocity = nav_2d_utils::twist2Dto3D(twist);
-  return controllers_[current_controller_]->isGoalReached(pose, velocity);
+  return goal_checker_->isGoalReached(pose.pose, end_pose_, velocity);
 }
 
 bool ControllerServer::getRobotPose(geometry_msgs::msg::PoseStamped & pose)

nav2_bringup/bringup/params/nav2_multirobot_params_1.yaml
@@ -86,8 +86,21 @@ controller_server:
     min_x_velocity_threshold: 0.001
     min_y_velocity_threshold: 0.5
     min_theta_velocity_threshold: 0.001
+    progress_checker_plugin: "progress_checker"
+    goal_checker_plugin: "goal_checker"
     controller_plugins: ["FollowPath"]
 
+    # Progress checker parameters
+    progress_checker:
+      plugin: "nav2_controller::SimpleProgressChecker"
+      required_movement_radius: 0.5
+      movement_time_allowance: 10.0
+    # Goal checker parameters
+    goal_checker:
+      plugin: "nav2_controller::SimpleGoalChecker"
+      xy_goal_tolerance: 0.25
+      yaw_goal_tolerance: 0.25
+      stateful: True
     # DWB parameters
     FollowPath:
       plugin: "dwb_core::DWBLocalPlanner"

nav2_bringup/bringup/params/nav2_multirobot_params_2.yaml
@@ -86,8 +86,21 @@ controller_server:
     min_x_velocity_threshold: 0.001
     min_y_velocity_threshold: 0.5
     min_theta_velocity_threshold: 0.001
+    progress_checker_plugin: "progress_checker"
+    goal_checker_plugin: "goal_checker"
     controller_plugins: ["FollowPath"]
 
+    # Progress checker parameters
+    progress_checker:
+      plugin: "nav2_controller::SimpleProgressChecker"
+      required_movement_radius: 0.5
+      movement_time_allowance: 10.0
+    # Goal checker parameters
+    goal_checker:
+      plugin: "nav2_controller::SimpleGoalChecker"
+      xy_goal_tolerance: 0.25
+      yaw_goal_tolerance: 0.25
+      stateful: True
     # DWB parameters
     FollowPath:
       plugin: "dwb_core::DWBLocalPlanner"

nav2_bringup/bringup/params/nav2_params.yaml
@@ -87,8 +87,21 @@ controller_server:
     min_x_velocity_threshold: 0.001
     min_y_velocity_threshold: 0.5
     min_theta_velocity_threshold: 0.001
+    progress_checker_plugin: "progress_checker"
+    goal_checker_plugin: "goal_checker"
     controller_plugins: ["FollowPath"]
 
+    # Progress checker parameters
+    progress_checker:
+      plugin: "nav2_controller::SimpleProgressChecker"
+      required_movement_radius: 0.5
+      movement_time_allowance: 10.0
+    # Goal checker parameters
+    goal_checker:
+      plugin: "nav2_controller::SimpleGoalChecker"
+      xy_goal_tolerance: 0.25
+      yaw_goal_tolerance: 0.25
+      stateful: True
     # DWB parameters
     FollowPath:
       plugin: "dwb_core::DWBLocalPlanner"

nav2_controller/CMakeLists.txt
@@ -4,6 +4,7 @@ project(nav2_controller)
 find_package(ament_cmake REQUIRED)
 find_package(nav2_core REQUIRED)
 find_package(nav2_common REQUIRED)
+find_package(angles REQUIRED)
 find_package(rclcpp REQUIRED)
 find_package(rclcpp_action REQUIRED)
 find_package(std_msgs REQUIRED)
@@ -29,12 +30,12 @@ set(library_name ${executable_name}_core)
 
 add_library(${library_name}
   src/nav2_controller.cpp
-  src/progress_checker.cpp
 )
 
 target_compile_definitions(${library_name} PUBLIC "PLUGINLIB__DISABLE_BOOST_FUNCTIONS")
 
 set(dependencies
+  angles
   rclcpp
   rclcpp_action
   std_msgs
@@ -46,6 +47,22 @@ set(dependencies
   pluginlib
 )
 
+add_library(simple_progress_checker SHARED plugins/simple_progress_checker.cpp)
+ament_target_dependencies(simple_progress_checker ${dependencies})
+# prevent pluginlib from using boost
+target_compile_definitions(simple_progress_checker PUBLIC "PLUGINLIB__DISABLE_BOOST_FUNCTIONS")
+
+add_library(simple_goal_checker SHARED plugins/simple_goal_checker.cpp)
+ament_target_dependencies(simple_goal_checker ${dependencies})
+# prevent pluginlib from using boost
+target_compile_definitions(simple_goal_checker PUBLIC "PLUGINLIB__DISABLE_BOOST_FUNCTIONS")
+
+add_library(stopped_goal_checker SHARED plugins/stopped_goal_checker.cpp)
+target_link_libraries(stopped_goal_checker simple_goal_checker)
+ament_target_dependencies(stopped_goal_checker ${dependencies})
+# prevent pluginlib from using boost
+target_compile_definitions(stopped_goal_checker PUBLIC "PLUGINLIB__DISABLE_BOOST_FUNCTIONS")
+
 ament_target_dependencies(${library_name}
   ${dependencies}
 )
@@ -53,13 +70,23 @@ ament_target_dependencies(${library_name}
 # prevent pluginlib from using boost
 target_compile_definitions(${library_name} PUBLIC "PLUGINLIB__DISABLE_BOOST_FUNCTIONS")
 
+if(BUILD_TESTING)
+  find_package(ament_lint_auto REQUIRED)
+  # the following line skips the linter which checks for copyrights
+  set(ament_cmake_copyright_FOUND TRUE)
+  ament_lint_auto_find_test_dependencies()
+
+  find_package(ament_cmake_gtest REQUIRED)
+  add_subdirectory(plugins/test)
+endif()
+
 ament_target_dependencies(${executable_name}
   ${dependencies}
 )
 
 target_link_libraries(${executable_name} ${library_name})
 
-install(TARGETS ${library_name}
+install(TARGETS simple_progress_checker simple_goal_checker stopped_goal_checker ${library_name}
   ARCHIVE DESTINATION lib
   LIBRARY DESTINATION lib
   RUNTIME DESTINATION bin
@@ -79,6 +106,10 @@ if(BUILD_TESTING)
 endif()
 
 ament_export_include_directories(include)
-ament_export_libraries(${library_name})
+ament_export_libraries(simple_progress_checker
+  simple_goal_checker
+  stopped_goal_checker
+  ${library_name})
+pluginlib_export_plugin_description_file(nav2_core plugins.xml)
 
 ament_package()

nav2_controller/include/nav2_controller/nav2_controller.hpp
@@ -22,6 +22,8 @@
 #include <vector>
 
 #include "nav2_core/controller.hpp"
+#include "nav2_core/progress_checker.hpp"
+#include "nav2_core/goal_checker.hpp"
 #include "nav2_costmap_2d/costmap_2d_ros.hpp"
 #include "tf2_ros/transform_listener.h"
 #include "nav2_msgs/action/follow_path.hpp"
@@ -201,6 +203,22 @@ protected:
   std::unique_ptr<nav_2d_utils::OdomSubscriber> odom_sub_;
   rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr vel_publisher_;
 
+  // Progress Checker Plugin
+  pluginlib::ClassLoader<nav2_core::ProgressChecker> progress_checker_loader_;
+  nav2_core::ProgressChecker::Ptr progress_checker_;
+  std::string default_progress_checker_id_;
+  std::string default_progress_checker_type_;
+  std::string progress_checker_id_;
+  std::string progress_checker_type_;
+
+  // Goal Checker Plugin
+  pluginlib::ClassLoader<nav2_core::GoalChecker> goal_checker_loader_;
+  nav2_core::GoalChecker::Ptr goal_checker_;
+  std::string default_goal_checker_id_;
+  std::string default_goal_checker_type_;
+  std::string goal_checker_id_;
+  std::string goal_checker_type_;
+
   // Controller Plugins
   pluginlib::ClassLoader<nav2_core::Controller> lp_loader_;
   ControllerMap controllers_;
@@ -209,8 +227,6 @@ protected:
   std::vector<std::string> controller_ids_;
   std::vector<std::string> controller_types_;
   std::string controller_ids_concat_, current_controller_;
-
-  std::unique_ptr<ProgressChecker> progress_checker_;
 
   double controller_frequency_;
   double min_x_velocity_threshold_;

nav2_controller/include/nav2_controller/plugins/simple_goal_checker.hpp
@@ -32,54 +32,46 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cmath>
-#include <string>
+#ifndef NAV2_CONTROLLER__PLUGINS__SIMPLE_GOAL_CHECKER_HPP_
+#define NAV2_CONTROLLER__PLUGINS__SIMPLE_GOAL_CHECKER_HPP_
+
 #include <memory>
-#include "dwb_plugins/stopped_goal_checker.hpp"
-#include "pluginlib/class_list_macros.hpp"
-#include "nav2_util/node_utils.hpp"
+#include <string>
 
-using std::hypot;
-using std::fabs;
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "nav2_core/goal_checker.hpp"
 
-namespace dwb_plugins
+namespace nav2_controller
 {
 
-StoppedGoalChecker::StoppedGoalChecker()
-: SimpleGoalChecker(), rot_stopped_velocity_(0.25), trans_stopped_velocity_(0.25)
+/**
+ * @class SimpleGoalChecker
+ * @brief Goal Checker plugin that only checks the position difference
+ *
+ * This class can be stateful if the stateful parameter is set to true (which it is by default).
+ * This means that the goal checker will not check if the xy position matches again once it is found to be true.
+ */
+class SimpleGoalChecker : public nav2_core::GoalChecker
 {
-}
+public:
+  SimpleGoalChecker();
+  // Standard GoalChecker Interface
+  void initialize(
+    const rclcpp_lifecycle::LifecycleNode::SharedPtr & nh,
+    const std::string & plugin_name) override;
+  void reset() override;
+  bool isGoalReached(
+    const geometry_msgs::msg::Pose & query_pose, const geometry_msgs::msg::Pose & goal_pose,
+    const geometry_msgs::msg::Twist & velocity) override;
 
-void StoppedGoalChecker::initialize(
-  const rclcpp_lifecycle::LifecycleNode::SharedPtr & nh,
-  const std::string & plugin_name)
-{
-  SimpleGoalChecker::initialize(nh, plugin_name);
+protected:
+  double xy_goal_tolerance_, yaw_goal_tolerance_;
+  bool stateful_, check_xy_;
+  // Cached squared xy_goal_tolerance_
+  double xy_goal_tolerance_sq_;
+};
 
-  nav2_util::declare_parameter_if_not_declared(
-    nh,
-    plugin_name + ".rot_stopped_velocity", rclcpp::ParameterValue(0.25));
-  nav2_util::declare_parameter_if_not_declared(
-    nh,
-    plugin_name + ".trans_stopped_velocity", rclcpp::ParameterValue(0.25));
+}  // namespace nav2_controller
 
-  nh->get_parameter(plugin_name + ".rot_stopped_velocity", rot_stopped_velocity_);
-  nh->get_parameter(plugin_name + ".trans_stopped_velocity", trans_stopped_velocity_);
-}
-
-bool StoppedGoalChecker::isGoalReached(
-  const geometry_msgs::msg::Pose & query_pose, const geometry_msgs::msg::Pose & goal_pose,
-  const geometry_msgs::msg::Twist & velocity)
-{
-  bool ret = SimpleGoalChecker::isGoalReached(query_pose, goal_pose, velocity);
-  if (!ret) {
-    return ret;
-  }
-
-  return fabs(velocity.angular.z) <= rot_stopped_velocity_ &&
-         hypot(velocity.linear.x, velocity.linear.y) <= trans_stopped_velocity_;
-}
-
-}  // namespace dwb_plugins
-
-PLUGINLIB_EXPORT_CLASS(dwb_plugins::StoppedGoalChecker, nav2_core::GoalChecker)
+#endif  // NAV2_CONTROLLER__PLUGINS__SIMPLE_GOAL_CHECKER_HPP_

nav2_controller/include/nav2_controller/plugins/simple_progress_checker.hpp
@@ -12,40 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_CONTROLLER__PROGRESS_CHECKER_HPP_
-#define NAV2_CONTROLLER__PROGRESS_CHECKER_HPP_
+#ifndef NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
+#define NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
 
+#include <string>
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "nav2_core/progress_checker.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/pose2_d.hpp"
 
 namespace nav2_controller
 {
 /**
- * @class nav2_controller::ProgressChecker
- * @brief This class is used to check the position of the robot to make sure
- * that it is actually progressing towards a goal.
- */
-class ProgressChecker
+* @class SimpleProgressChecker
+* @brief This plugin is used to check the position of the robot to make sure
+* that it is actually progressing towards a goal.
+*/
+
+class SimpleProgressChecker : public nav2_core::ProgressChecker
 {
 public:
-  /**
-   * @brief Constructor of ProgressChecker
-   * @param node Node pointer
-   */
-  explicit ProgressChecker(const rclcpp_lifecycle::LifecycleNode::SharedPtr & node);
-  /**
-   * @brief Checks if the robot has moved compare to previous
-   * pose
-   * @param current_pose Current pose of the robot
-   * @throw nav2_core::PlannerException when failed to make progress
-   */
-  void check(geometry_msgs::msg::PoseStamped & current_pose);
-  /**
-   * @brief Reset class state upon calling
-   */
-  void reset() {baseline_pose_set_ = false;}
+  void initialize(
+    const rclcpp_lifecycle::LifecycleNode::SharedPtr & node,
+    const std::string & plugin_name) override;
+  bool check(geometry_msgs::msg::PoseStamped & current_pose) override;
+  void reset() override;
 
 protected:
   /**
@@ -72,4 +64,4 @@ protected:
 };
 }  // namespace nav2_controller
 
-#endif  // NAV2_CONTROLLER__PROGRESS_CHECKER_HPP_
+#endif  // NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_

commit eec7094f7f5ab458381989281c03bb68367ee2b7

transform goal to costmap frame
The plan recieved is usually in global frame, but
our local costmap is often in odom frame. This
fixes a regression from #1857

nav2_controller/src/nav2_controller.cpp
@@ -20,6 +20,7 @@
 
 #include "nav2_core/exceptions.hpp"
 #include "nav_2d_utils/conversions.hpp"
+#include "nav_2d_utils/tf_help.hpp"
 #include "nav2_util/node_utils.hpp"
 #include "nav2_util/geometry_utils.hpp"
 #include "nav2_controller/nav2_controller.hpp"
@@ -336,6 +337,11 @@ void ControllerServer::setPlannerPath(const nav_msgs::msg::Path & path)
   controllers_[current_controller_]->setPlan(path);
 
   auto end_pose = path.poses.back();
+  end_pose.header.frame_id = path.header.frame_id;
+  rclcpp::Duration tolerance(costmap_ros_->getTransformTolerance() * 1e9);
+  nav_2d_utils::transformPose(
+    costmap_ros_->getTfBuffer(), costmap_ros_->getGlobalFrameID(),
+    end_pose, end_pose, tolerance);
   goal_checker_->reset();
 
   RCLCPP_DEBUG(