a941a33496f2d8fdda9a9f60d1dfc5c27f7b30cf
Florian Gramss <flo.gramss@gmail.com>
2020-08-18 21:20:07 +0200

    add personal copyright
    I think this is okay for finding a nasty bug.. :)

--- nav2_behavior_tree/src/behavior_tree_engine.cpp
+++ nav2_behavior_tree/src/behavior_tree_engine.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2020 Florian Gramss
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
