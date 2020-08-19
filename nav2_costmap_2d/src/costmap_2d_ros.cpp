f32d0471324901f9ff0fa62cafa2d2e05a791e5c
stevemacenski <stevenmacenski@gmail.com>
2020-08-18 17:53:58 -0700

    adding clearable layers to defaults

--- nav2_costmap_2d/src/costmap_2d_ros.cpp
+++ nav2_costmap_2d/src/costmap_2d_ros.cpp
@@ -84,7 +84,7 @@ Costmap2DROS::Costmap2DROS(
     {"--ros-args", "-r", std::string("__node:=") + get_name() + "_client", "--"});
   client_node_ = std::make_shared<rclcpp::Node>("_", options);
 
-  std::vector<std::string> clearable_layers{"obstacle_layer"};
+  std::vector<std::string> clearable_layers{"obstacle_layer", "voxel_layer", "range_layer"};
 
   declare_parameter("always_send_full_costmap", rclcpp::ParameterValue(false));
   declare_parameter("footprint_padding", rclcpp::ParameterValue(0.01f));
