0a84cc7a009f6de2506e7ca078c8290d436fc90d
Jose Miguel TORRES CAMARA <josemigueltorrescamara@gmail.com>
2020-09-03 23:07:32 +0200

    [SOLVER] Add proto-MCL to the dummy plugin. PF abstracted.

--- nav2_localization/src/plugins/solver.cpp
+++ nav2_localization/src/plugins/solver.cpp
@@ -9,8 +9,43 @@ geometry_msgs::msg::PoseWithCovariance DummySolver2d::solve(
 	const nav_msgs::msg::Odometry& curr_odom,
 	const sensor_msgs::msg::LaserScan& scan)
 {
-    geometry_msgs::msg::PoseWithCovariance dummy_pose{};
-    return dummy_pose;
+	// STEP 1 - Motion update
+	for(unsigned int i=0; i<pf.n_particles; i++)
+	{
+		pf.particles[i].pose = motionSampler_.getMostLikelyPose(prev_odom, curr_odom, pf.particles[i].pose); // TODO - Substitute 2 odoms for the transformation
+		// NOTE: The motion sampler (for MCL-based localizations) will get each particle's pose. No need to store "previous pose"?
+	}
+
+	// STEP 2 - Weight calculation
+	for(unsigned int i=0; i<pf.n_particles; i++)
+	{
+		pf.particles[i].weight = matcher_.match(scan, pf.particles[i].pose);
+		// NOTE: Send only scan and use scan+map to return most likely pose and compute the weight here??
+		// NOTE: As Steve said, to delegate as much as possible, the weight/score/likelihood could be computed in "match()" and, if needed, refined here
+		// NOTE: Will the weight/score/likelihood work for graph or KF based approaches?
+	}
+
+	// STEP 3 - Resample (using the already updated weights)
+	pf.resample();
+
+	// STEP 4 - Return a most likely pose
+	// TODO - Decide how
+	//   - Pose of particle with best score -> Chosen for now for simplicity
+	//   - Weighted mean of X best scores
+	//   - ???
+
+	float max_weight = 0;
+	int best_particle_idx = -1;	
+	for(unsigned int i=0; i<pf.n_particles; i++)
+	{
+		if(pf.particles[i].weight > max_weight)
+		{
+			max_weight = pf.particles[i].weight;
+			best_particle_idx = i;
+		}
+	}
+
+    return pf.particles[best_particle_idx];
 }
 
 void DummySolver2d::configure(
@@ -20,6 +55,7 @@ void DummySolver2d::configure(
 	const nav_msgs::msg::Odometry& odom,
 	const geometry_msgs::msg::Pose& pose)
 {
+	// TODO - Generate particle filter, sample the map randomly or use initial pose?
 	node_ = node;
 	motionSampler_ = motionSampler;
 	matcher_ = matcher
