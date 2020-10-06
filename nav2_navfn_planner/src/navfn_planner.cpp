999b2ceba311cf1d9bd080611d857cb2fc03cd16
Justin <jkearns@irobot.com>
2020-10-06 10:09:45 -0700

    Improve readability

--- nav2_navfn_planner/src/navfn_planner.cpp
+++ nav2_navfn_planner/src/navfn_planner.cpp
@@ -320,7 +320,9 @@ NavfnPlanner::getPlanFromPotential(
 
   planner_->setStart(map_goal);
 
-  int path_len = planner_->calcPath((costmap_->getSizeInCellsX() >= costmap_->getSizeInCellsY()) ? (costmap_->getSizeInCellsX() * 4) : (costmap_->getSizeInCellsY() * 4));
+  const int& max_cycles = (costmap_->getSizeInCellsX() >= costmap_->getSizeInCellsY()) ? (costmap_->getSizeInCellsX() * 4) : (costmap_->getSizeInCellsY() * 4);
+
+  int path_len = planner_->calcPath(max_cycles);
   if (path_len == 0) {
     return false;
   }
