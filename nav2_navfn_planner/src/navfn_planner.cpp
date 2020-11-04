96b47139d3c10f3847c66fe6bf130152dfd1a038
Steve Macenski <stevenmacenski@gmail.com>
2020-11-04 15:54:15 -0800

    Foxy sync 4 - SMAC planner, amongst others (#2072)
    * initialize variables in inflation layer (#1970)
    
    * Fix zero waypoints crash (#1978)
    
    * return if the number of waypoints is zero.
    
    * terminate the action.
    
    * Succeed action instead of terminating.
    
    * Add IsBatteryLow condition node (#1974)
    
    * Add IsBatteryLow condition node
    
    * Update default battery topic and switch to battery %
    
    * Fix test
    
    * Switch to sensor_msgs/BatteryState
    
    * Add option to use voltage by default or switch to percentage
    
    * Add sensor_msgs dependency in package.xml
    
    * Make percentage default over voltage
    
    * Update parameter list
    
    * Initialize inflate_cone_ variable. (#1988)
    
    * Initialize inflate_cone_ variable.
    
    * initialize inflate_cone_ based on parameter.
    
    * Increase the sleep time in the tests makes the costmap test always succeed on my machine.
    
    * Add timeouts to all spin_until_future_complete calls (#1998)
    
    * Add timeouts to all spin_until_future_complete calls
    
    Signed-off-by: Sarthak Mittal <sarthakmittal2608@gmail.com>
    
    * Update default timeout value
    
    Signed-off-by: Sarthak Mittal <sarthakmittal2608@gmail.com>
    
    * Controllers should not be influenced by time jumps or slew (#2012)
    
    * Controllers should not be influenced by time jumps
    
    Therefore use rclcpp::GenericRate<std::chrono::steady_clock> instead of
    rclcpp::Rate
    
    Signed-off-by: Martijn Buijs <martijn.buijs@gmail.com>
    
    * Change to using `rclcpp::WallRate` for better readability
    
    Signed-off-by: Martijn Buijs <martijn.buijs@gmail.com>
    
    * Fix max path cycles for case where map has larger Y dimension than X dimension (#2017)
    
    * Fix max path cycles for case where map has larger Y dimension than X dimension
    
    * Improve readability
    
    * fix ament_cpplint and ament_uncrustify issues
    
    * fix minor cherry pick conflict mistake
    
    * bump version to 0.4.4
    
    Co-authored-by: Michael Ferguson <mfergs7@gmail.com>
    Co-authored-by: Wilco Bonestroo <w.j.bonestroo@saxion.nl>
    Co-authored-by: Sarthak Mittal <sarthakmittal2608@gmail.com>
    Co-authored-by: Martijn Buijs <Martijn.buijs@gmail.com>
    Co-authored-by: justinIRBT <69175069+justinIRBT@users.noreply.github.com>

--- nav2_navfn_planner/src/navfn_planner.cpp
+++ nav2_navfn_planner/src/navfn_planner.cpp
@@ -331,7 +331,10 @@ NavfnPlanner::getPlanFromPotential(
 
   planner_->setStart(map_goal);
 
-  int path_len = planner_->calcPath(costmap_->getSizeInCellsX() * 4);
+  const int & max_cycles = (costmap_->getSizeInCellsX() >= costmap_->getSizeInCellsY()) ?
+    (costmap_->getSizeInCellsX() * 4) : (costmap_->getSizeInCellsY() * 4);
+
+  int path_len = planner_->calcPath(max_cycles);
   if (path_len == 0) {
     return false;
   }
