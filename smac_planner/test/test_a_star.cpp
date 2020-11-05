12b34bed469d32217401270eecb9ceba007079ae
stevemacenski <stevenmacenski@gmail.com>
2020-11-05 14:50:55 -0800

    update test values

--- smac_planner/test/test_a_star.cpp
+++ smac_planner/test/test_a_star.cpp
@@ -153,7 +153,7 @@ TEST(AStarTest, test_a_star_se2)
   EXPECT_TRUE(a_star.createPath(path, num_it, tolerance));
 
   // check path is the right size and collision free
-  EXPECT_EQ(num_it, 61);
+  EXPECT_EQ(num_it, 44);
   EXPECT_EQ(path.size(), 75u);
   for (unsigned int i = 0; i != path.size(); i++) {
     EXPECT_EQ(costmapA->getCost(path[i].x, path[i].y), 0);
