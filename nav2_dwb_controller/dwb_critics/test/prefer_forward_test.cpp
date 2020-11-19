184050eced7c7b59e1864d3a57a34ea97efe1703
Wilco Bonestroo <w.j.bonestroo@saxion.nl>
2020-11-19 20:09:35 +0100

    Use variables for parameters and add test for non-default values.

--- nav2_dwb_controller/dwb_critics/test/prefer_forward_test.cpp
+++ nav2_dwb_controller/dwb_critics/test/prefer_forward_test.cpp
@@ -44,6 +44,12 @@
 #include "dwb_critics/prefer_forward.hpp"
 #include "nav2_costmap_2d/costmap_2d.hpp"
 #include "nav2_util/lifecycle_node.hpp"
+#include "nav2_util/node_utils.hpp"
+
+static constexpr double default_penalty = 1.0;
+static constexpr double default_strafe_x = 0.1;
+static constexpr double default_strafe_theta = 0.2;
+static constexpr double default_theta_scale = 10.0;
 
 TEST(PreferForward, StartNode)
 {
@@ -63,10 +69,11 @@ TEST(PreferForward, StartNode)
 
   // rclcpp::spin_some(node->get_node_base_interface());
 
-  EXPECT_EQ(node->get_parameter(ns + "." + name + ".penalty").as_double(), 1.0);
-  EXPECT_EQ(node->get_parameter(ns + "." + name + ".strafe_x").as_double(), 0.1);
-  EXPECT_EQ(node->get_parameter(ns + "." + name + ".strafe_theta").as_double(), 0.2);
-  EXPECT_EQ(node->get_parameter(ns + "." + name + ".theta_scale").as_double(), 10.0);
+  EXPECT_EQ(node->get_parameter(ns + "." + name + ".penalty").as_double(), default_penalty);
+  EXPECT_EQ(node->get_parameter(ns + "." + name + ".strafe_x").as_double(), default_strafe_x);
+  EXPECT_EQ(
+    node->get_parameter(ns + "." + name + ".strafe_theta").as_double(), default_strafe_theta);
+  EXPECT_EQ(node->get_parameter(ns + "." + name + ".theta_scale").as_double(), default_theta_scale);
 }
 
 TEST(PreferForward, NegativeVelocityX)
@@ -76,19 +83,19 @@ TEST(PreferForward, NegativeVelocityX)
 
   // score must be equal to the penalty (1.0) for any negative x velocity
   trajectory.velocity.x = -1.0;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.x = -0.00001;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.x = -0.1;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.x = std::numeric_limits<double>::lowest();
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.x = -std::numeric_limits<double>::min();
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 }
 
 TEST(PreferForward, Strafe)
@@ -100,19 +107,19 @@ TEST(PreferForward, Strafe)
   // and theta is between -0.2 and 0.2
   trajectory.velocity.x = 0.05;
   trajectory.velocity.theta = -0.1;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.x = 0.0999999;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.x = 0.000001;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.theta = -0.19;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 
   trajectory.velocity.theta = 0.19;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), default_penalty);
 }
 
 TEST(PreferForward, Normal)
@@ -123,19 +130,93 @@ TEST(PreferForward, Normal)
   // score must be equal to the theta * scaling factor (10.0)
   trajectory.velocity.x = 0.2;
   trajectory.velocity.theta = -0.1;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.1 * 10.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.1 * default_theta_scale);
 
   trajectory.velocity.theta = 0.1;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.1 * 10.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.1 * default_theta_scale);
 
   trajectory.velocity.theta = -0.2;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.2 * 10.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.2 * default_theta_scale);
 
   trajectory.velocity.theta = 0.2;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.2 * 10.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.2 * default_theta_scale);
 
   trajectory.velocity.theta = 1.5;
-  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.5 * 10.0);
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.5 * default_theta_scale);
+}
+
+TEST(PreferForward, NoneDefaultValues)
+{
+  auto critic = std::make_shared<dwb_critics::PreferForwardCritic>();
+  auto costmap_ros = std::make_shared<nav2_costmap_2d::Costmap2DROS>("test_global_costmap");
+  auto node = nav2_util::LifecycleNode::make_shared("costmap_tester");
+  node->configure();
+  node->activate();
+
+  double penalty = 18.3;
+  double strafe_x = 0.5;
+  double strafe_theta = 0.4;
+  double theta_scale = 15.0;
+
+  std::string name = "test";
+  std::string ns = "ns";
+  nav2_util::declare_parameter_if_not_declared(
+    node, ns + "." + name + ".penalty",
+    rclcpp::ParameterValue(penalty));
+  nav2_util::declare_parameter_if_not_declared(
+    node, ns + "." + name + ".strafe_x",
+    rclcpp::ParameterValue(strafe_x));
+  nav2_util::declare_parameter_if_not_declared(
+    node, ns + "." + name + ".strafe_theta",
+    rclcpp::ParameterValue(strafe_theta));
+  nav2_util::declare_parameter_if_not_declared(
+    node, ns + "." + name + ".theta_scale",
+    rclcpp::ParameterValue(theta_scale));
+
+  critic->initialize(node, name, ns, costmap_ros);
+  // rclcpp::spin_some(node->get_node_base_interface());
+  critic->onInit();
+
+  dwb_msgs::msg::Trajectory2D trajectory;
+  trajectory.velocity.x = 0.05;
+  trajectory.velocity.theta = -0.1;
+
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), penalty);
+
+  // score must be equal to the penalty when x vel is lower than strafe_x
+  // and theta is between -strafe_theta and strafe_theta
+  trajectory.velocity.x = 0.4;
+  trajectory.velocity.theta = -0.39;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), penalty);
+
+  trajectory.velocity.x = 0.0999999;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), penalty);
+
+  trajectory.velocity.x = 0.000001;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), penalty);
+
+  trajectory.velocity.theta = -0.09999;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), penalty);
+
+  trajectory.velocity.theta = 0.09999;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), penalty);
+
+  // score must be equal to the theta * scaling factor (10.0)
+  trajectory.velocity.x = 0.5;
+  trajectory.velocity.theta = -0.1;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.1 * theta_scale);
+
+  trajectory.velocity.theta = 0.1;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.1 * theta_scale);
+
+  trajectory.velocity.theta = -0.2;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.2 * theta_scale);
+
+  trajectory.velocity.theta = 0.2;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 0.2 * theta_scale);
+
+  trajectory.velocity.theta = 1.5;
+  EXPECT_EQ(critic->scoreTrajectory(trajectory), 1.5 * theta_scale);
 }
 
 int main(int argc, char ** argv)
