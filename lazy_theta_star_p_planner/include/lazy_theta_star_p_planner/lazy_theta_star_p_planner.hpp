d2326b74d4c009fcc229ea22af551043105e03ef
Anshu-man567 <56119116+Anshu-man567@users.noreply.github.com>
2021-01-14 03:31:43 +0530

    Replace the old files
    - the structure of code has been changed
    - new functions have been added, namely : getTraversalCost, getEuclideanCost, getCellCost, isSafe[it is now an overloaded function]
    - documentation added for variables and functions
    - the parameters for the planner now consists of : how_many_corners, costmap_tolerance, euc_tolerance (documentation to added soon)
    - fixed a bug where the incorrect traversal cost of the node was taken

---
+++ lazy_theta_star_p_planner/include/lazy_theta_star_p_planner/lazy_theta_star_p_planner.hpp
@@ -0,0 +1,83 @@
+// Copyright 2020 Anshumaan Singh
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LAZY_THETA_STAR_P_PLANNER__LAZY_THETA_STAR_P_PLANNER_HPP_
+#define LAZY_THETA_STAR_P_PLANNER__LAZY_THETA_STAR_P_PLANNER_HPP_
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <queue>
+#include <algorithm>
+#include <memory>
+#include <vector>
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_core/global_planner.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_util/robot_utils.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_costmap_2d/cost_values.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "lazy_theta_star_p_planner/lazy_theta_star_p.hpp"
+
+namespace lazyThetaStarP_Planner
+{
+
+class LazyThetaStarP_Planner : public nav2_core::GlobalPlanner
+{
+public:
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  nav2_util::LifecycleNode::SharedPtr node_;
+
+  std::string globalFrame_, name_;
+
+  void configure(
+    rclcpp_lifecycle::LifecycleNode::SharedPtr parent,
+    std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmapRos) override;
+
+  void cleanup() override;
+
+  void activate() override;
+
+  void deactivate() override;
+
+  nav_msgs::msg::Path createPlan(
+    const geometry_msgs::msg::PoseStamped & start,
+    const geometry_msgs::msg::PoseStamped & goal) override;
+
+private:
+  std::unique_ptr<lazyThetaStarP::LazyThetaStarP> planner_;
+
+  void getPlan(nav_msgs::msg::Path & global_path);
+
+  void setStartAndGoal(
+    const geometry_msgs::msg::PoseStamped & start,
+    const geometry_msgs::msg::PoseStamped & goal);
+
+  /**
+   * @brief linearily interpolates between the adjacent waypoints of the path
+   * @param raw_path is used to send in the path recieved from the planner
+   * @param dist_bw_points is used to send in the interpolation_resolution
+   * @return the final path with waypoints at a distance of the value of interpolation_resolution of each other
+   */
+  static nav_msgs::msg::Path linearInterpolation(
+    const std::vector<coordsW> & raw_path,
+    const double & dist_bw_points);
+};
+}   //  namespace lazyThetaStarP_Planner
+
+#endif  //  LAZY_THETA_STAR_P_PLANNER__LAZY_THETA_STAR_P_PLANNER_HPP_
