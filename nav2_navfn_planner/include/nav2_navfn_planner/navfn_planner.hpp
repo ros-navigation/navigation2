ff81dafaa23f4dbf89f4f2cdc44eb8ecb9b651a1
G.Doisy <doisyg@users.noreply.github.com>
2021-02-25 20:58:50 +0100

    [NavFn] Make the 3 parameters changeable at runtime (#2181)
    * make the 3 params changeable at runtime
    
    * use parameter events callbacks
    
    * doxygen
    
    * lint

--- nav2_navfn_planner/include/nav2_navfn_planner/navfn_planner.hpp
+++ nav2_navfn_planner/include/nav2_navfn_planner/navfn_planner.hpp
@@ -213,6 +213,16 @@ protected:
 
   // Whether to use the astar planner or default dijkstras
   bool use_astar_;
+
+  // Subscription for parameter change
+  rclcpp::AsyncParametersClient::SharedPtr parameters_client_;
+  rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr parameter_event_sub_;
+
+  /**
+   * @brief Callback executed when a paramter change is detected
+   * @param event ParameterEvent message
+   */
+  void on_parameter_event_callback(const rcl_interfaces::msg::ParameterEvent::SharedPtr event);
 };
 
 }  // namespace nav2_navfn_planner
