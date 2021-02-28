f1d791ff9d56b1aa9c852e81c9f21d483611756f
ShivamPR21 <pandeyshivam2017robotics@gmail.com>
2021-02-28 22:19:00 +0530

    Python launch import sequence changed.
    Signed-off-by: ShivamPR21 <pandeyshivam2017robotics@gmail.com>

--- nav2_map_server/examples/multi_map_saver_node/multi_3d_map_saver.launch.py
+++ nav2_map_server/examples/multi_map_saver_node/multi_3d_map_saver.launch.py
@@ -16,8 +16,8 @@
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
