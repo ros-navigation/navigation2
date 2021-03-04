c9cda8929ed70155ea974cae8f8c46928a557d36
Brice <brice.renaudeau@wyca.fr>
2021-03-04 19:14:17 +0100

    unit test to do a service call in a topic callback

--- nav2_util/test/test_service_client.cpp
+++ nav2_util/test/test_service_client.cpp
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <memory>
 #include <string>
 #include "nav2_util/service_client.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_srvs/srv/empty.hpp"
+#include "std_msgs/msg/empty.hpp"
 #include "gtest/gtest.h"
 
 using nav2_util::ServiceClient;
@@ -47,4 +49,36 @@ TEST(ServiceClient, can_ServiceClient_use_passed_in_node)
   TestServiceClient t("bar", node);
   ASSERT_EQ(t.getNode(), node);
   ASSERT_EQ(t.name(), "test_node");
+}
+
+TEST(ServiceClient, can_ServiceClient_invoke_in_callback)
+{
+  int a = 0;
+  auto service_node = rclcpp::Node::make_shared("service_node");
+  service_node->create_service<std_srvs::srv::Empty>(
+    "empty_srv",
+    [&a](std_srvs::srv::Empty::Request::SharedPtr, std_srvs::srv::Empty::Response::SharedPtr) {
+      a = 1;
+    });
+  auto srv_thread = std::thread([&]() {rclcpp::spin(service_node);});
+
+  auto pub_node = rclcpp::Node::make_shared("pub_node");
+  auto pub = pub_node->create_publisher<std_msgs::msg::Empty>(
+    "empty_topic",
+    rclcpp::QoS(10).transient_local());
+  pub->publish(std_msgs::msg::Empty());
+  rclcpp::spin_some(pub_node);
+
+  auto node = rclcpp::Node::make_shared("test_node");
+  TestServiceClient t("empty_srv", node);
+  node->create_subscription<std_msgs::msg::Empty>(
+    "empty_topic",
+    rclcpp::QoS(10),
+    [&t](std_msgs::msg::Empty::SharedPtr) {
+      auto req = std::make_shared<std_srvs::srv::Empty::Request>();
+      auto res = t.invoke(req);
+    });
+  rclcpp::spin_some(node);
+
+  ASSERT_EQ(a, 1);
 }
