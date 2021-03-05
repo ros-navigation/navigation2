087d1374acf0ac216e8f5924e1f4f3d48941264d
G.Doisy <doisyg@users.noreply.github.com>
2021-03-05 19:26:24 +0100

    Add ClearAroundRobot and ClearExceptRegion action bt (#2204)
    * costmap bt
    
    * bt double type
    
    * change bt actions name to be consistent with service names
    
    * add tests
    
    * code style
    
    * readme update
    
    * improve readme

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/clear_costmap_service.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/clear_costmap_service.hpp
@@ -19,6 +19,8 @@
 
 #include "nav2_behavior_tree/bt_service_node.hpp"
 #include "nav2_msgs/srv/clear_entire_costmap.hpp"
+#include "nav2_msgs/srv/clear_costmap_around_robot.hpp"
+#include "nav2_msgs/srv/clear_costmap_except_region.hpp"
 
 namespace nav2_behavior_tree
 {
@@ -31,6 +33,47 @@ public:
     const BT::NodeConfiguration & conf);
 
   void on_tick() override;
+};
+
+class ClearCostmapExceptRegionService
+  : public BtServiceNode<nav2_msgs::srv::ClearCostmapExceptRegion>
+{
+public:
+  ClearCostmapExceptRegionService(
+    const std::string & service_node_name,
+    const BT::NodeConfiguration & conf);
+
+  void on_tick() override;
+
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts(
+      {
+        BT::InputPort<double>(
+          "reset_distance", 1,
+          "Distance from the robot above which obstacles are cleared")
+      });
+  }
+};
+
+class ClearCostmapAroundRobotService : public BtServiceNode<nav2_msgs::srv::ClearCostmapAroundRobot>
+{
+public:
+  ClearCostmapAroundRobotService(
+    const std::string & service_node_name,
+    const BT::NodeConfiguration & conf);
+
+  void on_tick() override;
+
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts(
+      {
+        BT::InputPort<double>(
+          "reset_distance", 1,
+          "Distance from the robot under which obstacles are cleared")
+      });
+  }
 };
 
 }  // namespace nav2_behavior_tree
