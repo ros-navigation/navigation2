febb5fe838bfa3633246a17fa0ce2d8a8d330084
BriceRenaudeau <48433002+BriceRenaudeau@users.noreply.github.com>
2021-03-09 20:21:34 +0100

    ServiceClient use callback group (#2216)
    * change timeout default value
    
    * create and use callback group
    
    * remove generate_internal_node option
    
    * unit test to do a service call in a topic callback
    
    * fix unit test
    
    * set default value to -1

--- nav2_util/include/nav2_util/service_client.hpp
+++ nav2_util/include/nav2_util/service_client.hpp
@@ -17,7 +17,6 @@
 
 #include <string>
 
-#include "nav2_util/node_utils.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 namespace nav2_util
@@ -29,22 +28,17 @@ class ServiceClient
 public:
   explicit ServiceClient(
     const std::string & service_name,
-    const rclcpp::Node::SharedPtr & provided_node = rclcpp::Node::SharedPtr())
-  : service_name_(service_name)
+    const rclcpp::Node::SharedPtr & provided_node)
+  : service_name_(service_name), node_(provided_node)
   {
-    if (provided_node) {
-      node_ = provided_node;
-    } else {
-      node_ = generate_internal_node(service_name + "_Node");
-    }
-    client_ = node_->create_client<ServiceT>(service_name);
-  }
-
-  ServiceClient(const std::string & service_name, const std::string & parent_name)
-  : service_name_(service_name)
-  {
-    node_ = generate_internal_node(parent_name + std::string("_") + service_name + "_client");
-    client_ = node_->create_client<ServiceT>(service_name);
+    callback_group_ = node_->create_callback_group(
+      rclcpp::CallbackGroupType::MutuallyExclusive,
+      false);
+    callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
+    client_ = node_->create_client<ServiceT>(
+      service_name,
+      rmw_qos_profile_services_default,
+      callback_group_);
   }
 
   using RequestType = typename ServiceT::Request;
@@ -52,7 +46,7 @@ public:
 
   typename ResponseType::SharedPtr invoke(
     typename RequestType::SharedPtr & request,
-    const std::chrono::nanoseconds timeout = std::chrono::nanoseconds::max())
+    const std::chrono::nanoseconds timeout = std::chrono::nanoseconds(-1))
   {
     while (!client_->wait_for_service(std::chrono::seconds(1))) {
       if (!rclcpp::ok()) {
@@ -69,7 +63,7 @@ public:
       service_name_.c_str());
     auto future_result = client_->async_send_request(request);
 
-    if (rclcpp::spin_until_future_complete(node_, future_result, timeout) !=
+    if (callback_group_executor_.spin_until_future_complete(future_result, timeout) !=
       rclcpp::FutureReturnCode::SUCCESS)
     {
       throw std::runtime_error(service_name_ + " service client: async_send_request failed");
@@ -97,7 +91,7 @@ public:
       service_name_.c_str());
     auto future_result = client_->async_send_request(request);
 
-    if (rclcpp::spin_until_future_complete(node_, future_result) !=
+    if (callback_group_executor_.spin_until_future_complete(future_result) !=
       rclcpp::FutureReturnCode::SUCCESS)
     {
       return false;
@@ -122,6 +116,8 @@ public:
 protected:
   std::string service_name_;
   rclcpp::Node::SharedPtr node_;
+  rclcpp::CallbackGroup::SharedPtr callback_group_;
+  rclcpp::executors::SingleThreadedExecutor callback_group_executor_;
   typename rclcpp::Client<ServiceT>::SharedPtr client_;
 };
 
