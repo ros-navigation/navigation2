de9ac209dbf7bf95a02f0e6a2d643f82959bcbdd
Steve Macenski <stevenmacenski@gmail.com>
2021-03-11 14:33:57 -0800

    new behavior tree node for triggering just once

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -55,7 +55,8 @@ BtNavigator::BtNavigator()
     "nav2_round_robin_node_bt_node",
     "nav2_transform_available_condition_bt_node",
     "nav2_time_expired_condition_bt_node",
-    "nav2_distance_traveled_condition_bt_node"
+    "nav2_distance_traveled_condition_bt_node",
+    "nav2_single_trigger_bt_node"
   };
 
   declare_parameter("plugin_lib_names", plugin_libs);
