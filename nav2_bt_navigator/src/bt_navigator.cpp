834416819f880412cb062f0ef30ff8d30fcc20d7
pabloinigoblasco <pablo@ibrobotics.com>
2021-03-24 00:04:33 +0100

    Controller selector

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -57,7 +57,8 @@ BtNavigator::BtNavigator()
     "nav2_time_expired_condition_bt_node",
     "nav2_distance_traveled_condition_bt_node",
     "nav2_single_trigger_bt_node",
-    "nav2_planner_selector_bt_node"
+    "nav2_planner_selector_bt_node",
+    "nav2_controller_selector_bt_node"
   };
 
   declare_parameter("plugin_lib_names", plugin_libs);
