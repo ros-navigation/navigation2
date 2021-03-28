afac49ae63c39998dbe99a8e2f3a949c08a23ed6
Sachin <sachin.guruswamy@gmail.com>
2021-03-28 10:47:40 -0700

    added subscribers for voxel marked and cost_cloud
    Signed-off-by: Sachin <sachin.guruswamy@gmail.com>

--- nav2_system_tests/src/costmap_filters/tester_node.py
+++ nav2_system_tests/src/costmap_filters/tester_node.py
@@ -96,16 +96,16 @@ class NavTester(Node):
         self.goal_pub = self.create_publisher(PoseStamped, 'goal_pose', 10)
 
         transient_local_qos = QoSProfile(
-          durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
-          reliability=QoSReliabilityPolicy.RELIABLE,
-          history=QoSHistoryPolicy.KEEP_LAST,
-          depth=1)
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1)
 
         volatile_qos = QoSProfile(
-          durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE,
-          reliability=QoSReliabilityPolicy.RELIABLE,
-          history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
-          depth=1)
+            durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE,
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+            depth=1)
 
         self.model_pose_sub = self.create_subscription(PoseWithCovarianceStamped,
                                                        'amcl_pose', self.poseCallback,
@@ -117,11 +117,25 @@ class NavTester(Node):
         self.test_type = test_type
         self.filter_test_result = True
         self.clearing_endpoints_received = False
+        self.voxel_marked_received = False
+        self.voxel_unknown_received = False
+        self.cost_cloud_received = False
 
         if self.test_type == TestType.KEEPOUT:
             self.plan_sub = self.create_subscription(Path, 'plan',
                                                      self.planCallback, volatile_qos)
-            
+            self.voxel_marked_sub = self.create_subscription(PointCloud2,
+                                                             'voxel_marked_cloud',
+                                                             self.voxelMarkedCallback,
+                                                             1)
+            self.voxel_unknown_sub = self.create_subscription(PointCloud2,
+                                                              'voxel_unknown_cloud',
+                                                              self.voxelUnknownCallback,
+                                                              1)
+            self.cost_cloud_sub = self.create_subscription(PointCloud2,
+                                                           'cost_cloud',
+                                                           self.dwbCostCloudCallback,
+                                                           1)
         elif self.test_type == TestType.SPEED:
             self.speed_it = 0
             # Expected chain of speed limits
@@ -259,6 +273,21 @@ class NavTester(Node):
         if len(msg.data) > 0:
             self.clearing_endpoints_received = True
 
+    def voxelMarkedCallback(self, msg):
+        self.info_msg('Received Voxel Marked Points')
+        if len(msg.data) > 0:
+            self.voxel_marked_received = True
+
+    def voxelUnknownCallback(self, msg):
+        self.info_msg('Received Voxel unknown points')
+        if len(msg.data) > 0:
+            self.voxel_unknown_received = True
+
+    def dwbCostCloudCallback(self, msg):
+        self.info_msg('Received cost_cloud points')
+        if len(msg.data) > 0:
+            self.cost_cloud_received = True
+
     def speedLimitCallback(self, msg):
         self.info_msg('Received speed limit: ' + str(msg.speed_limit))
         self.checkSpeed(self.speed_it, msg.speed_limit)
@@ -283,10 +312,23 @@ class NavTester(Node):
     def wait_for_clearning_endpoints(self, timeout):
         start_time = time.time()
         while not self.clearing_endpoints_received:
-            self.info_msg('Waiting for clearing_endpoints msg to be received ...')
+            self.info_msg(
+                'Waiting for clearing_endpoints msg to be received ...')
             rclpy.spin_once(self, timeout_sec=1)
             if (time.time() - start_time) > timeout:
                 self.error_msg('Time out to waiting for clearing_endpoints')
+                return False
+        return True
+
+    def wait_for_voxel_pointcloud(self, timeout):
+        start_time = time.time()
+        while not self.voxel_unknown_received or not self.voxel_marked_received:
+            self.info_msg(
+                'Waiting for voxel_unknown_received/voxel_marked_received msg to be received ...')
+            rclpy.spin_once(self, timeout_sec=1)
+            if (time.time() - start_time) > timeout:
+                self.error_msg(
+                    'Time out to waiting for voxel_unknown_received/voxel_marked_received')
                 return False
         return True
 
@@ -329,7 +371,8 @@ class NavTester(Node):
                 state = future.result().current_state.label
                 self.info_msg('Result of get_state: %s' % state)
             else:
-                self.error_msg('Exception while calling service: %r' % future.exception())
+                self.error_msg('Exception while calling service: %r' %
+                               future.exception())
             time.sleep(5)
 
     def shutdown(self):
@@ -337,9 +380,11 @@ class NavTester(Node):
         self.action_client.destroy()
 
         transition_service = 'lifecycle_manager_navigation/manage_nodes'
-        mgr_client = self.create_client(ManageLifecycleNodes, transition_service)
+        mgr_client = self.create_client(
+            ManageLifecycleNodes, transition_service)
         while not mgr_client.wait_for_service(timeout_sec=1.0):
-            self.info_msg(transition_service + ' service not available, waiting...')
+            self.info_msg(transition_service +
+                          ' service not available, waiting...')
 
         req = ManageLifecycleNodes.Request()
         req.command = ManageLifecycleNodes.Request().SHUTDOWN
@@ -348,13 +393,16 @@ class NavTester(Node):
             self.info_msg('Shutting down navigation lifecycle manager...')
             rclpy.spin_until_future_complete(self, future)
             future.result()
-            self.info_msg('Shutting down navigation lifecycle manager complete.')
+            self.info_msg(
+                'Shutting down navigation lifecycle manager complete.')
         except Exception as e:  # noqa: B902
             self.error_msg('Service call failed %r' % (e,))
         transition_service = 'lifecycle_manager_localization/manage_nodes'
-        mgr_client = self.create_client(ManageLifecycleNodes, transition_service)
+        mgr_client = self.create_client(
+            ManageLifecycleNodes, transition_service)
         while not mgr_client.wait_for_service(timeout_sec=1.0):
-            self.info_msg(transition_service + ' service not available, waiting...')
+            self.info_msg(transition_service +
+                          ' service not available, waiting...')
 
         req = ManageLifecycleNodes.Request()
         req.command = ManageLifecycleNodes.Request().SHUTDOWN
@@ -363,7 +411,8 @@ class NavTester(Node):
             self.info_msg('Shutting down localization lifecycle manager...')
             rclpy.spin_until_future_complete(self, future)
             future.result()
-            self.info_msg('Shutting down localization lifecycle manager complete')
+            self.info_msg(
+                'Shutting down localization lifecycle manager complete')
         except Exception as e:  # noqa: B902
             self.error_msg('Service call failed %r' % (e,))
 
@@ -406,10 +455,12 @@ def run_all_tests(robot_tester):
         robot_tester.wait_for_initial_pose()
         result = robot_tester.wait_for_clearning_endpoints(10)
         robot_tester.wait_for_node_active('bt_navigator')
-        # result = robot_tester.wait_for_filter_mask(10)
         result = result and robot_tester.wait_for_filter_mask(10)
     if (result):
         result = robot_tester.runNavigateAction()
+
+    if robot_tester.test_type == TestType.KEEPOUT:
+        result = result and robot_tester.wait_for_voxel_pointcloud(10)
 
     if (result):
         result = test_RobotMovesToGoal(robot_tester)
@@ -417,6 +468,7 @@ def run_all_tests(robot_tester):
     if (result):
         if robot_tester.test_type == TestType.KEEPOUT:
             result = robot_tester.filter_test_result
+            result = result and robot_tester.filter_test_result
         elif robot_tester.test_type == TestType.SPEED:
             result = test_SpeedLimitsAllCorrect(robot_tester)
 
@@ -463,7 +515,8 @@ def get_tester(args):
 
 def main(argv=sys.argv[1:]):
     # The robot(s) positions from the input arguments
-    parser = argparse.ArgumentParser(description='System-level costmap filters tester node')
+    parser = argparse.ArgumentParser(
+        description='System-level costmap filters tester node')
     parser.add_argument('-t', '--type', type=str, action='store', dest='type',
                         help='Type of costmap filter being tested.')
     group = parser.add_mutually_exclusive_group(required=True)
