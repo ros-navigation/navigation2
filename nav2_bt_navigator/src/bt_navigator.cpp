8c03c4cad29e843a4ce3f3e546dfac32405a2036
Deepak Talwar <deepaktalwardt@farmwise.io>
2021-03-31 18:29:14 -0700

    added try catch and template to blackboard get path
    Signed-off-by: Deepak Talwar <deepaktalwardt@farmwise.io>

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -220,49 +220,54 @@ BtNavigator::onLoop()
 
   auto blackboard = bt_action_server_->getBlackboard();
 
-  // Get current path points
-  nav_msgs::msg::Path current_path;
-  blackboard->get("path", current_path);
+  try {
+    // Get current path points
+    nav_msgs::msg::Path current_path;
+    blackboard->get<nav_msgs::msg::Path>("path", current_path);
 
-  // Find the closest pose to current pose on global path
-  auto find_closest_pose_idx =
-    [&current_pose, &current_path]() {
-      size_t closest_pose_idx = 0;
-      double curr_min_dist = std::numeric_limits<double>::max();
-      for (size_t curr_idx = 0; curr_idx < current_path.poses.size(); ++curr_idx) {
-        double curr_dist = nav2_util::geometry_utils::euclidean_distance(
-          current_pose, current_path.poses[curr_idx]);
-        if (curr_dist < curr_min_dist) {
-          curr_min_dist = curr_dist;
-          closest_pose_idx = curr_idx;
+    // Find the closest pose to current pose on global path
+    auto find_closest_pose_idx =
+      [&current_pose, &current_path]() {
+        size_t closest_pose_idx = 0;
+        double curr_min_dist = std::numeric_limits<double>::max();
+        for (size_t curr_idx = 0; curr_idx < current_path.poses.size(); ++curr_idx) {
+          double curr_dist = nav2_util::geometry_utils::euclidean_distance(
+            current_pose, current_path.poses[curr_idx]);
+          if (curr_dist < curr_min_dist) {
+            curr_min_dist = curr_dist;
+            closest_pose_idx = curr_idx;
+          }
         }
-      }
-      return closest_pose_idx;
-    };
+        return closest_pose_idx;
+      };
 
-  // Calculate distance on the path
-  double distance_remaining =
-    nav2_util::geometry_utils::calculate_path_length(current_path, find_closest_pose_idx());
+    // Calculate distance on the path
+    double distance_remaining =
+      nav2_util::geometry_utils::calculate_path_length(current_path, find_closest_pose_idx());
 
-  // Default value for time remaining
-  rclcpp::Duration estimated_time_remaining = rclcpp::Duration::from_seconds(0.0);
+    // Default value for time remaining
+    rclcpp::Duration estimated_time_remaining = rclcpp::Duration::from_seconds(0.0);
 
-  // Get current speed
-  geometry_msgs::msg::Twist current_odom = odom_smoother_->getTwist();
-  double current_linear_speed = std::hypot(current_odom.linear.x, current_odom.linear.y);
+    // Get current speed
+    geometry_msgs::msg::Twist current_odom = odom_smoother_->getTwist();
+    double current_linear_speed = std::hypot(current_odom.linear.x, current_odom.linear.y);
 
-  // Calculate estimated time taken to goal if speed is higher than 1cm/s
-  // and at least 10cm to go
-  if ((std::abs(current_linear_speed) > 0.01) && (distance_remaining > 0.1)) {
-    estimated_time_remaining =
-      rclcpp::Duration::from_seconds(distance_remaining / std::abs(current_linear_speed));
+    // Calculate estimated time taken to goal if speed is higher than 1cm/s
+    // and at least 10cm to go
+    if ((std::abs(current_linear_speed) > 0.01) && (distance_remaining > 0.1)) {
+      estimated_time_remaining =
+        rclcpp::Duration::from_seconds(distance_remaining / std::abs(current_linear_speed));
+    }
+
+    feedback_msg->current_pose = current_pose;
+    feedback_msg->distance_remaining = distance_remaining;
+    feedback_msg->estimated_time_remaining = estimated_time_remaining;
+  } catch(...) {
+    // Ignore if anything failed
   }
 
   int recovery_count = 0;
   blackboard->get<int>("number_recoveries", recovery_count);
-  feedback_msg->current_pose = current_pose;
-  feedback_msg->distance_remaining = distance_remaining;
-  feedback_msg->estimated_time_remaining = estimated_time_remaining;
   feedback_msg->number_of_recoveries = recovery_count;
   feedback_msg->navigation_time = now() - start_time_;
 
