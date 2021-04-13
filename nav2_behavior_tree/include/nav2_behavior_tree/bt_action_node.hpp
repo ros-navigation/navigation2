d77e5b981a13a367da8d62d25cc7a8c9e75d4d2d
Kazunari Tanaka <tzskp1@gmail.com>
2021-04-13 15:52:27 +0900

    Get rid of unnecessarily check.

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_node.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_node.hpp
@@ -268,7 +268,7 @@ protected:
         // TODO(#1652): a work around until rcl_action interface is updated
         // if goal ids are not matched, the older goal call this callback so ignore the result
         // if matched, it must be processed (including aborted)
-        if (this->goal_handle_ && (this->goal_handle_->get_goal_id() == result.goal_id)) {
+        if (this->goal_handle_->get_goal_id() == result.goal_id) {
           goal_result_available_ = true;
           result_ = result;
         }
