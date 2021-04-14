438e4d4df147db8dfce3ce08ef261c996cd2cc3c
Guillaume Doisy <guillaume.doisy@wyca.fr>
2021-04-14 13:53:43 +0200

    use namespace for service client node creation

--- nav2_lifecycle_manager/src/lifecycle_manager_client.cpp
+++ nav2_lifecycle_manager/src/lifecycle_manager_client.cpp
@@ -30,15 +30,11 @@ LifecycleManagerClient::LifecycleManagerClient(
   const std::string & name,
   std::string namespace_)
 {
-  if (namespace_ != "") {
-    namespace_ += "/";
-  }
-
-  manage_service_name_ = namespace_ + name + std::string("/manage_nodes");
-  active_service_name_ = namespace_ + name + std::string("/is_active");
+  manage_service_name_ = name + std::string("/manage_nodes");
+  active_service_name_ = name + std::string("/is_active");
 
   // Create the node to use for all of the service clients
-  node_ = std::make_shared<rclcpp::Node>(name + "_service_client");
+  node_ = std::make_shared<rclcpp::Node>(name + "_service_client", namespace_);
 
   // Create the service clients
   manager_client_ = node_->create_client<ManageLifecycleNodes>(manage_service_name_);
