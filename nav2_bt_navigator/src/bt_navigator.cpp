057a612a295d7f5056ad72772fdfe7240e9c2e81
Pau Carre <paucarre@cm-robotics.com>
2021-05-02 22:58:03 +0200

    Updated documentation

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -58,7 +58,7 @@ BtNavigator::BtNavigator()
     "nav2_round_robin_node_bt_node",
     "nav2_transform_available_condition_bt_node",
     "nav2_time_expired_condition_bt_node",
-    "nav2_distance_traveled_condition_bt_node"
+    "nav2_distance_traveled_condition_bt_node",
     "nav2_rotate_action_bt_node",
     "nav2_translate_action_bt_node",
     "nav2_is_battery_low_condition_bt_node",
