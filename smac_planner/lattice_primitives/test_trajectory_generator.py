bb8853395a8bb265955e8434db9662f3fa558854
mattbooker <mattbooker97@gmail.com>
2021-05-15 22:47:52 +0800

    Updated tester to match latest trajectory generator
    Signed-off-by: Matthew Booker <mattbooker97@gmail.com>

--- smac_planner/lattice_primitives/test_trajectory_generator.py
+++ smac_planner/lattice_primitives/test_trajectory_generator.py
@@ -3,12 +3,12 @@ from trajectory_generator import TrajectoryGenerator
 import numpy as np
 
 TURNING_RADIUS = 1
-NUMBER_OF_STEPS = 100
+STEP_DISTANCE = 0.1
 
 class TestTrajectoryGenerator(unittest.TestCase):
 
     def setUp(self) -> None:
-        config = {"TrajectoryGenerator":{"turning_radius":TURNING_RADIUS,"number_of_steps":NUMBER_OF_STEPS}}
+        config = {"turning_radius":TURNING_RADIUS,"step_distance":STEP_DISTANCE}
         self.trajectory_generator = TrajectoryGenerator(config)
 
     def test_generate_trajectory_only_arc(self):
@@ -16,28 +16,34 @@ class TestTrajectoryGenerator(unittest.TestCase):
         xs, ys = self.trajectory_generator.generate_trajectory(end_point, 0, 90)
 
         self.assertEqual(len(xs), len(ys))
-        self.assertEqual(len(xs), NUMBER_OF_STEPS)
+        self.assertGreater(len(xs), 0)
+
+        end_point = np.array([1,-1])
+        xs, ys = self.trajectory_generator.generate_trajectory(end_point, 0, -90)
+
+        self.assertEqual(len(xs), len(ys))
+        self.assertGreater(len(xs), 0)
     
     def test_generate_trajectory_only_line(self):
         end_point = np.array([1,1])
         xs, ys = self.trajectory_generator.generate_trajectory(end_point, 45, 45)
 
         self.assertEqual(len(xs), len(ys))
-        self.assertEqual(len(xs), NUMBER_OF_STEPS)
+        self.assertGreater(len(xs), 0)
 
     def test_generate_trajectory_line_to_arc(self):
         end_point = np.array([2,1])
         xs, ys = self.trajectory_generator.generate_trajectory(end_point, 0, 90)
 
         self.assertEqual(len(xs), len(ys))
-        self.assertEqual(len(xs), NUMBER_OF_STEPS)
+        self.assertGreater(len(xs), 0)
 
     def test_generate_trajectory_line_to_end(self):
         end_point = np.array([1,2])
         xs, ys = self.trajectory_generator.generate_trajectory(end_point, 0, 90)
 
         self.assertEqual(len(xs), len(ys))
-        self.assertEqual(len(xs), NUMBER_OF_STEPS)
+        self.assertGreater(len(xs), 0)
 
     def test_generate_trajectory_radius_too_small(self):
         end_point = np.array([.9,.9])
@@ -51,7 +57,7 @@ class TestTrajectoryGenerator(unittest.TestCase):
         xs, ys = self.trajectory_generator.generate_trajectory(end_point, 0, 0)
 
         self.assertEqual(len(xs), len(ys))
-        self.assertEqual(len(xs), NUMBER_OF_STEPS)
+        self.assertGreater(len(xs), 0)
 
     def test_generate_trajectory_parallel_lines_not_coincident(self):
         end_point = np.array([0, 3])
