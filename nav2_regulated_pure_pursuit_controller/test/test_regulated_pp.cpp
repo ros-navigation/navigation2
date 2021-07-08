3a7c439b51031bd7fc5b967eca7294429108abfb
Pradheep-office <padhupradheep@gmail.com>
2021-07-08 15:12:58 +0200

    Feature addition: capability for the RRP to drive the robot backwards

--- nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
+++ nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
@@ -85,11 +85,10 @@ public:
   void applyConstraintsWrapper(
     const double & dist_error, const double & lookahead_dist,
     const double & curvature, const geometry_msgs::msg::Twist & curr_speed,
-    const double & pose_cost, double & linear_vel)
+    const double & pose_cost, double & linear_vel, double & sign)
   {
-    return applyConstraints(
-      dist_error, lookahead_dist, curvature, curr_speed, pose_cost,
-      linear_vel);
+    return applyConstraints(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, 
+      linear_vel, sign);
   }
 };
 
@@ -286,6 +285,7 @@ TEST(RegulatedPurePursuitTest, applyConstraints)
   geometry_msgs::msg::Twist curr_speed;
   double pose_cost = 0.0;
   double linear_vel = 0.0;
+  double sign = 1.0;
 
   // since costmaps here are bogus, we can't access them
   ctrl->resetVelocityApproachScaling();
@@ -293,26 +293,25 @@ TEST(RegulatedPurePursuitTest, applyConstraints)
   // test curvature regulation (default)
   curr_speed.linear.x = 0.25;
   ctrl->applyConstraintsWrapper(
-    dist_error, lookahead_dist, curvature, curr_speed, pose_cost,
-    linear_vel);
+    dist_error, lookahead_dist, curvature, curr_speed, pose_cost, 
+    linear_vel, sign);
   EXPECT_EQ(linear_vel, 0.25);  // min set speed
 
   linear_vel = 1.0;
   curvature = 0.7407;
   curr_speed.linear.x = 0.5;
   ctrl->applyConstraintsWrapper(
-    dist_error, lookahead_dist, curvature, curr_speed, pose_cost,
-    linear_vel);
+    dist_error, lookahead_dist, curvature, curr_speed, pose_cost, 
+    linear_vel, sign);
   EXPECT_NEAR(linear_vel, 0.5, 0.01);  // lower by curvature
 
   linear_vel = 1.0;
   curvature = 1000.0;
   curr_speed.linear.x = 0.25;
   ctrl->applyConstraintsWrapper(
-    dist_error, lookahead_dist, curvature, curr_speed, pose_cost,
-    linear_vel);
+    dist_error, lookahead_dist, curvature, curr_speed, pose_cost, 
+    linear_vel, sign);
   EXPECT_NEAR(linear_vel, 0.25, 0.01);  // min out by curvature
-
 
   // now try with cost regulation (turn off velocity and only cost)
   // ctrl->setCostRegulationScaling();
