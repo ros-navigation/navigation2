7621941ae6bce4cb591345de22853f4c7b0649ec
Tim Clephas <tim.clephas@nobleo.nl>
2021-07-22 07:32:17 +0200

    Revert converting generated files

--- nav2_simple_commander/test/test_flake8.py
+++ nav2_simple_commander/test/test_flake8.py
@@ -21,5 +21,5 @@ import pytest
 def test_flake8():
     rc, errors = main_with_errors(argv=[])
     assert rc == 0, \
-        f'Found {len(errors)} code style errors / warnings:\n' + \
+        'Found %d code style errors / warnings:\n' % len(errors) + \
         '\n'.join(errors)
