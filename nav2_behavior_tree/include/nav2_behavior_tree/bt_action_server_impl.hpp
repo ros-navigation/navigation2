27c20999466f3d6ae4917ce56bbb06d5d89f8d46
anaelle <anaelle.sarazin@wyca.fr>
2021-07-26 09:59:12 +0200

    Code formatting

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
@@ -88,8 +88,9 @@ bool BtActionServer<ActionT>::on_configure()
   // Use suffix '_rclcpp_node' to keep parameter file consistency #1773
   auto options = rclcpp::NodeOptions().arguments(
     {"--ros-args",
-      "-r", std::string("__node:=") + std::string(node->get_name()) + client_node_name + "_rclcpp_node",
-      "--"});
+      "-r",
+      std::string("__node:=") + std::string(node->get_name()) + client_node_name + "_rclcpp_node",
+     "--"});
 
   // Support for handling the topic-based goal pose from rviz
   client_node_ = std::make_shared<rclcpp::Node>("_", options);
