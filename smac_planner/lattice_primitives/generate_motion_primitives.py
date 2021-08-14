b216659cb178c041250fba7aa45e3165521e74e0
mattbooker <mattbooker97@gmail.com>
2021-08-14 13:22:47 +0800

    Added printing of time taken
    Signed-off-by: Matthew Booker <mattbooker97@gmail.com>

--- smac_planner/lattice_primitives/generate_motion_primitives.py
+++ smac_planner/lattice_primitives/generate_motion_primitives.py
@@ -1,3 +1,4 @@
+import time
 import json
 import logging
 from datetime import datetime
@@ -26,15 +27,20 @@ def read_config():
 
 
 def create_header(config, minimal_set_trajectories):
-    header_dict = {"version": VERSION, "date_generated": datetime.today().strftime(
-        '%Y-%m-%d'), "lattice_metadata": dict(), "primitives": []}
+    header_dict = {
+        "version": VERSION,
+        "date_generated": datetime.today().strftime("%Y-%m-%d"),
+        "lattice_metadata": dict(),
+        "primitives": [],
+    }
 
     for key, value in config.items():
         header_dict["lattice_metadata"][key] = value
 
     heading_angles = set([angle for angle in minimal_set_trajectories.keys()])
     header_dict["lattice_metadata"]["heading_angles"] = sorted(
-        list(heading_angles), key=lambda x: (x < 0, x))
+        list(heading_angles), key=lambda x: (x < 0, x)
+    )
 
     return header_dict
 
@@ -47,20 +53,25 @@ def write_to_json(minimal_set_trajectories, config):
     idx = 0
     for start_angle in sorted(trajectory_start_angles, key=lambda x: (x < 0, x)):
 
-        for trajectory in sorted(minimal_set_trajectories[start_angle], key=lambda x: x.parameters.end_angle):
+        for trajectory in sorted(
+            minimal_set_trajectories[start_angle], key=lambda x: x.parameters.end_angle
+        ):
 
             traj_info = dict()
-            traj_info['trajectory_id'] = idx
-            traj_info['start_angle'] = trajectory.parameters.start_angle
-            traj_info['end_angle'] = trajectory.parameters.end_angle
-            traj_info['trajectory_radius'] = trajectory.parameters.turning_radius
-            traj_info['trajectory_length'] = round(
-                trajectory.parameters.total_length, 5)
-            traj_info['arc_length'] = round(
-                trajectory.parameters.arc_length, 5)
-            traj_info['straight_length'] = round(trajectory.parameters.start_to_arc_distance +
-                                                 trajectory.parameters.arc_to_end_distance, 5)
-            traj_info['poses'] = trajectory.path.to_output_format()
+            traj_info["trajectory_id"] = idx
+            traj_info["start_angle"] = trajectory.parameters.start_angle
+            traj_info["end_angle"] = trajectory.parameters.end_angle
+            traj_info["trajectory_radius"] = trajectory.parameters.turning_radius
+            traj_info["trajectory_length"] = round(
+                trajectory.parameters.total_length, 5
+            )
+            traj_info["arc_length"] = round(trajectory.parameters.arc_length, 5)
+            traj_info["straight_length"] = round(
+                trajectory.parameters.start_to_arc_distance
+                + trajectory.parameters.arc_to_end_distance,
+                5,
+            )
+            traj_info["poses"] = trajectory.path.to_output_format()
 
             output_dict["primitives"].append(traj_info)
             idx += 1
@@ -69,7 +80,7 @@ def write_to_json(minimal_set_trajectories, config):
 
     output_path = Path(__file__).parent / config["output_file"]
 
-    with open(output_path, 'w') as output_file:
+    with open(output_path, "w") as output_file:
         json.dump(output_dict, output_file, indent="\t")
 
 
@@ -83,7 +94,7 @@ def save_visualizations(minimal_set_trajectories):
             plt.plot(trajectory.path.xs, trajectory.path.ys, "b")
 
     plt.grid(True)
-    plt.axis('square')
+    plt.axis("square")
     left_x, right_x = plt.xlim()
     left_y, right_y = plt.ylim()
 
@@ -95,7 +106,7 @@ def save_visualizations(minimal_set_trajectories):
 
         angle_in_deg = np.rad2deg(start_angle)
 
-        if start_angle < 0 or start_angle > np.pi/2:
+        if start_angle < 0 or start_angle > np.pi / 2:
             continue
 
         for trajectory in minimal_set_trajectories[start_angle]:
@@ -105,7 +116,7 @@ def save_visualizations(minimal_set_trajectories):
 
         plt.grid(True)
 
-        output_path = visualizations_folder / f'{angle_in_deg}.png'
+        output_path = visualizations_folder / f"{angle_in_deg}.png"
         plt.savefig(output_path)
         plt.clf()
 
@@ -113,8 +124,10 @@ def save_visualizations(minimal_set_trajectories):
 if __name__ == "__main__":
     config = read_config()
 
+    start = time.time()
     lattice_gen = LatticeGenerator(config)
     minimal_set_trajectories = lattice_gen.run()
+    print(f"Finished Generating. Took {time.time() - start} seconds")
 
     write_to_json(minimal_set_trajectories, config)
     save_visualizations(minimal_set_trajectories)
