8316fa2abf0a390f5e516c61598a823fa15682fd
easylyou <38713965+easylyou@users.noreply.github.com>
2021-08-17 12:40:49 +0800

    catch runtime_error if the message from laser is malformed (#2511)
    * catch runtime_error if the message from laser is malformed
    
    * fix styling
    
    Co-authored-by: Kai-Tao Xie <kaitaoxie@qq.com>

--- nav2_costmap_2d/plugins/obstacle_layer.cpp
+++ nav2_costmap_2d/plugins/obstacle_layer.cpp
@@ -291,6 +291,13 @@ ObstacleLayer::laserScanCallback(
       global_frame_.c_str(),
       ex.what());
     projector_.projectLaser(*message, cloud);
+  } catch (std::runtime_error & ex) {
+    RCLCPP_WARN(
+      logger_,
+      "transformLaserScanToPointCloud error, it seems the message from laser is malformed."
+      " Ignore this message. what(): %s",
+      ex.what());
+    return;
   }
 
   // buffer the point cloud
@@ -327,6 +334,13 @@ ObstacleLayer::laserScanValidInfCallback(
       "High fidelity enabled, but TF returned a transform exception to frame %s: %s",
       global_frame_.c_str(), ex.what());
     projector_.projectLaser(message, cloud);
+  } catch (std::runtime_error & ex) {
+    RCLCPP_WARN(
+      logger_,
+      "transformLaserScanToPointCloud error, it seems the message from laser is malformed."
+      " Ignore this message. what(): %s",
+      ex.what());
+    return;
   }
 
   // buffer the point cloud
