19024c242c12ea4212956f11a57503aec5e3ff69
sathak93 <sathak0730@gmail.com>
2021-08-22 07:20:50 +0530

    change to trigger

--- nav2_bringup/launch/multi_tb3_simulation_launch.py
+++ nav2_bringup/launch/multi_tb3_simulation_launch.py
@@ -17,7 +17,7 @@ Example for spawning multiple robots in Gazebo.
 
 This is an example on how to create a launch file for spawning multiple robots into Gazebo
 and launch multiple instances of the navigation stack, each controlling one robot.
-The robots co-exist on a shared environment and are controlled by independent nav stacks
+The robots co-exist on a shared environment and are controlled by independent nav stacks.
 """
 
 import os
