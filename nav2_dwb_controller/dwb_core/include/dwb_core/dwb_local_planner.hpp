ros-navigation/navigation2

commit 3c7afed4fe9a51cfe92dd2e61928cfb5b8f6eeb7

Update dwb_local_planner.hpp (#2533)
Add remarks

nav2_dwb_controller/dwb_core/include/dwb_core/dwb_local_planner.hpp
@@ -73,10 +73,21 @@ public:
 
   virtual ~DWBLocalPlanner() {}
 
+  /**
+   * @brief Activate lifecycle node
+   */
   void activate() override;
+  
+  /**
+   * @brief Deactivate lifecycle node
+   */
   void deactivate() override;
-  void cleanup() override;
 
+  /**
+   * @brief Cleanup lifecycle node
+   */
+  void cleanup() override;
+   
   /**
    * @brief nav2_core setPlan - Sets the global plan
    * @param path The global plan

commit f97d930f08756dfa7e30d42c7911d9da5b0c0d40

Fix linting issue introduced in https://github.com/ros-planning/navigation2/pull/2533

nav2_dwb_controller/dwb_core/include/dwb_core/dwb_local_planner.hpp
@@ -77,7 +77,7 @@ public:
    * @brief Activate lifecycle node
    */
   void activate() override;
-  
+
   /**
    * @brief Deactivate lifecycle node
    */
@@ -87,7 +87,7 @@ public:
    * @brief Cleanup lifecycle node
    */
   void cleanup() override;
-   
+
   /**
    * @brief nav2_core setPlan - Sets the global plan
    * @param path The global plan