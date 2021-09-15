86f9b4a6207d8e33bd46634f4e41550a29b355a2
zhenpeng ge <zhenpeng.ge@qq.com>
2021-09-15 10:26:10 +0800

    Register nodes as components
    Signed-off-by: zhenpeng ge <zhenpeng.ge@qq.com>

--- nav2_controller/src/nav2_controller.cpp
+++ nav2_controller/src/nav2_controller.cpp
@@ -580,3 +580,10 @@ void ControllerServer::speedLimitCallback(const nav2_msgs::msg::SpeedLimit::Shar
 }
 
 }  // namespace nav2_controller
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(nav2_controller::ControllerServer)
