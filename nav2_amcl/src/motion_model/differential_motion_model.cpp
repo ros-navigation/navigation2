6d705525401ef9cd3df500040e9b5ab36f443d8e
Poornima JD <31023599+poornimajd@users.noreply.github.com>
2021-10-23 15:13:21 +0530

    Cleaned up differential_motion_model.cpp

--- nav2_amcl/src/motion_model/differential_motion_model.cpp
+++ nav2_amcl/src/motion_model/differential_motion_model.cpp
@@ -19,7 +19,6 @@
  *
  */
 
-
 #include <sys/types.h>
 #include <math.h>
 #include <algorithm>
@@ -27,106 +26,97 @@
 #include "nav2_amcl/motion_model/motion_model.hpp"
 #include "nav2_amcl/angleutils.hpp"
 
-
 namespace nav2_amcl
 {
-
 class DifferentialMotionModel : public nav2_amcl::MotionModel
 {
-  public:
+public:
   void initialize(double alpha1, double alpha2,double alpha3, double alpha4, double alpha5) override
   {
-  
-  alpha1_ = alpha1;
-  alpha2_ = alpha2;
-  alpha3_ = alpha3;
-  alpha4_ = alpha4;
-  alpha5_ = alpha5;
-
+		alpha1_ = alpha1;
+		alpha2_ = alpha2;
+		alpha3_ = alpha3;
+		alpha4_ = alpha4;
+		alpha5_ = alpha5;
   }
-  private:
+  
+  void odometryUpdate(pf_t * pf, const pf_vector_t & pose,const pf_vector_t & delta)
+  {  
+		// Compute the new sample poses
+		pf_sample_set_t * set;
+
+		set = pf->sets + pf->current_set;
+		pf_vector_t old_pose = pf_vector_sub(pose, delta);
+
+		// Implement sample_motion_odometry (Prob Rob p 136)
+		double delta_rot1, delta_trans, delta_rot2;
+		double delta_rot1_hat, delta_trans_hat, delta_rot2_hat;
+		double delta_rot1_noise, delta_rot2_noise;
+
+		// Avoid computing a bearing from two poses that are extremely near each
+		// other (happens on in-place rotation).
+		if (sqrt(
+		    delta.v[1] * delta.v[1] +
+		    delta.v[0] * delta.v[0]) < 0.01)
+		{
+		  delta_rot1 = 0.0;
+		} else {
+		  delta_rot1 = angleutils::angle_diff(
+		    atan2(delta.v[1], delta.v[0]),
+		    old_pose.v[2]);
+		}
+		delta_trans = sqrt(
+		  delta.v[0] * delta.v[0] +
+		  delta.v[1] * delta.v[1]);
+		delta_rot2 = angleutils::angle_diff(delta.v[2], delta_rot1);
+
+		// We want to treat backward and forward motion symmetrically for the
+		// noise model to be applied below.  The standard model seems to assume
+		// forward motion.
+		delta_rot1_noise = std::min(
+		  fabs(angleutils::angle_diff(delta_rot1, 0.0)),
+		  fabs(angleutils::angle_diff(delta_rot1, M_PI)));
+		delta_rot2_noise = std::min(
+		  fabs(angleutils::angle_diff(delta_rot2, 0.0)),
+		  fabs(angleutils::angle_diff(delta_rot2, M_PI)));
+
+		for (int i = 0; i < set->sample_count; i++) {
+		  pf_sample_t * sample = set->samples + i;
+
+		  // Sample pose differences
+		  delta_rot1_hat = angleutils::angle_diff(
+		    delta_rot1,
+		    pf_ran_gaussian(
+		      sqrt(
+		        alpha1_ * delta_rot1_noise * delta_rot1_noise +
+		        alpha2_ * delta_trans * delta_trans)));
+		  delta_trans_hat = delta_trans -
+		    pf_ran_gaussian(
+		    sqrt(
+		      alpha3_ * delta_trans * delta_trans +
+		      alpha4_ * delta_rot1_noise * delta_rot1_noise +
+		      alpha4_ * delta_rot2_noise * delta_rot2_noise));
+		  delta_rot2_hat = angleutils::angle_diff(
+		    delta_rot2,
+		    pf_ran_gaussian(
+		      sqrt(
+		        alpha1_ * delta_rot2_noise * delta_rot2_noise +
+		        alpha2_ * delta_trans * delta_trans)));
+
+		  // Apply sampled update to particle pose
+		  sample->pose.v[0] += delta_trans_hat *
+		    cos(sample->pose.v[2] + delta_rot1_hat);
+		  sample->pose.v[1] += delta_trans_hat *
+		    sin(sample->pose.v[2] + delta_rot1_hat);
+		  sample->pose.v[2] += delta_rot1_hat + delta_rot2_hat;
+  }
+  }
+private:
   double alpha1_;
   double alpha2_;
   double alpha3_;
   double alpha4_;
   double alpha5_;
-
-void
-odometryUpdate(
-  pf_t * pf, const pf_vector_t & pose,
-  const pf_vector_t & delta)
-{  
-  // Compute the new sample poses
-  pf_sample_set_t * set;
-
-  set = pf->sets + pf->current_set;
-  pf_vector_t old_pose = pf_vector_sub(pose, delta);
-
-  // Implement sample_motion_odometry (Prob Rob p 136)
-  double delta_rot1, delta_trans, delta_rot2;
-  double delta_rot1_hat, delta_trans_hat, delta_rot2_hat;
-  double delta_rot1_noise, delta_rot2_noise;
-
-  // Avoid computing a bearing from two poses that are extremely near each
-  // other (happens on in-place rotation).
-  if (sqrt(
-      delta.v[1] * delta.v[1] +
-      delta.v[0] * delta.v[0]) < 0.01)
-  {
-    delta_rot1 = 0.0;
-  } else {
-    delta_rot1 = angleutils::angle_diff(
-      atan2(delta.v[1], delta.v[0]),
-      old_pose.v[2]);
-  }
-  delta_trans = sqrt(
-    delta.v[0] * delta.v[0] +
-    delta.v[1] * delta.v[1]);
-  delta_rot2 = angleutils::angle_diff(delta.v[2], delta_rot1);
-
-  // We want to treat backward and forward motion symmetrically for the
-  // noise model to be applied below.  The standard model seems to assume
-  // forward motion.
-  delta_rot1_noise = std::min(
-    fabs(angleutils::angle_diff(delta_rot1, 0.0)),
-    fabs(angleutils::angle_diff(delta_rot1, M_PI)));
-  delta_rot2_noise = std::min(
-    fabs(angleutils::angle_diff(delta_rot2, 0.0)),
-    fabs(angleutils::angle_diff(delta_rot2, M_PI)));
-
-  for (int i = 0; i < set->sample_count; i++) {
-    pf_sample_t * sample = set->samples + i;
-
-    // Sample pose differences
-    delta_rot1_hat = angleutils::angle_diff(
-      delta_rot1,
-      pf_ran_gaussian(
-        sqrt(
-          alpha1_ * delta_rot1_noise * delta_rot1_noise +
-          alpha2_ * delta_trans * delta_trans)));
-    delta_trans_hat = delta_trans -
-      pf_ran_gaussian(
-      sqrt(
-        alpha3_ * delta_trans * delta_trans +
-        alpha4_ * delta_rot1_noise * delta_rot1_noise +
-        alpha4_ * delta_rot2_noise * delta_rot2_noise));
-    delta_rot2_hat = angleutils::angle_diff(
-      delta_rot2,
-      pf_ran_gaussian(
-        sqrt(
-          alpha1_ * delta_rot2_noise * delta_rot2_noise +
-          alpha2_ * delta_trans * delta_trans)));
-
-    // Apply sampled update to particle pose
-    sample->pose.v[0] += delta_trans_hat *
-      cos(sample->pose.v[2] + delta_rot1_hat);
-    sample->pose.v[1] += delta_trans_hat *
-      sin(sample->pose.v[2] + delta_rot1_hat);
-    sample->pose.v[2] += delta_rot1_hat + delta_rot2_hat;
-  }
-}
-
 };
 } // namespace nav2_amcl
 PLUGINLIB_EXPORT_CLASS(nav2_amcl::DifferentialMotionModel,nav2_amcl::MotionModel)
-
