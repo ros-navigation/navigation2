c2cc827f6bd4d9fc2f234e179cc5e9b9a2dd6488
Poornima JD <31023599+poornimajd@users.noreply.github.com>
2021-10-26 23:25:27 +0530

    Update differential_motion_model.hpp

--- nav2_amcl/include/nav2_amcl/motion_model/differential_motion_model.hpp
+++ nav2_amcl/include/nav2_amcl/motion_model/differential_motion_model.hpp
@@ -34,12 +34,14 @@ namespace nav2_amcl
 
 class DifferentialMotionModel : public nav2_amcl::MotionModel
 {
-  public:
-   virtual void initialize(double alpha1, double alpha2,double alpha3, double alpha4,
+public:
+  virtual void initialize(
+   double alpha1, double alpha2,double alpha3, double alpha4,
    double alpha5);
-   virtual void odometryUpdate(pf_t * pf, const pf_vector_t & pose, const pf_vector_t & delta);
-  private:
-   double alpha1_, alpha2_, alpha3_, alpha4_, alpha5_;
+  virtual void odometryUpdate(pf_t * pf, const pf_vector_t & pose, const pf_vector_t & delta);
+  
+private:
+  double alpha1_, alpha2_, alpha3_, alpha4_, alpha5_;
 };
 }  // namespace nav2_amcl
 #endif
