cb05bb632e655304e379e8b9f579e9bd8e1a3d0f
sathak93 <sathak0730@gmail.com>
2021-11-01 18:40:30 +0530

    fix

--- nav2_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
+++ nav2_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
@@ -299,10 +299,18 @@ geometry_msgs::msg::TwistStamped RegulatedPurePursuitController::computeVelocity
     (carrot_pose.pose.position.x * carrot_pose.pose.position.x) +
     (carrot_pose.pose.position.y * carrot_pose.pose.position.y);
 
+  // similarly find dist^2 to half of look_ahead_point to smooth the curvature
+  auto mid_pose = getLookAheadPoint(lookahead_dist / 2.0, transformed_plan);
+  const double mid_dist2 =
+    (mid_pose.pose.position.x * mid_pose.pose.position.x) +
+    (mid_pose.pose.position.y * mid_pose.pose.position.y);
+
   // Find curvature of circle (k = 1 / R)
   double curvature = 0.0;
   if (carrot_dist2 > 0.001) {
     curvature = 2.0 * carrot_pose.pose.position.y / carrot_dist2;
+    auto curvature2 = 2.0 * mid_pose.pose.position.y / mid_dist2;
+    curvature = (curvature + curvature2) / 2.0;
   }
 
   // Setting the velocity direction
@@ -442,7 +450,7 @@ bool RegulatedPurePursuitController::isCollisionImminent(
     dy = global_plan_.poses.back().pose.position.y - curr_pose.y;
     dist_to_goal = std::hypot(dx, dy);
 
-    if (dist_to_goal < 2 * costmap_->getResolution()) {
+    if (dist_to_goal < costmap_->getResolution()) {
       break;
     }
 
