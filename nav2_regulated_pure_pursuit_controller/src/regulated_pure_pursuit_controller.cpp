eb5e1c4e35437a18851ddd8c815eb1dc75685f7f
Steve Macenski <stevenmacenski@gmail.com>
2021-11-09 12:22:10 -0800

    Adding comment to clarify intent

--- nav2_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
+++ nav2_regulated_pure_pursuit_controller/src/regulated_pure_pursuit_controller.cpp
@@ -503,7 +503,10 @@ void RegulatedPurePursuitController::applyConstraints(
   linear_vel = std::max(linear_vel, regulated_linear_scaling_min_speed_);
 
   // if the actual lookahead distance is shorter than requested, that means we're at the
-  // end of the path. We'll scale linear velocity by error to slow to a smooth stop
+  // end of the path. We'll scale linear velocity by error to slow to a smooth stop.
+  // This expression is eq. to (1) holding time to goal, t, constant using the theoretical
+  // lookahead distance and proposed velocity and (2) using t with the actual lookahead
+  // distance to scale the velocity (e.g. t = lookahead / velocity, v = carrot / t).
   if (use_approach_vel_scaling_ && dist_error > 2.0 * costmap_->getResolution()) {
     double velocity_scaling = 1.0 - (dist_error / lookahead_dist);
     double unbounded_vel = approach_vel * velocity_scaling;
