f16e0c5bf3b38067a252e7714e4d0713e839a733
Sathakkadhullah <57533054+sathak93@users.noreply.github.com>
2021-11-12 02:00:23 +0530

    Footprint collision checking for nav2_regulated_pure_pursuit (#2556)
    * initial
    
    * initial
    
    * initialize
    
    * un
    
    * working
    
    * lint fix
    
    * wrk
    
    * clean_up
    
    * remove code repettion
    
    * rm unused variable
    
    * initialize footprint on configure itself
    
    * revert to defaulted constructer
    
    * remove rviz change
    
    * remove unneccessary changes
    
    * review changes
    
    * define traverse unkown
    
    * set true
    
    * reviw changes
    
    * styling changes
    
    * collision checking condition
    
    * used back()
    
    * make loop end condition fixed
    
    * review changes
    
    * fix
    
    * removed midpose curvature
    
    * removed stop collision check at goal pose

--- nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.hpp
+++ nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.hpp
@@ -22,6 +22,7 @@
 #include <algorithm>
 #include <mutex>
 
+#include "nav2_costmap_2d/footprint_collision_checker.hpp"
 #include "nav2_core/controller.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "pluginlib/class_loader.hpp"
@@ -184,13 +185,16 @@ protected:
     const double &, const double &);
 
   /**
-   * @brief Whether point is in collision
+   * @brief checks for collision at projected pose
    * @param x Pose of pose x
    * @param y Pose of pose y
+   * @param theta orientation of Yaw
    * @return Whether in collision
    */
-  bool inCollision(const double & x, const double & y);
-
+  bool inCollision(
+    const double & x,
+    const double & y,
+    const double & theta);
   /**
    * @brief Cost at a point
    * @param x Pose of pose x
@@ -273,6 +277,8 @@ protected:
   std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::PointStamped>>
   carrot_pub_;
   std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> carrot_arc_pub_;
+  std::unique_ptr<nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D *>>
+  collision_checker_;
 
   // Dynamic parameters handler
   std::mutex mutex_;
