a3562cd7f4618ead532c84b077edfd0fe757dc67
pradheep <padhupradheep@gmail.com>
2022-01-15 07:54:14 +0100

    Adding BT nodes inline

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -65,7 +65,9 @@ BtNavigator::BtNavigator(const rclcpp::NodeOptions & options)
     "nav2_remove_passed_goals_action_bt_node",
     "nav2_planner_selector_bt_node",
     "nav2_controller_selector_bt_node",
-    "nav2_goal_checker_selector_bt_node"
+    "nav2_goal_checker_selector_bt_node",
+    "nav2_is_new_path_shorter_bt_node",
+    "nav2_controller_cancel_bt_node"
   };
 
   declare_parameter("plugin_lib_names", plugin_libs);
