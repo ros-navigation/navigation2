f1e6a71bac950d9c3076a43b3c3e7ed066e59cde
Joshua Wallace <josho.wallace@gmail.com>
2022-01-26 12:07:03 -0500

    server timeout fix

--- nav2_behavior_tree/plugins/condition/is_path_valid_condition.cpp
+++ nav2_behavior_tree/plugins/condition/is_path_valid_condition.cpp
@@ -34,9 +34,9 @@ BT::NodeStatus IsPathValidCondition::tick()
   nav_msgs::msg::Path path;
   getInput("path", path);
 
-  int server_timeout;
-  getInput<int>("server_timeout_ms", server_timeout);
-  server_timeout_ = std::chrono::milliseconds(server_timeout);
+  server_timeout_ = 
+   config().blackboard->template get<std::chrono::milliseconds>("server_timeout"); 
+  getInput<std::chrono::milliseconds>("server_timeout", server_timeout_);
 
   auto request = std::make_shared<nav2_msgs::srv::IsPathValid::Request>();
 
