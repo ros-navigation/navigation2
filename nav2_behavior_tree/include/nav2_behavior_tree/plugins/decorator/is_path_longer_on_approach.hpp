3585feeafd7e28f5ea5e399cfea27cfb629ae658
pradheep <padhupradheep@gmail.com>
2022-02-04 21:15:01 +0100

    updating copyrights

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/decorator/is_path_longer_on_approach.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/decorator/is_path_longer_on_approach.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Pradheep Padmanabhan - Neobotix GmbH
+// Copyright (c) 2022 Neobotix GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
