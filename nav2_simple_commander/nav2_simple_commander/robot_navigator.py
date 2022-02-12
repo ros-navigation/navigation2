ad41863b8a132d1e30c052ff1a59018854065c9c
Tony Najjar <tony.najjar@logivations.com>
2022-02-12 19:53:27 +0000

    revert order

--- nav2_simple_commander/nav2_simple_commander/robot_navigator.py
+++ nav2_simple_commander/nav2_simple_commander/robot_navigator.py
@@ -267,10 +267,10 @@ class BasicNavigator(Node):
 
     def waitUntilNav2Active(self, navigator='bt_navigator', localizer='amcl'):
         """Block until the full navigation system is up and running."""
-        self._waitForNodeToActivate(navigator)
         self._waitForNodeToActivate(localizer)
         if localizer == 'amcl':
             self._waitForInitialPose()
+        self._waitForNodeToActivate(navigator)
         self.info('Nav2 is ready for use!')
         return
 
