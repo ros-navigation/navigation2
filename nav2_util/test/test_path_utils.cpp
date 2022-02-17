6a1da0bb02f0146441465b13a68481b169b0aafe
Adam Aposhian <aposhian.dev@gmail.com>
2022-02-17 16:27:28 -0700

    added full failing test for path_utils

--- nav2_util/test/test_path_utils.cpp
+++ nav2_util/test/test_path_utils.cpp
@@ -28,11 +28,14 @@ TEST(PathUtils, test_generate_straight)
   geometry_msgs::msg::PoseStamped start;
   start.header.frame_id = "test_frame";
 
+  constexpr double path_length = 2.0;
+  constexpr double spacing = 1.0;
+
   auto path = generate_path(
-    start, 1.0, {
-    std::make_unique<Straight>(1.0)
+    start, spacing, {
+    std::make_unique<Straight>(path_length)
   });
-  EXPECT_EQ(path.poses.size(), 2u);
+  EXPECT_EQ(path.poses.size(), static_cast<std::size_t>(path_length / spacing));
   for (const auto & pose : path.poses) {
     EXPECT_EQ(pose.header.frame_id, start.header.frame_id);
   }
@@ -43,4 +46,38 @@ TEST(PathUtils, test_generate_straight)
   EXPECT_DOUBLE_EQ(path.poses[1].pose.position.x, 1.0);
   EXPECT_DOUBLE_EQ(path.poses[1].pose.position.y, 0.0);
   EXPECT_DOUBLE_EQ(path.poses[1].pose.position.z, 0.0);
+
+  EXPECT_DOUBLE_EQ(path.poses[1].pose.position.x, 2.0);
+  EXPECT_DOUBLE_EQ(path.poses[1].pose.position.y, 0.0);
+  EXPECT_DOUBLE_EQ(path.poses[1].pose.position.z, 0.0);
+}
+
+TEST(PathUtils, test_generate_all)
+{
+  geometry_msgs::msg::PoseStamped start;
+  start.header.frame_id = "test_frame";
+
+  constexpr double spacing = 0.1;
+
+  auto path = generate_path(
+    start, spacing, {
+    std::make_unique<Straight>(1.0),
+    std::make_unique<LeftTurn>(1.0),
+    std::make_unique<RightTurn>(1.0),
+    std::make_unique<LeftTurnAround>(1.0),
+    std::make_unique<RightTurnAround>(1.0),
+    std::make_unique<LeftCircle>(1.0),
+    std::make_unique<RightCircle>(1.0),
+    std::make_unique<Arc>(1.0, M_2_PI), // another circle
+  });
+  constexpr double expected_path_length = 1.0 + M_PI_2 + M_PI_2 + M_PI + 3 * (M_2_PI);
+  EXPECT_NEAR(path.poses.size(), static_cast<std::size_t>(expected_path_length / spacing), 1.0);
+  for (const auto & pose : path.poses) {
+    EXPECT_EQ(pose.header.frame_id, start.header.frame_id);
+  }
+  auto& last_pose = path.poses.back();
+  auto& last_position = last_pose.pose.position;
+  EXPECT_NEAR(last_position.x, 3.0, 0.5);
+  EXPECT_NEAR(last_position.y, 6.0, 0.5);
+  EXPECT_DOUBLE_EQ(last_position.z, 0.0);
 }
