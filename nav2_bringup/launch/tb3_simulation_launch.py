67d52aa05cbe5618202d7cb50eed2b1db476fd85
Joshua Wallace <josho.wallace@gmail.com>
2022-03-01 16:45:51 -0500

    revert launch changes

--- nav2_bringup/launch/tb3_simulation_launch.py
+++ nav2_bringup/launch/tb3_simulation_launch.py
@@ -103,7 +103,7 @@ def generate_launch_description():
         description='Automatically startup the nav2 stack')
 
     declare_use_composition_cmd = DeclareLaunchArgument(
-        'use_composition', default_value='False',
+        'use_composition', default_value='True',
         description='Whether to use composed bringup')
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
@@ -129,7 +129,7 @@ def generate_launch_description():
 
     declare_simulator_cmd = DeclareLaunchArgument(
         'headless',
-        default_value='True',
+        default_value='False',
         description='Whether to execute gzclient)')
 
     declare_world_cmd = DeclareLaunchArgument(
