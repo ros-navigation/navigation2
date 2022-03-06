2fbd456e2bab2e0c41f737adb0dd89e4afea898c
Joshua Wallace <josho.wallace@gmail.com>
2022-03-06 18:33:53 -0500

    working global time expired node

---
+++ nav2_behavior_tree/plugins/condition/global_time_expired_condition.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2022 Joshua Wallace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <memory>
+
+#include "behaviortree_cpp_v3/condition_node.h"
+
+#include "nav2_behavior_tree/plugins/condition/global_time_expired_condition.hpp"
+
+namespace nav2_behavior_tree
+{
+
+GlobalTimeExpiredCondition::GlobalTimeExpiredCondition(
+  const std::string & condition_name,
+  const BT::NodeConfiguration & conf)
+: BT::ConditionNode(condition_name, conf),
+  period_(1.0),
+  first_time(true)
+{
+  getInput("seconds", period_);
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+  start_ = node_->now();
+}
+
+BT::NodeStatus GlobalTimeExpiredCondition::tick()
+{
+  if (first_time) {
+    first_time = false;
+    RCLCPP_INFO(node_->get_logger(), "FIRST TIME");
+    start_ = node_->now();
+    return BT::NodeStatus::FAILURE;
+  }
+
+  // Determine how long its been since we've started this iteration
+  auto elapsed = node_->now() - start_;
+
+  // Now, get that in seconds
+  auto seconds = elapsed.seconds();
+
+  if (seconds < period_) {
+    // std::cout << "Time expired: " << "Failure" << std::endl;
+    return BT::NodeStatus::FAILURE;
+  }
+
+  // std::cout << "Time expired: " << "Success" << std::endl;
+  start_ = node_->now();  // Reset the timer
+  return BT::NodeStatus::SUCCESS;
+}
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<nav2_behavior_tree::GlobalTimeExpiredCondition>("GlobalTimeExpired");
+}
