d866d49688c21fe03a391111b69f927cee974e67
Joshua Wallace <josho.wallace@gmail.com>
2022-03-09 17:33:01 -0500

    revert

--- nav2_bt_navigator/src/navigators/navigate_to_pose.cpp
+++ nav2_bt_navigator/src/navigators/navigate_to_pose.cpp
@@ -59,7 +59,7 @@ NavigateToPoseNavigator::getDefaultBTFilepath(
   node->declare_parameter<std::string>(
     "default_nav_to_pose_bt_xml",
     pkg_share_dir +
-    "/behavior_trees/navigate_w_recovery_and_constant_replanning_and_if_path_becomes_invalid.xml");
+    "/behavior_trees/navigate_to_pose_w_replanning_and_recovery.xml");
   node->get_parameter("default_nav_to_pose_bt_xml", default_bt_xml_filename);
 
   return default_bt_xml_filename;
