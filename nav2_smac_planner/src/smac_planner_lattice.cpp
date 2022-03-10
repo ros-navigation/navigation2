3903bafe6c5695fc52fe2a5d8d18b0a4e413eeb8
mattbooker <mattbooker97@gmail.com>
2022-03-10 17:18:23 -0500

    Replaced default primitive files
    Signed-off-by: mattbooker <mattbooker97@gmail.com>

--- nav2_smac_planner/src/smac_planner_lattice.cpp
+++ nav2_smac_planner/src/smac_planner_lattice.cpp
@@ -78,7 +78,8 @@ void SmacPlannerLattice::configure(
   // Default to a well rounded model: 16 bin, 0.4m turning radius, ackermann model
   nav2_util::declare_parameter_if_not_declared(
     node, name + ".lattice_filepath", rclcpp::ParameterValue(
-      ament_index_cpp::get_package_share_directory("nav2_smac_planner") + "/default_model.json"));
+      ament_index_cpp::get_package_share_directory("nav2_smac_planner") + 
+        "/sample_primitives/5cm_resolution/0.5m _turning_radius/ackermann/output.json"));
   node->get_parameter(name + ".lattice_filepath", _search_info.lattice_filepath);
   nav2_util::declare_parameter_if_not_declared(
     node, name + ".cache_obstacle_heuristic", rclcpp::ParameterValue(false));
