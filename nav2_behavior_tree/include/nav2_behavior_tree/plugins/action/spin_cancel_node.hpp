d24b23c53df53c7ea355f71d56bd73ecaf31d114
Pradheep-office <padhupradheep@gmail.com>
2022-03-21 11:36:10 +0100

    Adding cancel for spining recovery

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/spin_cancel_node.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/spin_cancel_node.hpp
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__WAIT_CANCEL_NODE_HPP_
-#define NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__WAIT_CANCEL_NODE_HPP_
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__SPIN_CANCEL_NODE_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__SPIN_CANCEL_NODE_HPP_
 
 #include <memory>
 #include <string>
 
-#include "nav2_msgs/action/wait.hpp"
+#include "nav2_msgs/action/spin.hpp"
 
 #include "nav2_behavior_tree/bt_cancel_action_node.hpp"
 
@@ -28,7 +28,7 @@ namespace nav2_behavior_tree
 /**
  * @brief A nav2_behavior_tree::BtActionNode class that wraps nav2_msgs::action::Wait
  */
-class WaitCancel : public BtCancelActionNode<nav2_msgs::action::Wait>
+class SpinCancel : public BtCancelActionNode<nav2_msgs::action::Spin>
 {
 public:
   /**
@@ -37,7 +37,7 @@ public:
    * @param action_name Action name this node creates a client for
    * @param conf BT node configuration
    */
-  WaitCancel(
+  SpinCancel(
     const std::string & xml_tag_name,
     const std::string & action_name,
     const BT::NodeConfiguration & conf);
@@ -56,4 +56,4 @@ public:
 
 }  // namespace nav2_behavior_tree
 
-#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__WAIT_CANCEL_NODE_HPP_
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__SPIN_CANCEL_NODE_HPP_
