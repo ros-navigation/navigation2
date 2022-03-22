e173995fde3ecc957e8b0269b0d75252ce30c32a
Alexey Merzlyakov <60094858+AlexeyMerzlyakov@users.noreply.github.com>
2022-03-22 20:05:04 +0300

    Apply raytraceLine 3D fixes to its 2D version (#2857)
    (back-port of #2460 and #2784)

--- nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d.hpp
+++ nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d.hpp
@@ -433,8 +433,31 @@ protected:
     unsigned int y1,
     unsigned int max_length = UINT_MAX, unsigned int min_length = 0)
   {
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx_full = x1 - x0;
+    int dy_full = y1 - y0;
+
+    // we need to chose how much to scale our dominant dimension,
+    // based on the maximum length of the line
+    double dist = std::hypot(dx_full, dy_full);
+    if (dist < min_length) {
+      return;
+    }
+
+    unsigned int min_x0, min_y0;
+    if (dist > 0.0) {
+      // Adjust starting point and offset to start from min_length distance
+      min_x0 = (unsigned int)(x0 + dx_full / dist * min_length);
+      min_y0 = (unsigned int)(y0 + dy_full / dist * min_length);
+    } else {
+      // dist can be 0 if [x0, y0]==[x1, y1].
+      // In this case only this cell should be processed.
+      min_x0 = x0;
+      min_y0 = y0;
+    }
+    unsigned int offset = min_y0 * size_x_ + min_x0;
+
+    int dx = x1 - min_x0;
+    int dy = y1 - min_y0;
 
     unsigned int abs_dx = abs(dx);
     unsigned int abs_dy = abs(dy);
@@ -442,39 +465,21 @@ protected:
     int offset_dx = sign(dx);
     int offset_dy = sign(dy) * size_x_;
 
-    // we need to chose how much to scale our dominant dimension,
-    // based on the maximum length of the line
-    double dist = std::hypot(dx, dy);
-    if (dist < min_length) {
-      return;
-    }
-
-    // Adjust starting point and offset to start from min_length distance
-    unsigned int min_x0 = (unsigned int)(x0 + dx / dist * min_length);
-    unsigned int min_y0 = (unsigned int)(y0 + dy / dist * min_length);
-    unsigned int offset = min_y0 * size_x_ + min_x0;
-
     double scale = (dist == 0.0) ? 1.0 : std::min(1.0, max_length / dist);
-    unsigned int length;
     // if x is dominant
     if (abs_dx >= abs_dy) {
       int error_y = abs_dx / 2;
 
-      // Subtract minlength from length since initial point (x0, y0)has been adjusted by min Z
-      length = (unsigned int)(scale * abs_dx) - min_length;
-
       bresenham2D(
-        at, abs_dx, abs_dy, error_y, offset_dx, offset_dy, offset, length);
+        at, abs_dx, abs_dy, error_y, offset_dx, offset_dy, offset, (unsigned int)(scale * abs_dx));
       return;
     }
 
     // otherwise y is dominant
     int error_x = abs_dy / 2;
 
-    // Subtract minlength from total length since initial point (x0, y0) has been adjusted by min Z
-    length = (unsigned int)(scale * abs_dy) - min_length;
     bresenham2D(
-      at, abs_dy, abs_dx, error_x, offset_dy, offset_dx, offset, length);
+      at, abs_dy, abs_dx, error_x, offset_dy, offset_dx, offset, (unsigned int)(scale * abs_dy));
   }
 
 private:
