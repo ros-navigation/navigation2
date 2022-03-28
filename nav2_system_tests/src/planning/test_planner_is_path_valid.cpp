4c083cc0fcc11a8affd5d513e4492071e6973d0c
Joshua Wallace <josho.wallace@gmail.com>
2022-03-28 14:55:54 -0400

    stating test

---
+++ nav2_system_tests/src/planning/test_planner_is_path_valid.cpp
@@ -0,0 +1,113 @@
+// Copyright (c) 2022 Joshua Wallace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. Reserved.
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <vector>
+
+#include "nav2_msgs/srv/is_path_valid.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "planner_tester.hpp"
+#include "nav2_util/lifecycle_utils.hpp"
+
+using nav2_system_tests::PlannerTester;
+using nav2_util::TestCostmap;
+
+
+template<class ServiceT>
+class TestClient : public rclcpp::Node
+{
+  explicit TestClient(
+    std::string client_name,
+    const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
+    : Node("test_client", options),
+    client_name_(client_name)
+  {
+    // rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
+    // node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+
+    client_ = create_client<ServiceT>(client_name);
+    // Wait for service to be up
+
+    // while (!client_->wait_for_service(1s))
+    // {
+    //   // waiting for service to load
+    // }
+  }
+
+  protected:
+    virtual ServiceT::Respone callService(std::shared_ptr<typename ServiceT::Request> request)
+    {
+      auto result = client->async_send_request(request);
+
+      // Wait for the result.
+      if (rclcpp::spin_until_future_complete(result) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+      {
+        RCLCPP_INFO(get_logger(), "Got result");
+      } else {
+        RCLCPP_ERROR_STREAM(get_logger(), "Failed to call service: " << client_name_);
+      }
+
+      return result;
+    }
+
+    typename rclcpp::Client<ServiceT>::SharedPtr client_;
+    std::string client_name_;
+};
+
+TEST(testIsPathValid, testIsPathValid)
+{
+  // auto planner_tester = std::make_shared<PlannerTester>();
+
+  // planner_tester->activate();
+
+  // // load open space cost map which is 10 by 10
+  // planner_tester->loadSimpleCostmap(TestCostmap::open_space);
+
+  // // create a fake service request
+  // auto is_path_valid = std::make_shared<nav2_msgs::srv::IsPathValid::Request>();
+
+  // nav_msgs::msg::Path path;
+  // for(int i = 1; i < 10; ++i)
+  // {
+  //   geometry_msgs::msg::PoseStamped pose;
+  //   pose.pose.position.x = i;
+  //   pose.pose.position.y = i;
+  //   is_path_valid->path.poses.push_back(pose);
+  // }
+
+  // //Need to spin this off in a different thread
+  // std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("is_path_valid_client");
+  // rclcpp::Client<nav2_msgs::srv::IsPathValid>::SharedPtr client =
+  //   node->create_client<nav2_msgs::srv::IsPathValid>("add_two_ints");
+
+  EXPECT_EQ(0,0);
+}
+
+int main(int argc, char ** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  // initialize ROS
+  rclcpp::init(argc, argv);
+
+  bool all_successful = RUN_ALL_TESTS();
+
+  // shutdown ROS
+  rclcpp::shutdown();
+  return all_successful;
+
+
+}
