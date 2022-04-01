ec7933fcfba295ff87ff51dbbcc76d85dbf531ba
pradheep <padhupradheep@gmail.com>
2022-04-01 22:24:38 +0200

    no linters

--- nav2_costmap_2d/plugins/inflation_layer.cpp
+++ nav2_costmap_2d/plugins/inflation_layer.cpp
@@ -441,8 +441,9 @@ InflationLayer::dynamicParametersCallback(
         inflation_radius_ = parameter.as_double();
         need_reinflation_ = true;
         need_cache_recompute = true;
-      } else if (param_name == name_ + "." + "cost_scaling_factor" &&
-        cost_scaling_factor_ != parameter.as_double()) {
+      } else if (param_name == name_ + "." + "cost_scaling_factor" && // NOLINT
+        cost_scaling_factor_ != parameter.as_double())
+      {
         cost_scaling_factor_ = parameter.as_double();
         need_reinflation_ = true;
         need_cache_recompute = true;
@@ -452,12 +453,14 @@ InflationLayer::dynamicParametersCallback(
         enabled_ = parameter.as_bool();
         need_reinflation_ = true;
         current_ = false;
-      } else if (param_name == name_ + "." + "inflate_unknown" &&
-        inflate_unknown_ != parameter.as_bool()) {
+      } else if (param_name == name_ + "." + "inflate_unknown" && // NOLINT
+        inflate_unknown_ != parameter.as_bool())
+      {
         inflate_unknown_ = parameter.as_bool();
         need_reinflation_ = true;
-      } else if (param_name == name_ + "." + "inflate_around_unknown" &&
-        inflate_around_unknown_ != parameter.as_bool()) {
+      } else if (param_name == name_ + "." + "inflate_around_unknown" && // NOLINT
+        inflate_around_unknown_ != parameter.as_bool())
+      {
         inflate_around_unknown_ = parameter.as_bool();
         need_reinflation_ = true;
       }
