37da351f90a1ca47ccd418cab1be5b65240e87fd
Steve Macenski <stevenmacenski@gmail.com>
2022-04-11 14:50:14 -0700

    remove dump tests

--- nav2_bringup/launch/tb3_simulation_launch.py
+++ nav2_bringup/launch/tb3_simulation_launch.py
@@ -129,7 +129,7 @@ def generate_launch_description():
 
     declare_simulator_cmd = DeclareLaunchArgument(
         'headless',
-        default_value='False',
+        default_value='True',
         description='Whether to execute gzclient)')
 
     declare_world_cmd = DeclareLaunchArgument(
