4e5ba4bebb8dbd715f020af4395ec194967cfbd4
Luca Bonamini <lucabonamini28@gmail.com>
2022-05-08 17:33:50 +0200

    WIP set environment variables correctly

--- nav2_system_tests/src/costmap_filters/test_keepout_launch.py
+++ nav2_system_tests/src/costmap_filters/test_keepout_launch.py
@@ -18,6 +18,9 @@
 import os
 import sys
 
+from os import environ
+from os import pathsep
+
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
@@ -32,10 +35,25 @@ from launch_testing.legacy import LaunchTestService
 
 from nav2_common.launch import RewrittenYaml
 
+from scripts import GazeboRosPaths
+
 
 def generate_launch_description():
+    model, plugin, media = GazeboRosPaths.get_paths()
+
+    if 'GAZEBO_MODEL_PATH' in environ:
+        model += pathsep+environ['GAZEBO_MODEL_PATH']
+    if 'GAZEBO_PLUGIN_PATH' in environ:
+        plugin += pathsep+environ['GAZEBO_PLUGIN_PATH']
+    if 'GAZEBO_RESOURCE_PATH' in environ:
+        media += pathsep+environ['GAZEBO_RESOURCE_PATH']
+
     aws_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
-    gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    env = {'GAZEBO_MODEL_PATH': model,
+           'GAZEBO_PLUGIN_PATH': plugin,
+           'GAZEBO_RESOURCE_PATH': media}
+
     map_yaml_file = os.path.join(aws_dir, 'maps', '005', 'map.yaml')
     filter_mask_file = os.getenv('TEST_MASK')
 
@@ -93,11 +111,11 @@ def generate_launch_description():
             description='name of the robot'),
 
         # Launch gazebo server for simulation
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(gazebo_ros, 'launch', 'gzserver.launch.py')),
-            launch_arguments={'world': world}.items()
-        ),
+        ExecuteProcess(
+            cmd=['gzserver', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so',
+                 '--minimal_comms', world],
+            additional_env=env,
+            cwd=[aws_dir], output='screen'),
 
         Node(
             package='gazebo_ros',
