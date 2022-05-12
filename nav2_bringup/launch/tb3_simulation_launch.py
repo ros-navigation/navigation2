017f10f70a61f519bdd0d31d2511c1ff4966f05f
Luca Bonamini <lucabonamini28@gmail.com>
2022-05-12 21:58:00 +0200

    Fix default value for headless parameter in tb3_simulation_launch.py

--- nav2_bringup/launch/tb3_simulation_launch.py
+++ nav2_bringup/launch/tb3_simulation_launch.py
@@ -136,7 +136,7 @@ def generate_launch_description():
 
     declare_simulator_cmd = DeclareLaunchArgument(
         'headless',
-        default_value='True',
+        default_value='False',
         description='Whether to execute gzclient)')
 
     declare_world_cmd = DeclareLaunchArgument(
