9756f85177b885c8f00d1a24c498e499fc5c80ac
Tony Najjar <tony.najjar@logivations.com>
2022-05-16 17:42:26 +0200

    Update navigate_to_pose_action.cpp

--- nav2_behavior_tree/plugins/action/navigate_to_pose_action.cpp
+++ nav2_behavior_tree/plugins/action/navigate_to_pose_action.cpp
@@ -35,8 +35,8 @@ void NavigateToPoseAction::on_tick()
       node_->get_logger(),
       "NavigateToPoseAction: goal not provided");
     return;
-    getInput("behavior_tree", goal_.behavior_tree);
   }
+  getInput("behavior_tree", goal_.behavior_tree);
 }
 
 }  // namespace nav2_behavior_tree
