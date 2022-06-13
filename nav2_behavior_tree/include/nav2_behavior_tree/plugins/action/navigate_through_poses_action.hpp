dcb426ca25d245337b3afe72b6691c083295bb83
shoufei <907575489@qq.com>
2022-06-14 04:34:19 +0800

    fix: wrong input type in navigate_to_pose_action.hpp and navigate_to_… (#2994)
    * fix: wrong input type in navigate_to_pose_action.hpp and navigate_to_pose_action.hpp
    
    * Update navigate_through_poses_action.hpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/navigate_through_poses_action.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/navigate_through_poses_action.hpp
@@ -56,7 +56,7 @@ public:
     return providedBasicPorts(
       {
         BT::InputPort<geometry_msgs::msg::PoseStamped>("goals", "Destinations to plan through"),
-        BT::InputPort<geometry_msgs::msg::PoseStamped>("behavior_tree", "Behavior tree to run"),
+        BT::InputPort<std::string>("behavior_tree", "Behavior tree to run"),
       });
   }
 };
