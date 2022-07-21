8e38e8e97918daf67cf8272e3386ef945779b230
nakai-omer <108797279+nakai-omer@users.noreply.github.com>
2022-07-21 15:22:58 +0300

    Update map_io.cpp

--- nav2_map_server/src/map_io.cpp
+++ nav2_map_server/src/map_io.cpp
@@ -8,7 +8,7 @@
  * modification, are permitted provided that the following conditions are met:
  *
  *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.151
+ *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
