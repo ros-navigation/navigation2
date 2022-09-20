f8e9107573a862718ab89c210493fad4a4a60f2f
Joshua Wallace <josho.wallace@gmail.com>
2022-09-20 15:06:28 -0400

    rename

--- nav2_simple_commander/launch/assisted_teleop_example_launch.py
+++ nav2_simple_commander/launch/assisted_teleop_example_launch.py
@@ -63,7 +63,7 @@ def generate_launch_description():
     # start the demo autonomy task
     demo_cmd = Node(
         package='nav2_simple_commander',
-        executable='demo_assisted_teleop',
+        executable='example_assisted_teleop',
         emulate_tty=True,
         output='screen')
 
