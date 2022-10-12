1c106173dbc5b2321e9d3f1bf111e40293d36a4f
Joshua Wallace <josho.wallace@gmail.com>
2022-10-12 16:07:15 -0400

    Expections (#3244)
    * added result codes for global planner
    
    * code review
    
    * code review
    
    * cleanup
    
    * cleanup
    
    * update smac lattice planner
    
    * update planner instances
    
    * cleanup
    
    * updates
    
    * renaming
    
    * fixes
    
    * cpplint
    
    * uncrusitfy
    
    * code review
    
    * navfn exceptions
    
    * theta_star_planner
    
    * fix code review
    
    * wrote timeout exception
    
    * consistent exception throwing across planners
    
    * code review
    
    * remove
    
    * uncrusitfy
    
    * uncrusify
    
    * catch exception
    
    * expect throw
    
    * update string of exceptions
    
    * throw with coords
    
    * removed start == goal error code
    
    * code review
    
    * code review
    
    * uncrustify
    
    * code review
    
    * message order
    
    * remove remarks
    
    * update xml
    
    * update xml
    
    * Update nav2_behavior_tree/nav2_tree_nodes.xml
    
    * fix
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Co-authored-by: Joshua Wallace <josho.wallace.com>

--- nav2_behavior_tree/plugins/action/compute_path_to_pose_action.cpp
+++ nav2_behavior_tree/plugins/action/compute_path_to_pose_action.cpp
@@ -40,6 +40,9 @@ void ComputePathToPoseAction::on_tick()
 BT::NodeStatus ComputePathToPoseAction::on_success()
 {
   setOutput("path", result_.result->path);
+  // Set empty error code, action was successful
+  result_.result->error_code = nav2_msgs::action::ComputePathToPose::Goal::NONE;
+  setOutput("compute_path_to_pose_error_code", result_.result->error_code);
   return BT::NodeStatus::SUCCESS;
 }
 
@@ -47,6 +50,7 @@ BT::NodeStatus ComputePathToPoseAction::on_aborted()
 {
   nav_msgs::msg::Path empty_path;
   setOutput("path", empty_path);
+  setOutput("compute_path_to_pose_error_code", result_.result->error_code);
   return BT::NodeStatus::FAILURE;
 }
 
@@ -54,6 +58,9 @@ BT::NodeStatus ComputePathToPoseAction::on_cancelled()
 {
   nav_msgs::msg::Path empty_path;
   setOutput("path", empty_path);
+  // Set empty error code, action was cancelled
+  result_.result->error_code = nav2_msgs::action::ComputePathToPose::Goal::NONE;
+  setOutput("compute_path_to_pose_error_code", result_.result->error_code);
   return BT::NodeStatus::SUCCESS;
 }
 
