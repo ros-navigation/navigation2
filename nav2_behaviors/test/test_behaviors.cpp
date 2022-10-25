0a11151cf70720de6b52a429ac26aea5cbad441c
Joshua Wallace <josho.wallace.com>
2022-10-25 19:58:31 -0400

    fix test

--- nav2_behaviors/test/test_behaviors.cpp
+++ nav2_behaviors/test/test_behaviors.cpp
@@ -103,11 +103,18 @@ protected:
       std::make_shared<rclcpp_lifecycle::LifecycleNode>(
       "LifecycleBehaviorTestNode", rclcpp::NodeOptions());
     node_lifecycle_->declare_parameter(
-      "costmap_topic",
+      "local_costmap_topic",
       rclcpp::ParameterValue(std::string("local_costmap/costmap_raw")));
     node_lifecycle_->declare_parameter(
-      "footprint_topic",
+      "local_footprint_topic",
       rclcpp::ParameterValue(std::string("local_costmap/published_footprint")));
+
+    node_lifecycle_->declare_parameter(
+        "global_costmap_topic",
+        rclcpp::ParameterValue(std::string("global_costmap/costmap_raw")));
+    node_lifecycle_->declare_parameter(
+        "global_footprint_topic",
+        rclcpp::ParameterValue(std::string("global_costmap/published_footprint")));
 
     tf_buffer_ = std::make_shared<tf2_ros::Buffer>(node_lifecycle_->get_clock());
     auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
