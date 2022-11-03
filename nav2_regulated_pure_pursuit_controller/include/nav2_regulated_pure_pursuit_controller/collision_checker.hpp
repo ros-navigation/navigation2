8ccf11fe392a1ec59b6c67b3a6d2eb6b31825b20
stevemacenski <stevenmacenski@gmail.com>
2022-11-02 21:30:16 -0700

    fixing header

--- nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/collision_checker.hpp
+++ nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/collision_checker.hpp
@@ -28,7 +28,7 @@
 #include "geometry_msgs/msg/pose2_d.hpp"
 #include "nav2_regulated_pure_pursuit_controller/parameter_handler.hpp"
 
-#include "nav2_core/exceptions.hpp"
+#include "nav2_core/controller_exceptions.hpp"
 #include "nav2_util/node_utils.hpp"
 #include "nav2_util/geometry_utils.hpp"
 #include "nav2_costmap_2d/costmap_filters/filter_values.hpp"
