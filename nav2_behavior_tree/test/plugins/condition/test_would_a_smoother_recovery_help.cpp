d71cd5b122c346a94b6d0767e1d285b15f57938c
Joshua Wallace <josho.wallace@gmail.com>
2023-01-09 19:17:11 -0500

    update copyright

--- nav2_behavior_tree/test/plugins/condition/test_would_a_smoother_recovery_help.cpp
+++ nav2_behavior_tree/test/plugins/condition/test_would_a_smoother_recovery_help.cpp
@@ -1,3 +1,6 @@
+
+
+
 // Copyright (c) 2023 Joshua Wallace
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -6,11 +9,11 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License. Reserved.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <gtest/gtest.h>
 #include <memory>
