f3e9a78b2e090c9a3a75373218d80d69d40c7e52
HovorunB <bohdan.hovorun@logviations.com>
2023-01-27 17:25:31 +0100

    revert

--- nav2_collision_monitor/src/pointcloud.cpp
+++ nav2_collision_monitor/src/pointcloud.cpp
@@ -143,10 +143,11 @@ void PointCloud::getParameters(std::string & source_topic)
 
   nav2_util::declare_parameter_if_not_declared(
     node, source_name_ + ".min_height", rclcpp::ParameterValue(0.05));
-  node->get_parameter(source_name_ + ".min_height", min_height_);
+  min_height_ = node->get_parameter(source_name_ + ".min_height").as_double();
   nav2_util::declare_parameter_if_not_declared(
     node, source_name_ + ".max_height", rclcpp::ParameterValue(0.5));
-  node->get_parameter(source_name_ + ".max_height", max_height_);
+  max_height_ = node->get_parameter(source_name_ + ".max_height").as_double();
+}
 }
 
 void PointCloud::dataCallback(sensor_msgs::msg::PointCloud2::ConstSharedPtr msg)
