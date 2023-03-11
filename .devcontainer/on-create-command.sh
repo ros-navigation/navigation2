beaddfd885069862f0047548363672e6bef7b48a
ruffsl <roxfoxpox@gmail.com>
2023-03-11 22:06:54 +0100

    Setup workspace on create

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 set -euxo pipefail
+
+export OVERLAY_MIXINS="release ccache lld"
+export CCACHE_DIR="$OVERLAY_WS/.ccache"
+env
+
+cd $OVERLAY_WS
+. $UNDERLAY_WS/install/setup.sh
+
+colcon cache lock
+
+colcon build \
+    --symlink-install \
+    --mixin $OVERLAY_MIXINS
+
+sed --in-place \
+    's|^source .*|source "$OVERLAY_WS/install/setup.bash"|' \
+    /ros_entrypoint.sh
