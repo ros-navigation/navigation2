2c2f0bad68202cf28010822f3fc117c8bd517e32
ruffsl <roxfoxpox@gmail.com>
2023-03-12 00:24:12 +0100

    Build incrementally from update content command by copying the build workspace steps from circleci config

--- .devcontainer/update-content-command.sh
+++ .devcontainer/update-content-command.sh
@@ -1,3 +1,56 @@
 #!/bin/bash
 
 set -exo pipefail
+
+env
+
+cd $OVERLAY_WS
+
+                    colcon cache lock
+
+                    BUILD_UNFINISHED=$(
+                      colcon list \
+                        --names-only \
+                        --packages-skip-build-finished \
+                      | xargs)
+                    echo BUILD_UNFINISHED: $BUILD_UNFINISHED
+
+                    BUILD_FAILED=$(
+                      colcon list \
+                        --names-only \
+                        --packages-select-build-failed \
+                      | xargs)
+                    echo BUILD_FAILED: $BUILD_FAILED
+
+                    BUILD_INVALID=$(
+                      colcon list \
+                        --names-only \
+                        --packages-select-cache-invalid \
+                        --packages-select-cache-key build \
+                      | xargs)
+                    echo BUILD_INVALID: $BUILD_INVALID
+
+                    BUILD_PACKAGES=""
+                    if [ -n "$BUILD_UNFINISHED" ] || \
+                       [ -n "$BUILD_FAILED" ] || \
+                       [ -n "$BUILD_INVALID" ]
+                    then
+                      BUILD_PACKAGES=$(
+                        colcon list \
+                          --names-only \
+                          --packages-above \
+                            $BUILD_UNFINISHED \
+                            $BUILD_FAILED \
+                            $BUILD_INVALID \
+                        | xargs)
+                    fi
+                    echo BUILD_PACKAGES: $BUILD_PACKAGES
+
+                    colcon clean packages --yes \
+                      --packages-select ${BUILD_PACKAGES} \
+                      --base-select install
+
+                    . << parameters.underlay >>/install/setup.sh
+                    colcon build \
+                      --packages-select ${BUILD_PACKAGES} \
+                      --mixin << parameters.mixins >>
