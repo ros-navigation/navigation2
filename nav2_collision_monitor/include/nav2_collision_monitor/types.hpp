6acedc37d54f135b184ea0af4e9b722c3e29a218
kaichie <kaichie.lee@gmail.com>
2023-03-24 16:50:59 +0000

    cleanup

--- nav2_collision_monitor/include/nav2_collision_monitor/types.hpp
+++ nav2_collision_monitor/include/nav2_collision_monitor/types.hpp
@@ -67,7 +67,7 @@ enum ActionType
   DO_NOTHING = 0,  // No action
   STOP = 1,  // Stop the robot
   SLOWDOWN = 2,  // Slowdown in percentage from current operating speed
-  APPROACH = 3,  // Keep constant time interval before collision
+  APPROACH = 3  // Keep constant time interval before collision
 };
 
 /// @brief Action for robot
