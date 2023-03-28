39d5c0f1f1a551f7239e0531c4341fefe08be2fd
Adam Aposhian <aposhian.dev@gmail.com>
2023-03-28 09:41:53 -0600

    add cross track error function

--- nav2_util/include/nav2_util/geometry_utils.hpp
+++ nav2_util/include/nav2_util/geometry_utils.hpp
@@ -120,6 +120,44 @@ inline double euclidean_distance(
 }
 
 /**
+ * @brief Get the squared euclidean distance between 2 geometry_msgs::Poses
+ * @param pos1 First pose
+ * @param pos1 Second pose
+ * @param is_3d True if a true 3D distance is desired (default false)
+ * @return double squared euclidean distance
+ */
+inline double squared_euclidean_distance(
+  const geometry_msgs::msg::Pose & pos1,
+  const geometry_msgs::msg::Pose & pos2,
+  const bool is_3d = false)
+{
+  double dx = pos1.position.x - pos2.position.x;
+  double dy = pos1.position.y - pos2.position.y;
+
+  if (is_3d) {
+    double dz = pos1.position.z - pos2.position.z;
+    return dx * dx + dy * dy + dz * dz;
+  }
+
+  return dx * dx + dy * dy;
+}
+
+/**
+ * @brief Get the squared L2 distance between 2 geometry_msgs::PoseStamped
+ * @param pos1 First pose
+ * @param pos1 Second pose
+ * @param is_3d True if a true squared L2 distance is desired (default false)
+ * @return double squared L2 distance
+ */
+inline double squared_euclidean_distance(
+  const geometry_msgs::msg::PoseStamped & pos1,
+  const geometry_msgs::msg::PoseStamped & pos2,
+  const bool is_3d = false)
+{
+  return squared_euclidean_distance(pos1.pose, pos2.pose, is_3d);
+}
+
+/**
  * Find element in iterator with the minimum calculated value
  */
 template<typename Iter, typename Getter>
@@ -177,6 +215,63 @@ inline double calculate_path_length(const nav_msgs::msg::Path & path, size_t sta
     path_length += euclidean_distance(path.poses[idx].pose, path.poses[idx + 1].pose);
   }
   return path_length;
+}
+
+std::pair<geometry_msgs::msg::PoseStamped, geometry_msgs::msg::PoseStamped>
+find_closest_path_segment(const nav_msgs::msg::Path & path, const geometry_msgs::msg::PoseStamped & pose)
+{
+  std::vector<double> distances(path.poses.size());
+  std::transform(
+    path.poses.begin(),
+    path.poses.end(),
+    distances.begin(),
+    [&pose](const auto & path_pose) {
+      return squared_euclidean_distance(path_pose, pose);
+    }
+  );
+  auto closest_distance_it = std::min_element(
+    distances.begin(),
+    distances.end()
+  );
+
+  auto next_closest_distance_it = distances.end();
+
+  if (std::next(closest_distance_it) == distances.end()) {
+    next_closest_distance_it = std::prev(closest_distance_it);
+  } else if (closest_distance_it == distances.begin()) {
+    next_closest_distance_it = std::next(closest_distance_it);
+  } else if (*std::prev(closest_distance_it) < *std::next(closest_distance_it)) {
+    next_closest_distance_it = std::prev(closest_distance_it);
+  } else {
+    next_closest_distance_it = std::next(closest_distance_it);
+  }
+
+  size_t closest_pose_index = std::distance(distances.begin(), closest_distance_it);
+  size_t next_closest_pose_index = std::distance(distances.begin(), next_closest_distance_it);
+
+  return std::make_pair(path.poses.at(closest_pose_index), path.poses.at(next_closest_pose_index));
+}
+
+double cross_track_error(const nav_msgs::msg::Path & path, const geometry_msgs::msg::PoseStamped & pose)
+{
+  // http://paulbourke.net/geometry/pointlineplane/
+
+  // Even if the target pose is "off the end" of this line segment, the projected point
+  // exists as if the segment was extended out.
+  const auto & [pose1, pose2] = find_closest_path_segment(path, pose);
+  const double x1 = pose1.pose.position.x;
+  const double y1 = pose1.pose.position.y;
+  const double x2 = pose2.pose.position.x;
+  const double y2 = pose2.pose.position.y;
+  const double x3 = pose.pose.position.x;
+  const double y3 = pose.pose.position.y;
+
+  const double u = ((x3 - x1) * (x2 - x1) + (y3 - y1) * (y2 - y1)) / ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+  const double x = x1 + u * (x2 - x1);
+  const double y = y1 + u * (y2 - y1);
+
+  return std::sqrt((x - x3) * (x - x3) + (y - y3) * (y - y3));
 }
 
 }  // namespace geometry_utils
