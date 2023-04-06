214907692482ff77e9c3b62205d18ab63bf37d87
ruffsl <roxfoxpox@gmail.com>
2023-04-06 12:33:43 +0200

    Revert "Comment out gzclient and rviz for debugging"
    This reverts commit 0addae2a1ee70c5771055c5dd8fa050af438b896.

--- nav2_simple_commander/launch/security_demo_launch.py
+++ nav2_simple_commander/launch/security_demo_launch.py
@@ -90,9 +90,9 @@ def generate_launch_description():
     ld.add_action(declare_use_rviz_cmd)
     ld.add_action(declare_simulator_cmd)
     ld.add_action(start_gazebo_server_cmd)
-    # ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(start_gazebo_client_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
-    # ld.add_action(rviz_cmd)
+    ld.add_action(rviz_cmd)
     ld.add_action(bringup_cmd)
     ld.add_action(demo_cmd)
     return ld
