4d93f4d8eeb9d673a33f1d6e5391f4d9278045cb
Guillaume Doisy <guillaume@dexory.com>
2023-04-09 12:02:11 +0100

    add rotation progress checker

--- nav2_controller/include/nav2_controller/plugins/rotation_progress_checker.hpp
+++ nav2_controller/include/nav2_controller/plugins/rotation_progress_checker.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2023 Dexory
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,12 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
-#define NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
+#ifndef NAV2_CONTROLLER__PLUGINS__ROTATION_PROGRESS_CHECKER_HPP_
+#define NAV2_CONTROLLER__PLUGINS__ROTATION_PROGRESS_CHECKER_HPP_
 
 #include <string>
 #include <vector>
 #include "rclcpp/rclcpp.hpp"
+#include "nav2_controller/plugins/simple_progress_checker.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "nav2_core/progress_checker.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
@@ -26,19 +27,18 @@
 namespace nav2_controller
 {
 /**
-* @class SimpleProgressChecker
-* @brief This plugin is used to check the position of the robot to make sure
-* that it is actually progressing towards a goal.
+* @class RotationProgressChecker
+* @brief This plugin is used to check the position and the angle of the robot to make sure
+* that it is actually progressing or rotating towards a goal.
 */
 
-class SimpleProgressChecker : public nav2_core::ProgressChecker
+class RotationProgressChecker : public SimpleProgressChecker
 {
 public:
   void initialize(
     const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
     const std::string & plugin_name) override;
   bool check(geometry_msgs::msg::PoseStamped & current_pose) override;
-  void reset() override;
 
 protected:
   /**
@@ -47,23 +47,11 @@ protected:
    * @return true, if movement is greater than radius_, or false
    */
   bool is_robot_moved_enough(const geometry_msgs::msg::Pose2D & pose);
-  /**
-   * @brief Resets baseline pose with the current pose of the robot
-   * @param pose Current pose of the robot
-   */
-  void reset_baseline_pose(const geometry_msgs::msg::Pose2D & pose);
 
-  static double pose_distance(const geometry_msgs::msg::Pose2D &, const geometry_msgs::msg::Pose2D &);
+  static double pose_angle_distance(const geometry_msgs::msg::Pose2D &, const geometry_msgs::msg::Pose2D &);
 
-  rclcpp::Clock::SharedPtr clock_;
+  double required_movement_angle_;
 
-  double radius_;
-  rclcpp::Duration time_allowance_{0, 0};
-
-  geometry_msgs::msg::Pose2D baseline_pose_;
-  rclcpp::Time baseline_time_;
-
-  bool baseline_pose_set_{false};
   // Dynamic parameters handler
   rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
   std::string plugin_name_;
@@ -77,4 +65,4 @@ protected:
 };
 }  // namespace nav2_controller
 
-#endif  // NAV2_CONTROLLER__PLUGINS__SIMPLE_PROGRESS_CHECKER_HPP_
+#endif  // NAV2_CONTROLLER__PLUGINS__ROTATION_PROGRESS_CHECKER_HPP_
--- nav2_controller/plugins/rotation_progress_checker.cpp
+++ nav2_controller/plugins/rotation_progress_checker.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2023 Dexory
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "nav2_controller/plugins/simple_progress_checker.hpp"
+#include "nav2_controller/plugins/rotation_progress_checker.hpp"
 #include <cmath>
 #include <string>
 #include <memory>
 #include <vector>
+#include "angles/angles.h"
 #include "nav_2d_utils/conversions.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/pose2_d.hpp"
@@ -28,73 +29,53 @@ using std::placeholders::_1;
 
 namespace nav2_controller
 {
-void SimpleProgressChecker::initialize(
+
+void RotationProgressChecker::initialize(
   const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
   const std::string & plugin_name)
 {
   plugin_name_ = plugin_name;
+  SimpleProgressChecker::initialize(parent, plugin_name);
   auto node = parent.lock();
 
-  clock_ = node->get_clock();
-
   nav2_util::declare_parameter_if_not_declared(
-    node, plugin_name + ".required_movement_radius", rclcpp::ParameterValue(0.5));
-  nav2_util::declare_parameter_if_not_declared(
-    node, plugin_name + ".movement_time_allowance", rclcpp::ParameterValue(10.0));
-  // Scale is set to 0 by default, so if it was not set otherwise, set to 0
-  node->get_parameter_or(plugin_name + ".required_movement_radius", radius_, 0.5);
-  double time_allowance_param = 0.0;
-  node->get_parameter_or(plugin_name + ".movement_time_allowance", time_allowance_param, 10.0);
-  time_allowance_ = rclcpp::Duration::from_seconds(time_allowance_param);
+    node, plugin_name + ".required_movement_angle", rclcpp::ParameterValue(0.5));
+  node->get_parameter_or(plugin_name + ".required_movement_angle", required_movement_angle_, 0.5);
 
   // Add callback for dynamic parameters
   dyn_params_handler_ = node->add_on_set_parameters_callback(
-    std::bind(&SimpleProgressChecker::dynamicParametersCallback, this, _1));
+    std::bind(&RotationProgressChecker::dynamicParametersCallback, this, _1));
 }
 
-bool SimpleProgressChecker::check(geometry_msgs::msg::PoseStamped & current_pose)
+bool RotationProgressChecker::check(geometry_msgs::msg::PoseStamped & current_pose)
 {
   // relies on short circuit evaluation to not call is_robot_moved_enough if
   // baseline_pose is not set.
   geometry_msgs::msg::Pose2D current_pose2d;
   current_pose2d = nav_2d_utils::poseToPose2D(current_pose.pose);
 
-  if ((!baseline_pose_set_) || (is_robot_moved_enough(current_pose2d))) {
+  if ((!baseline_pose_set_) || (RotationProgressChecker::is_robot_moved_enough(current_pose2d))) {
     reset_baseline_pose(current_pose2d);
     return true;
   }
   return !((clock_->now() - baseline_time_) > time_allowance_);
 }
 
-void SimpleProgressChecker::reset()
+bool RotationProgressChecker::is_robot_moved_enough(const geometry_msgs::msg::Pose2D & pose)
 {
-  baseline_pose_set_ = false;
+  return pose_distance(pose, baseline_pose_) > radius_ ||
+         pose_angle_distance(pose, baseline_pose_) > required_movement_angle_;
 }
 
-void SimpleProgressChecker::reset_baseline_pose(const geometry_msgs::msg::Pose2D & pose)
-{
-  baseline_pose_ = pose;
-  baseline_time_ = clock_->now();
-  baseline_pose_set_ = true;
-}
-
-bool SimpleProgressChecker::is_robot_moved_enough(const geometry_msgs::msg::Pose2D & pose)
-{
-  return pose_distance(pose, baseline_pose_) > radius_;
-}
-
-double SimpleProgressChecker::pose_distance(
+double RotationProgressChecker::pose_angle_distance(
   const geometry_msgs::msg::Pose2D & pose1,
   const geometry_msgs::msg::Pose2D & pose2)
 {
-  double dx = pose1.x - pose2.x;
-  double dy = pose1.y - pose2.y;
-
-  return std::hypot(dx, dy);
+  return abs(angles::shortest_angular_distance(pose1.theta,pose2.theta));
 }
 
 rcl_interfaces::msg::SetParametersResult
-SimpleProgressChecker::dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters)
+RotationProgressChecker::dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters)
 {
   rcl_interfaces::msg::SetParametersResult result;
   for (auto parameter : parameters) {
@@ -102,10 +83,8 @@ SimpleProgressChecker::dynamicParametersCallback(std::vector<rclcpp::Parameter> 
     const auto & name = parameter.get_name();
 
     if (type == ParameterType::PARAMETER_DOUBLE) {
-      if (name == plugin_name_ + ".required_movement_radius") {
-        radius_ = parameter.as_double();
-      } else if (name == plugin_name_ + ".movement_time_allowance") {
-        time_allowance_ = rclcpp::Duration::from_seconds(parameter.as_double());
+      if (name == plugin_name_ + ".required_movement_angle") {
+        required_movement_angle_ = parameter.as_double();
       }
     }
   }
@@ -115,4 +94,4 @@ SimpleProgressChecker::dynamicParametersCallback(std::vector<rclcpp::Parameter> 
 
 }  // namespace nav2_controller
 
-PLUGINLIB_EXPORT_CLASS(nav2_controller::SimpleProgressChecker, nav2_core::ProgressChecker)
+PLUGINLIB_EXPORT_CLASS(nav2_controller::RotationProgressChecker, nav2_core::ProgressChecker)
