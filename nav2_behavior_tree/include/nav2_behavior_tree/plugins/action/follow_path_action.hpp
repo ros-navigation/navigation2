a05959dbee7e62b33a962aea8725e58b05a308c9
Guillaume Doisy <guillaume@dexory.com>
2023-05-03 15:15:59 +0100

    allow multiple progress checker plugin

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/follow_path_action.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/follow_path_action.hpp
@@ -84,6 +84,7 @@ public:
         BT::InputPort<nav_msgs::msg::Path>("path", "Path to follow"),
         BT::InputPort<std::string>("controller_id", ""),
         BT::InputPort<std::string>("goal_checker_id", ""),
+        BT::InputPort<std::string>("progress_checker_id", ""),
         BT::OutputPort<ActionResult::_error_code_type>(
           "error_code_id", "The follow path error code"),
       });
