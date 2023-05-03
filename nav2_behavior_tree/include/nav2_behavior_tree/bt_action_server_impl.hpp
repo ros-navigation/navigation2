21052e5402719a6c0c9884be4bb91ac93d38c34e
Johannes Plapp <johannes.plapp@logivations.com>
2023-05-03 08:20:54 +0200

    AMRNAV-4638 behavior tree logs

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
@@ -243,8 +243,10 @@ void BtActionServer<ActionT>::executeCallback()
     };
 
   // Execute the BT that was previously created in the configure step
-  nav2_behavior_tree::BtStatus rc = bt_->run(tree_, on_loop, is_canceling, bt_loop_duration_);
+  nav2_behavior_tree::BtStatus rc = bt_->run(tree_, on_loop, is_canceling, logger_, bt_loop_duration_);
 
+
+  topic_logger_->flush();
   // Make sure that the Bt is not in a running state from a previous execution
   // note: if all the ControlNodes are implemented correctly, this is not needed.
   bt_->haltAllActions(tree_->rootNode());
@@ -261,7 +263,7 @@ void BtActionServer<ActionT>::executeCallback()
       break;
 
     case nav2_behavior_tree::BtStatus::FAILED:
-      RCLCPP_ERROR(logger_, "Goal failed");
+      RCLCPP_ERROR(logger_, "Goal failed. New");
       action_server_->terminate_current(result);
       break;
 
