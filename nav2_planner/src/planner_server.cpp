012c76c0bb9433342e2eda66d5c859d653aade45
Steve Macenski <stevenmacenski@gmail.com>
2023-06-15 16:32:09 -0700

    Update planner_server.cpp

--- nav2_planner/src/planner_server.cpp
+++ nav2_planner/src/planner_server.cpp
@@ -62,9 +62,6 @@ PlannerServer::PlannerServer(const rclcpp::NodeOptions & options)
   // Setup the global costmap
   costmap_ros_ = std::make_shared<nav2_costmap_2d::Costmap2DROS>(
     "global_costmap", std::string{get_namespace()}, "global_costmap");
-
-  // Launch a thread to run the costmap node
-  costmap_thread_ = std::make_unique<nav2_util::NodeThread>(costmap_ros_);
 }
 
 PlannerServer::~PlannerServer()
