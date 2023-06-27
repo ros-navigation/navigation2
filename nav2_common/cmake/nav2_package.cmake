7479b40fac0b6161e028eae696c1c2c9967b8e3a
Steve Macenski <stevenmacenski@gmail.com>
2023-06-27 12:26:17 -0700

    Resolves 3646: Update CMAKE_COMPILER_IS_GNUCXX (#3662)
    * Resolves 3646: Update CMAKE_COMPILER_IS_GNUCXX
    
    * Update CMakeLists.txt
    
    Signed-off-by: enricosutera <enricosutera@outlook.com>

--- nav2_common/cmake/nav2_package.cmake
+++ nav2_common/cmake/nav2_package.cmake
@@ -36,7 +36,7 @@ macro(nav2_package)
     endif()
   endif()
 
-  if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     add_compile_options(-Wall -Wextra -Wpedantic -Werror -Wdeprecated -fPIC -Wshadow -Wnull-dereference)
     add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-Wnon-virtual-dtor>")
   endif()
