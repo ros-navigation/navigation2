c635523496bb23d31f7c574a5a600ef749b41437
stevemacenski <stevenmacenski@gmail.com>
2023-06-30 11:01:13 -0700

    fixing last issue
    Signed-off-by: jncfa <20467009+jncfa@users.noreply.github.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/navigate_through_poses_action.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/navigate_through_poses_action.hpp
@@ -55,7 +55,8 @@ public:
   {
     return providedBasicPorts(
       {
-        BT::InputPort<geometry_msgs::msg::PoseStamped>("goals", "Destinations to plan through"),
+        BT::InputPort<std::vector<geometry_msgs::msg::PoseStamped>>(
+          "goals", "Destinations to plan through"),
         BT::InputPort<std::string>("behavior_tree", "Behavior tree to run"),
       });
   }
