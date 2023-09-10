6fe265a8766b2229e4f0e41b9216cc5c0d153c52
pepisg <pedro.gonzalez@eia.edu.co>
2023-09-10 16:10:58 +0000

    Bring back from LL client

--- nav2_waypoint_follower/src/waypoint_follower.cpp
+++ nav2_waypoint_follower/src/waypoint_follower.cpp
@@ -94,6 +94,12 @@ WaypointFollower::on_configure(const rclcpp_lifecycle::State & /*state*/)
       this), nullptr, std::chrono::milliseconds(
       500), false, server_options);
 
+  from_ll_to_map_client_ = std::make_unique<
+    nav2_util::ServiceClient<robot_localization::srv::FromLL,
+    std::shared_ptr<nav2_util::LifecycleNode>>>(
+    "/fromLL",
+    node);
+
   gps_action_server_ = std::make_unique<ActionServerGPS>(
     get_node_base_interface(),
     get_node_clock_interface(),
