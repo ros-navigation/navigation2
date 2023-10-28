52d36f6923b39ba7b590bc42b841006194544dbe
Steve Macenski <stevenmacenski@gmail.com>
2023-10-28 15:24:38 -0700

    Log if BT rate is exceeded (#3909)
    Signed-off-by: enricosutera <enricosutera@outlook.com>

--- nav2_behavior_tree/src/behavior_tree_engine.cpp
+++ nav2_behavior_tree/src/behavior_tree_engine.cpp
@@ -55,7 +55,12 @@ BehaviorTreeEngine::run(
 
       onLoop();
 
-      loopRate.sleep();
+      if (!loopRate.sleep()) {
+        RCLCPP_WARN(
+          rclcpp::get_logger("BehaviorTreeEngine"),
+          "Behavior Tree tick rate %0.2f was exceeded!",
+          1.0 / (loopRate.period().count() * 1.0e-9));
+      }
     }
   } catch (const std::exception & ex) {
     RCLCPP_ERROR(
