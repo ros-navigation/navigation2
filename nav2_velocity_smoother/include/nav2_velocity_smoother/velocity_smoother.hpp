9306ce52700162df809ec1a62af591e3db187b22
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-10-30 21:03:56 -0600

    Use TwistSubscriber in velocity smoother
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- nav2_velocity_smoother/include/nav2_velocity_smoother/velocity_smoother.hpp
+++ nav2_velocity_smoother/include/nav2_velocity_smoother/velocity_smoother.hpp
@@ -26,6 +26,9 @@
 #include "nav2_util/node_utils.hpp"
 #include "nav2_util/odometry_utils.hpp"
 #include "nav2_util/robot_utils.hpp"
+#include "nav2_util/twist_subscriber.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
 
 namespace nav2_velocity_smoother
 {
@@ -114,6 +117,7 @@ protected:
    * @param msg Twist message
    */
   void inputCommandCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
+  void inputCommandStampedCallback(const geometry_msgs::msg::TwistStamped::ConstSharedPtr msg);
 
   /**
    * @brief Main worker timer function
@@ -131,7 +135,7 @@ protected:
   std::unique_ptr<nav2_util::OdomSmoother> odom_smoother_;
   rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr
     smoothed_cmd_pub_;
-  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_sub_;
+  std::shared_ptr<nav2_util::TwistSubscriber> cmd_sub_;
   rclcpp::TimerBase::SharedPtr timer_;
 
   rclcpp::Clock::SharedPtr clock_;
