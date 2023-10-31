32221df48185e0001d55e537454af5b1f3523d01
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-10-31 17:41:49 +0100

    Fix nav2_bringup flake8 tests (#3893)
    Signed-off-by: Ignacio Vizzo <ignacio@dexory.com>
    Signed-off-by: enricosutera <enricosutera@outlook.com>

--- nav2_bringup/launch/cloned_multi_tb3_simulation_launch.py
+++ nav2_bringup/launch/cloned_multi_tb3_simulation_launch.py
@@ -14,6 +14,7 @@
 
 
 import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (DeclareLaunchArgument, ExecuteProcess, GroupAction,
