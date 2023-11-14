f815739d8b870314c9c9fbedbf9d635b0752128a
GoesM <130988564+GoesM@users.noreply.github.com>
2023-11-14 19:25:57 +0800

    add lock

--- nav2_costmap_2d/src/costmap_2d_ros.cpp
+++ nav2_costmap_2d/src/costmap_2d_ros.cpp
@@ -356,7 +356,7 @@ nav2_util::CallbackReturn
 Costmap2DROS::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 {
   RCLCPP_INFO(get_logger(), "Cleaning up");
-  std::unique_lock<Costmap2D::mutex_t> lock(*(layered_costmap_->getCostmap()->getMutex()));
+
   costmap_publisher_.reset();
   clear_costmap_service_.reset();
 
