4e941c2f2825328c5ff55ef3dd8df2cdb823b942
magda skoczen <m.sko@o2.pl>
2023-11-15 20:11:10 +0100

    WIP int test

--- nav2_costmap_2d/test/integration/test_costmap_subscriber.cpp
+++ nav2_costmap_2d/test/integration/test_costmap_subscriber.cpp
@@ -11,14 +11,12 @@ public:
   ~RclCppFixture() {rclcpp::shutdown();}
 };
 RclCppFixture g_rclcppfixture;
-
 class TestCostmapSubscriberShould : public ::testing::Test
 {
 public:
   TestCostmapSubscriberShould()
   : subscriberNode(nav2_util::LifecycleNode::make_shared("test_subscriber"))
   {
-
   }
 
   void SetUp() override
@@ -27,39 +25,6 @@ public:
     updateCostmapMsgCount = 0;
 
     topicName = "/costmap";
-
-    subscriberThread = std::thread(
-      [this]()
-      {
-        auto localExecutor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-        executor = localExecutor;
-
-
-        localExecutor->add_node(subscriberNode->get_node_base_interface());
-
-        auto costmap_sub = subscriberNode->create_subscription<nav2_msgs::msg::Costmap>(
-          topicName + "_raw", 10,
-          std::bind(&TestCostmapSubscriberShould::costmapCallback, this, std::placeholders::_1));
-
-        auto costmap_update_sub = subscriberNode->create_subscription<nav2_msgs::msg::CostmapUpdate>(
-          topicName + "_raw_updates", 10, std::bind(
-            &TestCostmapSubscriberShould::costmapUpdateCallback, this,
-            std::placeholders::_1));
-
-        // costmapSubscriber =
-        // std::make_shared<nav2_costmap_2d::CostmapSubscriber>(subscriberNode, topicName + "_raw");
-
-        localExecutor->spin();
-      }
-    );
-  }
-
-  void TearDown() override
-  {
-    if (std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> exec = executor.lock()) {
-      exec->cancel();
-    }
-    subscriberThread.join();
   }
 
   void costmapCallback(const nav2_msgs::msg::Costmap::SharedPtr)
@@ -76,47 +41,88 @@ protected:
   int updateCostmapMsgCount;
   std::string topicName;
 
-  std::weak_ptr<nav2_costmap_2d::CostmapSubscriber> costmapSubscriber;
-
   std::thread subscriberThread;
   std::weak_ptr<rclcpp::executors::SingleThreadedExecutor> executor;
   nav2_util::LifecycleNode::SharedPtr subscriberNode;
-
   rclcpp::Logger logger {rclcpp::get_logger("test_costmap_subscriber_should")};
 };
 
-TEST_F(TestCostmapSubscriberShould, handleFullCostmapMsgs)
+TEST_F(TestCostmapSubscriberShould, handleCostmapUpdateMsgs)
 {
-  std::vector<std::vector<std::uint8_t>> expectedCostmaps, recievedCostmaps;
+  // std::vector<std::vector<std::uint8_t>> expectedCostmaps(4, std::vector<std::uint8_t>(100, 0)),
+  // recievedCostmaps(4, std::vector<std::uint8_t>(100, 0));
+  auto dummyCostmapMsgSubscriber = subscriberNode->create_subscription<nav2_msgs::msg::Costmap>(
+    topicName + "_raw", 10,
+    std::bind(&TestCostmapSubscriberShould::costmapCallback, this, std::placeholders::_1));
 
-  bool always_send_full_costmap = true;
+  auto dummyCostmapUpdateMsgSubscriber =
+    subscriberNode->create_subscription<nav2_msgs::msg::CostmapUpdate>(
+    topicName + "_raw_updates", 10, std::bind(
+      &TestCostmapSubscriberShould::costmapUpdateCallback, this,
+      std::placeholders::_1));
 
+
+  auto costmapSubscriber =
+    std::make_shared<nav2_costmap_2d::CostmapSubscriber>(subscriberNode, topicName + "_raw");
+
+  bool always_send_full_costmap = false;
   auto costmap = std::make_shared<nav2_costmap_2d::Costmap2D>(10, 10, 1.0, 0.0, 0.0);
-
   nav2_util::LifecycleNode::SharedPtr publisherNode = nav2_util::LifecycleNode::make_shared(
     "test_publisher");
-
   auto costmapPublisher = std::make_unique<nav2_costmap_2d::Costmap2DPublisher>(
     publisherNode, costmap.get(), "", topicName, always_send_full_costmap);
-
   costmapPublisher->on_activate();
 
-  costmapPublisher->publishCostmap();
-  rclcpp::Rate(0.1).sleep();
 
   costmapPublisher->publishCostmap();
+  rclcpp::spin_some(subscriberNode->get_node_base_interface());
   rclcpp::Rate(0.1).sleep();
 
+
+  auto currentSubscriberCostmap = costmapSubscriber->getCostmap();
+
+  RCLCPP_WARN_STREAM(
+    logger,
+    "CURRENT COSTMAP SIZE " << currentSubscriberCostmap->getSizeInCellsX());
+
+  costmap->setCost(2, 2, 255);
+  costmap->setCost(2, 3, 255);
+  costmap->setCost(3, 2, 255);
+  costmapPublisher->updateBounds(2, 4, 2, 4);
   costmapPublisher->publishCostmap();
+  rclcpp::spin_some(subscriberNode->get_node_base_interface());
+  rclcpp::Rate(0.1).sleep();
+  auto currentSubscriberCostmap2 = costmapSubscriber->getCostmap();
+
+
+  costmap->setCost(7, 7, 255);
+  costmap->setCost(7, 8, 255);
+  costmapPublisher->updateBounds(7, 8, 7, 9);
+  costmapPublisher->publishCostmap();
+  rclcpp::spin_some(subscriberNode->get_node_base_interface());
   rclcpp::Rate(0.1).sleep();
 
-  ASSERT_EQ(fullCostmapMsgCount, 3);
-  ASSERT_EQ(updateCostmapMsgCount, 0);
+
+  costmap->setCost(2, 2, 0);
+  costmap->setCost(2, 3, 0);
+  costmap->setCost(3, 2, 0);
+  costmapPublisher->updateBounds(2, 4, 2, 4);
+  costmapPublisher->publishCostmap();
+  rclcpp::spin_some(subscriberNode->get_node_base_interface());
+  rclcpp::Rate(0.1).sleep();
+
+  ASSERT_EQ(fullCostmapMsgCount, 1);
+  ASSERT_EQ(updateCostmapMsgCount, 3);
 
   costmapPublisher->on_deactivate();
 }
 
-// TEST_F(TestCostmapSubscriberShould, handleCostmapUpdateMsgs)
+TEST_F(TestCostmapSubscriberShould, TestIfNotCollapsing)
+{
+  ASSERT_EQ(1, 1);
+}
+
+// TEST_F(TestCostmapSubscriberShould, handleFullCostmapMsgs)
 // {
 //   // std::vector<std::vector<std::uint8_t>> expectedCostmaps,recievedCostmaps;
 
