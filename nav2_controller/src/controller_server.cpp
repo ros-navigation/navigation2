b8e4f152f41d97a3208c9de5fb9eea9ab6fb95d6
Steve Macenski <stevenmacenski@gmail.com>
2023-11-20 12:41:01 -0800

    Enabling soft realtime prioritization to the Controller Server (#3914)
    * Enabling soft realtime prioritization to the controller server
    
    * abstracting to another function
    
    * changing default priorities
    
    * linting
    
    (cherry picked from commit fbe8f56042a07513630f50b563f5690a30b1346a)
    
    # Conflicts:
    #	nav2_controller/src/controller_server.cpp

--- nav2_controller/src/controller_server.cpp
+++ nav2_controller/src/controller_server.cpp
@@ -60,6 +60,7 @@ ControllerServer::ControllerServer(const rclcpp::NodeOptions & options)
   declare_parameter("speed_limit_topic", rclcpp::ParameterValue("speed_limit"));
 
   declare_parameter("failure_tolerance", rclcpp::ParameterValue(0.0));
+  declare_parameter("use_realtime_priority", rclcpp::ParameterValue(false));
 
   // The costmap node is used in the implementation of the controller
   costmap_ros_ = std::make_shared<nav2_costmap_2d::Costmap2DROS>(
@@ -119,6 +120,7 @@ ControllerServer::on_configure(const rclcpp_lifecycle::State & /*state*/)
   std::string speed_limit_topic;
   get_parameter("speed_limit_topic", speed_limit_topic);
   get_parameter("failure_tolerance", failure_tolerance_);
+  get_parameter("use_realtime_priority", use_realtime_priority_);
 
   costmap_ros_->configure();
   // Launch a thread to run the costmap node
@@ -196,6 +198,7 @@ ControllerServer::on_configure(const rclcpp_lifecycle::State & /*state*/)
   vel_publisher_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 1);
 
   // Create the action server that we implement with our followPath method
+<<<<<<< HEAD
   action_server_ = std::make_unique<ActionServer>(
     shared_from_this(),
     "follow_path",
@@ -203,6 +206,21 @@ ControllerServer::on_configure(const rclcpp_lifecycle::State & /*state*/)
     nullptr,
     std::chrono::milliseconds(500),
     true);
+=======
+  // This may throw due to real-time prioritzation if user doesn't have real-time permissions
+  try {
+    action_server_ = std::make_unique<ActionServer>(
+      shared_from_this(),
+      "follow_path",
+      std::bind(&ControllerServer::computeControl, this),
+      nullptr,
+      std::chrono::milliseconds(500),
+      true /*spin thread*/, server_options, use_realtime_priority_ /*soft realtime*/);
+  } catch (const std::runtime_error & e) {
+    RCLCPP_ERROR(get_logger(), "Error creating action server! %s", e.what());
+    return nav2_util::CallbackReturn::FAILURE;
+  }
+>>>>>>> fbe8f560 (Enabling soft realtime prioritization to the Controller Server (#3914))
 
   // Set subscribtion to the speed limiting topic
   speed_limit_sub_ = create_subscription<nav2_msgs::msg::SpeedLimit>(
