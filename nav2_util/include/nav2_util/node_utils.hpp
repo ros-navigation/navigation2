8f58fb43401a2a07e0b7a966e96b83fc32b4f78d
stevemacenski <stevenmacenski@gmail.com>
2023-11-21 14:22:42 -0800

    removing API with rclcpp new version

--- nav2_util/include/nav2_util/node_utils.hpp
+++ nav2_util/include/nav2_util/node_utils.hpp
@@ -154,25 +154,6 @@ std::string get_plugin_type_param(
 }
 
 /**
- * @brief A method to copy all parameters from one node (parent) to another (child).
- * May throw parameter exceptions in error conditions
- * @param parent Node to copy parameters from
- * @param child Node to copy parameters to
- */
-template<typename NodeT1, typename NodeT2>
-void copy_all_parameters(const NodeT1 & parent, const NodeT2 & child)
-{
-  using Parameters = std::vector<rclcpp::Parameter>;
-  std::vector<std::string> param_names = parent->list_parameters({}, 0).names;
-  Parameters params = parent->get_parameters(param_names);
-  for (Parameters::const_iterator iter = params.begin(); iter != params.end(); ++iter) {
-    if (!child->has_parameter(iter->get_name())) {
-      child->declare_parameter(iter->get_name(), iter->get_parameter_value());
-    }
-  }
-}
-
-/**
  * @brief Sets the caller thread to have a soft-realtime prioritization by
  * increasing the priority level of the host thread.
  * May throw exception if unable to set prioritization successfully
