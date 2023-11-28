feeadcfc1be179a3900757f0951b7ab2b64acd20
Steve Macenski <stevenmacenski@gmail.com>
2023-11-28 14:14:05 -0800

    adding default collision monitor to bringup (#3989)
    * adding default collision monitor to bringup
    
    * Update nav2_multirobot_params_1.yaml
    
    * adding to system test files
    
    * adjust  for test

--- nav2_bringup/launch/navigation_launch.py
+++ nav2_bringup/launch/navigation_launch.py
@@ -45,9 +45,10 @@ def generate_launch_description():
         'smoother_server',
         'planner_server',
         'behavior_server',
+        'velocity_smoother',
+        'collision_monitor',
         'bt_navigator',
         'waypoint_follower',
-        'velocity_smoother',
     ]
 
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
@@ -198,7 +199,18 @@ def generate_launch_description():
                 parameters=[configured_params],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings
-                + [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')],
+                + [('cmd_vel', 'cmd_vel_nav')],
+            ),
+            Node(
+                package='nav2_collision_monitor',
+                executable='collision_monitor',
+                name='collision_monitor',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings,
             ),
             Node(
                 package='nav2_lifecycle_manager',
@@ -266,7 +278,14 @@ def generate_launch_description():
                         name='velocity_smoother',
                         parameters=[configured_params],
                         remappings=remappings
-                        + [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')],
+                        + [('cmd_vel', 'cmd_vel_nav')],
+                    ),
+                    ComposableNode(
+                        package='nav2_collision_monitor',
+                        plugin='nav2_collision_monitor::CollisionMonitor',
+                        name='collision_monitor',
+                        parameters=[configured_params],
+                        remappings=remappings,
                     ),
                     ComposableNode(
                         package='nav2_lifecycle_manager',
