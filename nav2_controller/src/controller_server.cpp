aa9396e6c805bc787d8a7f3a46bfc16b1315939b
GoesM <130988564+GoesM@users.noreply.github.com>
2023-12-01 13:49:21 +0800

    fix bug mentioned in #3958 (#3972)
    * bug fixed
    
    * add space
    
    * Update planner_server.cpp
    
    * add space for code style
    
    * add childLifecycleNode mode to costmap_2d_ros
    
    * add childLifecycleNode mode to costmap_2d_ros
    
    * add childLifecycleNode mode to costmap_2d_ros
    
    * add childLifecycleNode mode in costmap_2d_ros
    
    * add childLifecycleNode mode in costmap_2d_ros
    
    * add childLifecycleNode mode in costmap_2d_ros
    
    * add ChildLifecycleNode mode in costmap_2d_ros
    
    * NodeOption: is_lifecycle_follower_
    
    * NodeOption: is_lifecycle_follower_
    
    * fit to NodeOption: is_lifecycle_follower_
    
    * NodeOption: is_lifecycle_follower_
    
    * fit to NodeOption: is_lifecycle_follower
    
    * fit to NodeOption: is_lifecycle_follower
    
    * fit reorder Werror
    
    * fix wrong use of is_lifecycle_follower
    
    * remove blank line
    
    * NodeOption: is_lifecycle_follower_
    
    * NodeOption: is_lifecycle_follower_
    
    * Add files via upload
    
    * NodeOption: is_lifecycle_follower_
    
    * NodeOption:is_lifecycle_follower_
    
    * NodeOption:is_lifecycle_follower
    
    * NodeOption:is_lifecycle_follower
    
    * NodeOption:is_lifecycle_follower
    
    * change default
    
    * add NodeOption for costmap_2d_ros
    
    * add node options for costmap2dros as an independent node
    
    * code style reformat
    
    * fit to NodeOption of Costmap2DROS
    
    * fit to NodeOption of Costmap2DROS
    
    * fit to NodeOption of Costmap2DROS
    
    * Update nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_ros.hpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * Update nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_ros.hpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * Update nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_ros.hpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * changes
    
    * comment changes
    
    * change get_parameter into =false
    
    * comment modification
    
    * missing line
    
    * Update nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_ros.hpp
    
    * Update nav2_costmap_2d/include/nav2_costmap_2d/costmap_2d_ros.hpp
    
    * delete last line
    
    * change lifecycle_test fit to NodeOption
    
    ---------
    
    Co-authored-by: GoesM <GoesM@buaa.edu.cn>
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_controller/src/controller_server.cpp
+++ nav2_controller/src/controller_server.cpp
@@ -290,11 +290,7 @@ ControllerServer::on_deactivate(const rclcpp_lifecycle::State & /*state*/)
    * unordered_set iteration. Once this issue is resolved, we can maybe make a stronger
    * ordering assumption: https://github.com/ros2/rclcpp/issues/2096
    */
-  if (costmap_ros_->get_current_state().id() ==
-    lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
-  {
-    costmap_ros_->deactivate();
-  }
+  costmap_ros_->deactivate();
 
   publishZeroVelocity();
   vel_publisher_->on_deactivate();
@@ -320,11 +316,9 @@ ControllerServer::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 
   goal_checkers_.clear();
   progress_checkers_.clear();
-  if (costmap_ros_->get_current_state().id() ==
-    lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE)
-  {
-    costmap_ros_->cleanup();
-  }
+
+  costmap_ros_->cleanup();
+
 
   // Release any allocated resources
   action_server_.reset();
