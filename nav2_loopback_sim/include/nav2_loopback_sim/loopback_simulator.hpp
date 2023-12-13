7e4eda2ed8404ae382e8bba6d9f798e6777c0c8a
smitdumore <smit.dumore17@vit.edu>
2023-12-13 12:17:35 -0500

    updated copyright

--- nav2_loopback_sim/include/nav2_loopback_sim/loopback_simulator.hpp
+++ nav2_loopback_sim/include/nav2_loopback_sim/loopback_simulator.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Samsung Research America
+// Copyright (c) 2023 Smit Dumore
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
