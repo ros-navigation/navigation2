7f3d93c3f230b10db85a9e2bff792f3b921ce5b5
Abhijith (AJ) Sampathkrishna <abhijith@dexory.com>
2023-12-20 17:13:32 +0000

    moving libraries and using parser in collision monitor

--- nav2_collision_monitor/include/nav2_collision_monitor/polygon.hpp
+++ nav2_collision_monitor/include/nav2_collision_monitor/polygon.hpp
@@ -210,6 +210,14 @@ protected:
    */
   bool isPointInside(const Point & point) const;
 
+  /**
+   * @brief Extracts Polygon points from a string with of the form [[x1,y1],[x2,y2],[x3,y3]...]
+   * @param poly_string Input String containing the verteceis of the polygon
+   * @param polygon Output Point vector with all the vertecies of the polygon
+   * @return True if all parameters were obtained or false in failure case
+   */
+  bool getPolygonFromString(std::string & poly_string, std::vector<Point> & polygon);
+
   // ----- Variables -----
 
   /// @brief Collision Monitor node
