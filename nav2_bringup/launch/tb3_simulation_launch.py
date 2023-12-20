b2deaee4e39e7d7e25ad39371d910b7a3f2c1343
Addisu Z. Taddese <addisu@openrobotics.org>
2023-12-20 16:43:07 -0600

    Support Garden and later, performance improvements
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- nav2_bringup/launch/tb3_simulation_launch.py
+++ nav2_bringup/launch/tb3_simulation_launch.py
@@ -181,6 +181,8 @@ def generate_launch_description():
             'libgazebo_ros_init.so',
             '-s',
             'libgazebo_ros_factory.so',
+            '-e',
+            'dart',
             world,
         ],
         cwd=[launch_dir],
@@ -293,6 +295,6 @@ def generate_launch_description():
     # Add the actions to launch all of the navigation nodes
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(rviz_cmd)
-    # ld.add_action(bringup_cmd)
+    ld.add_action(bringup_cmd)
 
     return ld
