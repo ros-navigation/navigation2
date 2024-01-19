5d8d50267f56c927b89e84b57b5e98d6f9518b24
maksymdidukh <maxym.didukh@logivations.com>
2024-01-19 12:07:38 +0100

    revert changes in distance_traveled

--- nav2_behavior_tree/plugins/condition/distance_traveled_condition.cpp
+++ nav2_behavior_tree/plugins/condition/distance_traveled_condition.cpp
@@ -34,17 +34,17 @@ DistanceTraveledCondition::DistanceTraveledCondition(
   robot_base_frame_("base_link"),
   initialized_(false)
 {
-  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
-  tf_ = config().blackboard->get<std::shared_ptr<tf2_ros::Buffer>>("tf_buffer");
 }
 
 void DistanceTraveledCondition::initialize()
 {
-  node_->get_parameter("transform_tolerance", transform_tolerance_);
   
   getInput("distance", distance_);
   getInput("global_frame", global_frame_);
   getInput("robot_base_frame", robot_base_frame_);
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+  tf_ = config().blackboard->get<std::shared_ptr<tf2_ros::Buffer>>("tf_buffer");
+  node_->get_parameter("transform_tolerance", transform_tolerance_);
   
   initialized_ = true;
 }
