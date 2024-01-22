274ea19dd5745a51be9651a8054e1ede38cadbbf
Davide Faconti <davide.faconti@gmail.com>
2024-01-22 18:15:23 +0100

    remove unused header
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/behavior_tree_engine.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/behavior_tree_engine.hpp
@@ -23,7 +23,6 @@
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
 #include "behaviortree_cpp_v3/xml_parsing.h"
-#include "behaviortree_cpp_v3/loggers/bt_zmq_publisher.h"
 
 #include "rclcpp/rclcpp.hpp"
 
