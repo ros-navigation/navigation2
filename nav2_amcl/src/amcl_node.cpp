06ce6f4ca074e68320b9d22d32f197f4ceb0bb89
GoesM <GoesM@buaa.edu.cn>
2024-01-26 00:31:00 +0800

    change reset-order in nav2_amcl

--- nav2_amcl/src/amcl_node.cpp
+++ nav2_amcl/src/amcl_node.cpp
@@ -328,6 +328,8 @@ AmclNode::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
   nomotion_update_srv_.reset();
   initial_pose_sub_.reset();
   laser_scan_connection_.disconnect();
+
+  tf_listener_.reset(); // tf_listener_ will access laser_scan_filter, so it should be reset earlier
   laser_scan_filter_.reset();
   laser_scan_sub_.reset();
 
@@ -341,7 +343,6 @@ AmclNode::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 
   // Transforms
   tf_broadcaster_.reset();
-  tf_listener_.reset();
   tf_buffer_.reset();
 
   // PubSub
