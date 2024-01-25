b5905a2475785ee7320bc99d51121d79550f12fc
GoesM <130988564+GoesM@users.noreply.github.com>
2024-01-26 05:51:32 +0800

    change pointer free order in amcl to avoid use-after-free bug mentioned in #4068 (#4070)
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    Co-authored-by: GoesM <GoesM@buaa.edu.cn>
    Signed-off-by: enricosutera <enricosutera@outlook.com>

--- nav2_amcl/src/amcl_node.cpp
+++ nav2_amcl/src/amcl_node.cpp
@@ -328,6 +328,7 @@ AmclNode::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
   nomotion_update_srv_.reset();
   initial_pose_sub_.reset();
   laser_scan_connection_.disconnect();
+  tf_listener_.reset();  //  listener may access lase_scan_filter_, so it should be reset earlier
   laser_scan_filter_.reset();
   laser_scan_sub_.reset();
 
@@ -341,7 +342,6 @@ AmclNode::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 
   // Transforms
   tf_broadcaster_.reset();
-  tf_listener_.reset();
   tf_buffer_.reset();
 
   // PubSub
