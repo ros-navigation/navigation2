29cf8a3d1160b5dec509f23de117c05509252468
Alberto Tudela <ajtudela@gmail.com>
2024-01-26 18:02:50 +0100

    Retain old headers
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- nav2_graceful_motion_controller/include/nav2_graceful_motion_controller/path_handler.hpp
+++ nav2_graceful_motion_controller/include/nav2_graceful_motion_controller/path_handler.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Alberto J. Tudela Roldán
+// Copyright (c) 2022 Samsung Research America
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
