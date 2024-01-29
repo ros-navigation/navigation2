effd2ec50f1b0ffbb25b8d473a63332e54c07628
BriceRenaudeau <48433002+BriceRenaudeau@users.noreply.github.com>
2024-01-29 14:38:01 +0100

    Update inflation_cost_critic.hpp
    - copyright
    - ifndef
    
    Signed-off-by: Brice <brice.renaudeau@gmail.com>

--- nav2_mppi_controller/include/nav2_mppi_controller/critics/inflation_cost_critic.hpp
+++ nav2_mppi_controller/include/nav2_mppi_controller/critics/inflation_cost_critic.hpp
@@ -1,5 +1,19 @@
-#ifndef VEH_NAV_ACTION__CRITICS__OBSTACLES_CRITIC_HPP_
-#define VEH_NAV_ACTION__CRITICS__OBSTACLES_CRITIC_HPP_
+// Copyright (c) 2023 Robocc Brice Renaudeau
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_MPPI_CONTROLLER__CRITICS__INFLATION_COST_CRITIC_HPP_
+#define NAV2_MPPI_CONTROLLER__CRITICS__INFLATION_COST_CRITIC_HPP_
 
 #include <memory>
 #include "nav2_costmap_2d/footprint_collision_checker.hpp"
@@ -76,4 +90,4 @@ protected:
 
 }  // namespace mppi::critics
 
-#endif  // VEH_NAV_ACTION__CRITICS__OBSTACLES_CRITIC_HPP_
+#endif  // NAV2_MPPI_CONTROLLER__CRITICS__INFLATION_COST_CRITIC_HPP_
