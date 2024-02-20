33ad593f95a776a4160a6311ae88ff3e333b13b1
Davide Faconti <davide.faconti@gmail.com>
2024-02-20 22:00:42 +0100

    [behavior_tree] Add generate_nav2_tree_nodes_xml (#4073)
    * Add generate_nav2_tree_nodes_xml
    
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>
    
    * behavior_tree: removed list of plugins from yaml
    
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>
    
    * check result of get_parameter(plugin_lib_names)
    
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>
    
    * fix previous commit (uncrustify)
    
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>
    
    * revert change
    
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>
    
    ---------
    
    Signed-off-by: Davide Faconti <davide.faconti@gmail.com>

--- nav2_bt_navigator/src/bt_navigator.cpp
+++ nav2_bt_navigator/src/bt_navigator.cpp
@@ -18,11 +18,14 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <boost/algorithm/string.hpp>
 
 #include "nav2_util/geometry_utils.hpp"
 #include "nav2_util/node_utils.hpp"
 #include "nav2_util/robot_utils.hpp"
 #include "nav2_behavior_tree/bt_utils.hpp"
+
+#include "nav2_behavior_tree/plugins_list.hpp"
 
 using nav2_util::declare_parameter_if_not_declared;
 
@@ -36,64 +39,8 @@ BtNavigator::BtNavigator(rclcpp::NodeOptions options)
 {
   RCLCPP_INFO(get_logger(), "Creating");
 
-  const std::vector<std::string> plugin_libs = {
-    "nav2_compute_path_to_pose_action_bt_node",
-    "nav2_compute_path_through_poses_action_bt_node",
-    "nav2_smooth_path_action_bt_node",
-    "nav2_follow_path_action_bt_node",
-    "nav2_spin_action_bt_node",
-    "nav2_wait_action_bt_node",
-    "nav2_assisted_teleop_action_bt_node",
-    "nav2_back_up_action_bt_node",
-    "nav2_drive_on_heading_bt_node",
-    "nav2_clear_costmap_service_bt_node",
-    "nav2_is_stuck_condition_bt_node",
-    "nav2_goal_reached_condition_bt_node",
-    "nav2_initial_pose_received_condition_bt_node",
-    "nav2_goal_updated_condition_bt_node",
-    "nav2_globally_updated_goal_condition_bt_node",
-    "nav2_is_path_valid_condition_bt_node",
-    "nav2_are_error_codes_active_condition_bt_node",
-    "nav2_would_a_controller_recovery_help_condition_bt_node",
-    "nav2_would_a_planner_recovery_help_condition_bt_node",
-    "nav2_would_a_smoother_recovery_help_condition_bt_node",
-    "nav2_reinitialize_global_localization_service_bt_node",
-    "nav2_rate_controller_bt_node",
-    "nav2_distance_controller_bt_node",
-    "nav2_speed_controller_bt_node",
-    "nav2_truncate_path_action_bt_node",
-    "nav2_truncate_path_local_action_bt_node",
-    "nav2_goal_updater_node_bt_node",
-    "nav2_recovery_node_bt_node",
-    "nav2_pipeline_sequence_bt_node",
-    "nav2_round_robin_node_bt_node",
-    "nav2_transform_available_condition_bt_node",
-    "nav2_time_expired_condition_bt_node",
-    "nav2_path_expiring_timer_condition",
-    "nav2_distance_traveled_condition_bt_node",
-    "nav2_single_trigger_bt_node",
-    "nav2_goal_updated_controller_bt_node",
-    "nav2_is_battery_low_condition_bt_node",
-    "nav2_navigate_through_poses_action_bt_node",
-    "nav2_navigate_to_pose_action_bt_node",
-    "nav2_remove_passed_goals_action_bt_node",
-    "nav2_planner_selector_bt_node",
-    "nav2_controller_selector_bt_node",
-    "nav2_goal_checker_selector_bt_node",
-    "nav2_controller_cancel_bt_node",
-    "nav2_path_longer_on_approach_bt_node",
-    "nav2_wait_cancel_bt_node",
-    "nav2_spin_cancel_bt_node",
-    "nav2_assisted_teleop_cancel_bt_node",
-    "nav2_back_up_cancel_bt_node",
-    "nav2_drive_on_heading_cancel_bt_node",
-    "nav2_is_battery_charging_condition_bt_node",
-    "nav2_progress_checker_selector_bt_node",
-    "nav2_smoother_selector_bt_node"
-  };
-
   declare_parameter_if_not_declared(
-    this, "plugin_lib_names", rclcpp::ParameterValue(plugin_libs));
+    this, "plugin_lib_names", rclcpp::ParameterValue(std::vector<std::string>{}));
   declare_parameter_if_not_declared(
     this, "transform_tolerance", rclcpp::ParameterValue(0.1));
   declare_parameter_if_not_declared(
@@ -126,7 +73,14 @@ BtNavigator::on_configure(const rclcpp_lifecycle::State & /*state*/)
   odom_topic_ = get_parameter("odom_topic").as_string();
 
   // Libraries to pull plugins (BT Nodes) from
-  auto plugin_lib_names = get_parameter("plugin_lib_names").as_string_array();
+  std::vector<std::string> plugin_lib_names;
+  boost::split(plugin_lib_names, nav2::details::BT_BUILTIN_PLUGINS, boost::is_any_of(";"));
+
+  auto user_defined_plugins = get_parameter("plugin_lib_names").as_string_array();
+  // append user_defined_plugins to plugin_lib_names
+  plugin_lib_names.insert(
+    plugin_lib_names.end(), user_defined_plugins.begin(),
+    user_defined_plugins.end());
 
   nav2_core::FeedbackUtils feedback_utils;
   feedback_utils.tf = tf_;
