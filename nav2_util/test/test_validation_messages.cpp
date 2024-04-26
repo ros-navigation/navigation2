0d315bcbd81a563fd80053319b5689f50adbc101
goes <GoesM@buaa.edu.cn>
2024-04-26 14:25:20 +0800

    change include-order
    Signed-off-by: goes <GoesM@buaa.edu.cn>

--- nav2_util/test/test_validation_messages.cpp
+++ nav2_util/test/test_validation_messages.cpp
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-#include "nav2_util/validate_messages.hpp"
 #include <gtest/gtest.h>
+#include "nav2_util/validate_messages.hpp"
 
 TEST(ValidateMessagesTest, DoubleValueCheck) {
   // Test valid double value
