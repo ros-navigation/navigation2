5c48aa08318fd21c8ce9cecfc3d0dbb18ce47a54
goes <GoesM@buaa.edu.cn>
2024-05-31 16:16:07 +0800

    reorder cleanup_queue
    Signed-off-by: goes <GoesM@buaa.edu.cn>

--- nav2_costmap_2d/src/costmap_2d_ros.cpp
+++ nav2_costmap_2d/src/costmap_2d_ros.cpp
@@ -354,6 +354,7 @@ nav2_util::CallbackReturn
 Costmap2DROS::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 {
   RCLCPP_INFO(get_logger(), "Cleaning up");
+  executor_thread_.reset();
 
   costmap_publisher_.reset();
   clear_costmap_service_.reset();
@@ -367,9 +368,7 @@ Costmap2DROS::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
 
   footprint_sub_.reset();
   footprint_pub_.reset();
-
-
-  executor_thread_.reset();
+ 
   return nav2_util::CallbackReturn::SUCCESS;
 }
 
