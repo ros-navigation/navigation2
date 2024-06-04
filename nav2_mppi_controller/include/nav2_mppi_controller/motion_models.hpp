3138f240c7aa57310f027f724aa410b8d6ce911c
Ayush Singh <64409716+Ayush1285@users.noreply.github.com>
2024-06-04 20:27:54 +0530

    Add acceleration constraints in the MPPI controller (#4352)
    * Check dynamic feasibility while predicting velocities
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Added y and z axes constraints
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Improved predict function, added acceleration related constraints in ControlConstraints
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Reduced lookups in predict function
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Fixed lint errors
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Fixed linter and build error
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Fixed a bug in predict function
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Added hard acceleration constraints
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Fixed unit test to consider acceleration constraints
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Updated default parameters
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    * Updated test for the sign of constraint value
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>
    
    ---------
    
    Signed-off-by: Ayush1285 <ayush.singhftw@gmail.com>

--- nav2_mppi_controller/include/nav2_mppi_controller/motion_models.hpp
+++ nav2_mppi_controller/include/nav2_mppi_controller/motion_models.hpp
@@ -16,9 +16,11 @@
 #define NAV2_MPPI_CONTROLLER__MOTION_MODELS_HPP_
 
 #include <cstdint>
+#include <string>
 
 #include "nav2_mppi_controller/models/control_sequence.hpp"
 #include "nav2_mppi_controller/models/state.hpp"
+#include "nav2_mppi_controller/models/constraints.hpp"
 
 // xtensor creates warnings that needs to be ignored as we are building with -Werror
 #pragma GCC diagnostic push
@@ -53,6 +55,17 @@ public:
   virtual ~MotionModel() = default;
 
   /**
+    * @brief Initialize motion model on bringup and set required variables
+    * @param control_constraints Constraints on control
+    * @param model_dt duration of a time step
+    */
+  void initialize(const models::ControlConstraints & control_constraints, float model_dt)
+  {
+    control_constraints_ = control_constraints;
+    model_dt_ = model_dt;
+  }
+
+  /**
    * @brief With input velocities, find the vehicle's output velocities
    * @param state Contains control velocities to use to populate vehicle velocities
    */
@@ -70,12 +83,30 @@ public:
     // }
 
     const bool is_holo = isHolonomic();
+    float max_delta_vx = model_dt_ * control_constraints_.ax_max;
+    float min_delta_vx = model_dt_ * control_constraints_.ax_min;
+    float max_delta_vy = model_dt_ * control_constraints_.ay_max;
+    float max_delta_wz = model_dt_ * control_constraints_.az_max;
     for (unsigned int i = 0; i != state.vx.shape(0); i++) {
+      float vx_last = state.vx(i, 0);
+      float vy_last = state.vy(i, 0);
+      float wz_last = state.wz(i, 0);
       for (unsigned int j = 1; j != state.vx.shape(1); j++) {
-        state.vx(i, j) = state.cvx(i, j - 1);
-        state.wz(i, j) = state.cwz(i, j - 1);
+        float & cvx_curr = state.cvx(i, j - 1);
+        cvx_curr = std::clamp(cvx_curr, vx_last + min_delta_vx, vx_last + max_delta_vx);
+        state.vx(i, j) = cvx_curr;
+        vx_last = cvx_curr;
+
+        float & cwz_curr = state.cwz(i, j - 1);
+        cwz_curr = std::clamp(cwz_curr, wz_last - max_delta_wz, wz_last + max_delta_wz);
+        state.wz(i, j) = cwz_curr;
+        wz_last = cwz_curr;
+
         if (is_holo) {
-          state.vy(i, j) = state.cvy(i, j - 1);
+          float & cvy_curr = state.cvy(i, j - 1);
+          cvy_curr = std::clamp(cvy_curr, vy_last - max_delta_vy, vy_last + max_delta_vy);
+          state.vy(i, j) = cvy_curr;
+          vy_last = cvy_curr;
         }
       }
     }
@@ -92,6 +123,11 @@ public:
    * @param control_sequence Control sequence to apply constraints to
    */
   virtual void applyConstraints(models::ControlSequence & /*control_sequence*/) {}
+
+protected:
+  float model_dt_{0.0};
+  models::ControlConstraints control_constraints_{0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f};
 };
 
 /**
