f192a3080f9b36d508a6c3dd424e6bf3a30ad475
GoesM <130988564+GoesM@users.noreply.github.com>
2024-06-11 23:43:02 +0800

    [amcl] check the recieved initialpose in the size of map (#4416)
    * reorder pointers-reset & remove rebundant code
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * check the initialpose in map
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * back
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * remove the redunbant code
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * code style
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * code style
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * code style
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * codestyle
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    
    * Update nav2_amcl/src/amcl_node.cpp
    
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * Update nav2_amcl/src/amcl_node.cpp
    
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    
    ---------
    
    Signed-off-by: GoesM <GoesM@buaa.edu.cn>
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    Co-authored-by: GoesM <GoesM@buaa.edu.cn>
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_amcl/src/amcl_node.cpp
+++ nav2_amcl/src/amcl_node.cpp
@@ -333,7 +333,6 @@ AmclNode::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
   global_loc_srv_.reset();
   initial_guess_srv_.reset();
   nomotion_update_srv_.reset();
-  executor_thread_.reset();  //  to make sure initial_pose_sub_ completely exit
   initial_pose_sub_.reset();
   laser_scan_connection_.disconnect();
   tf_listener_.reset();  //  listener may access lase_scan_filter_, so it should be reset earlier
