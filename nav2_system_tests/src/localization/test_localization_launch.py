8d0d5d5e7aa9b48018386ad9e41013e8169aa0ae
stevedan <stevedan.o.omodolor@gmail.com>
2024-06-14 21:47:58 +0200

    formmatting python
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>

--- nav2_system_tests/src/localization/test_localization_launch.py
+++ nav2_system_tests/src/localization/test_localization_launch.py
@@ -20,11 +20,11 @@ import sys
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch import LaunchService
-from launch.actions import ExecuteProcess
+from launch.actions import AppendEnvironmentVariable, ExecuteProcess
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch_ros.actions
 from launch_testing.legacy import LaunchTestService
-from launch.actions import AppendEnvironmentVariable, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def main(argv=sys.argv[1:]):
