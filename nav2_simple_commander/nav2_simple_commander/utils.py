d69fd361ec9ce487901c32aa3d0b18393bb6126a
stevedan <stevedan.o.omodolor@gmail.com>
2024-06-20 21:57:50 +0200

    correct year
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>

--- nav2_simple_commander/nav2_simple_commander/utils.py
+++ nav2_simple_commander/nav2_simple_commander/utils.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
-# Copyright 2021 Samsung Research America
-# Copyright 2022 Stevedan Ogochukwu Omodolor
+# Copyright 2024 Open Navigation LLC
+# Copyright 2024 Stevedan Ogochukwu Omodolor Omodia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
