134e747a8f93a2866febe16598bc7d858f5dcb75
goes <GoesM@buaa.edu.cn>
2024-07-17 01:24:21 +0800

    typo fixed
    Signed-off-by: goes <GoesM@buaa.edu.cn>

--- nav2_controller/src/controller_server.cpp
+++ nav2_controller/src/controller_server.cpp
@@ -295,7 +295,7 @@ ControllerServer::on_deactivate(const rclcpp_lifecycle::State & /*state*/)
   publishZeroVelocity();
   vel_publisher_->on_deactivate();
 
-  remove_on_set_parameters_callback(dyn_param_handler_.get());
+  remove_on_set_parameters_callback(dyn_params_handler_.get());
   dyn_params_handler_.reset();
 
   // destroy bond connection
