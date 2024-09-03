459721466a6f7f37b942d5d05e8a7b9c2938afc2
Alan <alan.xuefei@googlemail.com>
2024-09-04 06:20:36 +0800

    fix(bt_nodes): Correct default `server_timeout` behavior by using `getInputPortOrBlackboard()` (#4649)
    Signed-off-by: Alan Xue <alan.xuefei@googlemail.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_cancel_action_node.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_cancel_action_node.hpp
@@ -56,9 +56,7 @@ public:
     callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
 
     // Get the required items from the blackboard
-    server_timeout_ =
-      config().blackboard->template get<std::chrono::milliseconds>("server_timeout");
-    getInput<std::chrono::milliseconds>("server_timeout", server_timeout_);
+    getInputOrBlackboard("server_timeout", server_timeout_);
     wait_for_service_timeout_ =
       config().blackboard->template get<std::chrono::milliseconds>("wait_for_service_timeout");
 
