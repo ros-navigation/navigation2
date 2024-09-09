016326fac1a1052bd9c2d921472954505cdbb168
Steve Macenski <stevenmacenski@gmail.com>
2024-09-09 15:51:40 -0700

    Fixing #4661: MPPI ackermann reversing taking incorrect sign sometimes
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_mppi_controller/include/nav2_mppi_controller/motion_models.hpp
+++ nav2_mppi_controller/include/nav2_mppi_controller/motion_models.hpp
@@ -165,7 +165,7 @@ public:
     auto & wz = control_sequence.wz;
 
     auto view = xt::masked_view(wz, xt::fabs(vx) / xt::fabs(wz) < min_turning_r_);
-    view = xt::sign(wz) * vx / min_turning_r_;
+    view = xt::sign(wz) * xt::fabs(vx) / min_turning_r_;
   }
 
   /**
