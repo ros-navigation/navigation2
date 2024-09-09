5a8b46532231b0bdd9db2b521efe3bef0ece953d
huiyulhy <lhyleonghuiyu@gmail.com>
2024-09-08 18:23:12 -0700

    fix linting
    Signed-off-by: huiyulhy <lhyleonghuiyu@gmail.com>

--- nav2_dwb_controller/dwb_plugins/include/dwb_plugins/standard_traj_generator.hpp
+++ nav2_dwb_controller/dwb_plugins/include/dwb_plugins/standard_traj_generator.hpp
@@ -147,7 +147,7 @@ protected:
   /// @brief the name of the overlying plugin ID
   std::string plugin_name_;
 
-  /// @brief Option to limit velocity in the trajectory generator by using current velocity over current velocity in trajectory
+  /// @brief Option to limit velocity in the trajectory generator by using current velocity for trajectory
   bool limit_vel_cmd_in_traj_;
 
   /* Backwards Compatibility Parameter: include_last_point
