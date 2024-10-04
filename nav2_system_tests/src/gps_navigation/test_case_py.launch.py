5f730238c48b0f9ba6f44cec09f37102a6d3335a
Stevedan Ogochukwu Omodolor <61468301+stevedanomodolor@users.noreply.github.com>
2024-10-04 02:03:07 +0200

    Feat/migrate gps nav2 system test (#4682)
    * include missing docking station parameters
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * fix crach RL
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * lintering
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * Change naming
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * update submodule
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * minor changes
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * fix issue with caching
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * fix issue with caching, increase version number
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * Pin git ref via sha to bust underlay workspace cache
    
    ---------
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    Co-authored-by: ruffsl <roxfoxpox@gmail.com>
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>

--- nav2_system_tests/src/gps_navigation/test_case_py.launch.py
+++ nav2_system_tests/src/gps_navigation/test_case_py.launch.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import os
+from pathlib import Path
 import sys
 
 from ament_index_python.packages import get_package_share_directory
@@ -21,6 +22,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch import LaunchService
 from launch.actions import (
+    AppendEnvironmentVariable,
     ExecuteProcess,
     IncludeLaunchDescription,
     SetEnvironmentVariable,
@@ -33,100 +35,77 @@ from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
-    world = os.getenv('TEST_WORLD')
+    sim_dir = get_package_share_directory('nav2_minimal_tb3_sim')
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
 
+    world_sdf_xacro = os.path.join(sim_dir, 'worlds', 'tb3_empty_world.sdf.xacro')
+    robot_sdf = os.path.join(sim_dir, 'urdf', 'gz_waffle_gps.sdf.xacro')
+
+    urdf = os.path.join(sim_dir, 'urdf', 'turtlebot3_waffle_gps.urdf')
+
+    with open(urdf, 'r') as infp:
+        robot_description = infp.read()
+
+    # use local param file
     launch_dir = os.path.dirname(os.path.realpath(__file__))
     params_file = os.path.join(launch_dir, 'nav2_no_map_params.yaml')
-    bringup_dir = get_package_share_directory('nav2_bringup')
 
     configured_params = RewrittenYaml(
-        source_file=params_file, root_key='', param_rewrites='', convert_types=True
+        source_file=params_file,
+        root_key='',
+        param_rewrites='',
+        convert_types=True,
     )
 
     return LaunchDescription(
         [
             SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
             SetEnvironmentVariable('RCUTILS_LOGGING_USE_STDOUT', '1'),
-            # Launch gazebo server for simulation
+            AppendEnvironmentVariable(
+                'GZ_SIM_RESOURCE_PATH', os.path.join(sim_dir, 'models')
+            ),
+            AppendEnvironmentVariable(
+                'GZ_SIM_RESOURCE_PATH',
+                str(Path(os.path.join(sim_dir)).parent.resolve()),
+            ),
             ExecuteProcess(
-                cmd=[
-                    'gzserver',
-                    '-s',
-                    'libgazebo_ros_init.so',
-                    '--minimal_comms',
-                    world,
-                ],
+                cmd=['gz', 'sim', '-r', '-s', world_sdf_xacro],
                 output='screen',
             ),
-            # TODO(orduno) Launch the robot state publisher instead
-            #              using a local copy of TB3 urdf file
-            Node(
-                package='tf2_ros',
-                executable='static_transform_publisher',
-                output='screen',
-                arguments=[
-                    '--x', '0',
-                    '--y', '0',
-                    '--z', '0',
-                    '--roll', '0',
-                    '--pitch', '0',
-                    '--yaw', '0',
-                    '--frame-id', 'base_footprint',
-                    '--child-frame-id', 'base_link'
-                ],
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(sim_dir, 'launch', 'spawn_tb3_gps.launch.py')
+                ),
+                launch_arguments={
+                    'use_sim_time': 'True',
+                    'robot_sdf': robot_sdf,
+                    'x_pose': '0.0',
+                    'y_pose': '0.0',
+                    'z_pose': '0.0',
+                    'roll': '0.0',
+                    'pitch': '0.0',
+                    'yaw': '0.0',
+                }.items(),
             ),
             Node(
-                package='tf2_ros',
-                executable='static_transform_publisher',
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
                 output='screen',
-                arguments=[
-                    '--x', '0',
-                    '--y', '0',
-                    '--z', '0',
-                    '--roll', '0',
-                    '--pitch', '0',
-                    '--yaw', '0',
-                    '--frame-id', 'base_link',
-                    '--child-frame-id', 'base_scan'
-                ],
-            ),
-            Node(
-                package='tf2_ros',
-                executable='static_transform_publisher',
-                output='screen',
-                arguments=[
-                    '--x', '-0.32',
-                    '--y', '0',
-                    '--z', '0.068',
-                    '--roll', '0',
-                    '--pitch', '0',
-                    '--yaw', '0',
-                    '--frame-id', 'base_link',
-                    '--child-frame-id', 'imu_link'
-                ],
-            ),
-            Node(
-                package='tf2_ros',
-                executable='static_transform_publisher',
-                output='screen',
-                arguments=[
-                    '--x', '0',
-                    '--y', '0',
-                    '--z', '0',
-                    '--roll', '0',
-                    '--pitch', '0',
-                    '--yaw', '0',
-                    '--frame-id', 'base_link',
-                    '--child-frame-id', 'gps_link'
+                parameters=[
+                    {'use_sim_time': True, 'robot_description': robot_description}
                 ],
             ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    os.path.join(bringup_dir, 'launch', 'navigation_launch.py')
+                    os.path.join(nav2_bringup_dir, 'launch', 'navigation_launch.py')
                 ),
                 launch_arguments={
+                    'namespace': '',
+                    'use_namespace': 'False',
                     'use_sim_time': 'True',
                     'params_file': configured_params,
+                    'use_composition': 'False',
                     'autostart': 'True',
                 }.items(),
             ),
@@ -152,7 +131,8 @@ def main(argv=sys.argv[1:]):
     lts.add_test_action(ld, test1_action)
     ls = LaunchService(argv=argv)
     ls.include_launch_description(ld)
-    return lts.run(ls)
+    return_code = lts.run(ls)
+    return return_code
 
 
 if __name__ == '__main__':
