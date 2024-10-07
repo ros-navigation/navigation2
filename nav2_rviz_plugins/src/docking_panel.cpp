78db512f7ae1c8b4f2821110c03c27034908f4b5
Alberto Tudela <ajtudela@gmail.com>
2024-10-07 12:44:27 +0200

    Improved dock_panel state machine
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- nav2_rviz_plugins/src/docking_panel.cpp
+++ nav2_rviz_plugins/src/docking_panel.cpp
@@ -29,6 +29,7 @@
 
 #include "nav2_util/geometry_utils.hpp"
 #include "nav2_rviz_plugins/docking_panel.hpp"
+#include "nav2_rviz_plugins/ros_action_qevent.hpp"
 #include "nav2_rviz_plugins/utils.hpp"
 
 using namespace std::chrono_literals;
@@ -40,8 +41,7 @@ DockingPanel::DockingPanel(QWidget * parent)
 : Panel(parent),
   server_timeout_(100)
 {
-  client_node_ = std::make_shared<rclcpp::Node>("nav2_rviz_docking_panel_node");
-
+  // Create the control buttons and its tooltip
   main_layout_ = new QVBoxLayout;
   info_layout_ = new QHBoxLayout;
   feedback_layout_ = new QVBoxLayout;
@@ -50,8 +50,8 @@ DockingPanel::DockingPanel(QWidget * parent)
   dock_pose_layout_ = new QHBoxLayout;
   nav_stage_layout_ = new QHBoxLayout;
   dock_type_ = new QComboBox;
-  docking_button_ = new QPushButton("Dock robot");
-  undocking_button_ = new QPushButton("Undock robot");
+  docking_button_ = new QPushButton;
+  undocking_button_ = new QPushButton;
   docking_goal_status_indicator_ = new QLabel;
   docking_feedback_indicator_ = new QLabel;
   docking_result_indicator_ = new QLabel;
@@ -62,20 +62,126 @@ DockingPanel::DockingPanel(QWidget * parent)
   dock_pose_y_ = new QLineEdit;
   dock_pose_yaw_ = new QLineEdit;
 
-  docking_button_->setEnabled(false);
-  undocking_button_->setEnabled(false);
-  use_dock_id_checkbox_->setEnabled(false);
-  nav_to_staging_checkbox_->setEnabled(false);
-  dock_id_->setEnabled(false);
-  dock_pose_x_->setEnabled(false);
-  dock_pose_y_->setEnabled(false);
-  dock_pose_yaw_->setEnabled(false);
+  // Create the state machine used to present the proper control button states in the UI
+  const char * dock_msg = "Dock the robot at the specified docking station";
+  const char * undock_msg = "Undock the robot from the docking station";
+  const char * cancel_dock_msg = "Cancel the current docking action";
+  const char * cancel_undock_msg = "Cancel the current undocking action";
+
   docking_goal_status_indicator_->setText(nav2_rviz_plugins::getGoalStatusLabel());
   docking_feedback_indicator_->setText(getDockFeedbackLabel());
   docking_goal_status_indicator_->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
   docking_feedback_indicator_->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
   nav_to_staging_checkbox_->setFixedWidth(150);
 
+  pre_initial_ = new QState();
+  pre_initial_->setObjectName("pre_initial");
+  pre_initial_->assignProperty(docking_button_, "text", "Dock robot");
+  pre_initial_->assignProperty(docking_button_, "enabled", false);
+
+  pre_initial_->assignProperty(undocking_button_, "text", "Undock robot");
+  pre_initial_->assignProperty(undocking_button_, "enabled", false);
+
+  pre_initial_->assignProperty(nav_to_staging_checkbox_, "enabled", false);
+  pre_initial_->assignProperty(nav_to_staging_checkbox_, "checked", true);
+  pre_initial_->assignProperty(use_dock_id_checkbox_, "enabled", false);
+  pre_initial_->assignProperty(use_dock_id_checkbox_, "checked", true);
+  pre_initial_->assignProperty(dock_id_, "enabled", false);
+  pre_initial_->assignProperty(dock_type_, "enabled", false);
+  pre_initial_->assignProperty(dock_pose_x_, "enabled", false);
+  pre_initial_->assignProperty(dock_pose_y_, "enabled", false);
+  pre_initial_->assignProperty(dock_pose_yaw_, "enabled", false);
+
+  // State entered when the docking / undocking action is not active
+  idle_ = new QState();
+  idle_->setObjectName("idle");
+  idle_->assignProperty(docking_button_, "text", "Dock robot");
+  idle_->assignProperty(docking_button_, "toolTip", dock_msg);
+  idle_->assignProperty(docking_button_, "enabled", true);
+
+  idle_->assignProperty(undocking_button_, "text", "Undock robot");
+  idle_->assignProperty(undocking_button_, "toolTip", undock_msg);
+  idle_->assignProperty(undocking_button_, "enabled", true);
+
+  idle_->assignProperty(nav_to_staging_checkbox_, "enabled", true);
+  idle_->assignProperty(use_dock_id_checkbox_, "enabled", true);
+  idle_->assignProperty(dock_id_, "enabled", true);
+  idle_->assignProperty(dock_type_, "enabled", true);
+
+  // State entered to cancel the docking action
+  canceled_docking_ = new QState();
+  canceled_docking_->setObjectName("canceled_docking");
+
+  // State entered to cancel the undocking action
+  canceled_undocking_ = new QState();
+  canceled_undocking_->setObjectName("canceled_undocking");
+
+  // State entered while the docking action is active
+  docking_ = new QState();
+  docking_->setObjectName("docking");
+  docking_->assignProperty(docking_button_, "text", "Cancel docking");
+  docking_->assignProperty(docking_button_, "toolTip", cancel_dock_msg);
+
+  docking_->assignProperty(undocking_button_, "enabled", false);
+
+  // State entered while the undocking action is active
+  undocking_ = new QState();
+  undocking_->setObjectName("undocking");
+  undocking_->assignProperty(docking_button_, "enabled", false);
+
+  undocking_->assignProperty(undocking_button_, "text", "Cancel undocking");
+  undocking_->assignProperty(undocking_button_, "toolTip", cancel_undock_msg);
+
+  QObject::connect(docking_, SIGNAL(entered()), this, SLOT(onDockingButtonPressed()));
+  QObject::connect(undocking_, SIGNAL(entered()), this, SLOT(onUndockingButtonPressed()));
+  QObject::connect(canceled_docking_, SIGNAL(exited()), this, SLOT(onCancelDocking()));
+  QObject::connect(canceled_undocking_, SIGNAL(exited()), this, SLOT(onCancelUndocking()));
+
+  // Start/Cancel button click transitions
+  idle_->addTransition(docking_button_, SIGNAL(clicked()), docking_);
+  idle_->addTransition(undocking_button_, SIGNAL(clicked()), undocking_);
+  docking_->addTransition(docking_button_, SIGNAL(clicked()), canceled_docking_);
+  undocking_->addTransition(undocking_button_, SIGNAL(clicked()), canceled_undocking_);
+
+  // Internal state transitions
+  canceled_docking_->addTransition(canceled_docking_, SIGNAL(entered()), idle_);
+  canceled_undocking_->addTransition(canceled_undocking_, SIGNAL(entered()), idle_);
+
+  // ROSAction Transitions: So when actions are updated remotely (failing, succeeding, etc)
+  // the state of the application will also update. This means that if in the processing
+  // states and then goes inactive, move back to the idle state. Vise versa as well.
+  ROSActionQTransition * idleDockTransition = new ROSActionQTransition(QActionState::INACTIVE);
+  idleDockTransition->setTargetState(docking_);
+  idle_->addTransition(idleDockTransition);
+
+  ROSActionQTransition * idleUndockTransition = new ROSActionQTransition(QActionState::INACTIVE);
+  idleUndockTransition->setTargetState(undocking_);
+  idle_->addTransition(idleUndockTransition);
+
+  ROSActionQTransition * dockingTransition = new ROSActionQTransition(QActionState::ACTIVE);
+  dockingTransition->setTargetState(idle_);
+  docking_->addTransition(dockingTransition);
+
+  ROSActionQTransition * undockingTransition = new ROSActionQTransition(QActionState::ACTIVE);
+  undockingTransition->setTargetState(idle_);
+  undocking_->addTransition(undockingTransition);
+
+  client_node_ = std::make_shared<rclcpp::Node>("nav2_rviz_docking_panel_node");
+
+  state_machine_.addState(pre_initial_);
+  state_machine_.addState(idle_);
+  state_machine_.addState(docking_);
+  state_machine_.addState(undocking_);
+  state_machine_.addState(canceled_docking_);
+  state_machine_.addState(canceled_undocking_);
+
+  state_machine_.setInitialState(pre_initial_);
+
+  // Delay starting initial thread until state machine has started or a race occurs
+  QObject::connect(&state_machine_, SIGNAL(started()), this, SLOT(startThread()));
+  state_machine_.start();
+
+  // Create the layout for the panel
   info_layout_->addWidget(docking_goal_status_indicator_);
   info_layout_->addWidget(docking_result_indicator_);
   feedback_layout_->addWidget(docking_feedback_indicator_);
@@ -105,29 +211,42 @@ DockingPanel::DockingPanel(QWidget * parent)
   main_layout_->addWidget(undocking_button_);
 
   setLayout(main_layout_);
-  timer_.start(200, this);
+  action_timer_.start(200, this);
 
   dock_client_ = rclcpp_action::create_client<Dock>(client_node_, "dock_robot");
   undock_client_ = rclcpp_action::create_client<Undock>(client_node_, "undock_robot");
+  initial_thread_ = new InitialDockThread(dock_client_, undock_client_);
+  connect(initial_thread_, &InitialDockThread::finished, initial_thread_, &QObject::deleteLater);
+
+  QSignalTransition * activeDockSignal = new QSignalTransition(
+    initial_thread_, &InitialDockThread::dockingActive);
+  activeDockSignal->setTargetState(idle_);
+  pre_initial_->addTransition(activeDockSignal);
+
+  QSignalTransition * activeUndockSignal = new QSignalTransition(
+    initial_thread_, &InitialDockThread::undockingActive);
+  activeUndockSignal->setTargetState(idle_);
+  pre_initial_->addTransition(activeUndockSignal);
+
+  QObject::connect(
+    initial_thread_, &InitialDockThread::dockingActive,
+    [this] {
+      // Load the plugins if not already loaded
+      if (!plugins_loaded_) {
+        nav2_rviz_plugins::pluginLoader(
+        client_node_, server_failed_, "docking_server", "dock_plugins", dock_type_);
+        plugins_loaded_ = true;
+      }
+    });
 
   // Conect buttons with functions
-  QObject::connect(docking_button_, SIGNAL(clicked()), this, SLOT(onDockingButtonPressed()));
-  QObject::connect(undocking_button_, SIGNAL(clicked()), this, SLOT(onUndockingButtonPressed()));
   QObject::connect(
     use_dock_id_checkbox_, &QCheckBox::stateChanged, this, &DockingPanel::dockIdCheckbox);
 }
 
 void DockingPanel::onInitialize()
 {
-  node_ptr_ = getDisplayContext()->getRosNodeAbstraction().lock();
-  if (node_ptr_ == nullptr) {
-    // The node no longer exists, so just don't initialize
-    RCLCPP_ERROR(
-      rclcpp::get_logger("docking_panel"),
-      "Underlying ROS node no longer exists, initialization failed");
-    return;
-  }
-  rclcpp::Node::SharedPtr node = node_ptr_->get_raw_node();
+  auto node = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
 
   // Create action feedback subscriber
   docking_feedback_sub_ = node->create_subscription<Dock::Impl::FeedbackMessage>(
@@ -135,18 +254,6 @@ void DockingPanel::onInitialize()
     rclcpp::SystemDefaultsQoS(),
     [this](const Dock::Impl::FeedbackMessage::SharedPtr msg) {
       docking_feedback_indicator_->setText(getDockFeedbackLabel(msg->feedback));
-      docking_button_->setText("Cancel docking");
-      undocking_button_->setEnabled(false);
-      docking_in_progress_ = true;
-    });
-
-  undocking_feedback_sub_ = node->create_subscription<Undock::Impl::FeedbackMessage>(
-    "undock_robot/_action/feedback",
-    rclcpp::SystemDefaultsQoS(),
-    [this](const Undock::Impl::FeedbackMessage::SharedPtr /*msg*/) {
-      docking_button_->setEnabled(false);
-      undocking_button_->setText("Cancel undocking");
-      undocking_in_progress_ = true;
     });
 
   // Create action goal status subscribers
@@ -156,11 +263,6 @@ void DockingPanel::onInitialize()
     [this](const action_msgs::msg::GoalStatusArray::SharedPtr msg) {
       docking_goal_status_indicator_->setText(
         nav2_rviz_plugins::getGoalStatusLabel("Feedback", msg->status_list.back().status));
-      if (msg->status_list.back().status != action_msgs::msg::GoalStatus::STATUS_EXECUTING) {
-        docking_button_->setText("Dock robot");
-        undocking_button_->setEnabled(true);
-        docking_in_progress_ = false;
-      }
       // Reset values when action is completed
       if (msg->status_list.back().status == action_msgs::msg::GoalStatus::STATUS_SUCCEEDED) {
         docking_feedback_indicator_->setText(getDockFeedbackLabel());
@@ -173,12 +275,13 @@ void DockingPanel::onInitialize()
     [this](const action_msgs::msg::GoalStatusArray::SharedPtr msg) {
       docking_goal_status_indicator_->setText(
         nav2_rviz_plugins::getGoalStatusLabel("Feedback", msg->status_list.back().status));
-      if (msg->status_list.back().status != action_msgs::msg::GoalStatus::STATUS_EXECUTING) {
-        docking_button_->setEnabled(true);
-        undocking_button_->setText("Undock robot");
-        undocking_in_progress_ = false;
-      }
     });
+}
+
+void DockingPanel::startThread()
+{
+  // start initial thread now that state machine is started
+  initial_thread_->start();
 }
 
 DockingPanel::~DockingPanel()
@@ -196,24 +299,6 @@ void DockingPanel::save(rviz_common::Config config) const
 }
 
 void DockingPanel::onDockingButtonPressed()
-{
-  if (!docking_in_progress_) {
-    startDocking();
-  } else {
-    cancelDocking();
-  }
-}
-
-void DockingPanel::onUndockingButtonPressed()
-{
-  if (!undocking_in_progress_) {
-    startUndocking();
-  } else {
-    cancelUndocking();
-  }
-}
-
-void DockingPanel::startDocking()
 {
   auto is_action_server_ready =
     dock_client_->wait_for_action_server(std::chrono::seconds(5));
@@ -296,10 +381,10 @@ void DockingPanel::startDocking()
     return;
   }
 
-  timer_.start(200, this);
+  action_timer_.start(200, this);
 }
 
-void DockingPanel::startUndocking()
+void DockingPanel::onUndockingButtonPressed()
 {
   auto is_action_server_ready =
     undock_client_->wait_for_action_server(std::chrono::seconds(5));
@@ -354,7 +439,7 @@ void DockingPanel::startUndocking()
     return;
   }
 
-  timer_.start(200, this);
+  action_timer_.start(200, this);
 }
 
 void DockingPanel::dockIdCheckbox()
@@ -374,7 +459,7 @@ void DockingPanel::dockIdCheckbox()
   }
 }
 
-void DockingPanel::cancelDocking()
+void DockingPanel::onCancelDocking()
 {
   if (dock_goal_handle_) {
     auto future_cancel = dock_client_->async_cancel_goal(dock_goal_handle_);
@@ -387,9 +472,11 @@ void DockingPanel::cancelDocking()
       dock_goal_handle_.reset();
     }
   }
+
+  action_timer_.stop();
 }
 
-void DockingPanel::cancelUndocking()
+void DockingPanel::onCancelUndocking()
 {
   if (undock_goal_handle_) {
     auto future_cancel = undock_client_->async_cancel_goal(undock_goal_handle_);
@@ -402,35 +489,53 @@ void DockingPanel::cancelUndocking()
       undock_goal_handle_.reset();
     }
   }
+
+  action_timer_.stop();
 }
 
 void DockingPanel::timerEvent(QTimerEvent * event)
 {
-  if (event->timerId() == timer_.timerId()) {
-    if (!plugins_loaded_) {
-      nav2_rviz_plugins::pluginLoader(
-        client_node_, server_failed_, "docking_server", "dock_plugins", dock_type_);
-      plugins_loaded_ = true;
-      docking_button_->setEnabled(true);
-      undocking_button_->setEnabled(true);
-      use_dock_id_checkbox_->setEnabled(true);
-      use_dock_id_checkbox_->setChecked(true);
-      nav_to_staging_checkbox_->setEnabled(true);
-      nav_to_staging_checkbox_->setChecked(true);
-      dock_id_->setEnabled(true);
-    }
+  if (event->timerId() == action_timer_.timerId()) {
+    // Check the status of the action servers
+    if (state_machine_.configuration().contains(docking_)) {
+      if (!dock_goal_handle_) {
+        RCLCPP_DEBUG(client_node_->get_logger(), "Waiting for Goal");
+        state_machine_.postEvent(new ROSActionQEvent(QActionState::INACTIVE));
+        return;
+      }
 
-    // Restart the timer if the one of the server fails
-    if (server_failed_ && !tried_once_) {
-      RCLCPP_INFO(client_node_->get_logger(), "Retrying to connect to the failed server.");
-      server_failed_ = false;
-      plugins_loaded_ = false;
-      tried_once_ = true;
-      timer_.start(200, this);
-      return;
-    }
+      rclcpp::spin_some(client_node_);
+      auto status = dock_goal_handle_->get_status();
 
-    timer_.stop();
+      // Check if the goal is still executing
+      if (status == action_msgs::msg::GoalStatus::STATUS_ACCEPTED ||
+        status == action_msgs::msg::GoalStatus::STATUS_EXECUTING)
+      {
+        state_machine_.postEvent(new ROSActionQEvent(QActionState::ACTIVE));
+      } else {
+        state_machine_.postEvent(new ROSActionQEvent(QActionState::INACTIVE));
+        action_timer_.stop();
+      }
+    } else if (state_machine_.configuration().contains(undocking_)) {
+      if (!undock_goal_handle_) {
+        RCLCPP_DEBUG(client_node_->get_logger(), "Waiting for Goal");
+        state_machine_.postEvent(new ROSActionQEvent(QActionState::INACTIVE));
+        return;
+      }
+
+      rclcpp::spin_some(client_node_);
+      auto status = undock_goal_handle_->get_status();
+
+      // Check if the goal is still executing
+      if (status == action_msgs::msg::GoalStatus::STATUS_ACCEPTED ||
+        status == action_msgs::msg::GoalStatus::STATUS_EXECUTING)
+      {
+        state_machine_.postEvent(new ROSActionQEvent(QActionState::ACTIVE));
+      } else {
+        state_machine_.postEvent(new ROSActionQEvent(QActionState::INACTIVE));
+        action_timer_.stop();
+      }
+    }
   }
 }
 
