4893022400095400e437a288f4f44b39c009cf0f
Daniil Khaninaev <khaninaev@yahoo.com>
2024-10-16 21:21:49 +0300

    Added parameter `rotate_to_heading_once`
    Signed-off-by: Daniil Khaninaev <khaninaev@yahoo.com>

--- nav2_rotation_shim_controller/test/test_shim_controller.cpp
+++ nav2_rotation_shim_controller/test/test_shim_controller.cpp
@@ -62,6 +62,11 @@ public:
     return getSampledPathPt();
   }
 
+  bool isGoalChangedWrapper(const nav_msgs::msg::Path & path)
+  {
+    return isGoalChanged(path);
+  }
+
   geometry_msgs::msg::Pose transformPoseToBaseFrameWrapper(geometry_msgs::msg::PoseStamped pt)
   {
     return transformPoseToBaseFrame(pt);
@@ -382,6 +387,57 @@ TEST(RotationShimControllerTest, computeVelocityGoalRotationTests) {
   EXPECT_EQ(cmd_vel.twist.angular.z, 1.8);
 }
 
+TEST(RotationShimControllerTest, isGoalChangedTest)
+{
+  auto ctrl = std::make_shared<RotationShimShim>();
+  auto node = std::make_shared<rclcpp_lifecycle::LifecycleNode>("ShimControllerTest");
+  std::string name = "PathFollower";
+  auto tf = std::make_shared<tf2_ros::Buffer>(node->get_clock());
+  auto listener = std::make_shared<tf2_ros::TransformListener>(*tf, node, true);
+  auto costmap = std::make_shared<nav2_costmap_2d::Costmap2DROS>("fake_costmap");
+  rclcpp_lifecycle::State state;
+  costmap->on_configure(state);
+  auto tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
+
+  geometry_msgs::msg::TransformStamped transform;
+  transform.header.frame_id = "base_link";
+  transform.child_frame_id = "odom";
+  transform.transform.rotation.x = 0.0;
+  transform.transform.rotation.y = 0.0;
+  transform.transform.rotation.z = 0.0;
+  transform.transform.rotation.w = 1.0;
+  tf_broadcaster->sendTransform(transform);
+
+  // set a valid primary controller so we can do lifecycle
+  node->declare_parameter(
+    "PathFollower.primary_controller",
+    std::string("nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController"));
+  node->declare_parameter(
+    "PathFollower.rotate_to_heading_once",
+    true);
+
+  auto controller = std::make_shared<RotationShimShim>();
+  controller->configure(node, name, tf, costmap);
+  controller->activate();
+
+  nav_msgs::msg::Path path;
+  path.header.frame_id = "base_link";
+  path.poses.resize(2);
+  path.poses.back().pose.position.x = 2.0;
+  path.poses.back().pose.position.y = 2.0;
+
+  // Test: Current path is empty, should return true
+  EXPECT_EQ(controller->isGoalChangedWrapper(path), true);
+
+  // Test: Last pose of the current path is the same, should return false
+  controller->setPlan(path);
+  EXPECT_EQ(controller->isGoalChangedWrapper(path), false);
+
+  // Test: Last pose of the current path differs, should return true
+  path.poses.back().pose.position.x = 3.0;
+  EXPECT_EQ(controller->isGoalChangedWrapper(path), true);
+}
+
 TEST(RotationShimControllerTest, testDynamicParameter)
 {
   auto node = std::make_shared<rclcpp_lifecycle::LifecycleNode>("ShimControllerTest");
@@ -412,7 +468,8 @@ TEST(RotationShimControllerTest, testDynamicParameter)
       rclcpp::Parameter("test.max_angular_accel", 7.0),
       rclcpp::Parameter("test.simulate_ahead_time", 7.0),
       rclcpp::Parameter("test.primary_controller", std::string("HI")),
-      rclcpp::Parameter("test.rotate_to_goal_heading", true)});
+      rclcpp::Parameter("test.rotate_to_goal_heading", true),
+      rclcpp::Parameter("test.rotate_to_heading_once", true)});
 
   rclcpp::spin_until_future_complete(
     node->get_node_base_interface(),
@@ -424,4 +481,5 @@ TEST(RotationShimControllerTest, testDynamicParameter)
   EXPECT_EQ(node->get_parameter("test.max_angular_accel").as_double(), 7.0);
   EXPECT_EQ(node->get_parameter("test.simulate_ahead_time").as_double(), 7.0);
   EXPECT_EQ(node->get_parameter("test.rotate_to_goal_heading").as_bool(), true);
+  EXPECT_EQ(node->get_parameter("test.rotate_to_heading_once").as_bool(), true);
 }
