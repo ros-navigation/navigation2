275327286e6dd779f10bbb1d44d190fe370b80e2
aosmw <116058035+aosmw@users.noreply.github.com>
2024-11-22 02:27:08 +1100

    mppi parameters_handler: Improve verbose handling (#4704) (#4711)
    * mppi parameters_handler: Improve verbose handling (#4704)
    
    The "verbose" parameter of the parameters_handler is
    a special case that needs registration before the
    dynamic parameter handler callback is registered.
    
    In verbose mode make the parameter handler info/warn/debug
    messages more expressive.
    
    Signed-off-by: Mike Wake <michael.wake@aosgrp.com.au>
    
    * Address review comments. (#4704)
    
    * remove comments.
    * Use RCLCPP_DEBUG instead of INFO for low level messages.
    * Add test for trying to access parameters that are not declared.
    
    Signed-off-by: Mike Wake <michael.wake@aosgrp.com.au>
    
    * mppi parameters_handler: Improve static/dynamic/not defined logging (#4704)
    
    Attempts to change undefined parameters will not be successful
    and will log an error.
    
    Attempts to change static parameters will be ignored, a debug
    message is logged if a change in parameters is attempted.
    
    Signed-off-by: Mike Wake <michael.wake@aosgrp.com.au>
    
    * mppi parameters_handler: populate SetParametersResult (#4704)
    
    Provide a mechanism to populate an rcl_interfaces::msg::SetParametersResult
    with the reasons for unsuccessful parameter setting, so that it may be
    propogated back to a set parameter client.
    
    The mechanism provides feedback when attempting to set undefined
    parameters, static parameters and could be used to validate dynamic
    parameters and provide a reason for rejection.
    
    NOTE: This changes public interface of ParametersHandler class.
    s/setDynamicParamCallback/setParamCallback
    s/addDynamicParamCallback/addParamCallback which takes
    a callback function that is able to populate a
    rcl_interfaces::msg::SetParametersResult.
    
    In order to indicate an unsuccessful parameter change and the
    reason, callback functions should append a "\n"
    to the reason before appending to if it is not empty.
    
    Signed-off-by: Mike Wake <michael.wake@aosgrp.com.au>
    
    * mppi parameters_handler: fix reason handling and improve tests
    
    Signed-off-by: Mike Wake <michael.wake@aosgrp.com.au>
    
    ---------
    
    Signed-off-by: Mike Wake <michael.wake@aosgrp.com.au>
    Signed-off-by: Jakubach <jakubach@gmail.com>

--- nav2_mppi_controller/src/parameters_handler.cpp
+++ nav2_mppi_controller/src/parameters_handler.cpp
@@ -38,13 +38,14 @@ ParametersHandler::~ParametersHandler()
 void ParametersHandler::start()
 {
   auto node = node_.lock();
+
+  auto get_param = getParamGetter(node_name_);
+  get_param(verbose_, "verbose", false);
+
   on_set_param_handler_ = node->add_on_set_parameters_callback(
     std::bind(
       &ParametersHandler::dynamicParamsCallback, this,
       std::placeholders::_1));
-
-  auto get_param = getParamGetter(node_name_);
-  get_param(verbose_, "verbose", false);
 }
 
 rcl_interfaces::msg::SetParametersResult
@@ -52,6 +53,9 @@ ParametersHandler::dynamicParamsCallback(
   std::vector<rclcpp::Parameter> parameters)
 {
   rcl_interfaces::msg::SetParametersResult result;
+  result.successful = true;
+  result.reason = "";
+
   std::lock_guard<std::mutex> lock(parameters_change_mutex_);
 
   for (auto & pre_cb : pre_callbacks_) {
@@ -64,9 +68,13 @@ ParametersHandler::dynamicParamsCallback(
     if (auto callback = get_param_callbacks_.find(param_name);
       callback != get_param_callbacks_.end())
     {
-      callback->second(param);
+      callback->second(param, result);
     } else {
-      RCLCPP_WARN(logger_, "Parameter %s not found", param_name.c_str());
+      result.successful = false;
+      if (!result.reason.empty()) {
+        result.reason += "\n";
+      }
+      result.reason += "get_param_callback func for '" + param_name + "' not found.\n";
     }
   }
 
@@ -74,7 +82,9 @@ ParametersHandler::dynamicParamsCallback(
     post_cb();
   }
 
-  result.successful = true;
+  if (!result.successful) {
+    RCLCPP_ERROR(logger_, result.reason.c_str());
+  }
   return result;
 }
 
