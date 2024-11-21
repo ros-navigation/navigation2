eb7b986a982537e7a2a0849cd34e411fb2461808
Alexander Yuen <alex@polymathrobotics.com>
2024-11-21 18:16:46 +0000

    initial commit of plugin container layer
    Signed-off-by: alexander <alex@polymathrobotics.com>

---
+++ nav2_costmap_2d/include/nav2_costmap_2d/plugin_container_layer.hpp
@@ -0,0 +1,125 @@
+
+#ifndef NAV2_COSTMAP_2D__PLUGIN_CONTAINER_LAYER_HPP_
+#define NAV2_COSTMAP_2D__PLUGIN_CONTAINER_LAYER_HPP_
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_costmap_2d/layer.hpp"
+#include "nav2_costmap_2d/layered_costmap.hpp"
+#include "nav2_costmap_2d/costmap_layer.hpp"
+#include "nav2_costmap_2d/observation_buffer.hpp"
+#include "nav2_costmap_2d/inflation_layer.hpp"
+#include "tf2_ros/message_filter.h"
+#include "message_filters/subscriber.h"
+#include <Eigen/Dense>
+#include <cmath>
+#include "pluginlib/class_loader.hpp"
+using nav2_costmap_2d::LETHAL_OBSTACLE;
+using nav2_costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
+using nav2_costmap_2d::NO_INFORMATION;
+using rcl_interfaces::msg::ParameterType;
+namespace nav2_costmap_2d
+{
+/**
+ * @class PluginContainerLayer
+ * @brief Holds a list of plugins and applies them only to the specific layer
+ */
+class PluginContainerLayer : public CostmapLayer
+{
+public:
+  /**
+   * @brief A constructor
+   */
+  PluginContainerLayer();
+  /**
+   * @brief A destructor
+   */
+  virtual ~PluginContainerLayer();
+  /**
+   * @brief Initialization process of layer on startup
+   */
+  virtual void onInitialize();
+  /**
+   * @brief Update the bounds of the master costmap by this layer's update
+   *dimensions
+   * @param robot_x X pose of robot
+   * @param robot_y Y pose of robot
+   * @param robot_yaw Robot orientation
+   * @param min_x X min map coord of the window to update
+   * @param min_y Y min map coord of the window to update
+   * @param max_x X max map coord of the window to update
+   * @param max_y Y max map coord of the window to update
+   */
+  virtual void updateBounds(
+    double robot_x,
+    double robot_y,
+    double robot_yaw,
+    double * min_x,
+    double * min_y,
+    double * max_x,
+    double * max_y);
+  /**
+   * @brief Update the costs in the master costmap in the window
+   * @param master_grid The master costmap grid to update
+   * @param min_x X min map coord of the window to update
+   * @param min_y Y min map coord of the window to update
+   * @param max_x X max map coord of the window to update
+   * @param max_y Y max map coord of the window to update
+   */
+  virtual void updateCosts(
+    nav2_costmap_2d::Costmap2D & master_grid,
+    int min_i,
+    int min_j,
+    int max_i,
+    int max_j);
+  virtual void onFootprintChanged();
+  /** @brief Update the footprint to match size of the parent costmap. */
+  virtual void matchSize();
+  /**
+   * @brief Deactivate the layer
+   */
+  virtual void deactivate();
+  /**
+   * @brief Activate the layer
+   */
+  virtual void activate();
+  /**
+   * @brief Reset this costmap
+   */
+  virtual void reset();
+  /**
+   * @brief If clearing operations should be processed on this layer or not
+   */
+  virtual bool isClearable()
+  {
+    return true;
+  }
+  void addPlugin(std::shared_ptr<Layer> plugin);
+  pluginlib::ClassLoader<Layer> plugin_loader_{"nav2_costmap_2d", "nav2_costmap_2d::Layer"};
+  /**
+   * @brief Callback executed when a parameter change is detected
+   * @param event ParameterEvent message
+   */
+  rcl_interfaces::msg::SetParametersResult dynamicParametersCallback(
+    std::vector<rclcpp::Parameter> parameters);
+private:
+  /// @brief Dynamic parameters handler
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr
+    dyn_params_handler_;
+  bool rolling_window_;
+  int combination_method_;
+  std::vector<std::shared_ptr<Layer>> plugins_;
+  std::vector<std::shared_ptr<Layer>> filters_;
+  std::vector<std::string> default_plugins_;
+  std::vector<std::string> default_types_;
+  std::vector<std::string> plugin_names_;
+  std::vector<std::string> plugin_types_;
+  std::vector<std::string> filter_names_;
+  std::vector<std::string> filter_types_;
+  // primary_costmap_ is a bottom costmap used by plugins when costmap filters were enabled.
+  // combined_costmap_ is a final costmap where all results produced by plugins and filters (if any)
+  // to be merged.
+  // The separation is aimed to avoid interferences of work between plugins and filters.
+  // primay_costmap_ and combined_costmap_ have the same sizes, origins and default values.
+  Costmap2D primary_costmap_, combined_costmap_;
+};
+} // namespace nav2_costmap_2d
+#endif  // NAV2_COSTMAP_2D__PLUGIN_CONTAINER_LAYER_HPP_
---
+++ nav2_costmap_2d/plugins/plugin_container_layer.cpp
@@ -0,0 +1,209 @@
+#include "nav2_costmap_2d/plugin_container_layer.hpp"
+
+#include "nav2_costmap_2d/costmap_math.hpp"
+#include "nav2_costmap_2d/footprint.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "rclcpp/parameter_events_filter.hpp"
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(plugin_container_layer::PluginContainerLayer, nav2_costmap_2d::Layer)
+
+using std::vector;
+
+namespace nav2_costmap_2d
+{
+PluginContainerLayer::PluginContainerLayer()
+: default_plugins_{"static_layer", "obstacle_layer", "inflation_layer"},
+  default_types_{
+    "nav2_costmap_2d::StaticLayer",
+    "nav2_costmap_2d::ObstacleLayer",
+    "nav2_costmap_2d::InflationLayer"}
+{
+  costmap_ = NULL;
+}
+
+PluginContainerLayer::~PluginContainerLayer()
+{}
+
+void PluginContainerLayer::onInitialize()
+{
+
+  auto node = node_.lock();
+
+  if (!node) {
+    throw std::runtime_error{"Failed to lock node"};
+  }
+
+  primary_costmap_.setDefaultValue(0);
+  combined_costmap_.setDefaultValue(0);
+
+  node->declare_parameter(name_ + "." + "enabled", rclcpp::ParameterValue(false));
+  node->declare_parameter(name_ + "." + "plugins", rclcpp::ParameterValue(default_plugins_));
+  node->declare_parameter(name_ + "." + "combination_method", rclcpp::ParameterValue(0));
+
+  node->get_parameter(name_ + "." + "enabled", enabled_);
+  node->get_parameter(name_ + "." + "plugins", plugin_names_);
+  node->get_parameter(name_ + "." + "combination_method", combination_method_);
+
+  plugin_types_.resize(plugin_names_.size());
+  filter_types_.resize(filter_names_.size());
+
+  for (size_t i = 0; i < plugin_names_.size(); ++i) {
+    plugin_types_[i] = nav2_util::get_plugin_type_param(node, name_ + "." + plugin_names_[i]);
+  }
+  for (size_t i = 0; i < filter_names_.size(); ++i) {
+    filter_types_[i] = nav2_util::get_plugin_type_param(node, name_ + "." + filter_names_[i]);
+  }
+
+  // Then load and add the plug-ins to the costmap
+  for (unsigned int i = 0; i < plugin_names_.size(); ++i) {
+    std::shared_ptr<Layer> plugin = plugin_loader_.createSharedInstance(plugin_types_[i]); //this is a shared pointer to a Layer called plugin
+    std::unique_lock<Costmap2D::mutex_t> lock(*(layered_costmap_->getCostmap()->getMutex()));
+    addPlugin(plugin);
+    plugin->initialize(
+      layered_costmap_, name_ + "." + plugin_names_[i], tf_, node,
+      callback_group_);
+    lock.unlock();
+  }
+
+  PluginContainerLayer::matchSize();
+  current_ = true;
+
+}
+
+void PluginContainerLayer::addPlugin(std::shared_ptr<Layer> plugin)
+{
+  plugins_.push_back(plugin);
+}
+
+void PluginContainerLayer::updateBounds(
+  double robot_x,
+  double robot_y,
+  double robot_yaw,
+  double * min_x,
+  double * min_y,
+  double * max_x,
+  double * max_y)
+{
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->updateBounds(robot_x, robot_y, robot_yaw, min_x, min_y, max_x, max_y);
+  }
+}
+
+void PluginContainerLayer::updateCosts(
+  nav2_costmap_2d::Costmap2D & master_grid,
+  int min_i,
+  int min_j,
+  int max_i,
+  int max_j)
+{
+
+  std::lock_guard<Costmap2D::mutex_t> guard(*getMutex());
+  if (!enabled_) {
+    return;
+  }
+
+  //1. clear combined map
+  combined_costmap_.resetMap(min_i, min_j, max_i, max_j);
+  //2. update the internal costmap with the plugins associated with the layer
+
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->updateCosts(combined_costmap_, min_i, min_j, max_i, max_j);
+  }
+  //3. point the private member costmap to the char map of the Costmap2D
+  costmap_ = combined_costmap_.getCharMap(); //This will point to the CharMap...this feels memory leaky
+
+  //4. update master grid depending on what method we pick
+  switch (combination_method_) {
+    case 0:  // Overwrite
+      updateWithOverwrite(master_grid, min_i, min_j, max_i, max_j);
+      break;
+    case 1:  // Maximum
+      updateWithMax(master_grid, min_i, min_j, max_i, max_j);
+      break;
+    default: // Nothing
+      break;
+  }
+
+  current_ = true;
+}
+
+void PluginContainerLayer::activate()
+{
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->activate();
+  }
+}
+
+void PluginContainerLayer::deactivate()
+{
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->deactivate();
+  }
+}
+
+void PluginContainerLayer::reset()
+{
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->reset();
+  }
+}
+
+void PluginContainerLayer::onFootprintChanged()
+{
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->onFootprintChanged();
+  }
+}
+
+void PluginContainerLayer::matchSize()
+{
+
+  std::lock_guard<Costmap2D::mutex_t> guard(*getMutex());
+  Costmap2D * master = layered_costmap_->getCostmap();
+  resizeMap(
+    master->getSizeInCellsX(), master->getSizeInCellsY(),
+    master->getResolution(), master->getOriginX(), master->getOriginY());
+  primary_costmap_.resizeMap(size_x_, size_y_, resolution_, origin_x_, origin_y_); //Where do I get these numbers
+  combined_costmap_.resizeMap(size_x_, size_y_, resolution_, origin_x_, origin_y_); //Where do I get these numbers
+
+  for (vector<std::shared_ptr<Layer>>::iterator plugin = plugins_.begin(); plugin != plugins_.end();
+    ++plugin)
+  {
+    (*plugin)->matchSize();
+  }
+}
+
+rcl_interfaces::msg::SetParametersResult PluginContainerLayer::dynamicParametersCallback(
+  std::vector<rclcpp::Parameter> parameters)
+{
+  std::lock_guard<Costmap2D::mutex_t> guard(*getMutex());
+  rcl_interfaces::msg::SetParametersResult result;
+
+  for (auto parameter : parameters) {
+    const auto & param_type = parameter.get_type();
+    const auto & param_name = parameter.get_name();
+
+    if (param_type == ParameterType::PARAMETER_INTEGER) {
+      if (param_name == name_ + "." + "combination_method") {
+        combination_method_ = parameter.as_int();
+      }
+    }
+  }
+
+  result.successful = true;
+  return result;
+}
+
+} // namespace nav2_costmap_2d
