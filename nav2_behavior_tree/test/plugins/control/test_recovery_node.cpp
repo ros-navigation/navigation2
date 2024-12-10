4074d04d499d23318dd706d8e8207b80c951bfde
Tony Najjar <tony.najjar.1997@gmail.com>
2024-12-10 19:29:15 +0100

    Revert "Make RecoveryNode return Running (#4777)" (#4790)
    This reverts commit 9284c8a9969947b99e821e0c85768cca886920ce.

--- nav2_behavior_tree/test/plugins/control/test_recovery_node.cpp
+++ nav2_behavior_tree/test/plugins/control/test_recovery_node.cpp
@@ -105,7 +105,6 @@ TEST_F(RecoveryNodeTestFixture, test_failure_on_idle_child)
   first_child_->changeStatus(BT::NodeStatus::IDLE);
   EXPECT_THROW(bt_node_->executeTick(), BT::LogicError);
   first_child_->changeStatus(BT::NodeStatus::FAILURE);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
   second_child_->changeStatus(BT::NodeStatus::IDLE);
   EXPECT_THROW(bt_node_->executeTick(), BT::LogicError);
 }
@@ -120,9 +119,8 @@ TEST_F(RecoveryNodeTestFixture, test_success_one_retry)
   first_child_->returnSuccessOn(1);
   first_child_->changeStatus(BT::NodeStatus::FAILURE);
   second_child_->changeStatus(BT::NodeStatus::SUCCESS);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
   EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::SUCCESS);
+  EXPECT_EQ(bt_node_->status(), BT::NodeStatus::SUCCESS);
   EXPECT_EQ(first_child_->status(), BT::NodeStatus::IDLE);
   EXPECT_EQ(second_child_->status(), BT::NodeStatus::IDLE);
 }
@@ -132,8 +130,8 @@ TEST_F(RecoveryNodeTestFixture, test_failure_one_retry)
   // first child fails, second child fails
   first_child_->changeStatus(BT::NodeStatus::FAILURE);
   second_child_->changeStatus(BT::NodeStatus::FAILURE);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
   EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::FAILURE);
+  EXPECT_EQ(bt_node_->status(), BT::NodeStatus::FAILURE);
   EXPECT_EQ(first_child_->status(), BT::NodeStatus::IDLE);
   EXPECT_EQ(second_child_->status(), BT::NodeStatus::IDLE);
 
@@ -141,8 +139,6 @@ TEST_F(RecoveryNodeTestFixture, test_failure_one_retry)
   first_child_->returnFailureOn(1);
   first_child_->changeStatus(BT::NodeStatus::FAILURE);
   second_child_->changeStatus(BT::NodeStatus::SUCCESS);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
   EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::FAILURE);
   EXPECT_EQ(bt_node_->status(), BT::NodeStatus::FAILURE);
   EXPECT_EQ(first_child_->status(), BT::NodeStatus::IDLE);
@@ -162,7 +158,6 @@ TEST_F(RecoveryNodeTestFixture, test_skipping)
   // first child fails, second child skipped
   first_child_->changeStatus(BT::NodeStatus::FAILURE);
   second_child_->changeStatus(BT::NodeStatus::SKIPPED);
-  EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::RUNNING);
   EXPECT_EQ(bt_node_->executeTick(), BT::NodeStatus::FAILURE);
   EXPECT_EQ(bt_node_->status(), BT::NodeStatus::FAILURE);
   EXPECT_EQ(first_child_->status(), BT::NodeStatus::IDLE);
