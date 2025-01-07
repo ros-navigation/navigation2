d5521f6618c07dad1517d194ac012f340c928a85
Tony Najjar <tony.najjar.1997@gmail.com>
2025-01-07 14:32:27 +0100

    reformat
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/decorator/goal_updater_node.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/decorator/goal_updater_node.hpp
@@ -58,7 +58,8 @@ public:
       BT::InputPort<geometry_msgs::msg::PoseStampedArray>("input_goals", "Original Goals"),
       BT::OutputPort<geometry_msgs::msg::PoseStamped>("output_goal",
           "Received Goal by subscription"),
-      BT::OutputPort<geometry_msgs::msg::PoseStampedArray>("output_goals", "Received Goals by subscription")
+      BT::OutputPort<geometry_msgs::msg::PoseStampedArray>("output_goals",
+          "Received Goals by subscription")
     };
   }
 
