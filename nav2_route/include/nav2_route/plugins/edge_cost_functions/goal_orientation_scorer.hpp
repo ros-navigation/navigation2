822da7867f969f3e5ecd5ec98262e1f6a51d1037
Alexander Yuen <alex@polymathrobotics.com>
2025-01-18 07:28:41 +0000

    added goal_orientation scorer
    Signed-off-by: Alexander Yuen <alex@polymathrobotics.com>

---
+++ nav2_route/include/nav2_route/plugins/edge_cost_functions/goal_orientation_scorer.hpp
@@ -0,0 +1,80 @@
+// Copyright (c) 2024, Polymath Robotics Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_ROUTE__PLUGINS__EDGE_COST_FUNCTIONS__GOAl_ORIENTATION_SCORER_HPP_
+#define NAV2_ROUTE__PLUGINS__EDGE_COST_FUNCTIONS__GOAL_ORIENTATION_SCORER_HPP_
+
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "nav2_route/interfaces/edge_cost_function.hpp"
+#include "nav2_util/line_iterator.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "nav2_costmap_2d/costmap_subscriber.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/utils.h"
+
+namespace nav2_route
+{
+
+/**
+ * @class GoalOrientationScorer
+ * @brief Scores final edge by comparing the 
+ */
+class GoalOrientationScorer : public EdgeCostFunction
+{
+public:
+  /**
+   * @brief Constructor
+   */
+  GoalOrientationScorer() = default;
+
+  /**
+   * @brief destructor
+   */
+  virtual ~GoalOrientationScorer() = default;
+
+  /**
+   * @brief Configure
+   */
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::SharedPtr node,
+    const std::string & name) override;
+
+  /**
+   * @brief Main scoring plugin API
+   * @param edge The edge pointer to score, which has access to the
+   * start/end nodes and their associated metadata and actions
+   * @param cost of the edge scored
+   * @return bool if this edge is open valid to traverse
+   */
+  bool score(const EdgePtr edge, const geometry_msgs::msg::PoseStamped goal_pose, float & cost, bool final_edge) override;
+
+  /**
+   * @brief Get name of the plugin for parameter scope mapping
+   * @return Name
+   */
+  std::string getName() override;
+
+protected:
+  rclcpp::Logger logger_{rclcpp::get_logger("GoalOrientationScorer")};
+  std::string name_;
+  double orientation_tolerance_;
+};
+
+}  // namespace nav2_route
+
+#endif  // NAV2_ROUTE__PLUGINS__EDGE_COST_FUNCTIONS__GOAL_ORIENTATION_SCORER_HPP_
