6e1c85eba70ed9800b69886c15d73268d60f46e9
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2025-01-22 00:57:09 +0100

    fix simple smoother failing during final approach (#4817)
    * new test case for end of path approach
    
    Signed-off-by: rayferric <63957587+rayferric@users.noreply.github.com>
    
    * modify tests to match the more permissive smoother policy
    
    Signed-off-by: rayferric <63957587+rayferric@users.noreply.github.com>
    
    * implement steve's suggestions
    
    Signed-off-by: rayferric <63957587+rayferric@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: rayferric <63957587+rayferric@users.noreply.github.com>

--- nav2_smoother/include/nav2_smoother/simple_smoother.hpp
+++ nav2_smoother/include/nav2_smoother/simple_smoother.hpp
@@ -92,9 +92,8 @@ protected:
    * @param reversing_segment Return if this is a reversing segment
    * @param costmap Pointer to minimal costmap
    * @param max_time Maximum time to compute, stop early if over limit
-   * @return If smoothing was successful
    */
-  bool smoothImpl(
+  void smoothImpl(
     nav_msgs::msg::Path & path,
     bool & reversing_segment,
     const nav2_costmap_2d::Costmap2D * costmap,
