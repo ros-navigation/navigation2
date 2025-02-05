d14589c6e966e01bc446fa34617d0bd3003e22f7
Steve Macenski <stevenmacenski@gmail.com>
2025-02-04 16:14:22 -0800

    Revert "Adapt GoalUpdater to update goals as well (#4771)"
    This reverts commit 55d738742651e68934582a3ce829913a3950bd09.

--- nav2_behavior_tree/plugins/decorator/goal_updater_node.cpp
+++ nav2_behavior_tree/plugins/decorator/goal_updater_node.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 2018 Intel Corporation
 // Copyright (c) 2020 Francisco Martin Rico
-// Copyright (c) 2024 Angsa Robotics
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +16,7 @@
 #include <string>
 #include <memory>
 
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "behaviortree_cpp/decorator_node.h"
 
 #include "nav2_behavior_tree/plugins/decorator/goal_updater_node.hpp"
@@ -40,9 +40,7 @@ GoalUpdater::GoalUpdater(
   callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
 
   std::string goal_updater_topic;
-  std::string goals_updater_topic;
   node_->get_parameter_or<std::string>("goal_updater_topic", goal_updater_topic, "goal_update");
-  node_->get_parameter_or<std::string>("goals_updater_topic", goals_updater_topic, "goals_update");
 
   rclcpp::SubscriptionOptions sub_option;
   sub_option.callback_group = callback_group_;
@@ -51,72 +49,32 @@ GoalUpdater::GoalUpdater(
     rclcpp::SystemDefaultsQoS(),
     std::bind(&GoalUpdater::callback_updated_goal, this, _1),
     sub_option);
-  goals_sub_ = node_->create_subscription<geometry_msgs::msg::PoseStampedArray>(
-    goals_updater_topic,
-    rclcpp::SystemDefaultsQoS(),
-    std::bind(&GoalUpdater::callback_updated_goals, this, _1),
-    sub_option);
 }
 
 inline BT::NodeStatus GoalUpdater::tick()
 {
   geometry_msgs::msg::PoseStamped goal;
-  geometry_msgs::msg::PoseStampedArray goals;
 
   getInput("input_goal", goal);
-  getInput("input_goals", goals);
 
   // Spin multiple times due to rclcpp regression in Jazzy requiring a 'warm up' spin
   callback_group_executor_.spin_all(std::chrono::milliseconds(1));
   callback_group_executor_.spin_all(std::chrono::milliseconds(49));
 
-  if (last_goal_received_set_) {
-    if (last_goal_received_.header.stamp == rclcpp::Time(0)) {
-        // if the goal doesn't have a timestamp, we reject it
-      RCLCPP_WARN(
-          node_->get_logger(), "The received goal has no timestamp. Ignoring.");
-      setOutput("output_goal", goal);
+  if (last_goal_received_.header.stamp != rclcpp::Time(0)) {
+    auto last_goal_received_time = rclcpp::Time(last_goal_received_.header.stamp);
+    auto goal_time = rclcpp::Time(goal.header.stamp);
+    if (last_goal_received_time > goal_time) {
+      goal = last_goal_received_;
     } else {
-      auto last_goal_received_time = rclcpp::Time(last_goal_received_.header.stamp);
-      auto goal_time = rclcpp::Time(goal.header.stamp);
-      if (last_goal_received_time >= goal_time) {
-        setOutput("output_goal", last_goal_received_);
-      } else {
-        RCLCPP_INFO(
-            node_->get_logger(), "The timestamp of the received goal (%f) is older than the "
-            "current goal (%f). Ignoring the received goal.",
-            last_goal_received_time.seconds(), goal_time.seconds());
-        setOutput("output_goal", goal);
-      }
+      RCLCPP_WARN(
+        node_->get_logger(), "The timestamp of the received goal (%f) is older than the "
+        "current goal (%f). Ignoring the received goal.",
+        last_goal_received_time.seconds(), goal_time.seconds());
     }
-  } else {
-    setOutput("output_goal", goal);
   }
 
-  if (last_goals_received_set_) {
-    if (last_goals_received_.poses.empty()) {
-      setOutput("output_goals", goals);
-    } else if (last_goals_received_.header.stamp == rclcpp::Time(0)) {
-      RCLCPP_WARN(
-          node_->get_logger(), "The received goals array has no timestamp. Ignoring.");
-      setOutput("output_goals", goals);
-    } else {
-      auto last_goals_received_time = rclcpp::Time(last_goals_received_.header.stamp);
-      auto goals_time = rclcpp::Time(goals.header.stamp);
-      if (last_goals_received_time >= goals_time) {
-        setOutput("output_goals", last_goals_received_);
-      } else {
-        RCLCPP_INFO(
-            node_->get_logger(), "The timestamp of the received goals (%f) is older than the "
-            "current goals (%f). Ignoring the received goals.",
-            last_goals_received_time.seconds(), goals_time.seconds());
-        setOutput("output_goals", goals);
-      }
-    }
-  } else {
-    setOutput("output_goals", goals);
-  }
-
+  setOutput("output_goal", goal);
   return child_node_->executeTick();
 }
 
@@ -124,14 +82,6 @@ void
 GoalUpdater::callback_updated_goal(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
 {
   last_goal_received_ = *msg;
-  last_goal_received_set_ = true;
-}
-
-void
-GoalUpdater::callback_updated_goals(const geometry_msgs::msg::PoseStampedArray::SharedPtr msg)
-{
-  last_goals_received_ = *msg;
-  last_goals_received_set_ = true;
 }
 
 }  // namespace nav2_behavior_tree
