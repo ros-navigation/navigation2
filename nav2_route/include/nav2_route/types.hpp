08deb8aebc90f7565957cf40486f2ae1b118a205
Alexander Yuen <alex@polymathrobotics.com>
2025-03-27 19:48:26 +0000

    added doxygen for EdgeType
    Signed-off-by: Alexander Yuen <alex@polymathrobotics.com>

--- nav2_route/include/nav2_route/types.hpp
+++ nav2_route/include/nav2_route/types.hpp
@@ -286,6 +286,10 @@ struct ReroutingState
   }
 };
 
+/**
+ * @enum nav2_route::EdgeType
+ * @brief An enum class describing what type of edge connecting two nodes is 
+ */
 enum class EdgeType
 {
   NONE = 0,
