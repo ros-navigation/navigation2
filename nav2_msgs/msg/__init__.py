7300ef2797507c21cccebea23799c382aa2b821d
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-04-03 00:31:27 +0000

    Define attributes for nav2_msgs.
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

---
+++ nav2_msgs/msg/__init__.py
@@ -0,0 +1,29 @@
+from nav2_msgs.msg._behavior_tree_log import BehaviorTreeLog
+from nav2_msgs.msg._behavior_tree_status_change import BehaviorTreeStatusChange
+from nav2_msgs.msg._collision_detector_state import CollisionDetectorState
+from nav2_msgs.msg._collision_monitor_state import CollisionMonitorState
+from nav2_msgs.msg._costmap import Costmap
+from nav2_msgs.msg._costmap_filter_info import CostmapFilterInfo
+from nav2_msgs.msg._costmap_meta_data import CostmapMetaData
+from nav2_msgs.msg._costmap_update import CostmapUpdate
+from nav2_msgs.msg._missed_waypoint import MissedWaypoint
+from nav2_msgs.msg._particle import Particle
+from nav2_msgs.msg._particle_cloud import ParticleCloud
+from nav2_msgs.msg._speed_limit import SpeedLimit
+from nav2_msgs.msg._voxel_grid import VoxelGrid
+
+__all__ = [
+    'BehaviorTreeLog',
+    'BehaviorTreeStatusChange',
+    'CollisionDetectorState',
+    'CollisionMonitorState',
+    'Costmap',
+    'CostmapFilterInfo',
+    'CostmapMetaData',
+    'CostmapUpdate',
+    'MissedWaypoint',
+    'Particle',
+    'ParticleCloud',
+    'SpeedLimit',
+    'VoxelGrid',
+]
