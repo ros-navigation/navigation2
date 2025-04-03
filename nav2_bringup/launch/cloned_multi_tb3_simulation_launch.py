bff899ce814569d23192f15585e4432059542b3f
MaciejGroszyk <maciekgroszyk12@gmail.com>
2025-04-03 22:54:42 +0200

    revert commit 2cd166f without parameters.key >>-v32, return ParseMultiRobotPose to fix launch nav2_bringup unique_multi_tb3_simulation_launch.py
    Signed-off-by: MaciejGroszyk <maciekgroszyk12@gmail.com>

--- nav2_bringup/launch/cloned_multi_tb3_simulation_launch.py
+++ nav2_bringup/launch/cloned_multi_tb3_simulation_launch.py
@@ -24,43 +24,20 @@ from launch.actions import (
     AppendEnvironmentVariable,
     DeclareLaunchArgument,
     ExecuteProcess,
-    ForEach,
     GroupAction,
     IncludeLaunchDescription,
     LogInfo,
     OpaqueFunction,
     RegisterEventHandler,
 )
-
 from launch.conditions import IfCondition
 from launch.event_handlers import OnShutdown
-from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, TextSubstitution
-
-import yaml
-
-
-def count_robots(context: LaunchContext) -> list[LogInfo]:
-    """Count the number of robots from the 'robots' launch argument."""
-    robots_str = LaunchConfiguration('robots').perform(context).strip()
-    log_msg = ''
-
-    if not robots_str:
-        log_msg = 'No robots provided in the launch argument.'
-
-    try:
-        robots_list = [yaml.safe_load(robot.strip()) for robot in
-                       robots_str.split(';') if robot.strip()]
-        log_msg = f'number_of_robots={len(robots_list)}'
-    except yaml.YAMLError as e:
-        log_msg = f'Error parsing the robots launch argument: {e}'
-
-    return [LogInfo(msg=[log_msg])]
+from nav2_common.launch import ParseMultiRobotPose
 
 
-def generate_robot_actions(name: str = '', pose: dict = {}) -> list[GroupAction]:
-    """Generate the actions to launch a robot with the given name and pose."""
+def generate_robot_actions(context, *args, **kwargs):
     bringup_dir = get_package_share_directory('nav2_bringup')
     launch_dir = os.path.join(bringup_dir, 'launch')
     use_rviz = LaunchConfiguration('use_rviz')
@@ -70,50 +47,57 @@ def generate_robot_actions(name: str = '', pose: dict = {}) -> list[GroupAction]
     map_yaml_file = LaunchConfiguration('map')
     use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
 
+    robots_substitution = ParseMultiRobotPose(LaunchConfiguration('robots'))
+    robots_list = robots_substitution.perform(context)
+
     # Define commands for launching the navigation instances
-    group = GroupAction(
-        [
-            LogInfo(
-                msg=['Launching namespace=', name,
-                     ' init_pose=', str(pose), ]
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(launch_dir, 'rviz_launch.py')
+    bringup_cmd_group = []
+    for robot_name in robots_list:
+        init_pose = robots_list[robot_name]
+        group = GroupAction(
+            [
+                LogInfo(
+                    msg=['Launching namespace=', robot_name, ' init_pose=', str(init_pose),]
                 ),
-                condition=IfCondition(use_rviz),
-                launch_arguments={
-                    'namespace': TextSubstitution(text=name),
-                    'rviz_config': rviz_config_file,
-                }.items(),
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(bringup_dir, 'launch',
-                                 'tb3_simulation_launch.py')
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(launch_dir, 'rviz_launch.py')
+                    ),
+                    condition=IfCondition(use_rviz),
+                    launch_arguments={
+                        'namespace': TextSubstitution(text=robot_name),
+                        'rviz_config': rviz_config_file,
+                    }.items(),
                 ),
-                launch_arguments={
-                    'namespace': name,
-                    'map': map_yaml_file,
-                    'use_sim_time': 'True',
-                    'params_file': params_file,
-                    'autostart': autostart,
-                    'use_rviz': 'False',
-                    'use_simulator': 'False',
-                    'headless': 'False',
-                    'use_robot_state_pub': use_robot_state_pub,
-                    'x_pose': TextSubstitution(text=str(pose.get('x', 0.0))),
-                    'y_pose': TextSubstitution(text=str(pose.get('y', 0.0))),
-                    'z_pose': TextSubstitution(text=str(pose.get('z', 0.0))),
-                    'roll': TextSubstitution(text=str(pose.get('roll', 0.0))),
-                    'pitch': TextSubstitution(text=str(pose.get('pitch', 0.0))),
-                    'yaw': TextSubstitution(text=str(pose.get('yaw', 0.0))),
-                    'robot_name': TextSubstitution(text=name),
-                }.items(),
-            ),
-        ]
-    )
-    return [group]
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(bringup_dir, 'launch', 'tb3_simulation_launch.py')
+                    ),
+                    launch_arguments={
+                        'namespace': robot_name,
+                        'map': map_yaml_file,
+                        'use_sim_time': 'True',
+                        'params_file': params_file,
+                        'autostart': autostart,
+                        'use_rviz': 'False',
+                        'use_simulator': 'False',
+                        'headless': 'False',
+                        'use_robot_state_pub': use_robot_state_pub,
+                        'x_pose': TextSubstitution(text=str(init_pose['x'])),
+                        'y_pose': TextSubstitution(text=str(init_pose['y'])),
+                        'z_pose': TextSubstitution(text=str(init_pose['z'])),
+                        'roll': TextSubstitution(text=str(init_pose['roll'])),
+                        'pitch': TextSubstitution(text=str(init_pose['pitch'])),
+                        'yaw': TextSubstitution(text=str(init_pose['yaw'])),
+                        'robot_name': TextSubstitution(text=robot_name),
+                    }.items(),
+                ),
+            ]
+        )
+
+        bringup_cmd_group.append(group)
+    bringup_cmd_group.append(LogInfo(msg=['number_of_robots=', str(len(robots_list))]))
+    return bringup_cmd_group
 
 
 def generate_launch_description():
@@ -122,13 +106,9 @@ def generate_launch_description():
 
     Launch arguments consist of robot name(which is namespace) and pose for initialization.
     Keep general yaml format for pose information.
-
-    ex) robots:='{name: 'robot1', pose: {x: 1.0, y: 1.0, yaw: 1.5707}};
-                 {name: 'robot2', pose: {x: 1.0, y: 1.0, yaw: 1.5707}}'
-    ex) robots:='{name: 'robot3', pose: {x: 1.0, y: 1.0, z: 1.0,
-                                    roll: 0.0, pitch: 1.5707, yaw: 1.5707}};
-                 {name: 'robot4', pose: {x: 1.0, y: 1.0, z: 1.0,
-                                    roll: 0.0, pitch: 1.5707, yaw: 1.5707}}'
+    ex) robots:='robot1={x: 1.0, y: 1.0, yaw: 1.5707}; robot2={x: 1.0, y: 1.0, yaw: 1.5707}'
+    ex) robots:='robot3={x: 1.0, y: 1.0, z: 1.0, roll: 0.0, pitch: 1.5707, yaw: 1.5707};
+                 robot4={x: 1.0, y: 1.0, z: 1.0, roll: 0.0, pitch: 1.5707, yaw: 1.5707}'
     """
     # Get the launch directory
     bringup_dir = get_package_share_directory('nav2_bringup')
@@ -154,10 +134,8 @@ def generate_launch_description():
 
     declare_robots_cmd = DeclareLaunchArgument(
         'robots',
-        default_value=(
-            "{name: 'robot1', pose: {x: 0.5, y: 0.5, yaw: 0}};"
-            "{name: 'robot2', pose: {x: -0.5, y: -0.5, z: 0, roll: 0, pitch: 0, yaw: 1.5707}}"
-        ),
+        default_value="""robot1={x: 0.5, y: 0.5, yaw: 0};
+                   robot2={x: -0.5, y: -0.5, z: 0, roll: 0, pitch: 0, yaw: 1.5707}""",
         description='Robots and their initialization poses in YAML format',
     )
 
@@ -209,15 +187,14 @@ def generate_launch_description():
 
     remove_temp_sdf_file = RegisterEventHandler(event_handler=OnShutdown(
         on_shutdown=[
-            OpaqueFunction(function=lambda _: os.remove(world_sdf)
-                           if os.path.exists(world_sdf) else None)
+            OpaqueFunction(function=lambda _: os.remove(world_sdf))
         ]))
 
     set_env_vars_resources = AppendEnvironmentVariable(
         'GZ_SIM_RESOURCE_PATH', os.path.join(sim_dir, 'models'))
     set_env_vars_resources2 = AppendEnvironmentVariable(
-        'GZ_SIM_RESOURCE_PATH',
-        str(Path(os.path.join(sim_dir)).parent.resolve()))
+            'GZ_SIM_RESOURCE_PATH',
+            str(Path(os.path.join(sim_dir)).parent.resolve()))
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -240,12 +217,10 @@ def generate_launch_description():
     ld.add_action(remove_temp_sdf_file)
 
     ld.add_action(
-        LogInfo(condition=IfCondition(log_settings),
-                msg=['map yaml: ', map_yaml_file])
+        LogInfo(condition=IfCondition(log_settings), msg=['map yaml: ', map_yaml_file])
     )
     ld.add_action(
-        LogInfo(condition=IfCondition(log_settings),
-                msg=['params yaml: ', params_file])
+        LogInfo(condition=IfCondition(log_settings), msg=['params yaml: ', params_file])
     )
     ld.add_action(
         LogInfo(
@@ -260,12 +235,8 @@ def generate_launch_description():
         )
     )
     ld.add_action(
-        LogInfo(condition=IfCondition(log_settings),
-                msg=['autostart: ', autostart])
+        LogInfo(condition=IfCondition(log_settings), msg=['autostart: ', autostart])
     )
-
-    ld.add_action(OpaqueFunction(function=count_robots))
-    ld.add_action(ForEach(LaunchConfiguration('robots'),
-                  function=generate_robot_actions))
+    ld.add_action(OpaqueFunction(function=generate_robot_actions))
 
     return ld
