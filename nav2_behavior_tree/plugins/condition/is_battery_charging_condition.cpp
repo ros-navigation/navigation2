a16aea1b396f05e6e92a047964fff9531225ea30
Alberto Tudela <ajtudela@gmail.com>
2025-04-04 11:41:45 +0200

    Add comment
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- nav2_behavior_tree/plugins/condition/is_battery_charging_condition.cpp
+++ nav2_behavior_tree/plugins/condition/is_battery_charging_condition.cpp
@@ -64,6 +64,7 @@ BT::NodeStatus IsBatteryChargingCondition::tick()
     initialize();
   }
 
+  // Spin multiple times due to rclcpp regression in Jazzy requiring a 'warm up' spin
   callback_group_executor_.spin_some(std::chrono::nanoseconds(1));
   callback_group_executor_.spin_some();
   if (is_battery_charging_) {
