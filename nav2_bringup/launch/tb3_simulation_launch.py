bdbbeb52d469c70316f255db4ac6149e477d6c08
MaciejGroszyk <maciekgroszyk12@gmail.com>
2025-04-08 19:39:42 +0200

    C to c to fulfill ament_copyright
    Signed-off-by: MaciejGroszyk <maciekgroszyk12@gmail.com>

--- nav2_bringup/launch/tb3_simulation_launch.py
+++ nav2_bringup/launch/tb3_simulation_launch.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Open Source Robotics Foundation
+# Copyright (c) 2023 Open Source Robotics Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- nav2_bringup/launch/tb4_simulation_launch.py
+++ nav2_bringup/launch/tb4_simulation_launch.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 Open Navigation LLC
+# Copyright (c) 2024 Open Navigation LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
