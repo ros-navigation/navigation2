911c361ba0e4095155c9d56586214c2d73c7d3be
John Chrosniak <chrosniakj@gmail.com>
2025-04-09 16:46:48 -0400

    double -> single quotes
    Signed-off-by: John Chrosniak <chrosniakj@gmail.com>

--- nav2_rviz_plugins/launch/route_tool.launch.py
+++ nav2_rviz_plugins/launch/route_tool.launch.py
@@ -2,14 +2,14 @@
 
 # Copyright (c) 2024 John Chrosniak
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -26,44 +26,44 @@ import launch_ros.actions
 def generate_launch_description():
 
     # Nodes launching commands
-    map_file = LaunchConfiguration("yaml_filename")
+    map_file = LaunchConfiguration('yaml_filename')
 
     declare_map_file_cmd = DeclareLaunchArgument(
-        "yaml_filename",
-        default_value="",
-        description="Full path to an occupancy grid map yaml file",
+        'yaml_filename',
+        default_value='',
+        description='Full path to an occupancy grid map yaml file',
     )
 
     start_route_tool_cmd = launch_ros.actions.Node(
-        package="rviz2",
-        executable="rviz2",
-        output="screen",
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
         arguments=[
-            "-d"
+            '-d'
             + os.path.join(
-                get_package_share_directory("nav2_rviz_plugins"),
-                "rviz",
-                "route_tool.rviz",
+                get_package_share_directory('nav2_rviz_plugins'),
+                'rviz',
+                'route_tool.rviz',
             )
         ],
     )
 
     start_map_server = launch_ros.actions.Node(
-        package="nav2_map_server",
-        executable="map_server",
-        output="screen",
-        parameters=[{"yaml_filename": map_file}],
+        package='nav2_map_server',
+        executable='map_server',
+        output='screen',
+        parameters=[{'yaml_filename': map_file}],
     )
 
     start_lifecycle_manager_cmd = launch_ros.actions.Node(
-        package="nav2_lifecycle_manager",
-        executable="lifecycle_manager",
-        name="lifecycle_manager",
-        output="screen",
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager',
+        output='screen',
         parameters=[
-            {"use_sim_time": False},
-            {"autostart": True},
-            {"node_names": ["map_server"]},
+            {'use_sim_time': False},
+            {'autostart': True},
+            {'node_names': ['map_server']},
         ],
     )
 
