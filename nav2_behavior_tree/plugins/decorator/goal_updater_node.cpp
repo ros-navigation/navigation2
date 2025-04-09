27b7f38841699107ab9449c3777b919f4e33572f
Alberto Tudela <ajtudela@gmail.com>
2025-04-09 11:17:12 +0200

    Add double spin_some in some BT nodes
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- nav2_behavior_tree/plugins/decorator/goal_updater_node.cpp
+++ nav2_behavior_tree/plugins/decorator/goal_updater_node.cpp
@@ -49,6 +49,9 @@ GoalUpdater::GoalUpdater(
     rclcpp::SystemDefaultsQoS(),
     std::bind(&GoalUpdater::callback_updated_goal, this, _1),
     sub_option);
+
+  // Spin multiple times due to rclcpp regression in Jazzy requiring a 'warm up' spin
+  callback_group_executor_.spin_all(std::chrono::milliseconds(1));
 }
 
 inline BT::NodeStatus GoalUpdater::tick()
@@ -57,8 +60,6 @@ inline BT::NodeStatus GoalUpdater::tick()
 
   getInput("input_goal", goal);
 
-  // Spin multiple times due to rclcpp regression in Jazzy requiring a 'warm up' spin
-  callback_group_executor_.spin_all(std::chrono::milliseconds(1));
   callback_group_executor_.spin_all(std::chrono::milliseconds(49));
 
   if (last_goal_received_.header.stamp != rclcpp::Time(0)) {
