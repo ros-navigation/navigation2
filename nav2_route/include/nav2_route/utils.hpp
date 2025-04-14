e341c3116489542f792c989af3fe4044bd2bc70b
John Chrosniak <chrosniakj@gmail.com>
2025-04-14 14:08:54 -0400

    linter fix
    Signed-off-by: John Chrosniak <chrosniakj@gmail.com>

--- nav2_route/include/nav2_route/utils.hpp
+++ nav2_route/include/nav2_route/utils.hpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <limits>
 #include <string>
 #include <vector>
 #include "std_msgs/msg/color_rgba.hpp"
