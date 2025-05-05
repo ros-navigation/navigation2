011bd800c9c63924c340c36421c242e71a32e8ce
rohinraj <rohinsandro@gmail.com>
2025-05-05 09:26:48 +0530

    Add Keepout Simulation
    * Add keepout filter launch file
    * Add keepout navigation parameters
    * Add keepout warehouse map and yaml files

---
+++ nav2_bringup/launch/keepout_filter_launch.py
@@ -0,0 +1,50 @@
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    lifecycle_nodes = ['filter_mask_server', 'costmap_filter_info_server']
+
+            
+    params_file= os.path.join(get_package_share_directory('nav2_bringup'), 'params', 'keepout_params.yaml')
+    filter_map_file = os.path.join(get_package_share_directory('nav2_bringup'),'maps', 'keepout_warehouse.yaml')
+
+    # Nodes launching commands
+    start_lifecycle_manager_cmd = Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_costmap_filters',
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[{'use_sim_time': False},
+                        {'autostart': True},
+                        {'node_names': lifecycle_nodes}])
+
+    start_map_server_cmd = Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='filter_mask_server',
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[params_file,{'yaml_filename': filter_map_file}])
+
+    start_costmap_filter_info_server_cmd = Node(
+            package='nav2_map_server',
+            executable='costmap_filter_info_server',
+            name='costmap_filter_info_server',
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[params_file])
+
+    ld = LaunchDescription()
+
+
+    ld.add_action(start_lifecycle_manager_cmd)
+    ld.add_action(start_map_server_cmd)
+    ld.add_action(start_costmap_filter_info_server_cmd)
+
+    return ld
