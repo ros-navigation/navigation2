d2b60b6c9ee49e16fee2dfecce21c923a396fcad
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-05-11 23:11:20 +0000

    Preserve initial nature of costmap filters tests.
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- nav2_system_tests/src/costmap_filters/test_keepout_launch.py
+++ nav2_system_tests/src/costmap_filters/test_keepout_launch.py
@@ -147,6 +147,7 @@ def generate_launch_description() -> LaunchDescription:
                     'namespace': '',
                     'map': map_yaml_file,
                     'use_keepout_zones': 'False',
+                    'use_speed_zones': 'False',
                     'use_sim_time': 'True',
                     'params_file': new_yaml,
                     'bt_xml_file': bt_navigator_xml,
