bd8f422c572fc6928d85547df00f7d7c3ba880bd
PrabhavSaxena <98343010+PrabhavSaxena@users.noreply.github.com>
2025-05-16 22:54:14 +0530

    Addition of position goal checker, fix of rotation shim controller (#5162)
    * added position goal checker plugin
    
    Signed-off-by: PrabhavSaxena <prabhavsaxena2003@gmail.com>
    
    * updated rotation shim to use position goal checker
    
    Signed-off-by: PrabhavSaxena <prabhavsaxena2003@gmail.com>
    
    * fixed lint
    
    Signed-off-by: PrabhavSaxena <prabhavsaxena2003@gmail.com>
    
    * added comments
    
    Signed-off-by: PrabhavSaxena <prabhavsaxena2003@gmail.com>
    
    * fixed dynamic parameter, CMake
    
    Signed-off-by: PrabhavSaxena <prabhavsaxena2003@gmail.com>
    
    ---------
    
    Signed-off-by: PrabhavSaxena <prabhavsaxena2003@gmail.com>

--- nav2_rotation_shim_controller/include/nav2_rotation_shim_controller/nav2_rotation_shim_controller.hpp
+++ nav2_rotation_shim_controller/include/nav2_rotation_shim_controller/nav2_rotation_shim_controller.hpp
@@ -28,6 +28,7 @@
 #include "nav2_core/controller.hpp"
 #include "nav2_core/controller_exceptions.hpp"
 #include "nav2_costmap_2d/footprint_collision_checker.hpp"
+#include "nav2_controller/plugins/position_goal_checker.hpp"
 
 namespace nav2_rotation_shim_controller
 {
@@ -192,6 +193,7 @@ protected:
   // Dynamic parameters handler
   std::mutex mutex_;
   rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
+  std::unique_ptr<nav2_controller::PositionGoalChecker> position_goal_checker_;
 };
 
 }  // namespace nav2_rotation_shim_controller
