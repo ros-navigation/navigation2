d6fc89a401c4ad8d1d4e7a429ad547d2e4dcbcc4
Guillaume Doisy <doisyg@users.noreply.github.com>
2025-06-10 20:49:55 +0100

    Add min_distance_to_obstacle parameter to RPP (#4543)
    * min_distance_to_obstacle
    
    Signed-off-by: Guillaume Doisy <guillaume@dexory.com>
    
    * suggestion to time base and combine
    
    Signed-off-by: Guillaume Doisy <guillaume@dexory.com>
    
    * typo
    
    Signed-off-by: Guillaume Doisy <guillaume@dexory.com>
    
    * use min_approach_linear_velocity
    
    Signed-off-by: Guillaume Doisy <guillaume@dexory.com>
    
    ---------
    
    Signed-off-by: Guillaume Doisy <guillaume@dexory.com>
    Co-authored-by: Guillaume Doisy <guillaume@dexory.com>

--- nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
+++ nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
@@ -720,6 +720,7 @@ TEST(RegulatedPurePursuitTest, testDynamicParameter)
       rclcpp::Parameter("test.rotate_to_heading_angular_vel", 18.0),
       rclcpp::Parameter("test.min_approach_linear_velocity", 1.0),
       rclcpp::Parameter("test.max_allowed_time_to_collision_up_to_carrot", 2.0),
+      rclcpp::Parameter("test.min_distance_to_obstacle", 2.0),
       rclcpp::Parameter("test.cost_scaling_dist", 2.0),
       rclcpp::Parameter("test.cost_scaling_gain", 4.0),
       rclcpp::Parameter("test.regulated_linear_scaling_min_radius", 10.0),
@@ -749,6 +750,7 @@ TEST(RegulatedPurePursuitTest, testDynamicParameter)
   EXPECT_EQ(
     node->get_parameter(
       "test.max_allowed_time_to_collision_up_to_carrot").as_double(), 2.0);
+  EXPECT_EQ(node->get_parameter("test.min_distance_to_obstacle").as_double(), 2.0);
   EXPECT_EQ(node->get_parameter("test.cost_scaling_dist").as_double(), 2.0);
   EXPECT_EQ(node->get_parameter("test.cost_scaling_gain").as_double(), 4.0);
   EXPECT_EQ(node->get_parameter("test.regulated_linear_scaling_min_radius").as_double(), 10.0);
