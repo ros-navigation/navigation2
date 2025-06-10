e1699599c77d036653eba8d8891ad8b534d975c4
redvinaa <redvinaa@gmail.com>
2025-06-10 12:54:26 +0000

    Lint
    Signed-off-by: redvinaa <redvinaa@gmail.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/control/pause_resume_controller.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/control/pause_resume_controller.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONTROL__PAUSE_HPP_
-#define NAV2_BEHAVIOR_TREE__PLUGINS__CONTROL__PAUSE_HPP_
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONTROL__PAUSE_RESUME_CONTROLLER_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__CONTROL__PAUSE_RESUME_CONTROLLER_HPP_
 
 // Other includes
 #include <string>
@@ -126,4 +126,4 @@ private:
 
 }  // namespace nav2_behavior_tree
 
-#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONTROL__PAUSE_HPP_
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONTROL__PAUSE_RESUME_CONTROLLER_HPP_
