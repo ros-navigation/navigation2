92991effb51699777815ce0abc38f922fe2c7c3e
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-07-02 17:45:13 +0100

    Fix ament mypy (#5322)
    * Configured nav2_common to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_bringup to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_costmap_2d to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_docking to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_lifecycle_manager to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_map_server to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_simple_commander to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Configured nav2_system_tests to be compatible with ament_mypy.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    ---------
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- nav2_lifecycle_manager/test/launch_bond_test.py
+++ nav2_lifecycle_manager/test/launch_bond_test.py
@@ -43,17 +43,17 @@ def generate_launch_description() -> LaunchDescription:
 def main(argv: list[str] = sys.argv[1:]):  # type: ignore[no-untyped-def]
     ld = generate_launch_description()
 
-    testExecutable = os.getenv('TEST_EXECUTABLE')
+    testExecutable = os.getenv('TEST_EXECUTABLE', '')
 
     test1_action = ExecuteProcess(
         cmd=[testExecutable], name='test_bond_gtest', output='screen'
     )
 
-    lts = LaunchTestService()
-    lts.add_test_action(ld, test1_action)
+    lts = LaunchTestService()  # type: ignore[no-untyped-call]
+    lts.add_test_action(ld, test1_action)  # type: ignore[no-untyped-call]
     ls = LaunchService(argv=argv)
     ls.include_launch_description(ld)
-    return lts.run(ls)
+    return lts.run(ls)  # type: ignore[no-untyped-call]
 
 
 if __name__ == '__main__':
