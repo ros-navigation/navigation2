607edd200b05e05b21643e8afada21f9c5cc3c14
Dhruv Patel <dhruvpatel2991998@gmail.com>
2025-07-08 22:54:55 +0200

    Update lifecycle_node.hpp :: createBond() -> bond_ type fix
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>

--- nav2_ros_common/include/nav2_ros_common/lifecycle_node.hpp
+++ nav2_ros_common/include/nav2_ros_common/lifecycle_node.hpp
@@ -312,8 +312,8 @@ public:
   {
     if (bond_heartbeat_period > 0.0) {
       RCLCPP_INFO(get_logger(), "Creating bond (%s) to lifecycle manager.", this->get_name());
-
-      bond_ = std::make_unique<bond::Bond>(
+      
+      bond_ = std::make_shared<bond::Bond>(
         std::string("bond"),
         this->get_name(),
         shared_from_this());
