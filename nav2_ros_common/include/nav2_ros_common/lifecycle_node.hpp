9833be709fd09851be1c6bb6323d354acaa9a3d2
Dhruv Patel <dhruvpatel2991998@gmail.com>
2025-07-09 09:50:10 +0200

    Update lifecycle_node.hpp: removing extra spaces
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>

--- nav2_ros_common/include/nav2_ros_common/lifecycle_node.hpp
+++ nav2_ros_common/include/nav2_ros_common/lifecycle_node.hpp
@@ -312,7 +312,7 @@ public:
   {
     if (bond_heartbeat_period > 0.0) {
       RCLCPP_INFO(get_logger(), "Creating bond (%s) to lifecycle manager.", this->get_name());
-      
+
       bond_ = std::make_shared<bond::Bond>(
         std::string("bond"),
         this->get_name(),
