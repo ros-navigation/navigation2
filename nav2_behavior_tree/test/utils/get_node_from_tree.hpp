defefc0d98799e082306bf5b972d4764d9b474b1
redvinaa <redvinaa@gmail.com>
2025-07-15 15:41:27 +0200

    Update copyrights
    Signed-off-by: redvinaa <redvinaa@gmail.com>

--- nav2_behavior_tree/test/utils/get_node_from_tree.hpp
+++ nav2_behavior_tree/test/utils/get_node_from_tree.hpp
@@ -1,5 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
-// Copyright (c) 2020 Sarthak Mittal
+// Copyright (c) 2025 Enjoy Robotics
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
