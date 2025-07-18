8b79e8d76c65e3cd06adfb324580954439e62dc3
hello-ola <oghattas@hello-robot.com>
2025-07-18 13:05:51 -0700

    use binary_state rather than default state
    Signed-off-by: hello-ola <oghattas@hello-robot.com>

--- nav2_costmap_2d/plugins/costmap_filters/binary_filter.cpp
+++ nav2_costmap_2d/plugins/costmap_filters/binary_filter.cpp
@@ -97,8 +97,8 @@ void BinaryFilter::initializeFilter(
   base_ = BASE_DEFAULT;
   multiplier_ = MULTIPLIER_DEFAULT;
 
-  // Initialize state as "false" by-default
-  changeState(default_state_);
+  // Initialize state binary_state_ which is "false" by-default
+  changeState(binary_state_);
 }
 
 void BinaryFilter::filterInfoCallback(
@@ -230,7 +230,7 @@ void BinaryFilter::resetFilter()
   std::lock_guard<CostmapFilter::mutex_t> guard(*getMutex());
 
   RCLCPP_INFO(logger_, "BinaryFilter: Resetting the filter to default state");
-  changeState(default_state_);
+  changeState(binary_state_);
 
   filter_info_sub_.reset();
   mask_sub_.reset();
