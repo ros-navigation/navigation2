3064777f61dbe8cba2066b83dc47b3bf878be1d7
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-07-20 03:36:33 +0300

    Remove line
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>

--- nav2_ros_common/include/nav2_ros_common/lifecycle_node.hpp
+++ nav2_ros_common/include/nav2_ros_common/lifecycle_node.hpp
@@ -68,7 +68,6 @@ public:
         bond::msg::Constants::DISABLE_HEARTBEAT_TIMEOUT_PARAM, true));
 
     bond_heartbeat_period = this->declare_or_get_parameter<double>("bond_heartbeat_period", 0.1);
-
     bool autostart_node = this->declare_or_get_parameter("autostart_node", false);
     if (autostart_node) {
       autostart();
