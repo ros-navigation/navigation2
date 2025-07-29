84008cbbcdd0420175bb1e11b9cbcd2219b66561
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-07-29 21:38:45 +0300

    Rebase
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>

--- nav2_collision_monitor/src/pointcloud.cpp
+++ nav2_collision_monitor/src/pointcloud.cpp
@@ -45,7 +45,7 @@ PointCloud::PointCloud(
 PointCloud::~PointCloud()
 {
   RCLCPP_INFO(logger_, "[%s]: Destroying PointCloud", source_name_.c_str());
-  data_sub_.reset();
+  data_sub_.shutdown();
 }
 
 void PointCloud::configure()
@@ -60,10 +60,15 @@ void PointCloud::configure()
 
   getParameters(source_topic);
 
-  data_sub_ = node->create_subscription<sensor_msgs::msg::PointCloud2>(
-    source_topic,
+  const point_cloud_transport::TransportHints hint(transport_type_);
+
+  pct_ = std::make_shared<point_cloud_transport::PointCloudTransport>(*node);
+
+  data_sub_ = pct_->subscribe(
+    source_topic, nav2::qos::SensorDataQoS(),
     std::bind(&PointCloud::dataCallback, this, std::placeholders::_1),
-    nav2::qos::SensorDataQoS());
+    {}, &hint
+  );
 
   // Add callback for dynamic parameters
   dyn_params_handler_ = node->add_on_set_parameters_callback(
@@ -131,6 +136,8 @@ void PointCloud::getParameters(std::string & source_topic)
   nav2::declare_parameter_if_not_declared(
     node, source_name_ + ".min_range", rclcpp::ParameterValue(0.0));
   min_range_ = node->get_parameter(source_name_ + ".min_range").as_double();
+    node, source_name_ + ".transport_type", rclcpp::ParameterValue(std::string("raw")));
+  transport_type_ = node->get_parameter(source_name_ + ".transport_type").as_string();
 }
 
 void PointCloud::dataCallback(sensor_msgs::msg::PointCloud2::ConstSharedPtr msg)
