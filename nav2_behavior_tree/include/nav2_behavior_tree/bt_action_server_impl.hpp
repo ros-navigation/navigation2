87f45c5bcc3e7bac6e9250f90534757863260915
Jad El Hajj <jad.elhajj@inmind.ai>
2025-08-08 10:57:41 +0300

    Added recursive check to loadBehaviorTree and adapted unit test accordingly
    Signed-off-by: Jad El Hajj <jad.elhajj@inmind.ai>

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
@@ -204,7 +204,7 @@ template<class ActionT, class NodeT>
 bool BtActionServer<ActionT, NodeT>::on_activate()
 {
   resetInternalError();
-  if (!loadBehaviorTreesRecursive(default_bt_xml_filename_, search_directories_)) {
+  if (!loadBehaviorTree(default_bt_xml_filename_, search_directories_)) {
     RCLCPP_ERROR(logger_, "Error loading XML file: %s", default_bt_xml_filename_.c_str());
     return false;
   }
@@ -246,64 +246,7 @@ void BtActionServer<ActionT, NodeT>::setGrootMonitoring(
 }
 
 template<class ActionT, class NodeT>
-bool BtActionServer<ActionT, NodeT>::loadBehaviorTree(const std::string & bt_xml_filename)
-{
-  // Empty filename is default for backward compatibility
-  auto filename = bt_xml_filename.empty() ? default_bt_xml_filename_ : bt_xml_filename;
-
-  // Use previous BT if it is the existing one and always reload flag is not set to true
-  if (!always_reload_bt_xml_ && current_bt_xml_filename_ == filename) {
-    RCLCPP_DEBUG(logger_, "BT will not be reloaded as the given xml is already loaded");
-    return true;
-  }
-
-  // if a new tree is created, than the Groot2 Publisher must be destroyed
-  bt_->resetGrootMonitor();
-
-  // Read the input BT XML from the specified file into a string
-  std::ifstream xml_file(filename);
-
-  if (!xml_file.good()) {
-    setInternalError(ActionT::Result::FAILED_TO_LOAD_BEHAVIOR_TREE,
-      "Couldn't open input XML file: " + filename);
-    return false;
-  }
-
-  // Create the Behavior Tree from the XML input
-  try {
-    tree_ = bt_->createTreeFromFile(filename, blackboard_);
-    for (auto & subtree : tree_.subtrees) {
-      auto & blackboard = subtree->blackboard;
-      blackboard->set("node", client_node_);
-      blackboard->set<std::chrono::milliseconds>("server_timeout", default_server_timeout_);
-      blackboard->set<std::chrono::milliseconds>("bt_loop_duration", bt_loop_duration_);
-      blackboard->set<std::chrono::milliseconds>(
-        "wait_for_service_timeout",
-        wait_for_service_timeout_);
-    }
-  } catch (const std::exception & e) {
-    setInternalError(ActionT::Result::FAILED_TO_LOAD_BEHAVIOR_TREE,
-      std::string("Exception when loading BT: ") + e.what());
-    return false;
-  }
-
-  topic_logger_ = std::make_unique<RosTopicLogger>(client_node_, tree_);
-
-  current_bt_xml_filename_ = filename;
-
-  // Enable monitoring with Groot2
-  if (enable_groot_monitoring_) {
-    bt_->addGrootMonitoring(&tree_, groot_server_port_);
-    RCLCPP_DEBUG(
-      logger_, "Enabling Groot2 monitoring for %s: %d",
-      action_name_.c_str(), groot_server_port_);
-  }
-
-  return true;
-}
-
-template<class ActionT, class NodeT>
-bool BtActionServer<ActionT, NodeT>::loadBehaviorTreesRecursive(
+bool BtActionServer<ActionT, NodeT>::loadBehaviorTree(
   const std::string & bt_xml_filename,
   const std::vector<std::string> & search_directories)
 {
