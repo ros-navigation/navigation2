43251af9f154863c2592483847434456dd1a4aba
Sakshay Mahna <sakshum19@gmail.com>
2025-08-11 23:13:07 +0530

    Calculate next path point using lookahead for Graceful Controller (#5003)
    * Add controller utils
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    
    * Fix linting issues
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    
    * Update regulated pure pursuit
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    
    * Update graceful controller
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    
    * Remove interpolate after goal & Use orientationAroundZAxis
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    
    * Update nav2_util/src/controller_utils.cpp
    
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * Update nav2_util/src/controller_utils.cpp
    
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * Update nav2_util/src/controller_utils.cpp
    
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    
    * Remove interpolate_after_goal parameter from test
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    
    ---------
    
    Signed-off-by: Sakshay Mahna <sakshum19@gmail.com>
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.hpp
+++ nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.hpp
@@ -175,32 +175,6 @@ protected:
     double & linear_vel, double & sign);
 
   /**
-   * @brief Find the intersection a circle and a line segment.
-   * This assumes the circle is centered at the origin.
-   * If no intersection is found, a floating point error will occur.
-   * @param p1 first endpoint of line segment
-   * @param p2 second endpoint of line segment
-   * @param r radius of circle
-   * @return point of intersection
-   */
-  static geometry_msgs::msg::Point circleSegmentIntersection(
-    const geometry_msgs::msg::Point & p1,
-    const geometry_msgs::msg::Point & p2,
-    double r);
-
-  /**
-   * @brief Get lookahead point
-   * @param lookahead_dist Optimal lookahead distance
-   * @param path Current global path
-   * @param interpolate_after_goal If true, interpolate the lookahead point after the goal based
-   * on the orientation given by the position of the last two pose of the path
-   * @return Lookahead point
-   */
-  geometry_msgs::msg::PoseStamped getLookAheadPoint(
-    const double &, const nav_msgs::msg::Path &,
-    bool interpolate_after_goal = false);
-
-  /**
    * @brief checks for the cusp position
    * @param pose Pose input to determine the cusp position
    * @return robot distance from the cusp
