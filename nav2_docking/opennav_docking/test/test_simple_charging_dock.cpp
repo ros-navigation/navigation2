652ce150936a6eaebae42827a06717226066c633
bkoensgen <b.koensgen@gmail.com>
2025-08-25 16:00:26 +0200

    tests: adapt SimpleChargingDockTestable to initial_pose_received_ state

--- nav2_docking/opennav_docking/test/test_simple_charging_dock.cpp
+++ nav2_docking/opennav_docking/test/test_simple_charging_dock.cpp
@@ -49,10 +49,7 @@ public:
   using opennav_docking::SimpleChargingDock::SimpleChargingDock;
 
    // Expose detector state for test verification
-  bool isDetectorActive() const
-  {
-    return detector_enabled_;
-  }
+  bool isDetectorActive() const { return initial_pose_received_; }
 };
 
 TEST(SimpleChargingDockTests, ObjectLifecycle)
