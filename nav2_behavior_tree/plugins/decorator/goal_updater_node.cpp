2d93fdf37e475aaac380e814b3e49e33c50688dc
mini-1235 <mauricepurnawan@gmail.com>
2025-08-28 05:08:17 +0800

    Revert double spin (#5477)
    * Revert "Add double spin_some in some BT nodes (#5055)"
    
    This reverts commit 4e8469e5c9b33c8cf987ee8e83e70aefcbb34eba.
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Update spin some to use spin all
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Replace 50 ms with bt loop duration
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    ---------
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_behavior_tree/plugins/decorator/goal_updater_node.cpp
+++ nav2_behavior_tree/plugins/decorator/goal_updater_node.cpp
@@ -36,9 +36,8 @@ GoalUpdater::GoalUpdater(
   goals_updater_topic_("goals_update")
 {
   initialize();
-
-    // Spin multiple times due to rclcpp regression in Jazzy requiring a 'warm up' spin
-  callback_group_executor_.spin_all(std::chrono::milliseconds(1));
+  bt_loop_duration_ =
+    config().blackboard->template get<std::chrono::milliseconds>("bt_loop_duration");
 }
 
 void GoalUpdater::initialize()
@@ -91,7 +90,7 @@ inline BT::NodeStatus GoalUpdater::tick()
   getInput("input_goal", goal);
   getInput("input_goals", goals);
 
-  callback_group_executor_.spin_all(std::chrono::milliseconds(49));
+  callback_group_executor_.spin_all(bt_loop_duration_);
 
   if (last_goal_received_set_) {
     if (last_goal_received_.header.stamp == rclcpp::Time(0)) {
