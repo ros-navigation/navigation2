c0da22a1c8bcb43a7762e7c32569059f3257682f
Tony Najjar <tony.najjar.1997@gmail.com>
2025-09-04 21:11:58 +0200

    Use integral path distance for MPPI Critics  (#5495)
    * Implement integral distance for critics
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * .
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * linting
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * more linting
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix tests
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * reset path length
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix sign
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix return value
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * Revert "fix return value"
    
    This reverts commit 8b21e890de598ab6b35c4a0200c82ebfc4b0c9c8.
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * remove extra variable
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * add doxygen
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * rename getCriticGoalPathDistance
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * delete withinPositionGoalTolerance
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * use only one path distance
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * cleanup
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * linting
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * reorder
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * format
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix tests
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    ---------
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>

--- nav2_mppi_controller/src/critics/path_align_critic.cpp
+++ nav2_mppi_controller/src/critics/path_align_critic.cpp
@@ -20,12 +20,9 @@ namespace mppi::critics
 void PathAlignCritic::initialize()
 {
   auto getParentParam = parameters_handler_->getParamGetter(parent_name_);
-  getParentParam(enforce_path_inversion_, "enforce_path_inversion", false);
-
   auto getParam = parameters_handler_->getParamGetter(name_);
   getParam(power_, "cost_power", 1);
   getParam(weight_, "cost_weight", 10.0f);
-
   getParam(max_path_occupancy_ratio_, "max_path_occupancy_ratio", 0.07f);
   getParam(offset_from_furthest_, "offset_from_furthest", 20);
   getParam(trajectory_point_step_, "trajectory_point_step", 4);
@@ -42,16 +39,7 @@ void PathAlignCritic::initialize()
 
 void PathAlignCritic::score(CriticData & data)
 {
-  if (!enabled_) {
-    return;
-  }
-
-  geometry_msgs::msg::Pose goal = utils::getCriticGoal(data, enforce_path_inversion_);
-
-  // Don't apply close to goal, let the goal critics take over
-  if (utils::withinPositionGoalTolerance(
-      threshold_to_consider_, data.state.pose.pose, goal))
-  {
+  if (!enabled_ || data.state.local_path_length < threshold_to_consider_) {
     return;
   }
 
