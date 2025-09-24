16e63a46c0e25339dfddee468589b456cf0af7df
mini-1235 <mauricepurnawan@gmail.com>
2025-09-24 23:59:22 +0800

    Refactor common functions in smoother and planner (#5537)
    * Refactor common functions in planner and smoother
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Add unit tests for smoother utils
    
    Signed-off-by: Maurice <mauricepurnawan@gmail.com>
    
    * Update cmakelist and linting
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Update nav2_util/include/nav2_util/smoother_utils.hpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Update nav2_util/include/nav2_util/smoother_utils.hpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Update unit tests
    
    Signed-off-by: Maurice <mauricepurnawan@gmail.com>
    
    ---------
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    Signed-off-by: Maurice <mauricepurnawan@gmail.com>
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_smoother/test/test_savitzky_golay_smoother.cpp
+++ nav2_smoother/test/test_savitzky_golay_smoother.cpp
@@ -30,7 +30,6 @@
 #include "nav2_smoother/savitzky_golay_smoother.hpp"
 #include "ament_index_cpp/get_package_share_directory.hpp"
 
-using namespace smoother_utils;  // NOLINT
 using namespace nav2_smoother;  // NOLINT
 using namespace std::chrono_literals;  // NOLINT
 
