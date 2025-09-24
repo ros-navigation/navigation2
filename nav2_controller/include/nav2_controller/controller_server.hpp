ddc47b5f7a75a0b9dfb2dc5cf07afde815221e91
silanus23 <berkantali23@outlook.com>
2025-09-24 20:42:37 +0300

    changing tracking_error to tracking_error_feedback
    Signed-off-by: silanus23 <berkantali23@outlook.com>

--- nav2_controller/include/nav2_controller/controller_server.hpp
+++ nav2_controller/include/nav2_controller/controller_server.hpp
@@ -28,7 +28,7 @@
 #include "nav2_costmap_2d/costmap_2d_ros.hpp"
 #include "tf2_ros/transform_listener.hpp"
 #include "nav2_msgs/action/follow_path.hpp"
-#include "nav2_msgs/msg/tracking_error.hpp"
+#include "nav2_msgs/msg/tracking_error_feedback.hpp"
 #include "nav2_msgs/msg/speed_limit.hpp"
 #include "nav2_ros_common/lifecycle_node.hpp"
 #include "nav2_ros_common/simple_action_server.hpp"
@@ -174,10 +174,6 @@ protected:
    */
   void publishVelocity(const geometry_msgs::msg::TwistStamped & velocity);
   /**
-   * @brief Calculates and publishes the TrackingError's all components
-   */
-  void publishTrackingState();
-  /**
    * @brief Calls velocity publisher to publish zero velocity
    */
   void publishZeroVelocity();
@@ -241,7 +237,7 @@ protected:
   std::unique_ptr<nav2_util::OdomSmoother> odom_sub_;
   std::unique_ptr<nav2_util::TwistPublisher> vel_publisher_;
   nav2::Subscription<nav2_msgs::msg::SpeedLimit>::SharedPtr speed_limit_sub_;
-  nav2::Publisher<nav2_msgs::msg::TrackingError>::SharedPtr tracking_error_pub_;
+  nav2::Publisher<nav2_msgs::msg::TrackingErrorFeedback>::SharedPtr tracking_error_pub_;
 
   // Progress Checker Plugin
   pluginlib::ClassLoader<nav2_core::ProgressChecker> progress_checker_loader_;
@@ -293,7 +289,7 @@ protected:
   nav_msgs::msg::Path current_path_;
 
   // Last tracking error in the form of TrackingError.msg
-  nav2_msgs::msg::TrackingError  current_tracking_error_;
+  nav2_msgs::msg::TrackingErrorFeedback  current_tracking_error_;
 
 private:
   /**
