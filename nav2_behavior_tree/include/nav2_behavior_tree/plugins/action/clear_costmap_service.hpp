1d24384133ef595f597e88fac78e71946c9f85ef
Burak Can Kaya <146545020+BCKSELFDRIVEWORLD@users.noreply.github.com>
2025-09-25 15:07:03 +0300

    Update nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/clear_costmap_service.hpp
    Co-authored-by: Sushant Chavan <gitecsvc@gmail.com>
    Signed-off-by: Burak Can Kaya <146545020+BCKSELFDRIVEWORLD@users.noreply.github.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/clear_costmap_service.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/clear_costmap_service.hpp
@@ -102,7 +102,7 @@ public:
           "reset_distance", 1,
           "Distance from the robot above which obstacles are cleared"),
         BT::InputPort<std::vector<std::string>>("plugins",
-          "List of costmap layer names to clear. If empty, all layers will be cleared")
+          "List of costmap plugin names to clear. If empty, all plugins will be cleared")
       });
   }
 };
