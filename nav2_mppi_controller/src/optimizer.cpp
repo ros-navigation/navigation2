cc96f75f9127c4214b6c4c662687de0c0887c289
Adi Vardi <adi.vardi@enway.ai>
2025-10-09 16:36:44 +0200

    Undo "[tmp] use unconstrained u_virt for warm start"

--- nav2_mppi_controller/src/optimizer.cpp
+++ nav2_mppi_controller/src/optimizer.cpp
@@ -338,7 +338,7 @@ void Optimizer::prepare(
 
 void Optimizer::shiftControlSequence()
 {
-  control_sequence_ = control_sequence_virtual_;
+  // control_sequence_ = control_sequence_virtual_;
 
   auto size = control_sequence_.vx.size();
   utils::shiftColumnsByOnePlace(control_sequence_.vx, -1);
