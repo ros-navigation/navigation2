d06c1fe69940a3a3d9d67fe1d79b900255b3fd7c
mini-1235 <mauricepurnawan@gmail.com>
2025-10-18 10:59:17 +0000

    Fix typo
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_dwb_controller/dwb_plugins/src/kinematic_parameters.cpp
+++ nav2_dwb_controller/dwb_plugins/src/kinematic_parameters.cpp
@@ -204,8 +204,8 @@ rcl_interfaces::msg::SetParametersResult KinematicsHandler::validateParameterUpd
         param_name.c_str(), parameter.as_double());
         result.successful = false;
       } else if (parameter.as_double() > 0.0 &&
-        (param_name == plugin_name_ + ".acc_lim_x" || param_name == plugin_name_ + ".acc_lim_y" ||
-        param_name == plugin_name_ + ".acc_lim_y"))
+        (param_name == plugin_name_ + ".decel_lim_x" || param_name == plugin_name_ + ".decel_lim_y" ||
+        param_name == plugin_name_ + ".decel_lim_theta"))
       {
         RCLCPP_WARN(
         logger_, "The value of parameter '%s' is incorrectly set to %f, "
