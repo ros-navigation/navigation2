c23a8f09d7bfc83cfa37f6328122ca42ae4248fa
DMO <dmo@ipa.fhg.de>
2025-10-19 21:09:30 +0200

    Updated comments and return positive 1 in eta calculation
    Signed-off-by: DMO <dmo@ipa.fhg.de>

--- nav2_velocity_smoother/src/velocity_smoother.cpp
+++ nav2_velocity_smoother/src/velocity_smoother.cpp
@@ -275,7 +275,9 @@ double VelocitySmoother::findEtaConstraint(
   }
 
   if (std::abs(v_cmd) < 1e-6) {
-    return -1.0;
+    // This avoids generating an eta of inf, which would result in sudden accelerations
+    // of the other factors.
+    return 1.0;
   }
 
   if (dv > v_component_max) {
@@ -285,8 +287,8 @@ double VelocitySmoother::findEtaConstraint(
   if (dv < v_component_min) {
     return ( v_component_min + v_curr ) / v_cmd;
   }
-
-  return -1.0;
+  // no acceleration limit exceeded, so return "no change"
+  return 1.0;
 }
 
 double VelocitySmoother::applyConstraints(
@@ -386,6 +388,14 @@ void VelocitySmoother::smootherTimer()
   double eta = 1.0;
   if (scale_velocities_) {
     double curr_eta = -1.0;
+    // When a deceleration is limited, eta would be > 1.0.
+    // This would lead to a acceleration of the other vector elements,
+    // which is not intended with this smoother at the moment.
+    // When the commanded twist is changing the curvature sign, but the acceleration
+    // limits prevents this sign switch, eta would be negative.
+    // This would lead to surprising accelerations or deceleration of the other vector elements, 
+    // which is also not intended with this smoother.
+    // So eta is limited to [0.0 : 1.0]
     if(!is_6dof_) {
       curr_eta = findEtaConstraint(
         current_.twist.linear.x, command_->twist.linear.x, max_accels_[0], max_decels_[0]);
