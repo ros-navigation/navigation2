9e2d1252da9ece7a5e332a33c3d4db8b321fa18b
mini-1235 <mauricepurnawan@gmail.com>
2025-10-23 03:09:43 +0800

    Revert #4971 in Jazzy
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/parameter_handler.hpp
+++ nav2_regulated_pure_pursuit_controller/include/nav2_regulated_pure_pursuit_controller/parameter_handler.hpp
@@ -94,14 +94,12 @@ protected:
    * @brief Callback executed when a parameter change is detected
    * @param event ParameterEvent message
    */
-  void
-  updateParametersCallback(std::vector<rclcpp::Parameter> parameters);
   rcl_interfaces::msg::SetParametersResult
-  validateParameterUpdatesCallback(std::vector<rclcpp::Parameter> parameters);
+  dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters);
+
   // Dynamic parameters handler
   std::mutex mutex_;
-  rclcpp::node_interfaces::PostSetParametersCallbackHandle::SharedPtr post_set_params_handler_;
-  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr on_set_params_handler_;
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
   Parameters params_;
   std::string plugin_name_;
   rclcpp::Logger logger_ {rclcpp::get_logger("RegulatedPurePursuitController")};
