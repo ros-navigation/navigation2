066d3d0c57ebe6b31ddee31a841ce374180cf04c
silanus23 <berkantali23@outlook.com>
2025-10-24 16:33:45 +0300

    first styling changes
    Signed-off-by: silanus23 <berkantali23@outlook.com>

--- nav2_costmap_2d/plugins/tracking_error_layer.cpp
+++ nav2_costmap_2d/plugins/tracking_error_layer.cpp
@@ -22,6 +22,15 @@ namespace nav2_costmap_2d
 
 TrackingErrorLayer::TrackingErrorLayer() {}
 
+TrackingErrorLayer::~TrackingErrorLayer()
+{
+  auto node = node_.lock();
+  if (dyn_params_handler_ && node) {
+    node->remove_on_set_parameters_callback(dyn_params_handler_.get());
+  }
+  dyn_params_handler_.reset();
+}
+
 void TrackingErrorLayer::onInitialize()
 {
   auto node = node_.lock();
@@ -55,18 +64,6 @@ void TrackingErrorLayer::onInitialize()
     std::bind(
       &TrackingErrorLayer::dynamicParametersCallback,
       this, std::placeholders::_1));
-
-  path_sub_ = node->create_subscription<nav_msgs::msg::Path>(
-    "/plan",
-    std::bind(&TrackingErrorLayer::pathCallback, this, std::placeholders::_1),
-    rclcpp::QoS(10).reliable()
-  );
-
-  tracking_feedback_sub_ = node->create_subscription<nav2_msgs::msg::TrackingFeedback>(
-    "/tracking_feedback",
-    std::bind(&TrackingErrorLayer::trackingCallback, this, std::placeholders::_1),
-    rclcpp::QoS(10).reliable()
-  );
   tf_buffer_ = std::make_shared<tf2_ros::Buffer>(node->get_clock());
   tf_buffer_->setUsingDedicatedThread(true);
   tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
@@ -74,14 +71,14 @@ void TrackingErrorLayer::onInitialize()
 
 void TrackingErrorLayer::pathCallback(const nav_msgs::msg::Path::SharedPtr msg)
 {
-  std::lock_guard<std::mutex> lock(path_mutex_);
+  std::lock_guard<std::mutex> lock(data_mutex_);
   last_path_ = *msg;
 }
 
 void TrackingErrorLayer::trackingCallback(
   const nav2_msgs::msg::TrackingFeedback::SharedPtr msg)
 {
-  std::lock_guard<std::mutex> lock(tracking_error_mutex_);
+  std::lock_guard<std::mutex> lock(data_mutex_);
   last_tracking_feedback_ = *msg;
 }
 
@@ -167,8 +164,7 @@ std::vector<std::vector<double>> TrackingErrorLayer::getWallPoints(
 
 nav_msgs::msg::Path TrackingErrorLayer::getPathSegment()
 {
-  std::lock_guard<std::mutex> path_lock(path_mutex_);
-  std::lock_guard<std::mutex> error_lock(tracking_error_mutex_);
+  std::lock_guard<std::mutex> path_lock(data_mutex_);
 
   nav_msgs::msg::Path segment;
   if (last_path_.poses.empty() ||
@@ -342,20 +338,56 @@ rcl_interfaces::msg::SetParametersResult TrackingErrorLayer::dynamicParametersCa
   return result;
 }
 
-TrackingErrorLayer::~TrackingErrorLayer()
+void TrackingErrorLayer::activate()
 {
   auto node = node_.lock();
-  if (dyn_params_handler_ && node) {
-    node->remove_on_set_parameters_callback(dyn_params_handler_.get());
+  if (!node) {
+    throw std::runtime_error{"Failed to lock node"};
   }
-  dyn_params_handler_.reset();
+
+  // Create subscriptions when layer is activated
+  path_sub_ = node->create_subscription<nav_msgs::msg::Path>(
+    "plan",
+    std::bind(&TrackingErrorLayer::pathCallback, this, std::placeholders::_1),
+    nav2::qos::StandardTopicQoS()
+  );
+
+  tracking_feedback_sub_ = node->create_subscription<nav2_msgs::msg::TrackingFeedback>(
+    "tracking_feedback",
+    std::bind(&TrackingErrorLayer::trackingCallback, this, std::placeholders::_1),
+    nav2::qos::StandardTopicQoS()
+  );
+
+  enabled_ = true;
+  current_ = true;
 }
 
-void TrackingErrorLayer::reset() {}
-void TrackingErrorLayer::activate() {enabled_ = true;}
-void TrackingErrorLayer::deactivate() {enabled_ = false;}
+void TrackingErrorLayer::deactivate()
+{
+  // Destroy subscriptions when layer is deactivated
+  path_sub_.reset();
+  tracking_feedback_sub_.reset();
 
-void TrackingErrorLayer::onFootprintChanged() {}
-void TrackingErrorLayer::cleanup() {}
+  // Clear cached data
+  {
+    std::lock_guard<std::mutex> lock(data_mutex_);
+    last_path_.poses.clear();
+    last_path_.header = std_msgs::msg::Header();
+    last_tracking_feedback_ = nav2_msgs::msg::TrackingFeedback();
+  }
+
+  enabled_ = false;
+}
+
+void TrackingErrorLayer::reset()
+{
+  std::lock_guard<Costmap2D::mutex_t> guard(*getMutex());
+  
+  // Clear the costmap data
+  resetMaps();
+  
+  // Mark as current
+  current_ = true;
+}
 
 }  // namespace nav2_costmap_2d
