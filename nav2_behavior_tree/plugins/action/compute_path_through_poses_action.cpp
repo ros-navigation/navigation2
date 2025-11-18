3e119135f6e3194660d5a45b75aeec6fcca0ae21
Abhishekh Reddy <helloarm@pm.me>
2025-11-17 21:58:54 -0500

    Added placeholder value for last_reached_index field in remaining callbacks
    Signed-off-by: Abhishekh Reddy <helloarm@pm.me>

--- nav2_behavior_tree/plugins/action/compute_path_through_poses_action.cpp
+++ nav2_behavior_tree/plugins/action/compute_path_through_poses_action.cpp
@@ -52,6 +52,7 @@ BT::NodeStatus ComputePathThroughPosesAction::on_aborted()
 {
   nav_msgs::msg::Path empty_path;
   setOutput("path", empty_path);
+  setOutput("last_reached_index", ActionResult::ALL_GOALS);
   setOutput("error_code_id", result_.result->error_code);
   setOutput("error_msg", result_.result->error_msg);
   return BT::NodeStatus::FAILURE;
@@ -61,6 +62,7 @@ BT::NodeStatus ComputePathThroughPosesAction::on_cancelled()
 {
   nav_msgs::msg::Path empty_path;
   setOutput("path", empty_path);
+  setOutput("last_reached_index", ActionResult::ALL_GOALS);
   // Set empty error code, action was cancelled
   setOutput("error_code_id", ActionResult::NONE);
   setOutput("error_msg", "");
@@ -69,6 +71,7 @@ BT::NodeStatus ComputePathThroughPosesAction::on_cancelled()
 
 void ComputePathThroughPosesAction::on_timeout()
 {
+  setOutput("last_reached_index", ActionResult::ALL_GOALS);
   setOutput("error_code_id", ActionResult::TIMEOUT);
   setOutput("error_msg", "Behavior Tree action client timed out waiting.");
 }
@@ -77,6 +80,7 @@ void ComputePathThroughPosesAction::halt()
 {
   nav_msgs::msg::Path empty_path;
   setOutput("path", empty_path);
+  setOutput("last_reached_index", ActionResult::ALL_GOALS);
   // DO NOT reset "error_code_id" output port, we want to read it later
   // DO NOT reset "error_msg" output port, we want to read it later
   BtActionNode::halt();
