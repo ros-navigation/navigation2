a42379debf574307c3b1155fdebf47febf86971c
Mengting Yang <87471734+Lotusymt@users.noreply.github.com>
2025-11-17 20:25:19 -0800

    Update nav2_collision_monitor/src/costmap.cpp
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: Mengting Yang <87471734+Lotusymt@users.noreply.github.com>

--- nav2_collision_monitor/src/costmap.cpp
+++ nav2_collision_monitor/src/costmap.cpp
@@ -98,7 +98,6 @@ bool CostmapSource::getData(
   for (unsigned int y = 0; y < meta.size_y; ++y) {
     for (unsigned int x = 0; x < meta.size_x; ++x) {
       const int idx = y * meta.size_x + x;
-
       const uint8_t cell_cost = cm.data[idx];
       const bool is_obstacle =
         (cell_cost >= cost_threshold_ && cell_cost < nav2_costmap_2d::NO_INFORMATION) ||
