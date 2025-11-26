c87c44e2323f8e8ea61f6393c626f2d03a967a23
随风而散 <13459528+suifengersan@user.noreply.gitee.com>
2025-11-26 10:16:52 +0800

    fix bug UAF
    Signed-off-by: suifengersan123 <yangabc810@gmail.com>

--- nav2_dwb_controller/dwb_plugins/include/dwb_plugins/kinematic_parameters.hpp
+++ nav2_dwb_controller/dwb_plugins/include/dwb_plugins/kinematic_parameters.hpp
@@ -39,7 +39,7 @@
 #include <memory>
 #include <string>
 #include <vector>
-
+#include <stdexcept>
 #include "nav2_ros_common/lifecycle_node.hpp"
 #include "rclcpp/rclcpp.hpp"
 
@@ -145,7 +145,15 @@ public:
   void activate();
   void deactivate();
 
-  inline KinematicParameters getKinematics() {return *kinematics_.load();}
+  inline KinematicParameters getKinematics() {
+        KinematicParameters* ptr = kinematics_.load();
+        // Check for nullptr before dereferencing
+        if (ptr == nullptr) {
+          throw std::runtime_error(
+            "KinematicsHandler::getKinematics() called before kinematics_ is initialized");
+        }
+        return *ptr;
+      }
 
   void setSpeedLimit(const double & speed_limit, const bool & percentage);
 
