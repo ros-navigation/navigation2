f72965ceac9fdfb3ae118404d914855da38bcbb6
mini-1235 <mauricepurnawan@gmail.com>
2025-12-09 11:12:20 +0000

    Use event handler to avoid race condition
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_costmap_2d/test/integration/costmap_tests_launch.py
+++ nav2_costmap_2d/test/integration/costmap_tests_launch.py
@@ -18,7 +18,8 @@ import os
 import sys
 
 from launch import LaunchDescription, LaunchService
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, TimerAction, Shutdown
+from launch.event_handlers import OnProcessStart  
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch_ros.actions
 from launch_testing.legacy import LaunchTestService
@@ -86,8 +87,28 @@ def main(argv: list[str] = sys.argv[1:]):  # type: ignore[no-untyped-def]
         output='screen'
     )
 
+    ld.add_action(
+        RegisterEventHandler(
+            OnProcessStart(
+                target_action=lifecycle_manager,
+                on_start=[
+                    TimerAction(
+                        period=1.0,
+                        actions=[
+                            ExecuteProcess(
+                                cmd=[testExecutable],
+                                name='costmap_tests',
+                                output='screen',
+                                on_exit=Shutdown()
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+    )
+
     lts = LaunchTestService()  # type: ignore[no-untyped-call]
-    lts.add_test_action(ld, test1_action)  # type: ignore[no-untyped-call]
     ls = LaunchService(argv=argv)
     ls.include_launch_description(ld)
     return lts.run(ls)  # type: ignore[no-untyped-call]
