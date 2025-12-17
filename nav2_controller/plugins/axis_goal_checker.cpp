60a16b6047a42a940280939981d45319ad611ce4
Tony Najjar <tony.najjar@dexory.com>
2025-12-17 11:44:10 +0000

    unused import

--- nav2_controller/plugins/axis_goal_checker.cpp
+++ nav2_controller/plugins/axis_goal_checker.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <tf2/utils.h>
-
 #include <memory>
 #include <string>
 #include <limits>
