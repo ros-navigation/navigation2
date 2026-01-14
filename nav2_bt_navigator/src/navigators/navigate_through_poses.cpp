69a90134dbf541cc5f574fe195b70c93d680fac6
mini-1235 <mauricepurnawan@gmail.com>
2026-01-14 14:16:31 +0000

    Revert
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_bt_navigator/src/navigators/navigate_through_poses.cpp
+++ nav2_bt_navigator/src/navigators/navigate_through_poses.cpp
@@ -186,6 +186,7 @@ NavigateThroughPosesNavigator::onLoop()
       estimated_time_remaining =
         rclcpp::Duration::from_seconds(distance_remaining / std::abs(current_linear_speed));
     }
+
     feedback_msg->distance_remaining = distance_remaining;
     feedback_msg->estimated_time_remaining = estimated_time_remaining;
   }
