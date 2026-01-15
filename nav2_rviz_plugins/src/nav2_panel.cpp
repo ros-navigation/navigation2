804ef8c7020d356e9a52985cb90ee5d566db0066
Maurice Alexander Purnawan <mauricepurnawan@gmail.com>
2026-01-16 02:53:48 +0700

    Fix bt navigator feedback in looping path (#5864)
    * Fix bt navigator feedback in looping path
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Revert
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Revert
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Lint
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Update nav2_bt_navigator/src/navigators/navigate_to_pose.cpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: Maurice Alexander Purnawan <mauricepurnawan@gmail.com>
    
    * Update nav2_bt_navigator/src/navigators/navigate_through_poses.cpp
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: Maurice Alexander Purnawan <mauricepurnawan@gmail.com>
    
    * Add tracking error to Navigators feedback
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Lint
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Lint
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Add search window to bringup
    
    Signed-off-by: Maurice <mauricepurnawan@gmail.com>
    
    * Add test
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Lint
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    ---------
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    Signed-off-by: Maurice Alexander Purnawan <mauricepurnawan@gmail.com>
    Signed-off-by: Maurice <mauricepurnawan@gmail.com>
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: lotusymt <mengtiy5@uci.edu>

--- nav2_rviz_plugins/src/nav2_panel.cpp
+++ nav2_rviz_plugins/src/nav2_panel.cpp
@@ -1084,6 +1084,9 @@ Nav2Panel::onNewGoal(double x, double y, double theta, QString frame)
       syncTabsWithAccumulatedPoses();  // Sync tabs with new pose
     } else {
       acummulated_poses_ = nav_msgs::msg::Goals();
+      // Reset goal index for single goal navigation
+      goal_index_ = 0;
+      store_poses_.goals.push_back(pose);
       updateWpNavigationMarkers();
       std::cout << "Start navigation" << std::endl;
       startNavigation(pose);
@@ -1627,6 +1630,8 @@ inline std::string Nav2Panel::toLabel(T & msg)
     toString(rclcpp::Duration(msg.estimated_time_remaining).seconds(), 0) + " s"
     "</td></tr><tr><td width=150>Distance remaining:</td><td>" +
     toString(msg.distance_remaining, 2) + " m"
+    "</td></tr><tr><td width=150>Tracking error:</td><td>" +
+    toString(msg.tracking_error, 2) + " m"
     "</td></tr><tr><td width=150>Time taken:</td><td>" +
     toString(rclcpp::Duration(msg.navigation_time).seconds(), 0) + " s"
     "</td></tr><tr><td width=150>Recoveries:</td><td>" +
