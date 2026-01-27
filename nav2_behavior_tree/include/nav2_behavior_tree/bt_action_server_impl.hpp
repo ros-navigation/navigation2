77620161ef6b81a1c5bbdd9753340aabafa4eba6
mini-1235 <mauricepurnawan@gmail.com>
2026-01-27 20:31:56 +0800

    Declare parameter
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
@@ -67,6 +67,9 @@ BtActionServer<ActionT>::BtActionServer(
   if (!node->has_parameter("default_server_timeout")) {
     node->declare_parameter("default_server_timeout", 20);
   }
+  if (!node->has_parameter("default_cancel_timeout")) {
+    node->declare_parameter("default_cancel_timeout", 50);
+  }
   if (!node->has_parameter("always_reload_bt_xml")) {
     node->declare_parameter("always_reload_bt_xml", false);
   }
