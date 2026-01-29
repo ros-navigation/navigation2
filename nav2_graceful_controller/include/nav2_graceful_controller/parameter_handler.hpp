81d4143ae402886a44b249fc4a5e58c66d0c98fc
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2026-01-30 00:39:08 +0900

    Add footprint expansion to graceful controller (#5779)
    * Add final rotation tolerance
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Add footprint expansion
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Add parameter tests
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Preserve default behavior
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Add footprint expansion test
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Add final rotation tolerance test
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Safe access to path
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Throw NoValidControl when inflation scale less than 1.0
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Revert "Safe access to path"
    
    This reverts commit d3b7ca1caf75ee0fec6fb8f1079be88ee267383a.
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Revert "Add final rotation tolerance test"
    
    This reverts commit b7089c8577e1148a43a6f72992b749a64bb94ec9.
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Revert "Preserve default behavior"
    
    This reverts commit 75578aa9c24d8db25ab74ca683332ee9564db654.
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Revert "Add final rotation tolerance"
    
    This reverts commit d6977d295a8dcdfeabfecf42961a6c79a0dc93d6.
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Remove final_rotation_tolerance from parameter tests
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Set default footprint_scaling_factor to 25%
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    * Refactor parameter declarations
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
    
    ---------
    
    Signed-off-by: Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>

--- nav2_graceful_controller/include/nav2_graceful_controller/parameter_handler.hpp
+++ nav2_graceful_controller/include/nav2_graceful_controller/parameter_handler.hpp
@@ -50,6 +50,9 @@ struct Parameters
   bool allow_backward;
   double in_place_collision_resolution;
   bool use_collision_detection;
+  double footprint_scaling_linear_vel;
+  double footprint_scaling_factor;
+  double footprint_scaling_step;
 };
 
 /**
