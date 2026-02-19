d9e70fb0fc7099225bc1fbe48b7f47f50cc58bdf
Iacopo Moles <8940011+icpmoles@users.noreply.github.com>
2026-02-19 02:07:30 +0100

    Backport docking in reverse fixes for jazzy (#5971)
    * Increase docking coverage && fix backward docking (backport Jazzy) (#5097)
    
    * Increase docking coverage && fix backward docking (backport Jazzy)
    
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>
    
    Increase docking coverage && fix backward docking (#5097)
    
    * Add dock_database tests
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Add utils test
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Improve filter tests
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Added simple charging tests
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Improve comments
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Fix backward and redo main test
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Change test period to reduce test time
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Revert "Change test period to reduce test time"
    
    This reverts commit ef1555ec43cb4849fd658c32377189cf414ff9b7.
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    * Delete print pose
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    
    ---------
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>
    
    * bump package versions
    
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>
    
    * Revert "bump package versions"
    
    This reverts commit a35c23d7c2edd6c4a09cec957c2c464c64f85d76.
    
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>
    
    ---------
    
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>
    Co-authored-by: Alberto Tudela <ajtudela@gmail.com>

--- nav2_docking/opennav_docking/src/docking_server.cpp
+++ nav2_docking/opennav_docking/src/docking_server.cpp
@@ -431,13 +431,6 @@ bool DockingServer::approachDock(Dock * dock, geometry_msgs::msg::PoseStamped & 
     geometry_msgs::msg::PoseStamped target_pose = dock_pose;
     target_pose.header.stamp = rclcpp::Time(0);
 
-    // Make sure that the target pose is pointing at the robot when moving backwards
-    // This is to ensure that the robot doesn't try to dock from the wrong side
-    if (dock_backwards_) {
-      target_pose.pose.orientation = nav2_util::geometry_utils::orientationAroundZAxis(
-        tf2::getYaw(target_pose.pose.orientation) + M_PI);
-    }
-
     // The control law can get jittery when close to the end when atan2's can explode.
     // Thus, we backward project the controller's target pose a little bit after the
     // dock so that the robot never gets to the end of the spiral before its in contact
@@ -447,6 +440,13 @@ bool DockingServer::approachDock(Dock * dock, geometry_msgs::msg::PoseStamped & 
     target_pose.pose.position.x += cos(yaw) * backward_projection;
     target_pose.pose.position.y += sin(yaw) * backward_projection;
     tf2_buffer_->transform(target_pose, target_pose, base_frame_);
+
+    // Make sure that the target pose is pointing at the robot when moving backwards
+    // This is to ensure that the robot doesn't try to dock from the wrong side
+    if (dock_backwards_) {
+      target_pose.pose.orientation = nav2_util::geometry_utils::orientationAroundZAxis(
+        tf2::getYaw(target_pose.pose.orientation) + M_PI);
+    }
 
     // Compute and publish controls
     auto command = std::make_unique<geometry_msgs::msg::TwistStamped>();
@@ -614,6 +614,12 @@ void DockingServer::undockRobot()
 
     // Get "dock pose" by finding the robot pose
     geometry_msgs::msg::PoseStamped dock_pose = getRobotPoseInFrame(fixed_frame_);
+
+    // Make sure that the staging pose is pointing in the same direction when moving backwards
+    if (dock_backwards_) {
+      dock_pose.pose.orientation = nav2_util::geometry_utils::orientationAroundZAxis(
+        tf2::getYaw(dock_pose.pose.orientation) + M_PI);
+    }
 
     // Get staging pose (in fixed frame)
     geometry_msgs::msg::PoseStamped staging_pose =
