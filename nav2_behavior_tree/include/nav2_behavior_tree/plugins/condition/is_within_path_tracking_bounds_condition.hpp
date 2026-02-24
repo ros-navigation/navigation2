79ee2285f1273a7ce2367f1838cfb35d7053a157
mini-1235 <mauricepurnawan@gmail.com>
2026-02-24 12:58:30 +0000

    Add isWithinPathTrackingBounds node
    Co-authored-by: silanus23 <berkantali23@outlook.com>
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

---
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/is_within_path_tracking_bounds_condition.hpp
@@ -0,0 +1,87 @@
+// Copyright (c) 2025 Berkan Tali
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_WITHIN_PATH_TRACKING_BOUNDS_CONDITION_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_WITHIN_PATH_TRACKING_BOUNDS_CONDITION_HPP_
+
+#include <string>
+#include <memory>
+
+#include "behaviortree_cpp/condition_node.h"
+#include "nav2_ros_common/lifecycle_node.hpp"
+#include "nav2_msgs/msg/tracking_feedback.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace nav2_behavior_tree
+{
+
+/**
+ * @brief A BT::ConditionNode that returns SUCCESS when the current path tracking error is within specified bounds and FAILURE otherwise
+ */
+class IsWithinPathTrackingBoundsCondition : public BT::ConditionNode
+{
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::IsWithinPathTrackingBoundsCondition
+   * @param condition_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  IsWithinPathTrackingBoundsCondition(
+    const std::string & condition_name,
+    const BT::NodeConfiguration & conf);
+
+  IsWithinPathTrackingBoundsCondition() = delete;
+
+  /**
+   * @brief Function to read parameters and initialize class variables
+   */
+  void initialize();
+
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<double>("max_position_error_left", 0.5,
+        "Maximum allowed position tracking error left side"),
+      BT::InputPort<double>("max_position_error_right", 0.5,
+        "Maximum allowed position tracking error on the right side"),
+      BT::InputPort<double>("max_heading_error", 3.14,
+        "Maximum allowed heading tracking error in radians"),
+      BT::InputPort<nav2_msgs::msg::TrackingFeedback>("tracking_feedback",
+        "Current tracking feedback message")
+    };
+  }
+
+protected:
+  rclcpp::Clock::SharedPtr clock_;
+  rclcpp::Logger logger_{rclcpp::get_logger("IsWithinPathTrackingBoundsCondition")};
+
+  double max_position_error_left_;
+  double max_position_error_right_;
+  double max_heading_error_;
+  nav2_msgs::msg::TrackingFeedback tracking_feedback_;
+};
+
+}  // namespace nav2_behavior_tree
+
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_WITHIN_PATH_TRACKING_BOUNDS_CONDITION_HPP_
---
+++ nav2_behavior_tree/plugins/condition/is_within_path_tracking_bounds_condition.cpp
@@ -0,0 +1,89 @@
+// Copyright (c) 2025 Berkan Tali
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nav2_behavior_tree/plugins/condition/is_within_path_tracking_bounds_condition.hpp"
+
+namespace nav2_behavior_tree
+{
+
+IsWithinPathTrackingBoundsCondition::IsWithinPathTrackingBoundsCondition(
+  const std::string & condition_name,
+  const BT::NodeConfiguration & conf)
+: BT::ConditionNode(condition_name, conf)
+{
+  auto node = config().blackboard->get<nav2::LifecycleNode::SharedPtr>("node");
+  logger_ = node->get_logger();
+  clock_ = node->get_clock();
+
+  initialize();
+}
+
+void IsWithinPathTrackingBoundsCondition::initialize()
+{
+  getInput<double>("max_position_error_left", max_position_error_left_);
+  getInput<double>("max_position_error_right", max_position_error_right_);
+  getInput<double>("max_heading_error", max_heading_error_);
+  getInput<nav2_msgs::msg::TrackingFeedback>("tracking_feedback", tracking_feedback_);
+  if (max_position_error_right_ < 0.0) {
+    RCLCPP_WARN(logger_, "max_error_right should be positive, using absolute value");
+    max_position_error_right_ = std::abs(max_position_error_right_);
+  }
+  if (max_position_error_left_ < 0.0) {
+    RCLCPP_WARN(logger_, "max_error_left should be positive, using absolute value");
+    max_position_error_left_ = std::abs(max_position_error_left_);
+  }
+  if (max_heading_error_ < 0.0) {
+    RCLCPP_WARN(logger_, "max_heading_error should be positive, using absolute value");
+    max_heading_error_ = std::abs(max_heading_error_);
+  }
+}
+
+BT::NodeStatus IsWithinPathTrackingBoundsCondition::tick()
+{
+  if (!BT::isStatusActive(status())) {
+    initialize();
+  }
+
+  double position_tracking_error = tracking_feedback_.position_tracking_error;
+  double heading_tracking_error = tracking_feedback_.heading_tracking_error;
+  bool is_within_position_bounds;
+  if (position_tracking_error >= 0.0) {  // Positive or zero = left side (or on path)
+    is_within_position_bounds = (position_tracking_error <= max_position_error_left_);
+  } else {  // Negative = right side
+    is_within_position_bounds = (std::abs(position_tracking_error) <= max_position_error_right_);
+  }
+
+  bool is_within_heading_bounds = std::abs(heading_tracking_error) <= max_heading_error_;
+
+  if (is_within_position_bounds && is_within_heading_bounds) {
+    return BT::NodeStatus::SUCCESS;
+  } else {
+    RCLCPP_WARN_THROTTLE(
+      logger_,
+      *clock_,
+      1000,
+      "Robot is out of path tracking bounds! Position error: %.2f, Heading error: %.2f",
+      position_tracking_error, heading_tracking_error);
+    return BT::NodeStatus::FAILURE;
+  }
+}
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<nav2_behavior_tree::IsWithinPathTrackingBoundsCondition>(
+    "IsWithinPathTrackingBounds");
+}
