554e0ff999933dca6c4243ad94e08f4438388a9f
Maurice Alexander Purnawan <mauricepurnawan@gmail.com>
2026-03-09 11:48:13 +0700

    Move isStopped, isPathValid and isPoseOccupied from condition nodes to action nodes (#5991)
    * Move isStopped, isPathValid and isPoseOccupied from condition nodes to action nodes
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Fix tests
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Self review
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    * Update key for codecov
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>
    
    ---------
    
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_behavior_tree/test/plugins/action/test_check_pose_occupancy_action.cpp
+++ nav2_behavior_tree/test/plugins/action/test_check_pose_occupancy_action.cpp
@@ -21,14 +21,13 @@
 #include "behaviortree_cpp/bt_factory.h"
 
 #include "utils/test_service.hpp"
-#include "nav2_behavior_tree/plugins/condition/is_pose_occupied_condition.hpp"
+#include "nav2_behavior_tree/plugins/action/check_pose_occupancy_action.hpp"
 #include "utils/test_behavior_tree_fixture.hpp"
 
-
-class IsPoseOccupiedSuccessService : public TestService<nav2_msgs::srv::GetCosts>
+class CheckPoseOccupancySuccessService : public TestService<nav2_msgs::srv::GetCosts>
 {
 public:
-  IsPoseOccupiedSuccessService()
+  CheckPoseOccupancySuccessService()
   : TestService("/global_costmap/get_cost_global_costmap")
   {}
 
@@ -44,10 +43,10 @@ public:
   }
 };
 
-class IsPoseOccupiedFailureService : public TestService<nav2_msgs::srv::GetCosts>
+class CheckPoseOccupancyFailureService : public TestService<nav2_msgs::srv::GetCosts>
 {
 public:
-  IsPoseOccupiedFailureService()
+  CheckPoseOccupancyFailureService()
   : TestService("/local_costmap/get_cost_local_costmap")
   {}
 
@@ -63,12 +62,12 @@ public:
   }
 };
 
-class IsPoseOccupiedTestFixture : public ::testing::Test
+class CheckPoseOccupancyTestFixture : public ::testing::Test
 {
 public:
   static void SetUpTestCase()
   {
-    node_ = std::make_shared<nav2::LifecycleNode>("is_pose_occupied_test_fixture");
+    node_ = std::make_shared<nav2::LifecycleNode>("check_pose_occupancy_test_node");
     factory_ = std::make_shared<BT::BehaviorTreeFactory>();
 
     config_ = new BT::NodeConfiguration();
@@ -76,20 +75,28 @@ public:
     // Create the blackboard that will be shared by all of the nodes in the tree
     config_->blackboard = BT::Blackboard::create();
     // Put items on the blackboard
-    config_->blackboard->set("node", node_);
+    config_->blackboard->set(
+      "node",
+      node_);
     config_->blackboard->set<std::chrono::milliseconds>(
       "server_timeout",
+      std::chrono::milliseconds(20));
+    config_->blackboard->set<std::chrono::milliseconds>(
+      "bt_loop_duration",
       std::chrono::milliseconds(10));
+    config_->blackboard->set<std::chrono::milliseconds>(
+      "wait_for_service_timeout",
+      std::chrono::milliseconds(1000));
 
     BT::NodeBuilder builder =
       [](const std::string & name, const BT::NodeConfiguration & config)
       {
-        return std::make_unique<nav2_behavior_tree::IsPoseOccupiedCondition>(
+        return std::make_unique<nav2_behavior_tree::CheckPoseOccupancy>(
           name, config);
       };
 
-    factory_->registerBuilder<nav2_behavior_tree::IsPoseOccupiedCondition>(
-      "IsPoseOccupied", builder);
+    factory_->registerBuilder<nav2_behavior_tree::CheckPoseOccupancy>(
+      "CheckPoseOccupancy", builder);
   }
 
   static void TearDownTestCase()
@@ -106,8 +113,8 @@ public:
   {
     tree_.reset();
   }
-  static std::shared_ptr<IsPoseOccupiedSuccessService> success_server_;
-  static std::shared_ptr<IsPoseOccupiedFailureService> failure_server_;
+  static std::shared_ptr<CheckPoseOccupancySuccessService> success_server_;
+  static std::shared_ptr<CheckPoseOccupancyFailureService> failure_server_;
 
 protected:
   static nav2::LifecycleNode::SharedPtr node_;
@@ -116,24 +123,24 @@ protected:
   static std::shared_ptr<BT::Tree> tree_;
 };
 
-nav2::LifecycleNode::SharedPtr IsPoseOccupiedTestFixture::node_ = nullptr;
+nav2::LifecycleNode::SharedPtr CheckPoseOccupancyTestFixture::node_ = nullptr;
 
-BT::NodeConfiguration * IsPoseOccupiedTestFixture::config_ = nullptr;
-std::shared_ptr<IsPoseOccupiedSuccessService>
-IsPoseOccupiedTestFixture::success_server_ = nullptr;
-std::shared_ptr<IsPoseOccupiedFailureService>
-IsPoseOccupiedTestFixture::failure_server_ = nullptr;
-std::shared_ptr<BT::BehaviorTreeFactory> IsPoseOccupiedTestFixture::factory_ = nullptr;
-std::shared_ptr<BT::Tree> IsPoseOccupiedTestFixture::tree_ = nullptr;
+BT::NodeConfiguration * CheckPoseOccupancyTestFixture::config_ = nullptr;
+std::shared_ptr<CheckPoseOccupancySuccessService>
+CheckPoseOccupancyTestFixture::success_server_ = nullptr;
+std::shared_ptr<CheckPoseOccupancyFailureService>
+CheckPoseOccupancyTestFixture::failure_server_ = nullptr;
+std::shared_ptr<BT::BehaviorTreeFactory> CheckPoseOccupancyTestFixture::factory_ = nullptr;
+std::shared_ptr<BT::Tree> CheckPoseOccupancyTestFixture::tree_ = nullptr;
 
-TEST_F(IsPoseOccupiedTestFixture, test_tick_is_pose_occupied_success)
+TEST_F(CheckPoseOccupancyTestFixture, test_tick_check_pose_occupancy_success)
 {
   // create tree
   std::string xml_txt =
     R"(
       <root BTCPP_format="4">
         <BehaviorTree ID="MainTree">
-          <IsPoseOccupied service_name="/global_costmap/get_cost_global_costmap" pose="{pose}" cost_threshold="253"/>
+          <CheckPoseOccupancy service_name="/global_costmap/get_cost_global_costmap" pose="{pose}" cost_threshold="253"/>
         </BehaviorTree>
       </root>)";
 
@@ -145,18 +152,23 @@ TEST_F(IsPoseOccupiedTestFixture, test_tick_is_pose_occupied_success)
 
   config_->blackboard->set("pose", pose);
 
+  // tick until node is not running
   tree_->rootNode()->executeTick();
+  while (tree_->rootNode()->status() == BT::NodeStatus::RUNNING) {
+    tree_->rootNode()->executeTick();
+  }
+
   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
 }
 
-TEST_F(IsPoseOccupiedTestFixture, test_tick_is_pose_occupied_fail)
+TEST_F(CheckPoseOccupancyTestFixture, test_tick_check_pose_occupancy_failure)
 {
   // create tree
   std::string xml_txt =
     R"(
       <root BTCPP_format="4">
         <BehaviorTree ID="MainTree">
-          <IsPoseOccupied service_name="/local_costmap/get_cost_local_costmap" pose="{pose}" cost_threshold="253"/>
+          <CheckPoseOccupancy service_name="/local_costmap/get_cost_local_costmap" pose="{pose}" cost_threshold="253"/>
         </BehaviorTree>
       </root>)";
 
@@ -168,7 +180,12 @@ TEST_F(IsPoseOccupiedTestFixture, test_tick_is_pose_occupied_fail)
 
   config_->blackboard->set("pose", pose);
 
+  // tick until node is not running
   tree_->rootNode()->executeTick();
+  while (tree_->rootNode()->status() == BT::NodeStatus::RUNNING) {
+    tree_->rootNode()->executeTick();
+  }
+
   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::FAILURE);
 }
 
@@ -180,16 +197,16 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
 
   // initialize service and spin on new thread
-  IsPoseOccupiedTestFixture::success_server_ =
-    std::make_shared<IsPoseOccupiedSuccessService>();
+  CheckPoseOccupancyTestFixture::success_server_ =
+    std::make_shared<CheckPoseOccupancySuccessService>();
   std::thread success_server_thread([]() {
-      rclcpp::spin(IsPoseOccupiedTestFixture::success_server_);
+      rclcpp::spin(CheckPoseOccupancyTestFixture::success_server_);
     });
 
-  IsPoseOccupiedTestFixture::failure_server_ =
-    std::make_shared<IsPoseOccupiedFailureService>();
+  CheckPoseOccupancyTestFixture::failure_server_ =
+    std::make_shared<CheckPoseOccupancyFailureService>();
   std::thread failure_server_thread([]() {
-      rclcpp::spin(IsPoseOccupiedTestFixture::failure_server_);
+      rclcpp::spin(CheckPoseOccupancyTestFixture::failure_server_);
     });
 
   int all_successful = RUN_ALL_TESTS();
