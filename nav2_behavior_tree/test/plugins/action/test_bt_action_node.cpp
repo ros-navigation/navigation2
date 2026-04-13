a404757e689eee37bba65385d770d8d6f6ec2e4c
silanus23 <berkantali23@outlook.com>
2026-04-14 01:11:31 +0300

    Reverting unnecessary deletions.
    Signed-off-by: silanus23 <berkantali23@outlook.com>

--- nav2_behavior_tree/test/plugins/action/test_bt_action_node.cpp
+++ nav2_behavior_tree/test/plugins/action/test_bt_action_node.cpp
@@ -634,10 +634,13 @@ int main(int argc, char ** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
 
+  // initialize ROS
   rclcpp::init(argc, argv);
 
   bool all_successful = RUN_ALL_TESTS();
 
+  // shutdown ROS
   rclcpp::shutdown();
+
   return all_successful;
 }
