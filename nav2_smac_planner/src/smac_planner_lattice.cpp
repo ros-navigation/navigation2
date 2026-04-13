fff95ac4326283ffc0f1f8bc71912c58ec23a97a
Sanchit B <researchbadamikar@gmail.com>
2026-04-13 16:16:47 -0700

    feat(PlannerServer): BaseGlobalPlanner plugin accepts viapoints as part of API
    The ComputePlanToPose action now incorporates a list of stamped poses as intermediate viapoints that are forwarded to the planner plugin.
    
    Signed-off-by: Sanchit B <researchbadamikar@gmail.com>

--- nav2_smac_planner/src/smac_planner_lattice.cpp
+++ nav2_smac_planner/src/smac_planner_lattice.cpp
@@ -300,8 +300,13 @@ void SmacPlannerLattice::cleanup()
 nav_msgs::msg::Path SmacPlannerLattice::createPlan(
   const geometry_msgs::msg::PoseStamped & start,
   const geometry_msgs::msg::PoseStamped & goal,
+  const std::vector<geometry_msgs::msg::PoseStamped> & viapoints,
   std::function<bool()> cancel_checker)
 {
+  if (!viapoints.size()) {
+    RCLCPP_DEBUG(_logger, "Planning triggered with no viapoints");
+  }
+
   std::lock_guard<std::mutex> lock_reinit(_mutex);
   steady_clock::time_point a = steady_clock::now();
 
