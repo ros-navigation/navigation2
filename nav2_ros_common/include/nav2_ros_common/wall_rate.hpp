19e70170c2a9a21637ad41e4b81fb10e2f2248ec
Tony Najjar <tony.najjar@dexory.com>
2026-04-16 08:35:26 +0000

    fix date
    Signed-off-by: Tony Najjar <tony.najjar@dexory.com>

--- nav2_ros_common/include/nav2_ros_common/wall_rate.hpp
+++ nav2_ros_common/include/nav2_ros_common/wall_rate.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Dexory
+// Copyright (c) 2026 Dexory
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
