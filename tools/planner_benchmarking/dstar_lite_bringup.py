45d24aa5abadb8db8e1f74df0aa4a72946808b01
Yuchen Fan <2994114386@qq.com>
2026-04-27 22:17:35 +0800

    Fix flake8 unused imports and add missing Apache 2.0 license header
    Signed-off-by: Yuchen Fan <2994114386@qq.com>

--- tools/planner_benchmarking/dstar_lite_bringup.py
+++ tools/planner_benchmarking/dstar_lite_bringup.py
@@ -17,8 +17,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from nav2_common.launch import RewrittenYaml
 
