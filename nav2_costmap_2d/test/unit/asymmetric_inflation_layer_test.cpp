2f1b877d14732277d6225617b0310faf2fb2fac2
mbloechli <mbloechlinger@duatic.com>
2026-04-30 20:51:59 +0200

    updated license headers with my full name and authors who wrote the legacy_inflation_layer, of which I copied a lot of
    Signed-off-by: mbloechli <mbloechlinger@duatic.com>

--- nav2_costmap_2d/test/unit/asymmetric_inflation_layer_test.cpp
+++ nav2_costmap_2d/test/unit/asymmetric_inflation_layer_test.cpp
@@ -1,4 +1,4 @@
-// Copyright 2026 Duatic AG
+// Copyright (c) 2026 Marc Blöchlinger
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
