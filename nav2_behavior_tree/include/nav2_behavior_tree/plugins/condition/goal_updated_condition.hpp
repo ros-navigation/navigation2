3a3935f9a602e6dec42618ecdca071a875391c5c
silanus23 <berkantali23@outlook.com>
2026-06-06 02:08:31 +0300

    commit fix
    Signed-off-by: silanus23 <berkantali23@outlook.com>

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/goal_updated_condition.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/goal_updated_condition.hpp
@@ -31,7 +31,7 @@ namespace nav2_behavior_tree
 /**
  * @brief A BT::ConditionNode that returns SUCCESS when goal is
  * updated on the blackboard and FAILURE otherwise
- * @note Re-initializes (snapshots the current goal) on transition from IDLE to RUNNING.
+ *  @note It will re-initialize when halted.
  *
  * Usage in XML:
  * @code
