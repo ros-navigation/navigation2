44838c85a67dcf27e0dc48419258215ab10a8213
mini-1235 <mauricepurnawan@gmail.com>
2026-08-18 23:37:11 +0800

    Rename
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- nav2_mppi_controller/src/critics/goal_angle_critic.cpp
+++ nav2_mppi_controller/src/critics/goal_angle_critic.cpp
@@ -48,9 +48,9 @@ void GoalAngleCritic::score(CriticData & data)
     auto symmetric_distances = utils::shortest_angular_distance(data.trajectories.yaws,
       symmetric_goal_yaw).abs().eval();
     if (power_ > 1u) {
-      data.costs += (dist.min(symmetric_distances).rowwise().mean() * weight_).pow(power_);
+      data.costs += (angular_distances.min(symmetric_distances).rowwise().mean() * weight_).pow(power_);
     } else {
-      data.costs += dist.min(symmetric_distances).rowwise().mean() * weight_;
+      data.costs += angular_distances.min(symmetric_distances).rowwise().mean() * weight_;
     }
     return;
   }
