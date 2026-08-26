37ea76640a1ee1b5c977518d2f5aa71847258b0b
Nikolaos Drosinos <nikolaos.drosinos@ocado.com>
2026-08-26 12:27:54 +0300

    feat: return to dock staging pose on failure
    * if all docking retries have failed, then return to the dock staging
      pose before reporting failure
    * swallow any new exceptions that may occur, in order to report the
      original docking failure reason
    
    Fixes #6399
    
    Signed-off-by: Nikolaos Drosinos <nikolaos.drosinos@ocado.com>

--- nav2_docking/opennav_docking/src/docking_server.cpp
+++ nav2_docking/opennav_docking/src/docking_server.cpp
@@ -263,8 +263,19 @@ void DockingServer::dockRobot()
         tf2::getYaw(staging_pose.pose.orientation) + M_PI);
     }
 
+    auto reset_approach = [this, dock, dock_backward, goal, result, &staging_pose]() -> bool {
+        if (resetApproach(staging_pose, dock_backward)) {
+          return true;
+        }
+        // Cancelled, preempted, or shutting down
+        publishZeroVelocity();
+        dock->plugin->stopDetectionProcess();
+        stashDockData(goal->use_dock_id, dock, false);
+        docking_action_server_->terminate_all(result);
+        return false;
+      };
+
     // Docking control loop: while not docked, run controller
-    rclcpp::Time dock_contact_time;
     while (rclcpp::ok()) {
       try {
         // Perform a 180º to face away from the dock if needed
@@ -302,18 +313,21 @@ void DockingServer::dockRobot()
       } catch (opennav_docking_core::DockingException & e) {
         if (++num_retries_ > params_->max_retries) {
           RCLCPP_ERROR(get_logger(), "Failed to dock, all retries have been used");
+          try {  // swallow new exceptions, so as to report original failure
+            if (!reset_approach()) {
+              return;
+            }
+          } catch (const std::exception & ex) {
+            RCLCPP_ERROR(
+              get_logger(), "Failed to return to staging pose: %s", ex.what());
+          }
           throw;
         }
         RCLCPP_WARN(get_logger(), "Docking failed, will retry: %s", e.what());
       }
 
       // Reset to staging pose to try again
-      if (!resetApproach(staging_pose, dock_backward)) {
-        // Cancelled, preempted, or shutting down
-        publishZeroVelocity();
-        dock->plugin->stopDetectionProcess();
-        stashDockData(goal->use_dock_id, dock, false);
-        docking_action_server_->terminate_all(result);
+      if (!reset_approach()) {
         return;
       }
       RCLCPP_INFO(get_logger(), "Returned to staging pose, attempting docking again");
