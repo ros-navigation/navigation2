4d2ae033468419f8f3050773b826075b314f3a43
JanKim <sonysame@korea.ac.kr>
2026-09-06 14:21:40 +0900

    Fix concurrent access to NavFn planner costmap (Bug3)
    Signed-off-by: JanKim <sonysame@korea.ac.kr>

--- nav2_navfn_planner/src/navfn_planner.cpp
+++ nav2_navfn_planner/src/navfn_planner.cpp
@@ -124,6 +124,9 @@ nav_msgs::msg::Path NavfnPlanner::createPlan(
   const std::vector<geometry_msgs::msg::PoseStamped> & viapoints,
   std::function<bool()> cancel_checker)
 {
+  std::unique_lock<nav2_costmap_2d::Costmap2D::mutex_t> costmap_lock(
+    *(costmap_->getMutex()));
+
 #ifdef BENCHMARK_TESTING
   steady_clock::time_point a = steady_clock::now();
 #endif
