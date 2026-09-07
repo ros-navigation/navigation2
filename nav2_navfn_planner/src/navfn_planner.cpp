c6757efd01d9f8922fbe8dd9d727560abd04ab1d
Steve Macenski <stevenmacenski@gmail.com>
2026-09-07 11:47:54 -0700

    Update nav2_navfn_planner/src/navfn_planner.cpp
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>

--- nav2_navfn_planner/src/navfn_planner.cpp
+++ nav2_navfn_planner/src/navfn_planner.cpp
@@ -238,11 +238,9 @@ NavfnPlanner::makePlan(
   worldToMap(wx, wy, mx, my);
 
   std::unique_lock<nav2_costmap_2d::Costmap2D::mutex_t> lock(*(costmap_->getMutex()));
-
-std::unique_lock<nav2_costmap_2d::Costmap2D::mutex_t> lock(*(costmap_->getMutex()));
   
-unsigned int mx, my;
-worldToMap(wx, wy, mx, my);
+  unsigned int mx, my;
+  worldToMap(wx, wy, mx, my);
   // clear the starting cell within the costmap because we know it can't be an obstacle
   clearRobotCell(mx, my);
 
