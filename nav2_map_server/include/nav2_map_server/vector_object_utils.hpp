c3c1c7b9710991e3356d2672b7246677f0b7c3f9
Tony Najjar <tony.najjar@dexory.com>
2026-09-10 11:13:11 +0000

    Revert "Refine documentation and comments in vector object shapes and utilities for clarity"
    This reverts commit b64ea6b194e6bf341d6d2d46846ca100831c4bcb.

--- nav2_map_server/include/nav2_map_server/vector_object_utils.hpp
+++ nav2_map_server/include/nav2_map_server/vector_object_utils.hpp
@@ -88,7 +88,8 @@ inline void processVal(
 }
 
 /**
- * @brief Updates a run of consecutive cells with given shape value according to the overlay type
+ * @brief Updates a contiguous run of cells with given shape value according to the overlay type.
+ * Same result as processVal() on each cell; branch-free loops keep the run vectorizable
  * @param cells Pointer to the first cell of the run
  * @param count Number of cells in the run
  * @param shape_val Vector object value to be overlaid on map
